67004568051570ad747323c8e93c02fa8e9c07bb
Matt Ettus <matt@ettus.com>
1999-10-13 21:24:39 +0000

    successful netlisting, with error messages!

--- gnetlist/scheme/gnet-vipec.scm
+++ gnetlist/scheme/gnet-vipec.scm
@@ -17,31 +17,27 @@
 ;;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 
-;; Given a uref, returns the device associated nets(s) ordered by their pin#, 
-;; what when not defined?
-;;      problem is slotted components e.g. ../examples/singlenet_1.sch
-
 (define vipec:templates
    (list
       (cons 
          (cons "RESISTOR" "RES")
          (list 
-            (list "value" "R=" #t)))
+            (list "value" "R=" #t "use value attrib for resistance")))
       (cons
          (cons "INDUCTOR" "IND")
          (list
-            (list "value" "L=" #t)
+            (list "value" "L=" #t "use value attrib for inductance")
             (list "Q" "Q=" #f)))
       (cons
          (cons "CAPACITOR" "CAP")
          (list
-            (list "value" "C=" #t)))
+            (list "value" "C=" #t "use value attrib for capacitance")))
       (cons
          (cons "TLIN" "TLIN")
          (list
             (list "Z" "Z=" #t 50)
-            (list "E" "E=" #t)
-            (list "F" "F=" #t)))
+            (list "length" "E=" #t "length attrib for length")
+            (list "F" "F=" #t "F attrib for frequency")))
       (cons
          (cons "CLIN" "CLIN")
          (list
@@ -52,7 +48,7 @@
       (cons
          (cons "SPARAMBLOCK" "BLOCK")
          (list
-            (list "filename" "" #t)))            
+            (list "filename" "" #t "filename attrib for sparams")))            
 ))
 
 (define vipec:get-template
@@ -83,14 +79,15 @@
                (value (gnetlist:get-package-attribute package (car(car attribs)))))
             (if (not (string=? value "unknown"))
                (begin
-                  (display (car (cdr attrib)) port)
+                  (display (cadr attrib) port)
                   (display value port)
                   (display "\t" port))
                (if (and (caddr attrib)(not (null? (cdddr attrib))))
                   (begin
                      (display (cadr attrib) port)
                      (display (cadddr attrib) port)
-                     (display "\t" port))))))))
+                     (display "\t" port))))
+         (vipec:write-attribs package (cdr attribs) port)))))
 
 (define vipec:write-gen-component
    (lambda (package port netnumbers)
@@ -98,7 +95,8 @@
          (display "\t" port)
          (display (cdr (car template)) port)
          (display "\t" port)
-         (vipec:write-net-name-of-node package (length (gnetlist:get-pins package)) netnumbers port)
+         (vipec:write-net-name-of-node package
+            (length (gnetlist:get-pins package)) netnumbers port)
          (vipec:write-attribs package (cdr template) port)
          (display (string-append "\t% " package) port)
          (newline port))))
@@ -107,15 +105,25 @@
    (lambda (port ls netnumbers)
       (if (not (null? ls))
          (let ((package (car ls)))
-            (cond 	
-               ( else (vipec:write-gen-component package port netnumbers)))
+            (else (vipec:write-gen-component package port netnumbers))
             (vipec:component-writing port (cdr ls) netnumbers)))))
+
+(define vipec:misc-components
+   (lambda (netnumbers port)
+      (display "\tRES\t0 " port)
+      (display (get-net-number "GND" netnumbers) port)
+      (display " R=0.00001\t% Assign ground net\n" port)
+      (display "\tDEF2P\t" port)
+      (display (get-net-number "PORT1" netnumbers) port)
+      (display "  " port)
+      (display (get-net-number "PORT2" netnumbers) port)
+      (display "\n\tTERM\t50 50\n" port)))
 
 (define vipec:header
    (lambda (port)
-      (display "* ViPEC RF Netlister\n" port)  
-      (display "* Written by Matthew Ettus\n" port)
-      (display "* Based on code by Bas Gieltjes\n" port)))
+      (display "% ViPEC RF Netlister\n" port)  
+      (display "% Written by Matthew Ettus\n" port)
+      (display "% Based on code by Bas Gieltjes\n" port)))
 
 (define vipec
    (lambda (output-filename)
@@ -124,10 +132,6 @@
          (vipec:header port)
          (display "CKT\n" port)
          (vipec:component-writing port packages netnumbers)
-         (display "\tDEF2P\t" port)
-         (display (get-net-number "PORT1" netnumbers) port)
-         (display "  " port)
-         (display (get-net-number "PORT2" netnumbers) port)
-         (display "\n\tTERM\t50 50\n" port)
+         (vipec:misc-components netnumbers port)
          (close-output-port port))))
 
