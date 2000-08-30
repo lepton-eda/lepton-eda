565d6513174187697c81e22e974cbe4d90e6b2e8
Stefan Petersen <spetm@users.sourceforge.net>
2000-08-30 20:56:49 +0000

    Added auto-uref.scm and print-NB-attribs.scm

---
+++ gschem/scheme/auto-uref.scm
@@ -0,0 +1,54 @@
+;; gEDA - GPL Electronic Design Automation
+;; gschem - gEDA Schematic Capture
+;; Copyright (C) 1998-2000 Ales V. Hvezda
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA
+
+(use-modules (ice-9 regex))
+
+(define prefix-list '())
+
+(define (auto-uref attribs)
+
+  (define (get-next-uref prefix)
+    (let ((available-prefix (assoc prefix prefix-list)))
+      (cond (available-prefix
+	     (set-cdr! available-prefix 
+		       (+ (cdr available-prefix) 1))
+	     (cdr available-prefix))
+	    (else
+	     (set! prefix-list (cons (cons prefix 1) prefix-list))
+	     1))))
+  
+  
+  ;; Total Guile
+  (define (get-prefix value)
+    (let ((prefix (string-match "^[A-Z]*" value)))
+      (if (= 0 (match:end prefix))
+	  #f
+	  (match:substring prefix))))
+  
+
+  (for-each 
+   (lambda (attrib) 
+     (let* ((name-value (get-attribute-name-value attrib))
+	    (name (car name-value))
+	    (value (cdr name-value))
+	    (prefix (get-prefix value)))
+       (if (string=? name "uref")
+	   (set-attribute-value! attrib (string-append 
+					 prefix 
+					 (number->string (get-next-uref prefix)))))))
+   attribs))
---
+++ gschem/scheme/print-NB-attribs.scm
@@ -0,0 +1,27 @@
+;; gEDA - GPL Electronic Design Automation
+;; gschem - gEDA Schematic Capture
+;; Copyright (C) 1998-2000 Ales V. Hvezda
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA
+
+(define (print-NB-attribs attribs)
+  (for-each 
+   (lambda (attrib) 
+     (let* ((name-value (get-attribute-name-value attrib))
+	    (name (car name-value))
+	    (value (cdr name-value)))
+       (if (string=? name "NB")
+	   (display (string-append value "\n")))))
+   attribs))
