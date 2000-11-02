cf4c0c1120daeaafce43f7740da8cbba66eca16a
Stefan Petersen <spetm@users.sourceforge.net>
2000-11-02 19:17:00 +0000

    Updated copyright

--- gnetlist/scheme/gnet-PCBboard.scm
+++ gnetlist/scheme/gnet-PCBboard.scm
@@ -1,7 +1,7 @@
 
 ;;; gEDA - GNU Electronic Design Automation
 ;;; gnetlist - GNU Netlist
-;;; Copyright (C) 1998 Ales V. Hvezda
+;;; Copyright (C) 1998-2000 Ales V. Hvezda
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
