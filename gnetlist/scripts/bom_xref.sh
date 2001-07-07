7972513a56e81d8d0742e4b12927c82828586ddc
Ales Hvezda <ahvezda@seul.org>
2001-07-07 18:14:18 +0000

    Bunch of new symbols and scripts

---
+++ gnetlist/scripts/bom_xref.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# ha ha
+
+if [ -z $1 ] ; then
+    echo "usage $0 geda-bom  # output on stdout"
+    exit -1
+fi    
+cat $1 | sort -k1,1 |\
+awk '!/device/{printf("%-5s %-20s %-20s %-20s\n", toupper($1), toupper($2), toupper($3), toupper($4));} /device/{}'
+
