84a8fa82ab23ddda7e20e9cf433678e7bb8ee62b
Piotr Miarecki <tertul@poczta.onet.pl>
2003-11-07 08:54:17 +0000

    Main goal: make xinstall should not depend on GAF version (no more manual version changes required in this file)

--- setup/src/setup.sh
+++ setup/src/setup.sh
@@ -5,11 +5,6 @@
 
 GTK_VERSION=1.2.4
 
-# this should be derrived from Makefile
-VERSION=20030525
-CD_VERSION=-${VERSION}
-DIR_PREFIX=geda-
-
 ##############################################################################
 
 GLIB_LIB=libglib-1.2.so.0
@@ -24,10 +19,34 @@ PREFIX=${PWD}
 PATH=${PREFIX}:${PREFIX}/bin:${PATH}
 LD_LIBRARY_PATH=${PREFIX}:${PREFIX}/lib:${LD_LIBRARY_PATH}
 
-echo
-echo gEDA Suite setup ${VERSION}
+echo "gEDA Suite setup"
 echo "(c) 2001-2003 Piotr Miarecki"
-echo
+echo ""
+
+if test -z $1; then
+	echo "Usage:"
+	echo "        setup.sh <VERSION> [<DIR_PREFIX> [<CD_VERSION>]]"
+	echo ""
+	echo "Examples:"
+	echo "        setup.sh 20030901"
+	echo "        setup.sh 20030901 geda-"
+	echo "        setup.sh 20030901 geda- -20030901"
+	exit 1
+fi
+
+GAF_VERSION=$1
+
+if test -n $2; then
+	DIR_PREFIX=$2
+	if test -n $3; then
+		CD_VERSION=$3
+	else
+		CD_VERSION=
+	fi
+else
+	DIR_PREFIX=
+	CD_VERSION=
+fi
 
 echo Checking for tar ...
 if ! (which tar >/dev/null 2>/dev/null) ; then
@@ -93,24 +112,48 @@ if ! test -x /lib/${GTK_LIB} && ! test -x /usr/lib/${GTK_LIB} && ! test -x ./${G
 	fi
 fi
 
-echo Checking for ${DIR_PREFIX}setup ...
-if ! test -x ./${DIR_PREFIX}setup ; then
-	echo WARNING ! Setup not found, building ... >&2
-	if ! test -f ${DIR_PREFIX}setup${CD_VERSION}.tar.gz ; then
-		echo WARNING ! Cannot find ${DIR_PREFIX}setup${CD_VERSION}.tar.gz, downloading using wget ... >&2
-		wget -c -t0 ftp://ftp.seul.org/geda/devel/${VERSION}/${DIR_PREFIX}setup${CD_VERSION}.tar.gz >/dev/null 2>/dev/null
-		if ! test -f ${DIR_PREFIX}setup${CD_VERSION}.tar.gz ; then
-			echo ERROR ! Cannot download ${DIR_PREFIX}setup${CD_VERSION}.tar.gz, please copy it to ${PREFIX}, then restart setup >&2
-			exit
+echo "Checking for geda-setup ..."
+if test -d ${DIR_PREFIX}setup${CD_VERSION}; then
+	echo "Using CVS version of geda-setup and setup.cfg ..."
+	if ! test -x ${DIR_PREFIX}setup${CD_VERSION}/src/setup || ! test -f ${DIR_PREFIX}setup${CD_VERSION}/src/setup.cfg; then
+		echo "WARNING ! Setup not found, building ..."
+		make setup
+	fi
+	cp ${DIR_PREFIX}setup${CD_VERSION}/src/setup geda-setup 
+	sed -e s/\${CD_VERSION}/-${GAF_VERSION}/g -e s/\${GAF_VERSION}/${GAF_VERSION}/g -e s/\${DIR_PREFIX}/${DIR_PREFIX}/g ${DIR_PREFIX}setup${CD_VERSION}/src/setup.cfg > setup.cfg
+else
+	if test -f ${DIR_PREFIX}setup${CD_VERSION}.tar.gz ; then
+		echo "Using distributed version of geda-setup and setup.cfg (from existing ${DIR_PREFIX}setup${CD_VERSION}.tar.gz) ..."
+	else
+		echo "File ${DIR_PREFIX}setup${CD_VERSION}.tar.gz not found in current directory, trying to download ..."
+		wget -c -t0 ftp://ftp.seul.org/geda/devel/${GAF_VERSION}/${DIR_PREFIX}setup${CD_VERSION}.tar.gz >/dev/null 2>/dev/null
+		if test -f ${DIR_PREFIX}setup${CD_VERSION}.tar.gz ; then
+			echo "Using distributed version of geda-setup and setup.cfg (from downloaded ${DIR_PREFIX}setup${CD_VERSION}.tar.gz) ..."
 		fi
 	fi
-	tar -xzf ${DIR_PREFIX}setup${CD_VERSION}.tar.gz
-	cd ${DIR_PREFIX}setup${CD_VERSION}
-	./configure
-	make
-	mv src/setup ../${DIR_PREFIX}setup
-	cd ..
+	if test -f ${DIR_PREFIX}setup${CD_VERSION}.tar.gz ; then
+		echo "Forced setup building ..."
+		tar -xzf ${DIR_PREFIX}setup${CD_VERSION}.tar.gz
+		cd ${DIR_PREFIX}setup${CD_VERSION}
+		echo "   configuring ..."
+		./configure > /dev/null
+		echo "   compiling ..."
+		make > /dev/null
+		if ! test ${DIR_PREFIX}setup${CD_VERSION}/src/setup; then
+			echo "ERROR ! Cannot build setup, installation cannot be continued !" >&2
+			exit 0
+		fi
+		cd ..
+		cp ${DIR_PREFIX}setup${CD_VERSION}/src/setup geda-setup
+		sed -e s/\${CD_VERSION}/-${GAF_VERSION}/g -e s/\${GAF_VERSION}/${GAF_VERSION}/g -e s/\${DIR_PREFIX}/${DIR_PREFIX}/g ${DIR_PREFIX}setup${CD_VERSION}/src/setup.cfg > setup.cfg
+		rm -Rf ${DIR_PREFIX}setup${CD_VERSION}
+	elif test -x geda-setup && test -f setup.cfg ; then
+		echo "Using geda-setup and setup.cfg existing in current directory ..."
+	else
+		echo "ERROR ! Cannot find geda-setup and setup.cfg, installation cannnot be continued !" >&2
+		exit
+	fi
 fi
 
-echo Starting ${DIR_PREFIX}setup ...
-./${DIR_PREFIX}setup
+echo "Starting ${DIR_PREFIX}setup ..."
+./geda-setup
