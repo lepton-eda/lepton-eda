517b127448c8447b103e7df2b2cfcc8a30787ef8
Ales Hvezda <ahvezda@seul.org>
2004-01-05 06:11:56 +0000

    Oops forgot to add new rc file

---
+++ gnetlist/tests/gnetlistrc.vhdl
@@ -0,0 +1,10 @@
+;
+; This file is really a gnetlistrc file.
+; It is renamed to gnetlistrc before any vhdl backend test is run.
+;
+; The path is hardcoded for now.
+;
+
+(component-library "${HOME}/geda/share/gEDA/sym/vhdl")
+
+
