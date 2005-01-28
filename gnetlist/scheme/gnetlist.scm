a7fd0db667112d4fb650945d95eb03270ac25dfb
Dan McMahill <dan@mcmahill.net>
2005-01-28 12:09:14 +0000

    Add gnetlist:build-refdes-aliases, gnetlist:alias-refdes, and gnetlist:unalias-refdes functions.  These are used to deal with target netlist refdes requirements which may be more restrictive than gschem/gnetlist.  A backend uses these by calling gnetlist:build-refdes-aliases at the beginning of netlisting and then gnetlist:alias-refdes whenever printing out the refdes.
    While here add missing RCS Id.

--- gnetlist/scheme/gnetlist.scm
+++ gnetlist/scheme/gnetlist.scm
@@ -1,3 +1,5 @@
+;;; $Id$
+;;;
 ;;; gEDA - GNU Electronic Design Automation
 ;;; gnetlist - GNU Netlist
 ;;; Copyright (C) 1998-2000 Ales V. Hvezda
@@ -253,6 +255,12 @@
 (define gnetlist:net-hash-forward (make-hash-table  (length all-nets)))
 (define gnetlist:net-hash-reverse (make-hash-table  (length all-nets)))
 
+;; These will become hash tables which provide the mapping
+;; from gEDA refdes to netlist refdes and from netlist
+;; refdes to gEDA refdes.
+(define gnetlist:refdes-hash-forward (make-hash-table  (length packages)))
+(define gnetlist:refdes-hash-reverse (make-hash-table  (length packages)))
+
 ;; build the hash tables with the net name mappings and 
 ;; while doing so, check for any shorts which are created
 ;; by modifying the netnames.  If a short occurs, error out
@@ -295,6 +303,48 @@
     )
   )
 
+;; build the hash tables with the refdes mappings and 
+;; while doing so, check for any name clashes which are created
+;; by modifying the refdes's.  If a name clash occurs, error out
+;; with a descriptive message.
+;;
+;; This function should be called as one of the first steps
+;; in a netlister which needs to alias refdes's.
+(define gnetlist:build-refdes-aliases
+  (lambda (mapfn refdeses)
+    (if (not (null? refdeses))
+	(begin
+	  (let ( (refdes (car refdeses))
+		 (alias (mapfn (car refdeses)))
+		 )
+	    
+            (if (hash-ref gnetlist:refdes-hash-reverse alias)
+                (begin
+                  (display "***** ERROR *****\n")
+                  (display "There is a refdes name collision!\n")
+                  (display "The refdes \"")
+                  (display refdes)
+                  (display "\" will be mapped\nto \"")
+                  (display alias)
+                  (display "\" which is already used\n")
+                  (display "by \"")
+                  (display (hash-ref gnetlist:refdes-hash-reverse alias))
+                  (display "\".\n")
+                  (display "This may be caused by refdes attributes colliding with others\n")
+                  (display "due to truncation of the refdes, case insensitivity, or\n")
+                  (display "other limitations imposed by this netlist format.\n")
+                  (error)
+                  )
+                )
+            (hash-create-handle! gnetlist:refdes-hash-forward refdes alias)
+            (hash-create-handle! gnetlist:refdes-hash-reverse alias  refdes  )
+            (gnetlist:build-refdes-aliases mapfn (cdr refdeses))
+	    )
+          )
+        )
+    )
+  )
+
 ;; convert a gEDA netname into an output netlist net name
 (define gnetlist:alias-net
   (lambda (net)
@@ -302,9 +352,23 @@
     )
   )
 
+;; convert a gEDA refdes into an output netlist refdes
+(define gnetlist:alias-refdes
+  (lambda (refdes)
+    (hash-ref gnetlist:refdes-hash-forward refdes)
+    )
+  )
+
 ;; convert an output netlist net name into a gEDA netname
 (define gnetlist:unalias-net
   (lambda (net)
     (hash-ref gnetlist:net-hash-reverse net)
+    )
+  )
+
+;; convert an output netlist refdes into a gEDA refdes
+(define gnetlist:unalias-refdes
+  (lambda (refdes)
+    (hash-ref gnetlist:refdes-hash-reverse refdes)
     )
   )
