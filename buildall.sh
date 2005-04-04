2c12978549cc2ea5ede737e84d3bcb8b94a287c7
Ales Hvezda <ahvezda@seul.org>
2005-04-04 03:00:07 +0000

    Fixed buildall.sh to work on Debian (/bin/sh vs /bin/bash) and add some post build cleanup.

--- buildall.sh
+++ buildall.sh
@@ -7,7 +7,7 @@ export MAKE=make
 export GNUMAKE=make
 
 # Nothing below configurable
-# -------------------------------------------------------------
+########################################################################
 
 # Check for bzip2 as some tarballs come as .tar.bz2 files
 whichbzip2=`which bzip2`
@@ -23,6 +23,7 @@ if [ "$whichlynx" = "" ]
 then
 	touch $INSTALLDIR/bin/lynx
 	chmod 755 $INSTALLDIR/bin/lynx
+	CREATEDLYNX=1
 fi
 
 # Create a somewhat dumb docbook2pdf if it doesn't exist
@@ -38,6 +39,7 @@ touch \$newfilename
 EOF
 
 	chmod 755 $INSTALLDIR/bin/docbook2pdf
+	CREATEDDOCBOOK2PDF=1
 fi
 
 
@@ -49,9 +51,8 @@ export LDFLAGS=-L$INSTALLDIR/lib
 
 EXTRA_CONFIGURE_FLAGS=
 
-
 # $1 name of package and created subdirectory
-configure-build-install()
+configure_build_install()
 {
 	filename=$1.tar.gz
 	tarflags=xvfz
@@ -116,38 +117,62 @@ configure-build-install()
 	cd ..
 }
 
-configure-build-install pkgconfig-0.15.0 
+########################################################################
+#
+# Build the actual dependencies now 
+#
+########################################################################
+
+configure_build_install pkgconfig-0.15.0 
 
 export EXTRA_CONFIGURE_FLAGS=--disable-csharp 
-configure-build-install gettext-0.14.3 
+configure_build_install gettext-0.14.3 
 export EXTRA_CONFIGURE_FLAGS=
 
-configure-build-install freetype-2.1.9 
-configure-build-install render-0.8 
-configure-build-install libXrender-0.8.4 
-configure-build-install expat-1.95.8 
-configure-build-install fontconfig-2.3.1 
-configure-build-install libXft-2.1.6 
-configure-build-install xextensions-1.0.1 
-configure-build-install fixesext-2.0.1 
-configure-build-install libXfixes-2.0.1 
-configure-build-install libXcursor-1.1.2 
-configure-build-install zlib-1.2.2 
-configure-build-install libpng-1.2.8-config 
-configure-build-install libiconv-1.9.2
-configure-build-install glib-2.6.3 
-configure-build-install atk-1.9.0 
-configure-build-install pango-1.8.1 
+configure_build_install freetype-2.1.9 
+configure_build_install render-0.8 
+configure_build_install libXrender-0.8.4 
+configure_build_install expat-1.95.8 
+configure_build_install fontconfig-2.3.1 
+configure_build_install libXft-2.1.6 
+configure_build_install xextensions-1.0.1 
+configure_build_install fixesext-2.0.1 
+configure_build_install libXfixes-2.0.1 
+configure_build_install libXcursor-1.1.2 
+configure_build_install zlib-1.2.2 
+configure_build_install libpng-1.2.8-config 
+configure_build_install libiconv-1.9.2
+configure_build_install glib-2.6.3 
+configure_build_install atk-1.9.0 
+configure_build_install pango-1.8.1 
 
 export EXTRA_CONFIGURE_FLAGS=-"-without-libtiff --without-libjpeg"
-configure-build-install gtk+-2.6.4 
+configure_build_install gtk+-2.6.4 
 export EXTRA_CONFIGURE_FLAGS=
 
-configure-build-install guile-1.6.7 
-configure-build-install libstroke-0.5.1
-configure-build-install libgdgeda-2.0.15 
+configure_build_install guile-1.6.7 
+configure_build_install libstroke-0.5.1
+configure_build_install libgdgeda-2.0.15 
 
-# geda (all tarballs and Makefile)
+########################################################################
+#
+# Now build gEDA/gaf
+#
+########################################################################
 $MAKE open
 $MAKE install
 
+########################################################################
+#
+# Cleanup 
+#
+########################################################################
+if [ "$CREATEDLYNX" != "" ]
+then
+	rm -f $INSTALLDIR/bin/lynx
+fi
+
+if [ "$CREATEDDOCBOOK2PDF" != "" ]
+then
+	rm -f $INSTALLDIR/bin/docbook2pdf
+fi
