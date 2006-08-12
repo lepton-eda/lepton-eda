839c54cd1aebc6a52d972ee8f979aceb3a5389c2
Stuart Brorson <sdb@cloud9.net>
2006-08-12 22:03:05 +0000

    Implemented getopt_long for systems which have it.

--- gattrib/src/parsecmd.c
+++ gattrib/src/parsecmd.c
@@ -27,6 +27,25 @@
 #include <unistd.h>
 #endif
 
+
+#ifdef HAVE_GETOPT_H
+#include <getopt.h>
+#endif  /* Checking for getopt  */
+
+#if !defined(HAVE_GETOPT_LONG) || !defined(HAVE_GETOPT_H)
+#define OPTIONS "qvh"
+#ifndef OPTARG_IN_UNISTD
+extern char *optarg;
+extern int optind;
+#endif
+#endif   /* Checking for getopt_long  */
+
+
+#ifdef HAVE_LIBDMALLOC
+#include <dmalloc.h>
+#endif
+
+
 /*------------------------------------------------------------------
  * Gattrib specific includes
  *------------------------------------------------------------------*/
@@ -35,24 +54,32 @@
 #include "../include/prototype.h"  /* function prototypes */
 #include "../include/globals.h"
 
-#ifdef HAVE_LIBDMALLOC
-#include <dmalloc.h>
-#endif
-
-
-#define OPTIONS "qvh"
-
-#ifndef OPTARG_IN_UNISTD
-extern char *optarg;
-extern int optind;
-#endif
-
 
 void usage(char *cmd)
 {
+    printf("\n");
+    printf("Gattrib:  The gEDA project\'s attribute editor.\n");
+    printf("Presents schematic attributes in easy-to-edit spreadsheet format.\n");
+    printf("\n");
     printf("Usage: %s [OPTIONS] filename1 ... filenameN\n", cmd);
-    printf("  -q                Quiet mode\n");
-    printf("  -v                Verbose mode on\n");
+    printf("  -q, --quiet            Quiet mode\n");
+    printf("  -v, --verbose          Verbose mode on\n");
+    printf("  -h, --help             This help menu\n");
+    printf("\n");
+    printf("  FAQ:\n");
+    printf("  *  What do the colors of the cell text mean?\n");
+    printf("     The cell colors indicate the visibility of the attribute.\n");
+    printf("     Black = Visible attribute, value displayed only.\n");
+    printf("     Grey  = Invisible attribute.\n");
+    printf("     Red   = Visible attribute, name displayed only.\n");
+    printf("     Blue  = Visible attribute, both name and value displayed.\n");
+    printf("\n");
+    printf("  *  What does the period (\".\") at the end of some component refdeses mean?\n");
+    printf("     The period is placed after the refdeses of slotted components.\n");
+    printf("     If slots are present on the component, then the different slots appear\n");
+    printf("     in different rows with the slot number after the period.  Example:  C101.2.\n");
+    printf("\n");
+    printf("Copyright (C) 2003 -- 2006 Stuart D. Brorson.  E-mail: sdb (AT) cloud9 (DOT) net.\n");
     printf("\n");
     exit(0);
 }
@@ -62,28 +89,46 @@ int parse_commandline(int argc, char *argv[])
 {
     int ch;
 
+#if defined(HAVE_GETOPT_LONG) && defined(HAVE_GETOPT_H)
+    /* Use getopt_long if it is available */
+    int option_index = 0;
+    static struct option long_options[] = {
+      {"help", 0, 0, 'h'},
+      {"quiet", 0, 0, 'q'},
+      {"verbose", 0, 0, 'v'},
+      {0, 0, 0, 0}
+    };
+
+    while (1) {
+      ch = getopt_long(argc, argv, "hqv", long_options, &option_index);
+      if (ch == -1)
+	break;
+#else
+    /* Otherwise just use regular getopt */
     while ((ch = getopt(argc, argv, OPTIONS)) != -1) {
-	switch (ch) {
+#endif
 
-	case 'v':
-	    verbose_mode = TRUE;
-	    break;
-
-	case 'q':
-	    quiet_mode = TRUE;
-	    break;
-
-	case 'h':
-	    usage(argv[0]);
-	    break;
-
-	case '?':
-	default:
-	    usage(argv[0]);
-	    break;
-	}
+      switch (ch) {
+	
+      case 'v':
+	verbose_mode = TRUE;
+	break;
+	
+      case 'q':
+	quiet_mode = TRUE;
+	break;
+	
+      case 'h':
+	usage(argv[0]);
+	break;
+	
+      case '?':
+      default:
+	usage(argv[0]);
+	break;
+      }
     }
-
+    
     if (quiet_mode) {
 	verbose_mode = FALSE;
     }
