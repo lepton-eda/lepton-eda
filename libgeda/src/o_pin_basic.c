08b4be3b893a494b7350cb30c710c532525a81e7
Carlos Nieves Onega <cnieves@iespana.es>
2007-04-21 17:40:52 +0000

    Added back variables removed in previous patch. * src/o_bus_basic.c, src/o_circle_basic.c, src/o_line_basic.c,   src/o_pin_basic.c:   - Reverted the patch below. Those variables are used when     compiling with libgd.

--- libgeda/src/o_pin_basic.c
+++ libgeda/src/o_pin_basic.c
@@ -349,6 +349,7 @@ void o_pin_print(TOPLEVEL *w_current, FILE *fp, OBJECT *o_current,
 void o_pin_image_write(TOPLEVEL *w_current, OBJECT *o_current,
 		       int origin_x, int origin_y, int color_mode)
 {
+  int x[2], y[2];
   int color;
 
   if (o_current == NULL) {
