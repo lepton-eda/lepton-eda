88f34208e11f10497dde843d48dac57d01c27e6d
Ales Hvezda <ahvezda@seul.org>
2007-06-02 01:02:42 +0000

    Fixed the odd behavior of not cancelling the placement of components.
    Fixed a behavior where you would cancel (via right mouse button or escape)
    the placing of components, but gschem would still be in the add component
    mode (and clicks would still place components).
    (cherry picked from commit 14fb224ec0f09b0b95a083abcaf3f5eaaf41b74f)

--- gschem/include/x_states.h
+++ gschem/include/x_states.h
@@ -24,14 +24,59 @@
 
 /* NOTE: when adding states, also update i_status_string() function */
 
-enum x_states { NONE, SELECT, DRAWLINE, DRAWBOX, MOVE, COPY, DRAWCIRCLE, ZOOM,
-  PAN, DRAWNET, NETCONT, TEXTENTRY, DRAWPIN, DRAWARC, STARTDRAWNET, DRAWCOMP,
-  SBOX, STARTPAN, STARTSELECT, STARTCOPY, STARTMOVE, ENDCOPY, ENDMOVE, 
-  ENDLINE, ENDBOX, ENDCIRCLE, ENDARC, ENDPIN, ENDCOMP, DRAWATTRIB, ENDATTRIB,
-  DRAWTEXT, ENDTEXT, ENDROTATEP, ENDMIRROR, ZOOMBOXSTART, ZOOMBOXEND, 
-  STARTROUTENET, ENDROUTENET, MOUSEPAN, DRAWBUS, BUSCONT, STARTDRAWBUS, 
-  STARTPASTE, ENDPASTE, GRIPS, DRAWPICTURE, ENDPICTURE, MCOPY, STARTMCOPY,
-  ENDMCOPY};
+enum x_states { 
+  NONE, 		/* 0 */
+  SELECT, 		/* 1 */
+  DRAWLINE, 		/* 2 */
+  DRAWBOX, 		/* 3 */
+  MOVE, 		/* 4 */
+  COPY, 		/* 5 */
+  DRAWCIRCLE, 		/* 6 */
+  ZOOM,			/* 7 */
+  PAN, 			/* 8 */
+  DRAWNET, 		/* 9 */
+  NETCONT, 		/* 10 */
+  TEXTENTRY, 		/* 11 */
+  DRAWPIN, 		/* 12 */
+  DRAWARC, 		/* 13 */
+  STARTDRAWNET, 	/* 14 */
+  DRAWCOMP,		/* 15 */
+  SBOX, 		/* 16 */
+  STARTPAN, 		/* 17 */
+  STARTSELECT, 		/* 18 */
+  STARTCOPY, 		/* 19 */
+  STARTMOVE, 		/* 20 */
+  ENDCOPY, 		/* 21 */
+  ENDMOVE, 		/* 22 */
+  ENDLINE, 		/* 23 */
+  ENDBOX, 		/* 24 */
+  ENDCIRCLE, 		/* 25 */
+  ENDARC, 		/* 26 */
+  ENDPIN, 		/* 27 */
+  ENDCOMP, 		/* 28 */
+  DRAWATTRIB, 		/* 29 */
+  ENDATTRIB,		/* 30 */
+  DRAWTEXT, 		/* 31 */
+  ENDTEXT, 		/* 32 */
+  ENDROTATEP, 		/* 33 */
+  ENDMIRROR, 		/* 34 */
+  ZOOMBOXSTART, 	/* 35 */
+  ZOOMBOXEND, 		/* 36 */
+  STARTROUTENET, 	/* 37 */
+  ENDROUTENET, 		/* 38 */
+  MOUSEPAN, 		/* 39 */
+  DRAWBUS, 		/* 40 */
+  BUSCONT, 		/* 41 */
+  STARTDRAWBUS, 	/* 42 */
+  STARTPASTE, 		/* 43 */
+  ENDPASTE, 		/* 44 */
+  GRIPS, 		/* 45 */
+  DRAWPICTURE, 		/* 46 */
+  ENDPICTURE, 		/* 47 */
+  MCOPY, 		/* 48 */
+  STARTMCOPY,		/* 49 */
+  ENDMCOPY		/* 50 */
+};
 
 
 #endif
