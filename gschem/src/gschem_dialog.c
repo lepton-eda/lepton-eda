d86ac8ce4b16d41699ec64a3d88fd8d2c760b770
Cesar Strauss <cestrauss@gmail.com>
2007-09-16 16:56:18 -0300

    Fix for dialogs drifting downwards on Cygwin/X.
    On Cygwin/X, reopening a gschem dialog restores its original position,
    but with a small vertical offset.
    
    The workaround is to show the dialog before restoring its position.
    
    The docs for gtk_window_move() offer a possible justification:
      "Most window managers ignore requests for initial window positions
      (instead using a user-defined placement algorithm) and honor requests
      after the window has already been shown."
    
    The downside is, if you have a slow system, you could see the dialog
    appear briefly at the old position, I suppose.

--- gschem/src/gschem_dialog.c
+++ gschem/src/gschem_dialog.c
@@ -221,6 +221,9 @@ static void show_handler (GtkWidget *widget)
   gchar *group_name;
   GschemDialog *dialog = GSCHEM_DIALOG( widget );
 
+  /* Let GTK show the window */
+  GTK_WIDGET_CLASS (gschem_dialog_parent_class)->show (widget);
+
   group_name = dialog->settings_name;
   if (group_name != NULL) {
 
@@ -231,9 +234,6 @@ static void show_handler (GtkWidget *widget)
                      dialog_geometry, group_name);
     }
   }
-
-  /* Let GTK show the window */
-  GTK_WIDGET_CLASS (gschem_dialog_parent_class)->show (widget);
 }
 
 
