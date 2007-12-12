da5151addbfba61fb36fffbdd4710244493964c8
Peter TB Brett <peter@peter-b.co.uk>
2007-12-12 19:19:13 +0000

    gschem: Warn before overwriting files. [1846593]
    Also fixes a memory leak in the file save function.

--- gschem/src/x_fileselect.c
+++ gschem/src/x_fileselect.c
@@ -273,12 +273,34 @@ x_fileselect_save (TOPLEVEL *toplevel)
     gchar *filename =
       gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (dialog));
 
+    /* If the file already exists, display a dialog box to check if
+       the user really wants to overwrite it. */
+    if ((filename != NULL) && g_file_test (filename, G_FILE_TEST_EXISTS)) {
+      GtkWidget *checkdialog = 
+        gtk_message_dialog_new (GTK_WINDOW(dialog),
+                                (GTK_DIALOG_MODAL | 
+                                 GTK_DIALOG_DESTROY_WITH_PARENT),
+                                GTK_MESSAGE_QUESTION,
+                                GTK_BUTTONS_YES_NO,
+                                _("The selected file `%s' already exists.\n\n"
+                                  "Would you like to overwrite it?"),
+                                filename);
+      gtk_window_set_title (GTK_WINDOW (checkdialog), _("Overwrite file?"));
+      if (gtk_dialog_run (GTK_DIALOG (checkdialog)) != GTK_RESPONSE_YES) {
+        s_log_message (_("Save cancelled on user request\n"));
+        g_free (filename);
+        filename = NULL;
+      }
+      gtk_widget_destroy (checkdialog);
+    }
     /* try saving current page of toplevel to file filename */
     if (filename != NULL) {
       x_window_save_page (toplevel,
                           toplevel->page_current,
                           filename);
     }
+
+    g_free (filename);
   }
   gtk_widget_destroy (dialog);
 
