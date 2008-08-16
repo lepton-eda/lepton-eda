lepton-eda/lepton-eda

commit a3513b7e6dc7bf6c36d95d4da86082beeb79831d

Replace standard string manipulations with GLib string utility functions.

gschem/src/i_callbacks.c
@@ -75,19 +75,12 @@ static const char *fnameext_get(const char* fname)
 static char *fnameext_remove(const char *fname)
 {
   const char *p = fnameext_get(fname);
-  char *fname_new;
-  int len;
+  char *fname_new = NULL;
 
   if(*p == '\0') {
     fname_new = g_strdup (p);
   } else {
-    len = (p - fname); /*  + 1; this extra was causing grief */
-    fname_new = (char *) g_malloc(sizeof(char) * (len + 1));
-    if(fname_new == NULL) {
-      return NULL;
-    }
-    strncpy(fname_new, fname, len);
-    fname_new[len] = '\0';
+    fname_new = g_strndup (fname_new, p - fname);
   }
   return fname_new;
 }

gattrib/src/s_rename.c
@@ -183,15 +183,9 @@ void s_rename_add(char *src, char *dest)
 #endif
 
 		    rename_pairs[cur_set][rename_counter].src =
-			(char *) g_malloc(sizeof(char) * (strlen(src) + 1));
-		    strcpy(rename_pairs[cur_set][rename_counter].src, src);
+			g_strdup(src);
 		    rename_pairs[cur_set][rename_counter].dest =
-			(char *) g_malloc(sizeof(char) *
-					(strlen
-					 (rename_pairs[cur_set][i].dest) +
-					 1));
-		    strcpy(rename_pairs[cur_set][rename_counter].dest,
-			   rename_pairs[cur_set][i].dest);
+			g_strdup(rename_pairs[cur_set][i].dest);
 		    rename_counter++;
 		}
 	    }
@@ -199,11 +193,9 @@ void s_rename_add(char *src, char *dest)
     } else {
 
 	rename_pairs[cur_set][rename_counter].src =
-	    (char *) g_malloc(sizeof(char) * (strlen(src) + 1));
-	strcpy(rename_pairs[cur_set][rename_counter].src, src);
+	    g_strdup(src);
 	rename_pairs[cur_set][rename_counter].dest =
-	    (char *) g_malloc(sizeof(char) * (strlen(dest) + 1));
-	strcpy(rename_pairs[cur_set][rename_counter].dest, dest);
+	    g_strdup(dest);
 	rename_counter++;
     }
     if (rename_counter == MAX_RENAME) {
@@ -236,8 +228,7 @@ void s_rename_all_lowlevel(NETLIST * netlist_head, char *src, char *dest)
 			/*g_free(pl_current->net_name); */
 
 			pl_current->net_name =
-			    g_malloc(sizeof(char) * (strlen(dest) + 1));
-			strcpy(pl_current->net_name, dest);
+			    g_strdup(dest);
 		    }
 		}
 

gnetlist/src/gnetlist.c
@@ -118,9 +118,7 @@ void main_prog(void *closure, int argc, char *argv[])
     TOPLEVEL *pr_current;
 
     /* set default output filename */
-    output_filename =
-        (char *) g_malloc(sizeof(char) * (strlen("output.net") + 1));
-    strcpy(output_filename, "output.net");
+    output_filename = g_strdup("output.net");
 
     argv_index = parse_commandline(argc, argv);
     cwd = getcwd(NULL, MAXPATHLEN);

gnetlist/src/parsecmd.c
@@ -162,9 +162,7 @@ int parse_commandline(int argc, char *argv[])
 	    break;
 
 	case 'g':
-	    guile_proc = (char *) g_malloc(sizeof(char) *
-					 (strlen(optarg) + 1));
-	    strcpy(guile_proc, optarg);
+	    guile_proc = g_strdup(optarg);
 
 	    break;
 
@@ -183,9 +181,7 @@ int parse_commandline(int argc, char *argv[])
 
 	case 'o':
 	    g_free(output_filename);
-	    output_filename = (char *) g_malloc(sizeof(char) *
-					      (strlen(optarg) + 1));
-	    strcpy(output_filename, optarg);
+	    output_filename = g_strdup(optarg);
 	    break;
 
 	case 'O':        

gnetlist/src/s_hierarchy.c
@@ -564,9 +564,7 @@ s_hierarchy_remove_uref_mangling(TOPLEVEL * pr_current, NETLIST * head)
 		    sscanf(n_current->connected_to, "%s %s", uref, pin);
 		    new_uref =
 			s_hierarchy_return_baseuref(pr_current, uref);
-		    new_connected_to =
-			g_strdup(n_current->connected_to);
-		    sprintf(new_connected_to, "%s %s", new_uref, pin);
+		    new_connected_to = g_strdup_printf("%s %s", new_uref, pin);
 		    g_free(n_current->connected_to);
 		    n_current->connected_to = new_connected_to;
 		}
@@ -586,7 +584,6 @@ char *s_hierarchy_return_baseuref(TOPLEVEL * pr_current, char *uref)
     char *start_of_base = NULL;
     char *end_of_base = NULL;
     char *cptr = NULL;
-    int i = 0;
 
     /* use hierarchy separator */
 
@@ -618,14 +615,7 @@ char *s_hierarchy_return_baseuref(TOPLEVEL * pr_current, char *uref)
 
 	cptr = uref;
 
-	return_value = (char *) g_malloc(sizeof(char) * (strlen(uref)));
-	i = 0;
-	while (cptr != end_of_base) {
-	    return_value[i] = *cptr;
-	    i++;
-	    cptr++;
-	}
-	return_value[i] = '\0';
+	return_value = g_strndup(uref, end_of_base - uref);
     }
 
 #if DEBUG

gnetlist/src/s_net.c
@@ -180,30 +180,19 @@ char *s_net_return_connected_string(TOPLEVEL * pr_current, OBJECT * object,
     uref = s_hierarchy_create_uref(pr_current, temp_uref, hierarchy_tag);
 
     if (uref && pinnum) {
-	string = (char *) g_malloc(sizeof(char) *
-				 strlen(uref) + strlen(pinnum) +
-				 strlen("  ") + 1);
-
-	sprintf(string, "%s %s", uref, pinnum);
+	string = g_strdup_printf("%s %s", uref, pinnum);
     } else {
 	if (pinnum) {
-	    string = (char *) g_malloc(sizeof(char) *
-				     strlen(pinnum) +
-				     strlen("POWER") + strlen("  ") + 1);
-	    sprintf(string, "POWER %s", pinnum);
+	    string = g_strdup_printf("POWER %s", pinnum);
 	} else {
-	    string = (char *) g_malloc(sizeof(char) *
-				     strlen("U?") + strlen("?") +
-				     strlen("  ") + 1 + hierarchy_tag_len);
-
 	    if (hierarchy_tag) {
 		misc =
 		    s_hierarchy_create_uref(pr_current, "U?",
 					    hierarchy_tag);
-		sprintf(string, "%s ?", misc);
+		string = g_strdup_printf("%s ?", misc);
 		g_free(misc);
 	    } else {
-		sprintf(string, "U? ?");
+		string = g_strdup("U? ?");
 	    }
 
 	    fprintf(stderr, "Missing Attributes (refdes and pin number)\n");
@@ -424,12 +413,8 @@ char *s_net_name(TOPLEVEL * pr_current, NETLIST * netlist_head,
 
     if (net_head->nid == -1 && net_head->prev == NULL
 	&& net_head->next == NULL) {
-	string =
-	    (char *) g_malloc(sizeof(char) * (strlen("unconnected_pin-")) +
-			    10);
-
-	sprintf(string, "unconnected_pin-%d", 
-		unnamed_pin_counter++);
+	string = g_strdup_printf("unconnected_pin-%d",
+                           unnamed_pin_counter++);
 
 	return (string);
 
@@ -447,29 +432,21 @@ char *s_net_name(TOPLEVEL * pr_current, NETLIST * netlist_head,
 
 	if (netlist_mode == SPICE) {
 	    string =
-		(char *) g_malloc(sizeof(char) * (strlen("99999") + 10));
-	    sprintf(string, "%d", unnamed_net_counter++);
+        g_strdup_printf("%d", unnamed_net_counter++);
 
 	    return (string);
 	} else {
-	    string =
-		(char *) g_malloc(sizeof(char) *
-				(strlen(pr_current->unnamed_netname) + 10 +
-				 hierarchy_tag_len));
-
 	    if (hierarchy_tag) {
-		temp =
-		    (char *) g_malloc(sizeof(char) * (strlen("99999") + 10));
-		sprintf(temp, "%s%d", pr_current->unnamed_netname, 
+		temp = g_strdup_printf("%s%d", pr_current->unnamed_netname, 
 		        unnamed_net_counter++);
 
 		misc =
 		    s_hierarchy_create_netname(pr_current, temp,
 					       hierarchy_tag);
-		strcpy(string, misc);
+		string = g_strdup(misc);
 		g_free(misc);
 	    } else {
-		sprintf(string, "%s%d", pr_current->unnamed_netname, 
+		string = g_strdup_printf("%s%d", pr_current->unnamed_netname, 
 			unnamed_net_counter++);
 	    }
 

gnetlist/src/s_netattrib.c
@@ -130,14 +130,10 @@ s_netattrib_create_pins(TOPLEVEL * pr_current, OBJECT * o_current,
 		    s_hierarchy_create_netattrib(pr_current, net_name,
 						 hierarchy_tag);
 		old_cpin->nets->net_name_has_priority = TRUE;
-		connected_to =
-		    (char *) g_malloc(sizeof(char) *
-				    (strlen
-				     (netlist->component_uref) +
-				     strlen(current_pin) + 2));
-		sprintf(connected_to, "%s %s",
-			netlist->component_uref, current_pin);
-		old_cpin->nets->connected_to = g_strdup (connected_to);
+		connected_to = g_strdup_printf("%s %s",
+                                   netlist->component_uref,
+                                   current_pin);
+		old_cpin->nets->connected_to = g_strdup(connected_to);
 		old_cpin->nets->nid = o_current->sid;
 		g_free(connected_to);
 	    } else {
@@ -156,13 +152,9 @@ s_netattrib_create_pins(TOPLEVEL * pr_current, OBJECT * o_current,
 		    s_hierarchy_create_netattrib(pr_current, net_name,
 						 hierarchy_tag);
 
-		connected_to =
-		    (char *) g_malloc(sizeof(char) *
-				    (strlen
-				     (netlist->component_uref) +
-				     strlen(current_pin) + 2));
-		sprintf(connected_to, "%s %s",
-			netlist->component_uref, current_pin);
+		connected_to = g_strdup_printf("%s %s",
+                                   netlist->component_uref,
+                                   current_pin);
 		new_cpin->nets->connected_to = g_strdup(connected_to);
 		new_cpin->nets->nid = o_current->sid;
 

gnetlist/src/s_rename.c
@@ -192,10 +192,8 @@ void s_rename_add(char *src, char *dest)
 		if (new_rename)
 		{
 		     new_rename->next = NULL;
-                     new_rename->src = (char *) g_malloc(sizeof(char) * (strlen(src) + 1));
-                     strcpy(new_rename->src, src);
-                     new_rename->dest = (char *) g_malloc(sizeof(char) * (strlen(temp->dest) + 1));
-                     strcpy(new_rename->dest, temp->dest);
+                     new_rename->src = g_strdup(src);
+                     new_rename->dest = g_strdup(temp->dest);
 		     /* If the rename pair was found then a set already exists, so there's no need the check it */
 		     if (last_set->first_rename == NULL)
 		     {
@@ -230,10 +228,8 @@ void s_rename_add(char *src, char *dest)
         if (new_rename)
 	{
 	     new_rename->next = NULL;
-             new_rename->src = (char *) g_malloc(sizeof(char) * (strlen(src) + 1));
-             strcpy(new_rename->src, src);
-             new_rename->dest = (char *) g_malloc(sizeof(char) * (strlen(dest) + 1));
-             strcpy(new_rename->dest, dest);
+             new_rename->src = g_strdup(src);
+             new_rename->dest = g_strdup(dest);
 	     if (last_set->first_rename == NULL)
 	     {
 	         last_set->first_rename = last_set->last_rename = new_rename;
@@ -265,8 +261,7 @@ void s_rename_all_lowlevel(NETLIST * netlist_head, char *src, char *dest)
 		{
                     if (strcmp(pl_current->net_name, src) == 0) 
 		    {
-                        pl_current->net_name = g_malloc(sizeof(char) * (strlen(dest) + 1));
-                        strcpy(pl_current->net_name, dest);
+                        pl_current->net_name = g_strdup(dest);
                     }
                 }
                 pl_current = pl_current->next;

commit a19feb2bf0e13ad3d030dfbb126a2b0558db6add

Bring back default Postscript filename.
The print dialog has recently stopped providing a default filename for
the Postscript output, due to a regression introduced in
a3513b7e6dc7bf6c36d95d4da86082beeb79831d.

gschem/src/i_callbacks.c
@@ -74,9 +74,9 @@ static char *fnameext_remove(const char *fname)
   char *fname_new = NULL;
 
   if(*p == '\0') {
-    fname_new = g_strdup (p);
+    fname_new = g_strdup (fname);
   } else {
-    fname_new = g_strndup (fname_new, p - fname);
+    fname_new = g_strndup (fname, p - fname);
   }
   return fname_new;
 }