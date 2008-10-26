2d2d9d90d07080ed18a899dcccc34e99c83a7402
Peter Clifton <pcjc2@cam.ac.uk>
2008-10-26 10:42:58 +0000

    Add place-holders for printing paths with different line / fill styles
    Currently, paths are always drawn with a solid stroked outline, and
    only solid filling is implemented.

--- gschem/src/o_path.c
+++ gschem/src/o_path.c
@@ -29,6 +29,17 @@
 #endif
 
 
+typedef void (*DRAW_FUNC) (GdkDrawable *w, GdkGC *gc, GdkColor *color,
+                           GSCHEM_TOPLEVEL *w_current, PATH *path,
+                           GdkCapStyle cap,
+                           gint line_width, gint length, gint space);
+
+typedef void (*FILL_FUNC) (GdkDrawable *w, GdkGC *gc, GdkColor *color,
+                           GSCHEM_TOPLEVEL *w_currentm, PATH *path,
+                           gint fill_width,
+                           gint angle1, gint pitch1, gint angle2, gint pitch2);
+
+
 static void path_to_points_modify (GSCHEM_TOPLEVEL *w_current, PATH *path,
                                    int dx, int dy, int new_x, int new_y, int whichone,
                                    GdkPoint **points, int *num_points)
@@ -120,6 +131,280 @@ static void find_points_bounds (GdkPoint *points, int num_points,
   }
 }
 
+/*! \brief Draw a path with a solid line type.
+ *  \par Function Description
+ *  This function draws a path with a solid line type. The length and space
+ *  parameters are not used by this function.
+ *
+ *  \param [in] w           GdkDrawable to draw in
+ *  \param [in] gc          GdkGC graphics context to draw on
+ *  \param [in] color       Box line color
+ *  \param [in] cap         Box line end cap type (unused)
+ *  \param [in] path        The PATH object to draw
+ *  \param [in] line_width  Width of line to draw path
+ *  \param [in] length      (unused)
+ *  \param [in] space       (unused)
+ */
+void o_path_draw_solid(GdkDrawable *w, GdkGC *gc, GdkColor *color,
+                      GSCHEM_TOPLEVEL *w_current, PATH *path,
+                      GdkCapStyle cap, gint line_width,
+                      gint length, gint space)
+{
+  GdkPoint *points;
+  int num_points;
+
+  path_to_points (w_current, path, 0, 0, &points, &num_points);
+
+  if (num_points == 0) {
+    g_free (points);
+    return;
+  }
+
+
+  gdk_gc_set_foreground(gc, color);
+
+  /* Set the width, end type and join style of the line */
+  gdk_gc_set_line_attributes(gc, line_width, GDK_LINE_SOLID,
+                             cap, GDK_JOIN_MITER);
+
+  if (path->sections[path->num_sections - 1].code == PATH_END) {
+    /* Closed path */
+    gdk_draw_polygon (w_current->backingstore, w_current->gc,
+                      FALSE, points, num_points);
+  } else {
+    /* Open path */
+    gdk_draw_lines (w_current->backingstore, w_current->gc,
+                    points, num_points);
+  }
+
+  g_free (points);
+}
+
+/*! \brief Draw a path with a dotted line type.
+ *  \par Function Description
+ *  This function draws a path with a dotted line type. The parameter
+ *  <B>space</B> represents the distance between two of the dots. The
+ *  parameter <B>length</B> is unused. The diameter of the dots is given by
+ *  the width of the line given by <B>width</B>.
+ *
+ *  \param [in] w           GdkDrawable to draw in.
+ *  \param [in] gc          GdkGC graphics context to draw on.
+ *  \param [in] color       Box line color.
+ *  \param [in] cap         Box line end cap type (unused).
+ *  \param [in] path        The PATH object to draw
+ *  \param [in] line_width  Width of line to draw path.
+ *  \param [in] length      (unused)
+ *  \param [in] space       Space in pixels between dots.
+ */
+
+static void o_path_draw_dotted (GdkDrawable *w, GdkGC *gc, GdkColor *color,
+                                GSCHEM_TOPLEVEL *w_current, PATH *path,
+                                GdkCapStyle cap, gint line_width,
+                                gint length, gint space)
+{
+  o_path_draw_solid (w, gc, color, w_current, path, cap,
+                     line_width, length, space);
+}
+
+/*! \brief Draw a path with a dashed line type.
+ *  \par Function Description
+ *  This function draws a path with a dashed line type. The parameter
+ *  <B>space</B> represents the distance between two of the dash. The
+ *  parameter <B>length</B> represents the length of a dash.
+ *
+ *  \param [in] w           GdkDrawable to draw in.
+ *  \param [in] gc          GdkGC graphics context to draw on.
+ *  \param [in] color       Box line color.
+ *  \param [in] cap         Box line end cap type (unused).
+ *  \param [in] path        The PATH object to draw
+ *  \param [in] line_width  Width of line to draw path.
+ *  \param [in] length      Length of dash in pixels.
+ *  \param [in] space       Space between dashes in pixels.
+ */
+static void o_path_draw_dashed (GdkDrawable *w, GdkGC *gc, GdkColor *color,
+                                GSCHEM_TOPLEVEL *w_current, PATH *path,
+                                GdkCapStyle cap, gint line_width,
+                                gint length, gint space)
+{
+  o_path_draw_solid (w, gc, color, w_current, path, cap,
+                     line_width, length, space);
+}
+
+/*! \brief Draw a path with a centered line type.
+ *  \par Function Description
+ *  This function draws a path with a centered line type. The parameter
+ *  <B>space</B> represents the distance between a dot and the dash. The
+ *  parameter <B>length</B> represents the length of a dash.
+ *
+ *  \param [in] w           GdkDrawable to draw in.
+ *  \param [in] gc          GdkGC graphics context to draw on.
+ *  \param [in] color       Box line color.
+ *  \param [in] cap         Box line end cap type (unused).
+ *  \param [in] path        The PATH object to draw
+ *  \param [in] line_width  Width of line to draw path.
+ *  \param [in] length      (unused)?
+ *  \param [in] space       (unused)?
+ */
+static void o_path_draw_center (GdkDrawable *w, GdkGC *gc, GdkColor *color,
+                                GSCHEM_TOPLEVEL *w_current, PATH *path,
+                                GdkCapStyle cap, gint line_width,
+                                gint length, gint space)
+{
+  o_path_draw_solid (w, gc, color, w_current, path, cap,
+                     line_width, length, space);
+}
+
+/*! \brief Draw a path with a phantom line type.
+ *  \par Function Description
+ *  This function draws a path with a phantom line type. The parameter
+ *  <B>space</B> represents the distance between a dot and a dash.
+ *  The parameter <B>length</B> represents the length of a dash.
+ *
+ *  \param [in] w           GdkDrawable to draw in.
+ *  \param [in] gc          GdkGC graphics context to draw on.
+ *  \param [in] color       Box line color.
+ *  \param [in] cap         Box line end cap type (unused).
+ *  \param [in] path        The PATH object to draw
+ *  \param [in] line_width  Width of line to draw path.
+ *  \param [in] length      (unused)?
+ *  \param [in] space       (unused)?
+ */
+static void o_path_draw_phantom (GdkDrawable *w, GdkGC *gc, GdkColor *color,
+                                 GSCHEM_TOPLEVEL *w_current, PATH *path,
+                                 GdkCapStyle cap, gint line_width,
+                                 gint length, gint space)
+{
+  o_path_draw_solid (w, gc, color, w_current, path, cap,
+                     line_width, length, space);
+}
+
+/*! \brief Placeholder filling function.
+ *  \par Function Description
+ *  This function does nothing. It has the same prototype as all the
+ *  filling functions. It prevent from making a difference between filling
+ *  in function #o_path_draw().
+ *
+ *  \param [in] w           GdkDrawable to draw in.
+ *  \param [in] gc          GdkGC graphics context to draw on.
+ *  \param [in] color       Box fill color.
+ *  \param [in] path        The PATH object to draw
+ *  \param [in] fill_width  PATH pattern fill width.
+ *  \param [in] angle1      1st angle for pattern.
+ *  \param [in] pitch1      1st pitch for pattern.
+ *  \param [in] angle2      2nd angle for pattern.
+ *  \param [in] pitch2      2nd pitch for pattern.
+ */
+static void o_path_fill_hollow (GdkDrawable *w, GdkGC *gc, GdkColor *color,
+                                GSCHEM_TOPLEVEL *w_current, PATH *path,
+                                gint fill_width,
+                                gint angle1, gint pitch1,
+                                gint angle2, gint pitch2)
+{
+  /* NOP */
+}
+
+/*! \brief Fill inside of path with a solid pattern.
+ *  \par Function Description
+ *  This function fills the inside of the path with a solid pattern.
+ *  Parameters <B>angle1</B>, <B>pitch1</B> and <B>angle2</B>,
+ *  <B>pitch2</B> and <B>fill_width</B> are unused here but kept for compatibility
+ *  with other path filling functions.
+ *
+ *  \param [in] w           GdkDrawable to draw in.
+ *  \param [in] gc          GdkGC graphics context to draw on.
+ *  \param [in] color       Box fill color.
+ *  \param [in] path        The PATH object to draw
+ *  \param [in] fill_width  PATH pattern fill width.
+ *  \param [in] angle1      (unused)
+ *  \param [in] pitch1      (unused)
+ *  \param [in] angle2      (unused)
+ *  \param [in] pitch2      (unused)
+ */
+static void o_path_fill_fill (GdkDrawable *w, GdkGC *gc, GdkColor *color,
+                              GSCHEM_TOPLEVEL *w_current, PATH *path,
+                              gint fill_width,
+                              gint angle1, gint pitch1,
+                              gint angle2, gint pitch2)
+{
+  GdkPoint *points;
+  int num_points;
+
+  gdk_gc_set_foreground(gc, color);
+  gdk_gc_set_line_attributes(gc, 1, GDK_LINE_SOLID,
+                             GDK_CAP_BUTT, GDK_JOIN_MITER);
+  path_to_points (w_current, path, 0, 0, &points, &num_points);
+
+  if (num_points == 0) {
+    g_free (points);
+    return;
+  }
+
+  gdk_draw_polygon(w_current->backingstore, w_current->gc,
+                   TRUE, points, num_points);
+
+  g_free (points);
+}
+
+/*! \brief Fill inside of path with single line pattern.
+ *  \par Function Description
+ *  This function fills the inside of the path with a pattern made of lines.
+ *  The lines are drawn inside the path with an angle <B>angle1</B> from the
+ *  horizontal. The distance between two of these lines is given by
+ *  <B>pitch1</B> and their width by <B>fill_width</B>.
+ *  Parameters <B>angle2</B> and <B>pitch2</B> are unused here but kept for
+ *  compatbility with other path filling functions.
+ *
+ *  \param [in] w           GdkDrawable to draw in.
+ *  \param [in] gc          GdkGC graphics context to draw on.
+ *  \param [in] color       Box fill color.
+ *  \param [in] path        The PATH object to draw
+ *  \param [in] fill_width  PATH pattern fill width.
+ *  \param [in] angle1      1st angle for pattern.
+ *  \param [in] pitch1      1st pitch for pattern.
+ *  \param [in] angle2      (unused)
+ *  \param [in] pitch2      (unused)
+ */
+static void o_path_fill_hatch (GdkDrawable *w, GdkGC *gc, GdkColor *color,
+                               GSCHEM_TOPLEVEL *w_current, PATH *path,
+                               gint fill_width,
+                               gint angle1, gint pitch1,
+                               gint angle2, gint pitch2)
+{
+  /* Not implemented */
+}
+
+
+/*! \brief Fill inside of path with mesh pattern.
+ *  \par Function Description
+ *  This function fills the inside of the path with a pattern made of two
+ *  sets of parallel lines in two directions. The first set is drawn inside
+ *  the path with an angle <B>angle1</B> from the horizontal. The distance
+ *  between two of these lines is given by <B>pitch1</B>.
+ *  The second set is drawn inside the path with an angle <B>angle2</B> from
+ *  the horizontal. The distance between two of these lines is given
+ *  by <B>pitch2</B>.
+ *
+ *  \param [in] w           GdkDrawable to draw in.
+ *  \param [in] gc          GdkGC graphics context to draw on.
+ *  \param [in] color       Box fill color.
+ *  \param [in] path        The PATH object to draw
+ *  \param [in] fill_width  PATH pattern fill width.
+ *  \param [in] angle1      1st angle for pattern.
+ *  \param [in] pitch1      1st pitch for pattern.
+ *  \param [in] angle2      2nd angle for pattern.
+ *  \param [in] pitch2      2nd pitch for pattern.
+ */
+static void o_path_fill_mesh (GdkDrawable *w, GdkGC *gc, GdkColor *color,
+                              GSCHEM_TOPLEVEL *w_current, PATH *path,
+                              gint fill_width,
+                              gint angle1, gint pitch1,
+                              gint angle2, gint pitch2)
+{
+  o_path_fill_hatch (w, gc, color, w_current, path,
+                     fill_width, angle1, pitch1, -1, -1);
+  o_path_fill_hatch (w, gc, color, w_current, path,
+                     fill_width, angle2, pitch2, -1, -1);
+}
 
 /*! \brief Draw a path on screen.
  *  \par Function Description
@@ -127,9 +412,6 @@ static void find_points_bounds (GdkPoint *points, int num_points,
  *  in the object which is referred by <B>o_current</B>. The path is displayed
  *  according to the current state, described in the GSCHEM_TOPLEVEL object pointed
  *  by <B>w_current</B>.
- *
- *  It first checks if the object is valid or not. If not it returns and do
- *  not output anything. That should never happen though.
  *
  *  \param [in] w_current  The GSCHEM_TOPLEVEL object.
  *  \param [in] o_current  The path OBJECT to draw.
@@ -139,21 +421,14 @@ void o_path_draw(GSCHEM_TOPLEVEL *w_current, OBJECT *o_current)
   TOPLEVEL *toplevel = w_current->toplevel;
   PATH *path = o_current->path;
   int line_width, length, space;
-  int wleft, wtop, wright, wbottom;
-  GdkPoint *points;
-  int num_points;
+  int fill_width, angle1, pitch1, angle2, pitch2;
+  DRAW_FUNC draw_func = NULL;
+  FILL_FUNC fill_func;
 
   GdkColor *color;
   GdkCapStyle path_end;
 
   if (path == NULL) {
-    return;
-  }
-
-  world_get_single_object_bounds(toplevel, o_current,
-                                 &wleft, &wtop, &wright, &wbottom);
-  if ( (toplevel->DONT_REDRAW == 1) ||
-       (!visible(toplevel, wleft, wtop, wright, wbottom)) ) {
     return;
   }
 
@@ -181,37 +456,117 @@ void o_path_draw(GSCHEM_TOPLEVEL *w_current, OBJECT *o_current)
   length = SCREENabs( toplevel, o_current->line_length );
   space = SCREENabs( toplevel, o_current->line_space );
 
-  path_to_points (w_current, path, 0, 0, &points, &num_points);
+  switch(o_current->line_type) {
+    case TYPE_SOLID:
+      length = -1;
+      space = -1;
+      draw_func = o_path_draw_solid;
+      break;
 
-  if (num_points == 0) {
-    g_free (points);
-    return;
+    case TYPE_DOTTED:
+      length = -1; /* ..._draw_dotted only space is used */
+      draw_func = o_path_draw_dotted;
+      break;
+
+    case TYPE_DASHED:
+      draw_func = o_path_draw_dashed;
+      break;
+
+    case TYPE_CENTER:
+      draw_func = o_path_draw_center;
+      break;
+
+    case TYPE_PHANTOM:
+      draw_func = o_path_draw_phantom;
+      break;
+
+    case TYPE_ERASE:
+      break;
+
+    default:
+      length = -1;
+      space = -1;
+      line_width = 0; /* just to be careful */
+      draw_func = o_path_draw_solid;
+      fprintf(stderr, _("Unknown type for path !\n"));
+      break;
   }
 
-  /* TODO: Currently we only support solid line drawing */
-  gdk_gc_set_foreground(w_current->gc, color);
-  gdk_gc_set_line_attributes(w_current->gc, line_width, GDK_LINE_SOLID,
-                             path_end, GDK_JOIN_MITER);
+  if((length == 0) || (space == 0))
+  draw_func = o_path_draw_solid;
 
-  /* Stroke */
-  if (path->sections[path->num_sections - 1].code == PATH_END)
-    gdk_draw_polygon (w_current->backingstore, w_current->gc,
-                      FALSE, points, num_points);
-  else
-    gdk_draw_lines (w_current->backingstore, w_current->gc,
-                    points, num_points);
+  (*draw_func) (w_current->backingstore, w_current->gc, color, w_current,
+                o_current->path, path_end, line_width, length, space);
 
-  /* TODO: Currently we only support solid fill drawing */
-  /* Fill */
-  if (o_current->fill_type != FILLING_HOLLOW)
-    gdk_draw_polygon(w_current->backingstore, w_current->gc,
-                     TRUE, points, num_points);
 
-  /* reset line width and reset back to default */
-  gdk_gc_set_line_attributes(w_current->gc, 0, GDK_LINE_SOLID,
-                             GDK_CAP_NOT_LAST, GDK_JOIN_MITER);
+  /*
+   * The values needed for the fill operation are taken from the
+   * <B>o_current</B> pointed OBJECT. It include the type of fill required,
+   * the width of the lines (if the fill use line) and angles and pitchs
+   * for hatch based filling.
+   *
+   * Once again the width of the line is important as if it is equal to
+   * 0 it may not be displayed. That is definetely not what we are looking for.
+   *
+   * Depending on the type of fill that has to be used inside the path the
+   * appropriate function is called. Values of <B>angle1</B>,
+   * <B>angle2</B>, <B>pitch1</B> and <B>pitch2</B> are adapted to the type of
+   * filling. The possible functions are the following :
+   * #o_path_fill_hollow(), #o_path_fill_fill(), #o_path_fill_mesh() and
+   * #o_path_fill_hatch().
+   */
+  fill_width = SCREENabs( toplevel, o_current->fill_width );
+  if(fill_width <= 0) {
+    fill_width = 1;
+  }
 
-  g_free (points);
+  angle1 = o_current->fill_angle1;
+  pitch1 = o_current->fill_pitch1;
+  angle2 = o_current->fill_angle2;
+  pitch2 = o_current->fill_pitch2;
+
+  switch(o_current->fill_type) {
+    case FILLING_HOLLOW:
+      angle1 = -1; angle2 = -1;
+      pitch1 = 1; pitch2 = 1;
+      /* this function is empty ! however if it do not use it we have to add
+       * a test before the call. Simply putting a return here instead is not
+       * possible as it would prevent any hollow path from having its grips
+       * drawn
+       */
+      fill_func = o_path_fill_hollow;
+      break;
+
+    case FILLING_FILL:
+      angle1 = -1; angle2 = -1;
+      pitch1 = 1; pitch2 = 1;
+      fill_func = o_path_fill_fill;
+      break;
+
+    case FILLING_MESH:
+      fill_func = o_path_fill_mesh;
+      break;
+
+    case FILLING_HATCH:
+      angle2 = -1;
+      pitch2 = 1;
+      fill_func = o_path_fill_hatch;
+      break;
+
+    case FILLING_VOID:
+    default:
+      angle1 = -1; angle2 = -1;
+      pitch1 = 1; pitch2 = 1;
+      fill_func = o_path_fill_hollow;
+      fprintf(stderr, _("Unknown type for path (fill)!\n"));
+  }
+
+  if((pitch1 <= 0) || (pitch2 <= 0)) {
+    fill_func = o_path_fill_fill;
+  }
+
+  (*fill_func) (w_current->backingstore, w_current->gc, color,
+                w_current, path, fill_width, angle1, pitch1, angle2, pitch2);
 
   if (o_current->draw_grips && w_current->draw_grips == TRUE) {
     if (!o_current->selected) {
@@ -223,7 +578,6 @@ void o_path_draw(GSCHEM_TOPLEVEL *w_current, OBJECT *o_current)
       o_path_draw_grips(w_current, o_current);
     }
   }
-
 }
 
 
