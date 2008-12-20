lepton-eda/lepton-eda

commit 51c2a5c9f7f9ad1c99b9da2774a051ae97f3e62e

gnetlist: Improve bom2 backend performance.
Change part of bom2 backend to use tail recursion, avoiding stack
overflows for very large schematics. Tested with up to 1024 devices,
but gnetlist core performance made testing with larger schematics
problematic.

gnetlist/scheme/gnet-bom2.scm
@@ -101,16 +101,22 @@
         (cons (cons (merge (list uref) (caar bomlist) string<? ) (cdar bomlist))(cdr bomlist))
         (cons (car bomlist)(bom2:match? uref attriblist (cdr bomlist)))))))
 
-(define bom2:components
-  (lambda (ls attriblist)
-    (if (null? ls)
-      '()
-      (let ((package (car ls))
-            (bomlist (bom2:components (cdr ls) attriblist))
-            (attribs (bom2:find-attribs (car ls) attriblist)))
-        (if (not (string=? "unknown" (gnetlist:get-package-attribute package "nobom")))
-          bomlist
-          (bom2:match? package attribs bomlist))))))
+(define (bom2:in-bom? package)
+  (string=? "unknown"
+            (gnetlist:get-package-attribute package "nobom")))
+
+(define (bom2:components-impl ls attriblist bomlist)
+  (if (null? ls)
+      bomlist
+      (let* ((package (car ls))
+             (attribs (bom2:find-attribs package attriblist)))
+        (bom2:components-impl (cdr ls) attriblist
+                              (if (bom2:in-bom? package)
+                                  (bom2:match? package attribs bomlist)
+                                  bomlist)))))
+
+(define (bom2:components ls attriblist)
+   (bom2:components-impl ls attriblist '()))
 
 (define bom2:find-attribs
   (lambda (package attriblist)

commit 581f8cec3d5e717fa78417772759b4525a52f458

gnetlist: Restore bom2 ordering
The changes in 51c2a5c9f7f9ad1c99b9da2774a051ae97f3e62e caused the
ordering of components in the BOM output to be reversed. This changes
the ordering back to the way it was before.

gnetlist/scheme/gnet-bom2.scm
@@ -107,7 +107,7 @@
 
 (define (bom2:components-impl ls attriblist bomlist)
   (if (null? ls)
-      bomlist
+      (reverse bomlist)
       (let* ((package (car ls))
              (attribs (bom2:find-attribs package attriblist)))
         (bom2:components-impl (cdr ls) attriblist