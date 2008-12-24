lepton-eda/lepton-eda

commit b635ebb641383360e5e7864830d42d13cf7f4d12

Print useful messages when f_open() fails.
This reinstates error messages removed by the patch which modified
f_open() to use GError for error reporting.

utils/gschlas/gschlas.c
@@ -106,6 +106,8 @@ main_prog(void *closure, int argc, char *argv[])
   while (argv[i] != NULL) {
 
     gchar *filename;
+    GError *err = NULL;
+
     if (g_path_is_absolute(argv[i]))
     {
       /* Path is already absolute so no need to do any concat of cwd */
@@ -114,25 +116,18 @@ main_prog(void *closure, int argc, char *argv[])
       filename = g_build_path (G_DIR_SEPARATOR_S, cwd, argv[i], NULL);
     }
 
-    if (stat(filename, &buf) != 0) {
-      s_log_message("Could not open [%s]\n", filename);
-      s_log_message("Exiting...\n");
-      exit(2); /* error */
+    if (!f_open (pr_current,
+                 pr_current->page_current->page_filename,
+                 &err)) {
+      /* Not being able to load a file is apparently a fatal error */
+      logging_dest = STDOUT_TTY;
+      g_warning ("%s\n", err->message);
+      g_error_free (err);
+      exit(2);
     } else {
-      if (verbose_mode) {
-        s_log_message("Loading file [%s]\n", filename);
-      }
-
-      s_page_goto (pr_current,
-                   s_page_new (pr_current, filename));
-      
-      if (!f_open(pr_current, filename, NULL)) {
-        s_log_message("gsymcheck: Could not load [%s]\n", filename);
-        s_log_message("Exiting...\n");
-        exit(2); // error
-      }
+      g_message ("Loaded file [%s]\n", filename);
     }
-    
+
     i++;
     g_free (filename);
   }

gattrib/src/s_toplevel.c
@@ -57,12 +57,20 @@
 int s_toplevel_read_page(char *filename)
 {
   int file_return_code;
+  GError *err = NULL;
     
   /* Set the new filename */
   pr_current->page_current->page_filename = g_strdup(filename);
   
   /* read in and fill out pr_current using f_open and its callees */
-  file_return_code = f_open(pr_current, filename, NULL);
+  file_return_code = f_open(pr_current, filename, &err);
+
+  /* If an error occurred, print message */
+  if (err != NULL) {
+    g_warning (err->message);
+    g_error_free (err);
+  }
+
   return file_return_code;
 }
 

gnetlist/src/gnetlist.c
@@ -226,6 +226,7 @@ void main_prog(void *closure, int argc, char *argv[])
     i = argv_index;
     while (argv[i] != NULL) {
       gchar *filename; 
+      GError *err = NULL;
 
       if (g_path_is_absolute(argv[i]))
       {
@@ -243,7 +244,9 @@ void main_prog(void *closure, int argc, char *argv[])
       s_page_goto (pr_current, s_page_new (pr_current, filename));
       
       if (!f_open (pr_current, filename, NULL)) {
-        fprintf (stderr, "Couldn't load schematic [%s]\n", filename);
+        g_warning ("%s\n", err->message);
+        fprintf (stderr, "%s\n", err->message);
+        g_error_free (err);
       }
       
       i++;

gsymcheck/src/gsymcheck.c
@@ -50,9 +50,8 @@ main_prog(void *closure, int argc, char *argv[])
   int i;
   int argv_index;
   int exit_status;
-  int fopen_status;
   char *cwd;
-  struct stat buf;
+
   char *logfile;
   
   TOPLEVEL *pr_current;
@@ -106,6 +105,8 @@ main_prog(void *closure, int argc, char *argv[])
   while (argv[i] != NULL) {
 
     gchar *filename;
+    GError *err = NULL;
+
     if (g_path_is_absolute(argv[i]))
     {
       /* Path is already absolute so no need to do any concat of cwd */
@@ -114,29 +115,19 @@ main_prog(void *closure, int argc, char *argv[])
       filename = g_build_path (G_DIR_SEPARATOR_S, cwd, argv[i], NULL);
     }
 
-    if (stat(filename, &buf) != 0) {
+    s_page_goto (pr_current,
+                 s_page_new (pr_current, filename));
+
+    if (!f_open (pr_current,
+                 pr_current->page_current->page_filename,
+                 &err)) {
+      /* Not being able to load a file is apparently a fatal error */
       logging_dest = STDOUT_TTY;
-      s_log_message("Could not open [%s]\n", filename);
-      s_log_message("Exiting...\n");
-      exit(2); /* error */
+      g_warning ("%s\n", err->message);
+      g_error_free (err);
+      exit(2);
     } else {
-      if (verbose_mode) {
-        s_log_message("Loading file [%s]\n", filename);
-      }
-
-      s_page_goto (pr_current,
-                   s_page_new (pr_current, filename));
-      
-      fopen_status = f_open (pr_current, 
-                             pr_current->page_current->page_filename,
-                             NULL);
-
-      if (!fopen_status) {
-        s_log_message("gsymcheck: Could not load [%s]\n", 
-                      pr_current->page_current->page_filename);
-        s_log_message("Exiting...\n");
-        exit(2); /* error */
-      }
+      g_message ("Loaded file [%s]\n", filename);
     }
     i++;
     g_free (filename);

commit 46fdb00b19d300dfcc5f9009f9c7dbbf0940834f

gschlas: Fix segfault opening file
Segfault introduced in commit b635ebb641383360e5e7864830d42d13cf7f4d12.

utils/gschlas/gschlas.c
@@ -113,9 +113,9 @@ main_prog(void *closure, int argc, char *argv[])
       filename = g_build_filename (cwd, argv[i], NULL);
     }
 
-    if (!f_open (pr_current,
-                 pr_current->page_current->page_filename,
-                 &err)) {
+    s_page_goto (pr_current, s_page_new (pr_current, filename));
+
+    if (!f_open (pr_current, pr_current->page_current->page_filename, &err)) {
       /* Not being able to load a file is apparently a fatal error */
       logging_dest = STDOUT_TTY;
       g_warning ("%s\n", err->message);