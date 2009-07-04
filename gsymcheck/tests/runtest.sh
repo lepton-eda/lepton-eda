6d4ba095d2894e9cc21e272d28af07b0d74a7b99
Dan McMahill <dan@mcmahill.net>
2009-07-04 12:14:54 -0400

    Fix several minor bugs in the testsuite.
    - Make the list of arguments passed into the runtest.sh script match
    what the script actually expects.
    
    - Make the use of temp files more secure.
    
    - Be more consistent about the usage of the build directory versus
      the source directory.
    
    - When a single test fails, just increment a failed count and continue on.
      Don't abort until all the tests have run.

--- gsymcheck/tests/runtest.sh
+++ gsymcheck/tests/runtest.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 INPUT=$1
-BACKEND=$2
-BUILDDIR=$3
-SRCDIR=$4
-EXTRADIFF=$5
+BUILDDIR=$2
+SRCDIR=$3
+
 here=`pwd`
 rundir=${here}/run
 
 # create temporary run directory and required subdirs
-if [ ! -d $rundir ]
-then
-   mkdir -p $rundir
+mkdir -m 0700 -p ${rundir}
+rc=$?
+if test $rc -ne 0 ; then
+	echo "Failed to create directory ${rundir} with appropriate permissions"
+	echo "mkdir returned $rc"
+	exit 1
 fi
 
 TESTDIR=${BUILDDIR}
@@ -19,26 +21,28 @@ export TESTDIR
 
 symbasename=`basename $INPUT .sym`
 
-tmpfile=$rundir/tmp$$
-../src/gsymcheck -vv $INPUT 1> $tmpfile 2> $rundir/allerrors.output 
+in="${SRCDIR}/${INPUT}"
+ref="${SRCDIR}/${symbasename}.output"
+new="${rundir}/new_${symbasename}.output"
+tmpfile=${rundir}/tmp$$
 
-cat $tmpfile | \
+${BUILDDIR}/../src/gsymcheck -vv ${in} 1> ${tmpfile} 2> ${rundir}/allerrors.output
+
+cat ${tmpfile} | \
 	grep -v "gEDA/gsymcheck version" | \
 	grep -v "ABSOLUTELY NO WARRANTY" | \
 	grep -v "This is free software" | \
 	grep -v "the COPYING file" | \
 	grep -v "Checking: " | \
-	grep -v '^$' > $rundir/${BUILDDIR}/new_${symbasename}.output 
-rm -f $tmpfile
+	grep -v '^$' > ${new}
+rm -f ${tmpfile}
 
-diff $EXTRADIFF ${BUILDDIR}/${symbasename}.output \
-	 $rundir/${BUILDDIR}/new_${symbasename}.output
+diff "${ref}" "${new}"
 status=$?
 
 rm -rf $rundir
 
-if [ "$status" != 0 ]
-then
+if test $status -ne 0; then
 	exit 2
 fi
 
