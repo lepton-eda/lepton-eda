lepton-eda/lepton-eda

commit 8af5fba2ec0248331ceb5d2746771e70b2813004

libgeda: Cope better with not finding the font.
Calling exit(-1) if the font doesn't appear to have been found is
stupid and not the way that a library is supposed to do things.
Instead, use a built-in question mark character '?' for any characters
that can't be loaded (which, if the font is missing, is all of them).

libgeda/src/o_text_basic.c
@@ -103,6 +103,64 @@ GHashTable *font_char_to_file = NULL;
 /*! Size of a tab in characters */
 int tab_in_chars = 8;
 
+/*! Data for built-in question mark character.
+ *
+ * See builtin_question_mark().
+ */
+static int question_mark_data[][4] = {
+  {0, 22, 4, 26},
+  {4, 26, 10, 26},
+  {10, 26, 14, 22},
+  {14, 22, 14, 18},
+  {14, 18, 12, 14},
+  {12, 14, 6, 8},
+  {6, 8, 6, 6},
+  {6, -1, 7, -1},
+  {7, -1, 8, 0},
+  {8, 0, 8, 1},
+  {8, 1, 7, 2},
+  {7, 2, 6, 2},
+  {6, 2, 5, 1},
+  {5, 1, 5, 0},
+  {5, 0, 6, -1},
+  {-128, -128, -128, -128} /* Daft choice of sentinel */
+  };
+
+/*! \brief Get the built-in question mark font character.
+ * \par Function Description
+ * If the font can't be found (e.g. because it isn't installed
+ * correctly), this function generates a question mark character. This
+ * is based on the "quest.sym" font character.
+ */
+static OBJECT *
+builtin_question_mark (TOPLEVEL *toplevel) {
+  OBJECT *q;
+  int i;
+
+  q = g_new0 (OBJECT, 1);
+  q->font_prim_objs = NULL;
+  q->font_text_size = 20;
+  q->name = g_strdup_printf ("?");
+  q->font_prim_objs = NULL;
+
+  for (i = 0; ; i++) {
+    if (question_mark_data[i][0] == -128) break;
+
+    OBJECT *l = o_line_new (toplevel, OBJ_LINE,
+                            DEFAULT_COLOR,
+                            question_mark_data[i][0],
+                            question_mark_data[i][1],
+                            question_mark_data[i][2],
+                            question_mark_data[i][3]);
+    o_set_line_options (toplevel, l, 0, 0, 0, -1, -1);
+    o_set_fill_options (toplevel, l,
+                        FILLING_HOLLOW,
+                        -1, -1, -1, -1, -1);
+    q->font_prim_objs = g_list_prepend (q->font_prim_objs, l);
+  }
+
+  return q;
+}
 
 /*! \brief update the visible part of a string
  *  \par Function Description
@@ -266,9 +324,10 @@ GList *o_text_load_font (TOPLEVEL *toplevel, gunichar needed_char)
 {
   gchar *temp_string = NULL;
   OBJECT *o_font_set;
-  int not_found = FALSE;
+  int found = TRUE;
   gchar *aux_str2;
   GError *err = NULL;
+  GList *prim_objs = NULL;
 
   /* retrieve the name of the file where the char is defined */
   aux_str2 = g_hash_table_lookup (font_char_to_file,
@@ -303,52 +362,35 @@ GList *o_text_load_font (TOPLEVEL *toplevel, gunichar needed_char)
     s_log_message(_("Could not find character '%s' definition.\n"), outbuf);
     
     g_free (temp_string);
-    temp_string = g_build_filename (toplevel->font_directory, "quest.sym", NULL);
-    if ( access(temp_string, R_OK) != 0 ) {
-      fprintf(stderr, _("Could not load question font char -- check font-directory keyword\n"));
-      exit(-1);
-    }
-    not_found = TRUE; 
+    temp_string = NULL;
+    found = FALSE;
   }
 
-  /* Make new object for the font set list */
-  o_font_set = (OBJECT*)g_new (OBJECT, 1);	
-  
-  o_font_set->font_prim_objs = NULL;
-  o_font_set->font_text_size = 100;
+  if (found) {
+    prim_objs = o_read(toplevel, prim_objs, temp_string, &err);
+    if (err != NULL) {
+      g_warning ("o_text_basic.c: Failed to read font file: %s\n",
+                 err->message);
+      g_error_free (err);
+      found = FALSE;
+    }
+  }
+
+  if (!found) {
+    /* Get a question mark. */
+    o_font_set = builtin_question_mark (toplevel);
+  } else {
+    /* Make new object for the font set list */
+    o_font_set = (OBJECT*)g_new (OBJECT, 1);
+    o_font_set->font_prim_objs = prim_objs;
+    o_font_set->font_text_size = 100;
+  }
 
   o_font_set->name = g_strdup_printf ("%c", needed_char);
-  o_font_set->font_prim_objs = NULL;
-  
+
   /* Add it to the list and hash table. Some functions will need it */
   g_hash_table_insert (font_loaded,
                        GUINT_TO_POINTER (needed_char), o_font_set);
-
-  if (not_found == TRUE) {
-    /* set the font text size (width) to the question mark's size */
-    /* yes, the question mark character was loaded instead of the real char */
-    /* 63 == question mark character */
-
-    OBJECT *aux_obj;
-    
-    aux_obj = g_hash_table_lookup (font_loaded,
-                                   GUINT_TO_POINTER ((gunichar)'?'));
-    if (aux_obj == NULL) {
-      o_text_load_font(toplevel, (gunichar) '?');
-      aux_obj = g_hash_table_lookup (font_loaded,
-                                     GUINT_TO_POINTER ((gunichar)'?'));
-    }
-
-    o_font_set->font_text_size = aux_obj->font_text_size;
-  }
-
-  o_font_set->font_prim_objs = o_read(toplevel, o_font_set->font_prim_objs,
-				      temp_string, &err);
-  if (err != NULL) {
-    g_warning ("o_text_basic.c: Failed to read font file: %s\n",
-               err->message);
-    g_error_free (err);
-  }
 
   g_free(temp_string);
 

commit c8628e4df819998e40d7954c5a5d41a2fbfbbb7d

libgeda: Fix font loading.
Fixes a bug introduced in 8af5fba2ec0248331ceb5d2746771e70b2813004
which caused libgeda to not detect character widths correctly.

This was subtle.  A new font OBJECT must be created and added to the
global character hashtable *before* o_read() is called to populate the
OBJECT with its constituent lines.  This is because when o_read()
encounters the 'F' font parameter line, it calls
o_text_set_info_font() to process it.  o_text_set_info_font() in turn
needs to be able to get at the font object for that character, which
it looks for using the global hashtable.

I have added some comments to the source code explaining this.

libgeda/src/o_text_basic.c
@@ -366,8 +366,20 @@ GList *o_text_load_font (TOPLEVEL *toplevel, gunichar needed_char)
     found = FALSE;
   }
 
+  /* We need to make sure to create and add a font OBJECT to the
+   * font_loaded hashtable *before* attempting to call o_read().  This
+   * is because o_text_set_info_font() expects to find it. */
+  o_font_set = (OBJECT*)g_new (OBJECT, 1);
+  o_font_set->font_prim_objs = NULL;
+  o_font_set->font_text_size = 100;
+
+  /* Add it to the list and hash table. Some functions will need it */
+  g_hash_table_insert (font_loaded,
+                       GUINT_TO_POINTER (needed_char), o_font_set);
+
   if (found) {
-    prim_objs = o_read(toplevel, prim_objs, temp_string, &err);
+    o_font_set->font_prim_objs = o_read(toplevel, o_font_set->font_prim_objs,
+                                        temp_string, &err);
     if (err != NULL) {
       g_warning ("o_text_basic.c: Failed to read font file: %s\n",
                  err->message);
@@ -377,20 +389,17 @@ GList *o_text_load_font (TOPLEVEL *toplevel, gunichar needed_char)
   }
 
   if (!found) {
-    /* Get a question mark. */
-    o_font_set = builtin_question_mark (toplevel);
-  } else {
-    /* Make new object for the font set list */
-    o_font_set = (OBJECT*)g_new (OBJECT, 1);
-    o_font_set->font_prim_objs = prim_objs;
-    o_font_set->font_text_size = 100;
+    /* Get a question mark instead. */
+    OBJECT *q = builtin_question_mark (toplevel);
+    /* Replace the current o_font_set object in the hashtable.
+     * Because its font_prim_objs GList will be empty (== NULL), we
+     * can just rely on the hashtables free func to clean it up for
+     * us.  This is horrible, but effective. */
+    g_hash_table_insert (font_loaded, GUINT_TO_POINTER (needed_char), q);
+    o_font_set = q;
   }
 
   o_font_set->name = g_strdup_printf ("%c", needed_char);
-
-  /* Add it to the list and hash table. Some functions will need it */
-  g_hash_table_insert (font_loaded,
-                       GUINT_TO_POINTER (needed_char), o_font_set);
 
   g_free(temp_string);
 