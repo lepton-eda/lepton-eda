d03c03949e17313a222645fc38c376b802f5fb4c
Peter Clifton <pcjc2@cam.ac.uk>
2010-02-10 22:18:45 +0000

    gschem: Remove the exit(-1) error path from o_slot_end()
    This is not a friendly way to treat the user.. g_return_if_fail() instead.

--- gschem/src/o_slot.c
+++ gschem/src/o_slot.c
@@ -75,13 +75,7 @@ void o_slot_end(GSCHEM_TOPLEVEL *w_current, const char *string)
   int new_slot_number;
   int status;
 
-  status = o_attrib_string_get_name_value (string, NULL, &value);
-  if (!status) {
-    s_log_message(_("Slot attribute malformed\n"));
-    return;
-  }
-
-  object = o_select_return_first_object(w_current);
+  object = o_select_return_first_object (w_current);
 
   /* get the parent object if the selection is only a text object */
   if (object != NULL && object->type == OBJ_TEXT) {
@@ -90,79 +84,79 @@ void o_slot_end(GSCHEM_TOPLEVEL *w_current, const char *string)
     }
   }
 
-  if (object != NULL) {
-    numslots_value =
-      o_attrib_search_object_attribs_by_name (object, "numslots", 0);
+  g_return_if_fail (object != NULL);
 
-    if (!numslots_value) {
-      s_log_message(_("numslots attribute missing\n"));
-      s_log_message(_("Slotting not allowed for this component\n"));
-      g_free(value);
-      return;
-    }
+  status = o_attrib_string_get_name_value (string, NULL, &value);
+  if (!status) {
+    s_log_message (_("Slot attribute malformed\n"));
+    return;
+  }
 
-    numslots = atoi(numslots_value);
-    g_free(numslots_value);
+  numslots_value =
+    o_attrib_search_object_attribs_by_name (object, "numslots", 0);
 
-    new_slot_number = atoi(value);
+  if (!numslots_value) {
+    s_log_message (_("numslots attribute missing\n"));
+    s_log_message (_("Slotting not allowed for this component\n"));
+    g_free (value);
+    return;
+  }
+
+  numslots = atoi (numslots_value);
+  g_free (numslots_value);
+
+  new_slot_number = atoi (value);
 
 #if DEBUG
-    printf("numslots = %d\n", numslots);
+  printf ("numslots = %d\n", numslots);
 #endif
 
-    if (new_slot_number > numslots || new_slot_number <=0 ) {
-      s_log_message(_("New slot number out of range\n"));
-      g_free(value);
-      return;
+  if (new_slot_number > numslots || new_slot_number <=0 ) {
+    s_log_message (_("New slot number out of range\n"));
+    g_free (value);
+    return;
+  }
+
+  /* first see if slot attribute already exists outside
+   * complex */
+  slot_value = s_slot_search_slot (object, &o_slot);
+  g_free (slot_value);
+
+  if (o_slot != NULL && !o_attrib_is_inherited (o_slot)) {
+    o_text_set_string (toplevel, o_slot, string);
+
+    if (o_slot->visibility == VISIBLE ||
+        (o_slot->visibility == INVISIBLE && toplevel->show_hidden_text)) {
+      o_invalidate (w_current, o_slot);
     }
 
-    /* first see if slot attribute already exists outside
-     * complex */
-    slot_value = s_slot_search_slot (object, &o_slot);
-    g_free (slot_value);
+    o_text_recreate (toplevel, o_slot);
 
-    if (o_slot != NULL && !o_attrib_is_inherited (o_slot)) {
-      o_text_set_string (toplevel, o_slot, string);
-
-      if (o_slot->visibility == VISIBLE ||
-          (o_slot->visibility == INVISIBLE && toplevel->show_hidden_text)) {
-        o_invalidate (w_current, o_slot);
-      }
-
-      o_text_recreate(toplevel, o_slot);
-
-      /* this doesn't deal with the selection list
-       * item */
-      if (o_slot->visibility == VISIBLE ||
-          (o_slot->visibility == INVISIBLE && toplevel->show_hidden_text)) {
-        o_invalidate (w_current, o_slot);
-      }
-
-    } else {
-      /* here you need to do the add the slot
-         attribute since it doesn't exist */
-      new_obj = o_text_new (toplevel, OBJ_TEXT, ATTRIBUTE_COLOR,
-                            object->complex->x, object->complex->y,
-                            LOWER_LEFT, 0, /* zero is angle */
-                            string, 10, INVISIBLE, SHOW_NAME_VALUE);
-      s_page_append (toplevel, toplevel->page_current, new_obj);
-
-      /* manually attach attribute */
-      o_attrib_attach (toplevel, new_obj, object, FALSE);
+    /* this doesn't deal with the selection list
+     * item */
+    if (o_slot->visibility == VISIBLE ||
+        (o_slot->visibility == INVISIBLE && toplevel->show_hidden_text)) {
+      o_invalidate (w_current, o_slot);
     }
-
-    o_invalidate (w_current, object);
-    s_slot_update_object (toplevel, object);
-
-    o_invalidate (w_current,object);
-
-    toplevel->page_current->CHANGED = 1;
-    g_free(value);
 
   } else {
-    fprintf(stderr,
-            _("uggg! you tried to slot edit something that doesn't exist!\n"));
-    g_free(value);
-    exit(-1);
+    /* here you need to do the add the slot
+       attribute since it doesn't exist */
+    new_obj = o_text_new (toplevel, OBJ_TEXT, ATTRIBUTE_COLOR,
+                          object->complex->x, object->complex->y,
+                          LOWER_LEFT, 0, /* zero is angle */
+                          string, 10, INVISIBLE, SHOW_NAME_VALUE);
+    s_page_append (toplevel, toplevel->page_current, new_obj);
+
+    /* manually attach attribute */
+    o_attrib_attach (toplevel, new_obj, object, FALSE);
   }
+
+  o_invalidate (w_current, object);
+  s_slot_update_object (toplevel, object);
+
+  o_invalidate (w_current,object);
+
+  toplevel->page_current->CHANGED = 1;
+  g_free (value);
 }
