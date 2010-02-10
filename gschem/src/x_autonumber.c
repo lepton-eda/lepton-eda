9c71110a49be8a677f7ea3eabaebf6479873c7af
Peter Clifton <pcjc2@cam.ac.uk>
2010-02-10 21:05:38 +0000

    gschem: Fix update of pinnumbers after auto-renumberering slots
    Call o_slot_end() rather than o_text_set_string(), in order to
    ensure the object's pinnumbers are updated correctly.
    
    NB: This patch requires the changes making o_slot_end() to take an
    OBJECT * arguement, as the objects being updated aren't necessarily
    selected.

--- gschem/src/x_autonumber.c
+++ gschem/src/x_autonumber.c
@@ -611,32 +611,12 @@ void autonumber_remove_number(AUTONUMBER_TEXT * autotext, OBJECT *o_current)
 void autonumber_apply_new_text(AUTONUMBER_TEXT * autotext, OBJECT *o_current,
 			       gint number, gint slot)
 {
-  OBJECT *o_parent, *o_slot;
-  gchar *slot_str;
-  gchar *str = NULL;
+  char *str;
 
-  /* add the slot as attribute to the object */
-  o_parent = o_current->attached_to;
-  if (slot != 0 && o_parent != NULL) {
-    slot_str = s_slot_search_slot (o_parent, &o_slot);
-    g_free (slot_str);
-    if (o_slot != NULL && !o_attrib_is_inherited (o_slot)) {
-      /* update the attached slot attribute */
-      slot_str = g_strdup_printf("slot=%d",slot);
-      o_text_set_string (autotext->w_current->toplevel, o_slot, slot_str);
-      g_free (slot_str);
-      o_invalidate (autotext->w_current, o_slot);
-      o_text_recreate(autotext->w_current->toplevel, o_slot);
-      o_invalidate (autotext->w_current, o_slot);
-    }
-    else {
-      /* create a new attribute and attach it */
-      o_attrib_add_attrib(autotext->w_current,
-			  g_strdup_printf("slot=%d",slot),
-			  INVISIBLE, SHOW_NAME_VALUE,
-			  o_parent);
-    }
-  }
+  /* update the slot on the owning object */
+  str = g_strdup_printf ("slot=%d", slot);
+  o_slot_end (autotext->w_current, o_current->attached_to, str);
+  g_free (str);
 
   /* replace old text */
   str = g_strdup_printf("%s%d", autotext->current_searchtext, number);
