19e4de650dad95977bc07bae4f897ab6b9d7b110
Ales Hvezda <ahvezda@seul.org>
2010-02-14 18:00:02 -0500

    Tweaked scripts with updated paths
    Eventually these scripts should be fixed or rewritten so that they
    do not have any hardcoded paths.

--- docs/scripts/copygitignores.sh
+++ docs/scripts/copygitignores.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-if [ ! -d "wiki" -a ! -d "wip" ]
+if [ ! -d "wiki" -a ! -d "wikiold" ]
 then
         echo "You must start this program in the toplevel docs directory"
-        echo "wherever wiki and wip live."
+        echo "wherever wiki and wikiold live."
         exit 1
 fi
 
-files=`find wiki -name .gitignore -print`
+files=`find wikiold -name .gitignore -print`
 
 for i in $files
 do
 	src=$i
-	dest=`echo $i | sed "s/^wiki/wip/"`	
+	dest=`echo $i | sed "s/^wikiold/wiki/"`	
 	echo Copying $src to $dest
 	cp -f $src $dest
 done
