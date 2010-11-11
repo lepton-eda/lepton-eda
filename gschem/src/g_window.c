7f85f28e67c59dbccb0f2a321794b0ddbb3e578f
Peter TB Brett <peter@peter-b.co.uk>
2010-11-11 19:33:27 +0000

    Add e-mail address to copyright headers.

--- gschem/src/g_window.c
+++ gschem/src/g_window.c
@@ -1,6 +1,6 @@
 /* gEDA - GPL Electronic Design Automation
  * gschem - gEDA Schematic Capture
- * Copyright (C) 2010 Peter Brett
+ * Copyright (C) 2010 Peter Brett <peter@peter-b.co.uk>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
