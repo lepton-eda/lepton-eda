lepton-eda/lepton-eda

commit d8803215ea591b9349999c17c5a85aee7da473af

Remove warnings when building with "--disable-nls".
Closes-bug: lp-698450

gschem/src/x_dialog.c
@@ -3702,6 +3702,7 @@ close_confirmation_dialog_constructor (GType type,
   GtkTreeIter iter;
   gboolean ret, single_page;
   gchar *tmp, *str;
+  const gchar *cstr;
 
   /* chain up to constructor of parent class */
   object =
@@ -3811,7 +3812,7 @@ close_confirmation_dialog_constructor (GType type,
   }
 
   /* secondary label */
-  str = _("If you don't save, all your changes will be permanently lost.");
+  cstr = _("If you don't save, all your changes will be permanently lost.");
   label = GTK_WIDGET (g_object_new (GTK_TYPE_LABEL,
                                     /* GtkMisc */
                                     "xalign",     0.0,

gschem/src/gschem.c
@@ -214,9 +214,10 @@ void main_prog(void *closure, int argc, char *argv[])
   o_undo_init(); 
 
   if (s_path_sys_data () == NULL) {
-    gchar *message = _("You must set the GEDADATA environment variable!\n\n"
-                       "gschem cannot locate its data files. You must set the GEDADATA\n"
-                       "environment variable to point to the correct location.\n");
+    const gchar *message = 
+      _("You must set the GEDADATA environment variable!\n\n"
+        "gschem cannot locate its data files. You must set the GEDADATA\n"
+        "environment variable to point to the correct location.\n");
     GtkWidget* error_diag =
       gtk_message_dialog_new (NULL, 0, GTK_MESSAGE_ERROR,
                               GTK_BUTTONS_OK,

gschem/src/x_menus.c
@@ -231,7 +231,11 @@ get_main_menu(GSCHEM_TOPLEVEL *w_current)
 static gchar* gettext_fn(const gchar *path,
 			 gpointer func_data ATTRIBUTE_UNUSED)
 {
-	return gettext(path);
+  /*! \bug Note that we have to discard the 'const' qualifier here to
+   * avoid build warnings when gettext is disabled.  This is required
+   * due to the prototype of the function pointer argument to
+   * gtk_item_factory_set_translate_func() */
+  return (gchar *) gettext(path);
 }
 
 GtkWidget *get_main_popup(GSCHEM_TOPLEVEL *w_current)

libgeda/src/libgeda.c
@@ -46,9 +46,11 @@
  */
 void libgeda_init(void)
 {
+#ifdef ENABLE_NLS
   /* Initialise gettext */
   bindtextdomain (LIBGEDA_GETTEXT_DOMAIN, LOCALEDIR);
   bind_textdomain_codeset(LIBGEDA_GETTEXT_DOMAIN, "UTF-8");
+#endif
 
   /* Initialise gobject */
   g_type_init ();

commit 355fd9bae3392c120d85f81feff04cae5a0ed42e

gschem: fix secondary label in close confirmation dialog
Secondary label used a pointer string that was previously freed.  This
bug was introduced in commit d8803215ea59.

Reviewed-by: Peter TB Brett <peter@peter-b.co.uk>
Closes-bug: lp-700189

gschem/src/x_dialog.c
@@ -3820,7 +3820,7 @@ close_confirmation_dialog_constructor (GType type,
                                     "selectable", TRUE,
                                     /* GtkLabel */
                                     "wrap",       TRUE,
-                                    "label",      str,
+                                    "label",      cstr,
                                     NULL));
   gtk_box_pack_start (GTK_BOX (vbox), label,
                       FALSE, FALSE, 0);