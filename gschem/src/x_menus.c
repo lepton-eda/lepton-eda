2dfb5547bdf486ca4375af9e73cbe318768cb59f
Krzysztof Kościuszkiewicz <k.kosciuszkiewicz@gmail.com>
2011-05-20 23:47:09 +0200

    gschem: recent file list checks file extensions
    In the recently used files both symbol and schematic files are shown.
    Change the filtering method, so recently used files are chosen not only
    by MIME type, but also by the file extension as well.
    
    Rationale: if gEDA is installed in a location other than default (say in
    users home dir), the MIME type databases will not be updated and list
    of recently used files will always be empty.

--- gschem/src/x_menus.c
+++ gschem/src/x_menus.c
@@ -401,6 +401,8 @@ void x_menu_attach_recent_files_submenu(GSCHEM_TOPLEVEL *w_current)
   recent_filter = gtk_recent_filter_new();
   gtk_recent_filter_add_mime_type(recent_filter, "application/x-geda-schematic");
   gtk_recent_filter_add_mime_type(recent_filter, "application/x-geda-symbol");
+  gtk_recent_filter_add_pattern(recent_filter, "*.sch");
+  gtk_recent_filter_add_pattern(recent_filter, "*.sym");
   gtk_recent_chooser_add_filter(GTK_RECENT_CHOOSER(menuitem_file_recent_items), recent_filter);
 
   gtk_recent_chooser_set_show_tips(GTK_RECENT_CHOOSER(menuitem_file_recent_items), TRUE);
