7c88e88e38bcd20f6ad3e46604f2b117b6fef0a6
Peter TB Brett <peter@peter-b.co.uk>
2011-06-24 14:00:10 +0100

    scheme-api: Core API documentation (attributes).

--- libgeda/scheme/geda/attrib.scm
+++ libgeda/scheme/geda/attrib.scm
@@ -30,57 +30,27 @@
 (define-public attrib-attachment %attrib-attachment)
 (define-public promotable-attribs %promotable-attribs)
 
-;; attribute? a
-;;
-;; Returns #t if a is an text object in attribute format.
 (define-public (attribute? a)
   (false-if-exception (and (parse-attrib a) #t)))
 
-;; attrib-name a
-;;
-;; Returns the attribute name of a, or raises an attribute-format
-;; error if a is not in attribute format.
 (define-public (attrib-name a)
   (let ((v (parse-attrib a)))
     (if v (car v) v)))
 
-;; attrib-value a
-;;
-;; Returns the attribute value of a, or raises an attribute-format
-;; error if a is not in attribute format.
 (define-public (attrib-value a)
   (let ((v (parse-attrib a)))
     (if v (cdr v) v)))
 
-;; set-attrib-value! a val
-;;
-;; Updates the attribute a with the new value val.
 (define-public (set-attrib-value! a val)
   (let ((name (attrib-name a)))
     (set-text-string! a (string-join (list name val) "="))))
 
-;; inherited-attribs object
-;;
-;; Returns the inherited attributes of object, if object is a
-;; component.  The inherited attributes are the unattached top-level
-;; attributes in the component.  If object is not a component, returns
-;; the empty list.
 (define-public (inherited-attribs object)
   (if (component? object)
       (filter! (lambda (x) (and (attribute? x) (not (attrib-attachment x))))
                (component-contents object))
       '()))
 
-;; promote-attribs! object
-;;
-;; Promotes any promotable attributes from an object into its current
-;; page, if object is a component, keeping the original attributes as
-;; invisible attributes inside the component.  Returns a list of the
-;; objects that were added to the page.  If object is not a component,
-;; returns the empty list.  If object is not in a page, throws an
-;; object-state error.
-;;
-;; See also promotable-attribs.
 (define-public (promote-attribs! object)
   (let ((p (or (object-page object)
                (scm-error 'object-state #f
@@ -99,10 +69,6 @@
                (promotable-attribs object))
         '())))
 
-;; attrib-inherited? attrib
-;;
-;; Returns #t if attrib is a toplevel un-attached attribute inside a
-;; component.
 (define-public (attrib-inherited? attrib)
   (not (or (attrib-attachment attrib)
            (not (object-component attrib)))))
