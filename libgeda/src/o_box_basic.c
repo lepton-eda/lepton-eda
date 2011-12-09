e85e8c5843f23432cb03d4dd8c95fb34753f7801
Peter TB Brett <peter@peter-b.co.uk>
2011-12-09 21:07:48 +0000

    libgeda: Add missing pre change notify to o_box_modify_all().

--- libgeda/src/o_box_basic.c
+++ libgeda/src/o_box_basic.c
@@ -151,6 +151,8 @@ void
 o_box_modify_all (TOPLEVEL *toplevel, OBJECT *object,
                   int x1, int y1, int x2, int y2)
 {
+  o_emit_pre_change_notify (toplevel, object);
+
   object->box->lower_x = (x1 > x2) ? x1 : x2;
   object->box->lower_y = (y1 > y2) ? y2 : y1;
 
