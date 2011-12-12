lepton-eda/lepton-eda

commit 8d4c574efc3a081324ef92d16e6feb6720c86c33

scheme-api: Translate strings.
Makes the messages emitted by Scheme API functions
internationalizable.  This required an update to the gnetlist
testsuite to set the library path for built-but-not-installed Scheme
modules.

Closes-bug: lp-901013

gschem/scheme/gschem/core/gettext.scm
@@ -1,0 +1,27 @@
+;; gEDA - GPL Electronic Design Automation
+;; gschem - gEDA Schematic Capture - Scheme API
+;; Copyright (C) 2011 Peter Brett <peter@peter-b.co.uk>
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA
+;;
+
+;; I18n for libgeda Scheme API.
+;;
+;; This module is for internal use only.
+
+(define-module (geda core gschem))
+
+(define %gschem-gettext-domain "gschem")
+(define-public (_ msg) (gettext msg %gschem-gettext-domain))

gnetlist/tests/common/inputs/gafrc
@@ -2,6 +2,7 @@
 ;; 'make check' during development can work correctly.
 (scheme-directory "${SCMDIR}")
 (scheme-directory "${GEDASCMDIR}")
+(scheme-directory "${GEDABUILTSCMDIR}")
 
 ;; We want to use the symbols that haven't been installed yet
 (map (lambda (x) (component-library (string-join (list "${SYMDIR}/" x) "")))

gnetlist/tests/common/run_backend_tests.sh
@@ -94,11 +94,13 @@ GEDADATA="${srcdir}/../.." # HACKHACKHACK
 GEDADATARC="${builddir}/../../lib"
 SCMDIR="${builddir}/../../scheme"
 GEDASCMDIR="${srcdir}/../../../libgeda/scheme"
+GEDABUILTSCMDIR="${builddir}/../../../libgeda/scheme"
 SYMDIR="${srcdir}/../../../symbols"
 export GEDADATA
 export GEDADATARC
 export SCMDIR
 export GEDASCMDIR
+export GEDABUILTSCMDIR
 export SYMDIR
 
 rundir=${here}/run
@@ -144,6 +146,7 @@ GEDADATA:   ${GEDATADA}
 GEDADATARC: ${GEDATADARC}
 SCMDIR:     ${SCMDIR}
 GEDASCMDIR: ${GEDASCMDIR}
+GEDABUILTSCMDIR: ${GEDABUILTSCMDIR}
 SYMDIR:     ${SYMDIR}
 all_tests:
 

gschem/po/POTFILES.in
@@ -1,10 +1,12 @@
 gschem/src/a_pan.c
 gschem/src/a_zoom.c
+gschem/src/g_attrib.c
 gschem/src/g_funcs.c
 gschem/src/g_hook.c
 gschem/src/g_keys.c
 gschem/src/g_rc.c
 gschem/src/g_register.c
+gschem/src/g_select.c
 gschem/src/g_util.c
 gschem/src/g_window.c
 gschem/src/globals.c
@@ -63,3 +65,7 @@ gschem/src/x_window.c
 gschem/data/geda-gschem.desktop.in
 
 gschem/lib/system-gschemrc.scm
+
+gschem/scheme/gschem/deprecated.scm
+gschem/scheme/gschem/gschemdoc.scm
+gschem/scheme/gschem/keymap.scm

gschem/scheme/Makefile.am
@@ -10,6 +10,7 @@ nobase_dist_scmdata_DATA = \
 	auto-place-attribs.scm \
 	pcb.scm \
 	default-attrib-positions.scm \
+	gschem/core/gettext.scm \
 	gschem/gschemdoc.scm \
 	gschem/window.scm \
 	gschem/selection.scm \

gschem/scheme/gschem/deprecated.scm
@@ -22,6 +22,8 @@
 
 (define-module (gschem deprecated)
 
+  #:use-module (gschem core gettext)
+
   #:use-module (geda page)
   #:use-module (geda object)
   #:use-module (geda attrib)
@@ -97,7 +99,7 @@
                  ("Upper Right" . upper-right))
                alignment)
               (and (string=? "" alignment) (text-align attrib))
-              (error "Invalid text alignment ~A." alignment))
+              (error (_ "Invalid text alignment ~A.") alignment))
              ;; angle
              (if (= rotation -1) (text-angle attrib) rotation)
              ;; string

gschem/scheme/gschem/gschemdoc.scm
@@ -18,6 +18,7 @@
 ;;
 
 (define-module (gschem gschemdoc)
+  #:use-module (gschem core gettext)
   #:use-module (geda os)
   #:use-module (geda object)
   #:use-module (geda attrib)
@@ -221,4 +222,4 @@ system associated viewer application."
          (directory-doc-search (sys-doc-dir) name ".pdf"))))
 
      ;; 4) Fail miserably
-     (error "No documentation found"))))
+     (error (_ "No documentation found")))))

gschem/scheme/gschem/keymap.scm
@@ -19,6 +19,7 @@
 
 (define-module (gschem keymap)
   #:use-module (gschem core keymap)
+  #:use-module (gschem core gettext)
   #:use-module (ice-9 optargs)
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-9)
@@ -35,7 +36,8 @@
 (define-public (string->key str)
   (or (%string->key str)
       (scm-error 'key-format #f
-                 "~S is not a valid key combination." (list str) #f)))
+                 (_ "~S is not a valid key combination.")
+                 (list str) #f)))
 
 ;; -------------------- Key sequences --------------------
 
@@ -149,7 +151,7 @@
            ((keymap? binding) (lookup binding keys (1+ ofs)))
 
            ;; Otherwise, generate an error.
-           (else (error "~S is not a prefix key sequence."
+           (else (error (_ "~S is not a prefix key sequence.")
                         (keys->display-string (prefix-keys keys))))))))
 
   (lookup keymap keys 0))

commit a957588adee2ec54f0cc3c23f9650dff6ed03d09

gschem: Correct error in (gschem core gettext)
Corrects an error introduced in commit 8d4c574efc3a, where the wrong
Scheme module name was defined.

gschem/scheme/gschem/core/gettext.scm
@@ -21,7 +21,7 @@
 ;;
 ;; This module is for internal use only.
 
-(define-module (geda core gschem))
+(define-module (gschem core gettext))
 
 (define %gschem-gettext-domain "gschem")
 (define-public (_ msg) (gettext msg %gschem-gettext-domain))