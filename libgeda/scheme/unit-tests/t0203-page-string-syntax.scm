3ffd19131319cd74689dcb388e53cf12948c26d5
Eivind Kvedalen <eivind@kvedalen.name>
2012-01-03 00:27:12 +0100

    Updated unit tests for scheme api (string->page function).

--- libgeda/scheme/unit-tests/t0203-page-string-syntax.scm
+++ libgeda/scheme/unit-tests/t0203-page-string-syntax.scm
@@ -5,7 +5,5 @@
 (use-modules (geda page))
 (use-modules (geda object))
 
-;; string->page should bork if the string contains invalid syntax.  It
-;; might not throw a misc-error; this is just a placeholder key.
 (begin-test 'string->page
   (assert-thrown 'string-format (string->page "/test/page/A" "__GARBAGE__")))
