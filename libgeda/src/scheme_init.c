lepton-eda/lepton-eda

commit 5cdf4de59028fad80072611a1194a4a8e224c363

scheme-api: Ensure init functions are called in Scheme mode.
This patch makes sure that any libguile functions called by
edascm_init() are called in Guile mode, even if for some reason
edascm_init() is called from a thread that is not yet in Guile mode.

libgeda/src/scheme_init.c
@@ -1,6 +1,6 @@
 /* gEDA - GPL Electronic Design Automation
  * libgeda - gEDA's library - Scheme API
- * Copyright (C) 2010 Peter Brett <peter@peter-b.co.uk>
+ * Copyright (C) 2010-2012 Peter Brett <peter@peter-b.co.uk>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,6 +31,25 @@ static int init_called = 0;
 
 SCM_GLOBAL_SYMBOL (edascm_object_state_sym, "object-state");
 
+/*! \brief Scheme API initialisation worker function.
+ * \par Function Description
+ * Called by edascm_init() with current thread in Guile mode.
+ */
+static void
+edascm_init_impl (void *data)
+{
+  #include "scheme_init.x"
+
+  edascm_init_smob ();
+  edascm_init_toplevel ();
+  edascm_init_object ();
+  edascm_init_complex ();
+  edascm_init_page ();
+  edascm_init_attrib ();
+  edascm_init_os ();
+  edascm_init_deprecated ();
+}
+
 /*! \brief Initialise the Scheme API.
  * \par Function Description
  * Registers all modules, procedures and variables exported by the
@@ -42,14 +61,5 @@ edascm_init ()
   if (init_called) return;
   init_called = 1;
 
-  #include "scheme_init.x"
-
-  edascm_init_smob ();
-  edascm_init_toplevel ();
-  edascm_init_object ();
-  edascm_init_complex ();
-  edascm_init_page ();
-  edascm_init_attrib ();
-  edascm_init_os ();
-  edascm_init_deprecated ();
+  scm_with_guile (edascm_init_impl, NULL);
 }

commit 80845206868cb5d5b70357a5ca23f8da07151fc2

scheme-api: Correct prototype of edascm_init_impl().
Fixes bug introduced in commit 5cdf4de59028.

libgeda/src/scheme_init.c
@@ -35,7 +35,7 @@ SCM_GLOBAL_SYMBOL (edascm_object_state_sym, "object-state");
  * \par Function Description
  * Called by edascm_init() with current thread in Guile mode.
  */
-static void
+static void *
 edascm_init_impl (void *data)
 {
   #include "scheme_init.x"
@@ -48,6 +48,7 @@ edascm_init_impl (void *data)
   edascm_init_attrib ();
   edascm_init_os ();
   edascm_init_deprecated ();
+  return NULL;
 }
 
 /*! \brief Initialise the Scheme API.