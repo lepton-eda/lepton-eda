lepton-eda/lepton-eda

commit 7f88749446b61493e881ad6aeb0a82f909a8c0d7

gschem: Don't require select to drag objects
This comes from watching users interacting with gschem and their
clear expectations that objects can be dragged around the canvas
without an explicit selection step first.

When a mouse drag starts on non-selected object, starting dragging
that object rather than forming a box selection.

gschem/src/x_event.c
@@ -718,6 +718,7 @@ gint x_event_motion(GtkWidget *widget, GdkEventMotion *event,
   int w_x, w_y;
   int unsnapped_wx, unsnapped_wy;
   int skip_event=0;
+  int do_move;
   GdkEvent *test_event;
 
   exit_if_null(w_current);
@@ -791,20 +792,37 @@ gint x_event_motion(GtkWidget *widget, GdkEventMotion *event,
     break;
 
     case(STARTSELECT):
-    if (!o_find_selected_object (w_current,
-                                 w_current->first_wx, w_current->first_wy)) {
+    /* Don't move anything if the shift key is pressed, that means
+     * the user definately wants to drag out a selection box.
+     */
+    do_move = 0;
+    if (!w_current->SHIFTKEY) {
+      /* If there is a selected object under the cursor, start moving it.
+       * If we don't find anything selected, look for an object we could
+       * select to start moving it.
+       */
+      if (o_find_selected_object (w_current, w_current->first_wx, w_current->first_wy) ||
+          (o_find_object (w_current,
+                          w_current->first_wx, w_current->first_wy, TRUE) &&
+           o_select_selected (w_current)))
+        do_move = 1;
+    }
+
+    if (!do_move) {
+      /* Drag out a selection box */
       if (o_select_box_start(w_current, unsnapped_wx, unsnapped_wy)) {
         w_current->event_state = SBOX;
         w_current->inside_action = 1;
       }
       break;
     } else {
-      /* Start the object movement */
+      /* Start moving the selected object(s) */
       o_move_start(w_current, w_x, w_y);
       w_current->event_state = ENDMOVE;
       w_current->inside_action = 1;
+      /* Fall through bottom of case to finish the move */
     }
-    /* Fall through */
+    /* Fall through to handle move */
     case(ENDMOVE):
     case(MOVE):
     if (w_current->inside_action)

commit 58af3327f4fbca32597f7081cc84453fdf4c8dee

gschem: Fix bug with inverted (control key) selection.
Fixes a regression caused by commit 7f88749446b6.  The previous commit
made it so that making a selection with the control key pressed no
longer inverts the selection state of the objects selected, but
instead starts a new selection.

Reviewed-by: Peter TB Brett <peter@peter-b.co.uk>
Closes-bug: lp-721445

gschem/src/x_event.c
@@ -731,7 +731,6 @@ gint x_event_motion(GtkWidget *widget, GdkEventMotion *event,
   int w_x, w_y;
   int unsnapped_wx, unsnapped_wy;
   int skip_event=0;
-  int do_move;
   GdkEvent *test_event;
 
   g_return_val_if_fail ((w_current != NULL), 0);
@@ -809,24 +808,14 @@ gint x_event_motion(GtkWidget *widget, GdkEventMotion *event,
     break;
 
     case(STARTSELECT):
-    /* Don't move anything if the shift key is pressed, that means
-     * the user definately wants to drag out a selection box.
+    /* If the shift or control keys are pressed, that means the user definately wants to drag out a
+     * selection box.  Otherwise, if there is not a selected object under the cursor, look for one
+     * that could be selected and start moving it.
      */
-    do_move = 0;
-    if (!w_current->SHIFTKEY) {
-      /* If there is a selected object under the cursor, start moving it.
-       * If we don't find anything selected, look for an object we could
-       * select to start moving it.
-       */
-      if (o_find_selected_object (w_current, w_current->first_wx, w_current->first_wy) ||
-          (o_find_object (w_current,
-                          w_current->first_wx, w_current->first_wy, TRUE) &&
-           o_select_selected (w_current)))
-        do_move = 1;
-    }
-
-    if (!do_move) {
-      /* Drag out a selection box */
+    if (w_current->SHIFTKEY || w_current->CONTROLKEY
+            || (!o_find_selected_object(w_current, w_current->first_wx, w_current->first_wy)
+                && (!o_find_object(w_current, w_current->first_wx, w_current->first_wy, TRUE)
+                    || !o_select_selected(w_current)))) {
       if (o_select_box_start(w_current, unsnapped_wx, unsnapped_wy)) {
         w_current->event_state = SBOX;
         w_current->inside_action = 1;