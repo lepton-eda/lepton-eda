b0a8dc828c4a94f90ca90b966f141924d45d3c64
Peter TB Brett <peter@peter-b.co.uk>
2012-01-15 18:51:08 +0000

    gschem: Use correct textdomain in Scheme code.
    Reported-by: Sergey Alyoshin
    Closes-bug: lp-916803

--- gschem/scheme/gschem/core/gettext.scm
+++ gschem/scheme/gschem/core/gettext.scm
@@ -23,5 +23,5 @@
 
 (define-module (gschem core gettext))
 
-(define %gschem-gettext-domain "gschem")
+(define %gschem-gettext-domain "geda-gschem")
 (define-public (_ msg) (gettext msg %gschem-gettext-domain))
