lepton-eda/lepton-eda

commit 65c68f98cc4bf5d26a091abf336b2e00c7bbcabb

libgeda: skip printing endpoint cues on fully connected nets
Affects-bug: lp-707064

libgeda/src/s_cue.c
@@ -256,8 +256,12 @@ void s_cue_output_single(TOPLEVEL * toplevel, OBJECT * object, FILE * fp,
 	return;
       }
 
-  s_cue_output_lowlevel(toplevel, object, 0, fp, type);
-  s_cue_output_lowlevel(toplevel, object, 1, fp, type);
+  if (object->type != OBJ_NET
+      || ((object->type == OBJ_NET)
+          && !o_net_is_fully_connected (toplevel, object))) {
+    s_cue_output_lowlevel(toplevel, object, 0, fp, type);
+    s_cue_output_lowlevel(toplevel, object, 1, fp, type);
+  }
   s_cue_output_lowlevel_midpoints(toplevel, object, fp, type);
 }
 

commit f8a9c86e2e2fa7fa451cd504e62ebee6a3e73d2d

libgeda: fix printing of junction cues
Closes bug "disappearing dots in gschem" reported by
Gabriel Paubert <paubert@iram.es>.  Issue was introduced in
commit 65c68f9.

Additionally, now junction cues are also printed when 3 or
more pins are connected (same behavior as for net segments).

libgeda/src/s_cue.c
@@ -170,7 +170,7 @@ void s_cue_output_lowlevel(TOPLEVEL * toplevel, OBJECT * object, int whichone,
   switch (type) {
 
     case (CONN_ENDPOINT):
-      if (object->type == OBJ_NET) {	/* only nets have these cues */
+      if (object->type == OBJ_NET || object->type == OBJ_PIN) {
         if (count < 1) {	/* Didn't find anything connected there */
           if (output_type == POSTSCRIPT) {
             s_cue_postscript_fillbox(toplevel, fp, x, y);
@@ -180,13 +180,6 @@ void s_cue_output_lowlevel(TOPLEVEL * toplevel, OBJECT * object, int whichone,
         } else if (count >= 2) {
           if (output_type == POSTSCRIPT)
             s_cue_postscript_junction (toplevel, fp, x, y, bus_involved);
-        }
-      }
-      else if (object->type == OBJ_PIN
-               && count == 0
-               && whichone == object->whichend) {
-        if (output_type == POSTSCRIPT) {
-          s_cue_postscript_fillbox(toplevel, fp, x, y);
         }
       }
       break;