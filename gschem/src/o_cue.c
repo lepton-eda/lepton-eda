lepton-eda/lepton-eda

commit 6cae96ed0629a87bd71cecce4cc966e58b8bd0af

gschem: skip drawing endpoint cues on fully connected nets
Affects-bug: lp-707064

gschem/src/o_cue.c
@@ -258,8 +258,12 @@ void o_cue_draw_single(GSCHEM_TOPLEVEL *w_current, OBJECT *object)
       }
 
   if (object->type != OBJ_PIN) {
-    o_cue_draw_lowlevel(w_current, object, 0);
-    o_cue_draw_lowlevel(w_current, object, 1);
+    if (object->type != OBJ_NET
+        || ((object->type == OBJ_NET)
+            && !o_net_is_fully_connected (w_current->toplevel, object))) {
+      o_cue_draw_lowlevel(w_current, object, 0);
+      o_cue_draw_lowlevel(w_current, object, 1);
+    }
     o_cue_draw_lowlevel_midpoints(w_current, object);
   } else {
     o_cue_draw_lowlevel(w_current, object, object->whichend);

commit 0df45c9f859537bf9683b7fa8fb3d4b35828672e

gschem: fix drawing of junction cues
Closes bug "disappearing dots in gschem" reported by
Gabriel Paubert <paubert@iram.es>.  Issue was introduced in
commit 6cae96e.

Additionally, now junction cues are also drawn when 3 or more pins are
connected (same behavior as for net segments).

gschem/src/o_cue.c
@@ -181,27 +181,14 @@ void o_cue_draw_lowlevel(GSCHEM_TOPLEVEL *w_current, OBJECT *object, int whichon
   switch(type) {
 
     case(CONN_ENDPOINT):
-      if (object->type == OBJ_NET) { /* only nets have these cues */
+      if (object->type == OBJ_NET || object->type == OBJ_PIN) {
         if (count < 1) { /* Didn't find anything connected there */
           size = CUE_BOX_SIZE;
           gschem_cairo_center_box (w_current, -1, -1, x, y, size, size);
           o_cue_set_color (w_current, NET_ENDPOINT_COLOR);
           cairo_fill (w_current->cr);
-
         } else if (count >= 2) {
           draw_junction_cue (w_current, x, y, bus_involved);
-        }
-      } else if (object->type == OBJ_PIN) {
-        /* Didn't find anything connected there */
-        if (count < 1 && object->whichend == whichone) {
-          size = (bus_involved) ? PIN_CUE_SIZE_BUS : PIN_CUE_SIZE_NET;
-
-          otherone = !whichone;
-
-          o_cue_set_color (w_current, NET_ENDPOINT_COLOR);
-          gschem_cairo_center_box (w_current, -1, -1, x, y, size, size);
-          o_cue_set_color (w_current, NET_ENDPOINT_COLOR);
-          cairo_fill (w_current->cr);
         }
       }
       break;