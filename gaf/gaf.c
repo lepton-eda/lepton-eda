4ac55147880a8d49b19dca2b472ff392d0067b02
Peter TB Brett <peter@peter-b.co.uk>
2012-11-26 14:06:29 +0000

    Add `gaf' program.
    Add a utility program to wrap various command-line tools, called
    `gaf'.  At the moment, it supports exactly one sub-command, `gaf
    shell'.

---
+++ gaf/gaf.c
@@ -0,0 +1,163 @@
+/*
+ * gEDA/gaf command-line utility
+ * Copyright (C) 2012 Peter Brett <peter@peter-b.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+#include <version.h>
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <getopt.h>
+
+#include <glib.h>
+
+#include "builtins.h"
+
+#define short_options "+hV"
+
+static struct option long_options[] =
+  {
+    {"help", 0, NULL, 'h'},
+    {"version", 0, NULL, 'V'},
+    {"no-rcfiles", 0, NULL, 2},
+    {NULL, 0, NULL, 0},
+  };
+
+struct internal_command {
+  char *name;
+  int (*func)(int, char **);
+};
+
+static struct internal_command commands[] =
+  {
+    {"shell", cmd_shell},
+    {NULL, NULL},
+  };
+
+/* Print help info and exit */
+static void
+usage (void)
+{
+  printf (
+"Usage: gaf [OPTION...] COMMAND [ARGS ...]\n"
+"\n"
+"gEDA/gaf command-line utility.\n"
+"\n"
+"General options:\n"
+"  --no-rcfiles   inhibit loading of 'gafrc' files\n"
+"  -h, --help     display usage information and exit\n"
+"  -V, --version  display version information and exit\n"
+"\n"
+"Commonly-used commands (type `gaf <cmd> --help' for usage):\n"
+"  shell          Scheme REPL for interactive gEDA data processing\n"
+"\n"
+"Please report bugs to %s.\n",
+PACKAGE_BUGREPORT);
+  exit (0);
+}
+
+/* Print version info and exit */
+static void
+version (void)
+{
+  printf(
+"gEDA/gaf %s (g%.7s)\n"
+"Copyright (C) 1998-2012 gEDA developers\n"
+"This is free software, and you are welcome to redistribute it under\n"
+"certain conditions. For details, see the file `COPYING', which is\n"
+"included in the gEDA distribution.\n"
+"There is NO WARRANTY, to the extent permitted by law.\n",
+         PACKAGE_DOTTED_VERSION, PACKAGE_GIT_COMMIT);
+  exit (0);
+}
+
+int
+main (int argc, char **argv)
+{
+  int c;
+  char *cmd = NULL;
+  int cmd_argc = 0;
+  char **cmd_argv = NULL;
+  int (*cmd_func)(int, char **) = NULL;
+
+  while (-1 != (c = getopt_long (argc, argv, short_options,
+                                 long_options, NULL))) {
+    switch (c) {
+
+    case 0:
+      /* This is a long-form-only flag option, and has already been
+       * dealt with by getopt_long(). */
+      break;
+
+    case 2: /* --no-rcfiles */
+      setenv ("GAF_INHIBIT_RCFILES", "1", 1);
+      break;
+
+    case 'V':
+      version ();
+
+    case 'h':
+      usage ();
+
+    case '?':
+      /* getopt_long already printed an error message */
+      fprintf (stderr, "\nRun `gaf --help' for more information.\n");
+      exit (1);
+      break;
+
+    default:
+      g_assert_not_reached ();
+    }
+  }
+
+  /* The next argument should be a command */
+  if (optind == argc) {
+    fprintf (stderr,
+             "ERROR: You must specify a command to run.\n"
+             "\n"
+             "Run `gaf --help' for more information.\n");
+    exit (1);
+  }
+
+  cmd = argv[optind];
+
+  /* Look up the command */
+  int i;
+  for (i = 0; commands[i].name != NULL; i++) {
+    if (strcmp (cmd, commands[i].name) == 0) {
+      cmd_func = commands[i].func;
+      break;
+    }
+  }
+  if (cmd_func == NULL) {
+    fprintf (stderr,
+             "ERROR: Unrecognised command `%s'.\n"
+             "\n"
+             "Run `gaf --help' for more information.\n", cmd);
+    exit (1);
+  }
+
+  cmd_argc = argc - optind;
+  cmd_argv = argv + optind;
+
+  return cmd_func (cmd_argc, cmd_argv);
+}
---
+++ gaf/shell.c
@@ -0,0 +1,141 @@
+/*
+ * gEDA/gaf command-line utility
+ * Copyright (C) 2012 Peter Brett <peter@peter-b.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+#include <version.h>
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <getopt.h>
+
+#include <libgeda/libgeda.h>
+#include <libgeda/libgedaguile.h>
+
+#define shell_short_options "s:c:L:l:h"
+
+static struct option shell_long_options[] =
+  {
+    {"help", 0, NULL, 'h'},
+    {NULL, 0, NULL, 0},
+  };
+
+static void
+shell_usage (void)
+{
+  printf (
+"Usage: gaf shell [OPTION ...]\n"
+"\n"
+"Shell for interactive processing of gEDA data using Scheme.\n"
+"\n"
+"  -s FILE        load Scheme source code from FILE, and exit\n"
+"  -c EXPR        evaluate Scheme expression EXPR, and exit\n"
+"  --             stop scanning arguments; run interactively\n"
+"\n"
+"The above switches stop argument processing, and pass all\n"
+"remaining arguments as the value of (command-line).\n"
+"\n"
+"  -L DIRECTORY   add DIRECTORY to the front of the Scheme load path\n"
+"  -l FILE        load Scheme source code from FILE\n"
+"  -h, --help     display usage information and exit\n"
+"\n"
+"Please report bugs to %s.\n",
+PACKAGE_BUGREPORT);
+  exit (0);
+}
+
+/* Some symbols we need */
+SCM_SYMBOL (sym_begin, "begin");
+SCM_SYMBOL (sym_use_modules, "use-modules");
+SCM_SYMBOL (sym_ice_9, "ice-9");
+SCM_SYMBOL (sym_readline, "readline");
+SCM_SYMBOL (sym_activate_readline, "activate-readline");
+
+static void
+cmd_shell_impl (void *data, int argc, char **argv)
+{
+  int c, interactive = 1;
+  TOPLEVEL *toplevel;
+
+  #include "shell.x"
+
+  /* Parse command-line arguments */
+  while ((c = getopt_long (argc, argv, shell_short_options,
+                           shell_long_options, NULL)) != -1) {
+    switch (c) {
+    case 0:
+      /* This is a long-form-only flag option, and has already been
+       * dealt with by getopt_long(). */
+      break;
+    case 's':
+    case 'c':
+      interactive = 0;
+      /* Intentionally falls through */
+    case 'l':
+    case 'L':
+      /* Do nothing, scm_shell() will deal with these. */
+      break;
+    case 'h':
+      shell_usage ();
+      break;
+    case '?':
+      /* getopt_long already printed an error message */
+      fprintf (stderr, "\nRun `%s --help' for more information.\n", argv[0]);
+      exit (1);
+      break;
+    default:
+      g_assert_not_reached ();
+    }
+  }
+
+  libgeda_init ();
+  scm_dynwind_begin (0);
+  toplevel = s_toplevel_new ();
+  edascm_dynwind_toplevel (toplevel);
+
+  /* Interactive, so enable readline support and print an abbreviated
+   * version message. */
+  if (interactive) {
+    printf ("gEDA %s (g%.7s)\n", PACKAGE_DOTTED_VERSION, PACKAGE_GIT_COMMIT);
+    SCM expr = scm_list_3 (sym_begin,
+                           scm_list_2 (sym_use_modules,
+                                       scm_list_2 (sym_ice_9, sym_readline)),
+                           scm_list_1 (sym_activate_readline));
+
+    scm_eval_x (expr, scm_current_module ());
+  }
+
+  /* Now load rc files, if necessary */
+  if (getenv ("GAF_INHIBIT_RCFILES") == NULL) {
+    g_rc_parse (toplevel, "gaf shell", NULL, NULL);
+  }
+  i_vars_libgeda_set (toplevel); /* Ugh */
+
+  scm_shell (argc, argv); /* Doesn't return */
+
+  scm_dynwind_end ();
+}
+
+int
+cmd_shell (int argc, char **argv)
+{
+  scm_boot_guile (argc, argv, cmd_shell_impl, NULL); /* Doesn't return */
+  return 0;
+}
