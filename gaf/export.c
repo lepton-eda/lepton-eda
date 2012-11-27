2f2ce169a5ec9bcb9499e29e6f3366a6eb864414
Peter TB Brett <peter@peter-b.co.uk>
2012-11-27 23:10:45 +0000

    gaf-export: [WIP] Get defaults from config system.

--- gaf/export.c
+++ gaf/export.c
@@ -42,6 +42,21 @@
 #include <cairo-pdf.h>
 #include <cairo-ps.h>
 
+static void export_command_line (EdaConfig *cfg);
+static void export_usage (void);
+static struct OutputFormat *export_get_format (void);
+static GArray *export_get_color_map (void);
+static void export_page (PAGE *page);
+static void export_simple_multipage (cairo_surface_t *surface);
+static void export_png (void);
+static void export_ps  (void);
+static void export_eps (void);
+static void export_pdf (void);
+static void export_svg (void);
+
+#define DEFAULT_IMAGE_SIZE "paper"
+#define DEFAULT_MARGINS "auto"
+#define DEFAULT_ORIENTATION "auto"
 #define DEFAULT_DPI 300
 
 #define shell_short_options "f:o:p:s:ch"
@@ -59,32 +74,6 @@ static struct option shell_long_options[] =
     {"help", 0, NULL, 'h'},
     {NULL, 0, NULL, 0},
   };
-
-static void
-export_usage (void)
-{
-  printf (_("Usage: gaf export [OPTION ...] -o OUTPUT [--] FILE ...\n"
-"\n"
-"Export gEDA files in various image formats.\n"
-"\n"
-"  -f, --format=TYPE      select output format [TYPE=auto]\n"
-"  -o, --output=OUTPUT    output filename\n"
-"  -p, --paper=SIZE       select paper size [SIZE=%s]\n"
-"  -s, --size=ROWSxCOLS   select image size in pixels.\n"
-"      --dpi=DPI          select image dpi [DPI=%i].\n"
-"  -c, --color            enable color output\n"
-"  --landscape            output in landscape format\n"
-"  --portrait             output in portrait format\n"
-"  -h, --help     display usage information and exit\n"
-"\n"
-"Please report bugs to %s.\n"),
-          gtk_paper_size_get_default (),
-          DEFAULT_DPI,
-          PACKAGE_BUGREPORT);
-  exit (0);
-}
-
-
 enum OutputFormatFlags {
   OUTPUT_MULTIPAGE = 1,
   OUTPUT_POINTS = 2,
@@ -98,14 +87,6 @@ struct OutputFormat {
   void (*func)(void);
 };
 
-static void export_page (PAGE *page);
-static void export_simple_multipage (cairo_surface_t *surface);
-static void export_png (void);
-static void export_ps  (void);
-static void export_eps (void);
-static void export_pdf (void);
-static void export_svg (void);
-
 static struct OutputFormat formats[] =
   {
     {"Portable Network Graphics (PNG)", "png", OUTPUT_PIXELS, export_png},
@@ -116,52 +97,127 @@ static struct OutputFormat formats[] =
     {NULL, NULL, 0, NULL},
   };
 
+static EdaConfig *cmdline_cfg = NULL;
 static EdaRenderer *renderer = NULL;
 static TOPLEVEL *toplevel = NULL;
-static char *filename = NULL;
-static double width, height;
-
-#define bad_arg_msg _("ERROR: Bad argument '%s' to %s option.\n")
-#define bad_arg_prefix_msg _("ERROR: Bad argument '%s' to %s option: %s\n")
-#define see_help_msg _("\nRun `gaf export --help' for more information.\n")
+static gint *num_inputs = 0;
+static gchar **input_filenames = NULL; /* Filename encoding */
+static gchar *output_filename = NULL; /* Filename encoding */
+static gchar *output_format = NULL;
 
 void
 cmd_export (int argc, char **argv)
 {
   int c, i, status;
-  gchar *format_name = NULL;
-  gchar *paper_name = g_strdup (gtk_paper_size_get_default ());
-  gdouble dpi = DEFAULT_DPI;
-  gboolean color = FALSE;
+  EdaConfig *cfg;
   GError *err = NULL;
   gchar *tmp;
-  const gchar *out_arg = NULL;
   const gchar *out_suffix;
-  gint pixel_width = -1, pixel_height = -1;
-  gboolean portrait = TRUE;
   GtkPaperSize *paper = NULL;
   struct OutputFormat *exporter = NULL;
   GArray *render_color_map = NULL;
   gchar *original_cwd = g_get_current_dir ();
 
-  /* We *don't* call gtk_init(), because we're only using GTK for
-   * GtkPaperSize, which doesn't require gtk_init().*/
-  g_type_init ();
+  gtk_init_check (&argc, &argv);
   scm_init_guile ();
   libgeda_init ();
   scm_dynwind_begin (0);
   toplevel = s_toplevel_new ();
   edascm_dynwind_toplevel (toplevel);
 
-  /* Parse command-line arguments */
+  /* Set default configuration */
+  export_init_defaults ();
+
+  /* Parse command-line options */
+  export_command_line (argc, argv);
+
+  /* Now load rc files, if necessary */
+  if (getenv ("GAF_INHIBIT_RCFILES") == NULL) {
+    g_rc_parse (toplevel, "gaf export", NULL, NULL);
+  }
+  i_vars_libgeda_set (toplevel); /* Ugh */
+  eda_config_set_parent (cmdline_cfg,
+                         eda_config_get_context_for_path ("."));
+
+  /* Load schematic files */
+  i = 0;
+  while (i < num_inputs) {
+    PAGE *page;
+    tmp = input_filenames[i++];
+
+    page = s_page_new (toplevel, tmp);
+    if (!f_open (toplevel, page, tmp, &err)) {
+      fprintf (stderr, _("ERROR: Failed to load '%s': %s\n"),
+               tmp, err->message);
+      exit (1);
+    }
+    /* f_open() obnoxiously chdirs, so make sure to go back to the
+     * original cwd after each load. */
+    if (g_chdir (original_cwd) != 0) {
+      fprintf (stderr, _("ERROR: Failed to change directory to '%s': %s\n"),
+               original_cwd, g_strerror (errno));
+      exit (1);
+    }
+  }
+
+  /* Figure out output format */
+  exporter = export_get_format ();
+
+  /* If more than one schematic/symbol file was specified, check that
+   * exporter supports multipage output. */
+  if ((argc - optind > 1) && !(exporter->flags & OUTPUT_MULTIPAGE)) {
+    fprintf (stderr,
+             _("ERROR: Selected output format does not support multipage output\n"));
+    exit (1);
+  }
+
+  /* Create renderer */
+  renderer = eda_renderer_new (NULL, NULL);
+  render_color_map = export_get_color_map ();
+  eda_renderer_set_color_map (renderer, render_color_map);
+
+  /* Render */
+  exporter->func ();
+
+  scm_dynwind_end ();
+  exit (0);
+}
+
+static void
+export_init_defaults (void)
+{
+  EdaConfig *cfg = eda_config_get_default_context ();
+  eda_config_set_string (cfg, "export", "paper", gtk_paper_size_get_default ());
+  eda_config_set_string (cfg, "export", "orientation", DEFAULT_ORIENTATION);
+  eda_config_set_int (cfg, "export", "dpi", DEFAULT_DPI);
+  eda_config_set_string (cfg, "export", "image-size", DEFAULT_IMAGE_SIZE);
+  eda_config_set_boolean (cfg, "export", "fit-drawing", TRUE);
+  eda_config_set_boolean (cfg, "export", "monochrome", TRUE);
+  eda_config_set_string (cfg, "export", "margins", DEFAULT_MARGINS);
+}
+
+static void
+export_command_line (int argc, const char **argv)
+{
+  EdaConfig *cfg;
+  GError *err;
+  gint pixel_size[] = {0,0};
+  gint dpi;
+  gint status;
+  gint c;
+  gchar *tmp;
+  const gchar *bad_arg_msg = _("ERROR: Bad argument '%s' to %s option.\n");
+  const gchar *bad_arg_prefix_msg = _("ERROR: Bad argument '%s' to %s option: %s\n");
+  const gchar *see_help_msg = _("\nRun `gaf export --help' for more information.\n");
+
+  /* Create a new trusted configuration context to put the
+   * command-line options in.  They'll be used to override all of the
+   * configuration files. */
+  cfg = g_object_new (EDA_TYPE_CONFIG, "trusted", TRUE, NULL);
+
   while ((c = getopt_long (argc, argv, shell_short_options,
                            shell_long_options, NULL)) != -1) {
     switch (c) {
-    case 0:
-      /* This is a long-form-only flag option, and has already been
-       * dealt with by getopt_long(). */
-      break;
-
     case 'f':
       g_free (format_name);
       format_name = g_locale_to_utf8 (optarg, -1, NULL, NULL, &err);
@@ -173,36 +229,33 @@ cmd_export (int argc, char **argv)
       break;
 
     case 'o':
-      g_free (filename);
-      /* We need an absolute path to the output file, because f_open()
-       * obnoxiously changes directory. */
-      if (g_path_is_absolute (optarg)) {
-        filename = g_strdup (optarg);
-      } else {
-        tmp = g_get_current_dir ();
-        filename = g_build_filename (tmp, optarg, NULL);
-        g_free (tmp);
-      }
-      out_arg = optarg; /* Save this for error messages */
+      g_free (output_filename);
+      output_filename = g_strdup (optarg);
       break;
 
     case 'p':
-      g_free (paper_name);
-      paper_name = g_locale_to_utf8 (optarg, -1, NULL, NULL, &err);
-      if (paper_name == NULL) {
+      tmp = g_locale_to_utf8 (optarg, -1, NULL, NULL, &err);
+      if (tmp == NULL) {
         fprintf (stderr, bad_arg_prefix_msg,
                  optarg, "-p,--paper", err->message);
         exit (1);
       }
+      eda_config_set_string (cfg, "export", "paper", tmp);
+      g_free (tmp);
       break;
 
     case 's':
-      status = sscanf (optarg, "%ix%i", &pixel_width, &pixel_height);
-      if (status != 2 || pixel_width <= 0 || pixel_height <= 0) {
+      status = sscanf (optarg, "%ix%i", &pixel_size[0], &pixel_size[1]);
+      if (status != 2 || pixel_size[0] < 0 || pixel_size[1] < 0) {
         fprintf (stderr, bad_arg_msg, optarg, "-s,--size");
         fprintf (stderr, see_help_msg);
         exit (1);
       }
+      eda_config_set_int_list (cfg, "export", "image-size", pixel_size, 2);
+      break;
+
+    case 'c':
+      eda_config_set_boolean (cfg, "export", "monochrome", FALSE);
       break;
 
     case 2: /* --dpi */
@@ -211,18 +264,15 @@ cmd_export (int argc, char **argv)
         fprintf (stderr, bad_arg_msg, optarg, "--dpi");
         exit (1);
       }
+      eda_config_set_int (cfg, "export", "dpi", dpi);
       break;
 
     case 3: /* --portrait */
-      portrait = TRUE;
+      eda_config_set_string (cfg, "export", "orientation", "portrait");
       break;
 
     case 4: /* --landscape */
-      portrait = FALSE;
-      break;
-
-    case 'c':
-      color = TRUE;
+      eda_config_set_string (cfg, "export", "orientation", "landscape");
       break;
 
     case 'h':
@@ -234,6 +284,7 @@ cmd_export (int argc, char **argv)
       fprintf (stderr, see_help_msg);
       exit (1);
       break;
+
     default:
       g_assert_not_reached ();
     }
@@ -253,26 +304,46 @@ cmd_export (int argc, char **argv)
     exit (1);
   }
 
-  /* Now load rc files, if necessary */
-  if (getenv ("GAF_INHIBIT_RCFILES") == NULL) {
-    g_rc_parse (toplevel, "gaf export", NULL, NULL);
-  }
-  i_vars_libgeda_set (toplevel); /* Ugh */
+  num_inputs = argc - optind;
+  input_filenames = &argv[optind];
 
-  /* Validate paper size, if given */
-  tmp = g_utf8_strdown (paper_name, -1);
-  paper = gtk_paper_size_new (tmp);
-  if (paper == NULL) {
-    fprintf (stderr, _("ERROR: Invalid paper size '%s'.\n"),
-             paper_name);
-    exit (1);
-  }
-  g_free (tmp);
+  cmdline_cfg = cfg;
+}
+
+static void
+export_usage (void)
+{
+  printf (_("Usage: gaf export [OPTION ...] -o OUTPUT [--] FILE ...\n"
+"\n"
+"Export gEDA files in various image formats.\n"
+"\n"
+"  -f, --format=TYPE      output file format (autodetected)\n"
+"  -o, --output=OUTPUT    output filename\n"
+"  -p, --paper=SIZE       paper size\n"
+"  -s, --size=ROWSxCOLS   image size in pixels\n"
+"      --dpi=DPI          image dpi\n"
+"  -c, --color            enable color output\n"
+"  --landscape            output in landscape format\n"
+"  --portrait             output in portrait format\n"
+"  -h, --help     display usage information and exit\n"
+"\n"
+"Please report bugs to %s.\n"),
+          gtk_paper_size_get_default (),
+          DEFAULT_DPI,
+          PACKAGE_BUGREPORT);
+  exit (0);
+}
+
+static struct OutputFormat *
+export_get_format (void)
+{
+  const gchar *out_suffix;
+  struct OutputFormat *exporter;
 
   /* If no format was specified, try and guess from output
    * filename. */
   if (format_name == NULL) {
-    out_suffix = strrchr (filename, '.');
+    out_suffix = strrchr (output_filename, '.');
     if (out_suffix != NULL) {
       out_suffix++; /* Skip '.' */
     } else {
@@ -307,64 +378,21 @@ cmd_export (int argc, char **argv)
   }
   g_free (tmp);
 
-  /* Try to figure out dimensions for the output */
-  if (exporter->flags & OUTPUT_POINTS) {
-    width = gtk_paper_size_get_width (paper, GTK_UNIT_POINTS);
-    height = gtk_paper_size_get_height (paper, GTK_UNIT_POINTS);
-  } else if (exporter->flags & OUTPUT_PIXELS) {
-    if (pixel_width > 0) {
-      width = pixel_width;
-      height = pixel_height;
-    } else {
-      width = ceil (dpi * gtk_paper_size_get_width (paper, GTK_UNIT_INCH));
-      height = ceil (dpi * gtk_paper_size_get_height (paper, GTK_UNIT_INCH));
-    }
-  } else g_assert_not_reached ();
+  return exporter;
+}
 
-  /* If portrait mode, swap width/height */
-  if (portrait) {
-    double dummy = width;
-    width = height;
-    height = dummy;
-  }
-
-  /* If more than one schematic/symbol file was specified, check that
-   * exporter supports multipage output. */
-  if ((argc - optind > 1) && !(exporter->flags & OUTPUT_MULTIPAGE)) {
-    fprintf (stderr,
-             _("ERROR: Selected output format does not support multipage output\n"));
-    exit (1);
-  }
-
-  /* Load schematic files */
-  while (optind < argc) {
-    PAGE *page;
-    tmp = argv[optind++];
-
-    page = s_page_new (toplevel, tmp);
-    if (!f_open (toplevel, page, tmp, &err)) {
-      fprintf (stderr,
-               _("ERROR: Failed to load '%s': %s\n"), tmp,
-               err->message);
-      exit (1);
-    }
-    if (g_chdir (original_cwd) != 0) {
-      fprintf (stderr,
-               _("ERROR: Failed to change directory to '%s': %s\n"),
-               original_cwd, g_strerror (errno));
-      exit (1);
-    }
-  }
-
-  /* Create renderer */
-  renderer = eda_renderer_new (NULL, NULL);
-
+static GArray *
+export_get_color_map (void)
+{
+  GArray *render_color_map;
+  gboolean bw = eda_config_get_boolean (cmdline_cfg,
+                                        "export", "monochrome", NULL);
   /* Create color map */
   render_color_map =
     g_array_sized_new (FALSE, FALSE, sizeof(COLOR), MAX_COLORS);
   render_color_map =
     g_array_append_vals (render_color_map, print_colors, MAX_COLORS);
-  if (!color) {
+  if (bw) {
     /* Create a black and white color map.  All non-background colors
      * are black. */
     COLOR white = {~0, ~0, ~0, ~0, TRUE};
@@ -385,13 +413,7 @@ cmd_export (int argc, char **argv)
       }
     }
   }
-  eda_renderer_set_color_map (renderer, render_color_map);
-
-  /* Render */
-  exporter->func ();
-
-  scm_dynwind_end ();
-  exit (0);
+  return render_color_map;
 }
 
 static void
@@ -401,6 +423,35 @@ export_cairo_check_error (cairo_status_t status)
     fprintf (stderr, _("ERROR: %s.\n"), cairo_status_to_string (status));
     exit (1);
   }
+}
+
+static GtkPageSetup *
+export_get_page_setup (double w_width, double w_height,
+                       gboolean is_raster)
+{
+  GtkPaperSize *paper;
+  GtkPageSetup *setup = gtk_page_setup_new ();
+
+  /* First check if we're just fitting a sheet to the drawing */
+  if (eda_config_get_boolean (cmdline_cfg, "export",
+                              "fit-drawing", NULL)) {
+    paper = gtk_paper_size_new_custom ("user",
+                                       _("User paper size"),
+                                       1000*w_width, 1000*w_height,
+                                       GTK_UNIT_INCH);
+  } else {
+    gchar *paper_name = eda_config_get_string (cmdline_cfg, "export",
+                                               "paper", NULL);
+    paper = gtk_paper_size_new (paper_name);
+    if (paper == NULL) {
+      fprintf (stderr, _("ERROR: Unknown paper size '%s'.\n"),
+               paper_name);
+      exit (1);
+    }
+  }
+
+  /* Just use default margins for now */
+  gtk_page_setup_set_paper_size_and_default_margins (setup, paper);
 }
 
 static void
