lepton-eda/lepton-eda

commit 18f64a9810ec99250b45c3a29edae04b080a8a8f

gnetlist: Begin porting options to new config system.

gnetlist/src/s_traverse.c
@@ -157,7 +157,17 @@ s_traverse_sheet (TOPLEVEL * pr_current, const GList *obj_list, char *hierarchy_
   SCM scm_uref;
   char *temp_uref;
   gboolean is_graphical=FALSE;
+  gboolean is_hierarchy = TRUE;
   const GList *iter;
+  GError *err = NULL;
+  EdaConfig *cfg;
+
+  cfg = eda_config_get_context_for_file (NULL);
+  is_hierarchy = eda_config_get_boolean (cfg, "gnetlist", "traverse-hierarchy", &err);
+  if (err == NULL) {
+    is_hierarchy = TRUE;
+    g_clear_error (&err);
+  }
 
   if (verbose_mode) {
     printf("- Starting internal netlist creation\n");
@@ -252,7 +262,7 @@ s_traverse_sheet (TOPLEVEL * pr_current, const GList *obj_list, char *hierarchy_
 			 hierarchy_tag);
       
       /* now you need to traverse any underlying schematics */
-      if (pr_current->hierarchy_traversal == TRUE) {
+      if (is_hierarchy) {
 	s_hierarchy_traverse(pr_current, o_current, netlist);
       }
     }

gnetlist/include/i_vars.h
@@ -17,8 +17,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-extern int default_net_naming_priority;
-extern int default_hierarchy_traversal;
 extern int default_hierarchy_uref_mangle;
 extern int default_hierarchy_netname_mangle;
 extern int default_hierarchy_netattrib_mangle;
@@ -29,5 +27,3 @@ extern int default_hierarchy_uref_order;
 extern char *default_hierarchy_netname_separator;
 extern char *default_hierarchy_netattrib_separator;
 extern char *default_hierarchy_uref_separator;
-extern char *default_unnamed_netname;
-extern char *default_unnamed_busname;

gnetlist/include/prototype.h
@@ -19,8 +19,6 @@ SCM g_get_toplevel_attribute(SCM scm_wanted_attrib);
 SCM g_graphical_objs_in_net_with_attrib_get_attrib(SCM scm_netname, SCM scm_has_attribute, SCM scm_wanted_attribute);
 /* g_rc.c */
 SCM g_rc_gnetlist_version(SCM version);
-SCM g_rc_net_naming_priority(SCM mode);
-SCM g_rc_hierarchy_traversal(SCM mode);
 SCM g_rc_hierarchy_uref_mangle(SCM mode);
 SCM g_rc_hierarchy_netname_mangle(SCM mode);
 SCM g_rc_hierarchy_netattrib_mangle(SCM mode);
@@ -30,8 +28,6 @@ SCM g_rc_hierarchy_uref_separator(SCM name);
 SCM g_rc_hierarchy_netattrib_order(SCM mode);
 SCM g_rc_hierarchy_netname_order(SCM mode);
 SCM g_rc_hierarchy_uref_order(SCM mode);
-SCM g_rc_unnamed_netname(SCM name);
-SCM g_rc_unnamed_busname(SCM name);
 /* g_register.c */
 void g_register_funcs(void);
 SCM g_quit(void);
@@ -42,6 +38,7 @@ void main_prog(void *closure, int argc, char *argv[]);
 int main(int argc, char *argv[]);
 /* i_vars.c */
 void i_vars_set(TOPLEVEL *pr_current);
+void i_vars_init_gnetlist_defaults (void);
 /* parsecmd.c */
 void usage(char *cmd);
 GSList *create_input_files_list(int argi, int argc, char *argv[]);

gnetlist/lib/system-gnetlistrc.in
@@ -17,24 +17,6 @@
 ;
 (gnetlist-version "@DATE_VERSION@")
 
-; net-naming-priority string
-;
-; Specifies which netname has priority if a net is found with two names.
-; If this variable is set to "netattrib" then any netname specified with the 
-; net= attribute is used as the netname.  Likewise if this variable is set to
-; "netname" then any netname conflict is resolved using the netname= value.
-;
-(net-naming-priority "netattrib")
-;(net-naming-priority "netname")
-
-; hierarchy-traversal string
-;
-; Decides if the hierarchy is traversed or not.  If this is disabled then
-; gnetlist will not go down searching for any underlying sources
-;
-(hierarchy-traversal "enabled")
-;(hierarchy-traversal "disabled")
-
 ; hierarchy-uref-mangle string
 ;
 ; Controls if uref names are mangled to make them uniq when traversing 
@@ -132,22 +114,6 @@
 ;
 (hierarchy-netattrib-order "append")
 ;(hierarchy-netattrib-order "prepend")
-
-; unnamed-netname string
-;
-; Specifies the default string which is placed in front of all nets that 
-; have not been explicitly named by the user (using the netname= attribute 
-; in the schematic).
-;
-(unnamed-netname "unnamed_net")
-
-; unnamed-busname string
-;
-; Specifies the default string which is placed in front of all buses that
-; have not been explicitly named by the user.
-; (This is not currently useful, since gnetlist does not netlist buses).
-;
-(unnamed-busname "unnamed_bus")
 
 ;
 ; End of mode related keywords

gnetlist/src/g_rc.c
@@ -70,28 +70,6 @@ SCM g_rc_gnetlist_version(SCM scm_version)
 }
 
 
-SCM g_rc_net_naming_priority(SCM mode)
-{
-  static const vstbl_entry mode_table[] = {
-    {NETATTRIB_ATTRIBUTE, "netattrib"},
-    {NETNAME_ATTRIBUTE, "netname"}
-  };
-
-  RETURN_G_RC_MODE("net-naming-priority", default_net_naming_priority,
-                   2);
-}
-
-SCM g_rc_hierarchy_traversal(SCM mode)
-{
-  static const vstbl_entry mode_table[] = {
-    {TRUE, "enabled"},
-    {FALSE, "disabled"}
-  };
-
-  RETURN_G_RC_MODE("hierarchy-traversal", default_hierarchy_traversal,
-                   2);
-}
-
 SCM g_rc_hierarchy_uref_mangle(SCM mode)
 {
   static const vstbl_entry mode_table[] = {
@@ -204,31 +182,6 @@ SCM g_rc_hierarchy_uref_order(SCM mode)
     RETURN_G_RC_MODE("hierarchy-uref-order",
 		     default_hierarchy_uref_order, 2);
 }
-
-SCM g_rc_unnamed_netname(SCM name)
-{
-  SCM_ASSERT (scm_is_string (name), name,
-              SCM_ARG1, "unamed-netname");
-
-  g_free(default_unnamed_netname);
-
-  default_unnamed_netname = g_strdup_scm_string (name);
-
-  return SCM_BOOL_T;
-}
-
-SCM g_rc_unnamed_busname(SCM name)
-{
-  SCM_ASSERT (scm_is_string (name), name,
-              SCM_ARG1, "unamed-busname");
-
-  g_free(default_unnamed_busname);
-
-  default_unnamed_busname = g_strdup_scm_string (name);
-
-  return SCM_BOOL_T;
-}
-
 
 /*************************** GUILE end done *********************************/
 

gnetlist/src/g_register.c
@@ -57,8 +57,6 @@ static struct gsubr_t gnetlist_funcs[] = {
   /* gnetlistrc functions */
   { "gnetlist-version",             1, 0, 0, g_rc_gnetlist_version },
     
-  { "net-naming-priority",          1, 0, 0, g_rc_net_naming_priority },
-  { "hierarchy-traversal",          1, 0, 0, g_rc_hierarchy_traversal },
   { "hierarchy-uref-mangle",        1, 0, 0, g_rc_hierarchy_uref_mangle },
   { "hierarchy-netname-mangle",     1, 0, 0, g_rc_hierarchy_netname_mangle },
   { "hierarchy-netattrib-mangle",   1, 0, 0, g_rc_hierarchy_netattrib_mangle },
@@ -68,8 +66,6 @@ static struct gsubr_t gnetlist_funcs[] = {
   { "hierarchy-netattrib-order",    1, 0, 0, g_rc_hierarchy_netattrib_order },
   { "hierarchy-netname-order",      1, 0, 0, g_rc_hierarchy_netname_order },
   { "hierarchy-uref-order",         1, 0, 0, g_rc_hierarchy_uref_order },
-  { "unnamed-netname",              1, 0, 0, g_rc_unnamed_netname },
-  { "unnamed-busname",              1, 0, 0, g_rc_unnamed_busname },
 
   /* netlist functions */
   { "gnetlist:get-packages",        1, 0, 0, g_get_packages },

gnetlist/src/gnetlist.c
@@ -196,6 +196,7 @@ void main_prog(void *closure, int argc, char *argv[])
 
     g_rc_parse (pr_current, argv[0], "gnetlistrc", rc_filename);
     /* immediately setup user params */
+    i_vars_init_gnetlist_defaults ();
     i_vars_set (pr_current);
 
     s_rename_init();

gnetlist/src/i_vars.c
@@ -38,8 +38,6 @@
 #define DEFAULT_HIERARCHY_NETNAME_SEPARATOR "/"
 #define DEFAULT_HIERARCHY_NETATTRIB_SEPARATOR "/"
 #define DEFAULT_HIERARCHY_UREF_SEPARATOR "/"
-#define DEFAULT_UNNAMED_NETNAME "unnamed_net"
-#define DEFAULT_UNNAMED_BUSNAME "unnamed_bus"
 
 #define INIT_STR(w, name, str) {                            \
     g_free((w)->name);                                      \
@@ -47,8 +45,6 @@
                           (default_ ## name) : (str));      \
 }
 
-int default_net_naming_priority = NETATTRIB_ATTRIBUTE;
-int default_hierarchy_traversal = TRUE;
 int default_hierarchy_uref_mangle = TRUE;
 int default_hierarchy_netname_mangle = TRUE;
 int default_hierarchy_netattrib_mangle = TRUE;
@@ -58,15 +54,11 @@ int default_hierarchy_uref_order = APPEND;
 char *default_hierarchy_netname_separator = NULL;
 char *default_hierarchy_netattrib_separator = NULL;
 char *default_hierarchy_uref_separator = NULL;
-char *default_unnamed_netname = NULL;
-char *default_unnamed_busname = NULL;
 
 void i_vars_set(TOPLEVEL * pr_current)
 {
     i_vars_libgeda_set(pr_current);
 
-    pr_current->net_naming_priority = default_net_naming_priority;
-    pr_current->hierarchy_traversal = default_hierarchy_traversal;
     pr_current->hierarchy_uref_mangle = default_hierarchy_uref_mangle;
     pr_current->hierarchy_netname_mangle =
 	default_hierarchy_netname_mangle;
@@ -97,14 +89,6 @@ void i_vars_set(TOPLEVEL * pr_current)
       default_hierarchy_uref_separator =
         g_strdup (DEFAULT_HIERARCHY_UREF_SEPARATOR);
     }
-    if (!default_unnamed_netname) {
-      default_unnamed_netname =
-        g_strdup (DEFAULT_UNNAMED_NETNAME);
-    }
-    if (!default_unnamed_busname) {
-      default_unnamed_busname =
-        g_strdup (DEFAULT_UNNAMED_BUSNAME);
-    }
 
     INIT_STR(pr_current, hierarchy_netname_separator,
              default_hierarchy_netname_separator);
@@ -112,8 +96,30 @@ void i_vars_set(TOPLEVEL * pr_current)
              default_hierarchy_netattrib_separator);
     INIT_STR(pr_current, hierarchy_uref_separator,
              default_hierarchy_uref_separator);
-
-    INIT_STR(pr_current, unnamed_netname, default_unnamed_netname);
-    INIT_STR(pr_current, unnamed_busname, default_unnamed_busname);
 }
 
+
+/*! \brief Setup gnetlist default configuration.
+ * \par Function Description
+ * Populate the default configuration context with compiled-in
+ * defaults.
+ */
+void
+i_vars_init_gnetlist_defaults(void)
+{
+  EdaConfig *cfg = eda_config_get_default_context ();
+
+  /* This is the default name used for nets for which the user has set
+   * no explicit name via the netname= or net= attributes. */
+  eda_config_set_string (cfg, "gnetlist", "default-net-name", "unnamed_net");
+
+  /* This is the default name used for buses for which the user has set
+   * no explicit name via the netname= or net= attributes. */
+  eda_config_set_string (cfg, "gnetlist", "default-bus-name", "unnamed_bus");
+
+  /* By default, hierarchy processing is enabled. */
+  eda_config_set_boolean (cfg, "gnetlist", "traverse-hierarchy", TRUE);
+
+  /* By default, net= attributes beat netname= attributes. */
+  eda_config_set_string (cfg, "gnetlist", "net-naming-priority", "net-attribute");
+}

commit 705ee43ad6dfe335ad63d0bf7e7af7b379c87f9d

gnetlist: Correct hierarchy traversal setting logic
gnetlist was always traversing hierarchy because of taking a
error-handling branch branch even when a valid config setting was in
place (bug introduced in 18f64a9810ec).

Reported-by: John Doty <jpd@noqsi.com>

gnetlist/src/s_traverse.c
@@ -164,7 +164,7 @@ s_traverse_sheet (TOPLEVEL * pr_current, const GList *obj_list, char *hierarchy_
 
   cfg = eda_config_get_context_for_file (NULL);
   is_hierarchy = eda_config_get_boolean (cfg, "gnetlist", "traverse-hierarchy", &err);
-  if (err == NULL) {
+  if (err != NULL) {
     is_hierarchy = TRUE;
     g_clear_error (&err);
   }