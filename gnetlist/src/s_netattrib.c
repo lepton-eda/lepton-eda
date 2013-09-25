f5cda460b09a7af60d4a00a469321c524816af3e
Peter TB Brett <peter@peter-b.co.uk>
2013-09-25 22:40:12 +0100

    gnetlist: Don't use gettext macros.
    Commit dcd9b1c6eb47 introduced a translatable string to gnetlist, but
    in the stable-1.8 branch gnetlist is not yet internationalised.

--- gnetlist/src/s_netattrib.c
+++ gnetlist/src/s_netattrib.c
@@ -72,7 +72,7 @@ s_netattrib_check_connected_string (const gchar *str)
   if (s_netattrib_connected_string_get_pinnum (str) == NULL) return;
 
   fprintf (stderr,
-           _("ERROR: `%s' is reserved for internal use."), PIN_NET_PREFIX);
+           "ERROR: `%s' is reserved for internal use.", PIN_NET_PREFIX);
   exit (1); /*! \bug Use appropriate exit code */
 }
 
