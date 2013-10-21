lepton-eda/lepton-eda

commit fb958cc4f6aec2b2a3b13a72f78d3dff2ae6ada9

gschem: Add scrollbar support to GschemPageView

gschem/src/x_event.c
@@ -1086,21 +1086,18 @@ void x_manual_resize(GschemToplevel *w_current)
  *  \par Function Description
  *
  */
-void x_event_hschanged (GtkAdjustment *adj, GschemToplevel *w_current)
+void x_event_hschanged (GtkAdjustment *hadjustment, GschemToplevel *w_current)
 {
   TOPLEVEL *toplevel = gschem_toplevel_get_toplevel (w_current);
   int current_left;
   int new_left;
-  GtkAdjustment        *hadjustment;
 
+  g_return_if_fail (hadjustment != NULL);
   g_return_if_fail (w_current != NULL);
 
   if (w_current->scrollbars_flag == FALSE) {
     return;
   }
-
-  hadjustment =
-  gtk_range_get_adjustment(GTK_RANGE(w_current->h_scrollbar));
 
   current_left = toplevel->page_current->left;
   new_left = (int) hadjustment->value;
@@ -1118,21 +1115,18 @@ void x_event_hschanged (GtkAdjustment *adj, GschemToplevel *w_current)
  *  \par Function Description
  *
  */
-void x_event_vschanged (GtkAdjustment *adj, GschemToplevel *w_current)
+void x_event_vschanged (GtkAdjustment *vadjustment, GschemToplevel *w_current)
 {
   TOPLEVEL *toplevel = gschem_toplevel_get_toplevel (w_current);
   int current_bottom;
   int new_bottom;
-  GtkAdjustment        *vadjustment;
 
   g_return_if_fail (w_current != NULL);
+  g_return_if_fail (vadjustment != NULL);
 
   if (w_current->scrollbars_flag == FALSE) {
     return;
   }
-
-  vadjustment = gtk_range_get_adjustment(
-                                         GTK_RANGE(w_current->v_scrollbar));
 
   current_bottom = toplevel->page_current->bottom;
   new_bottom = toplevel->init_bottom - (int) vadjustment->value;

gschem/src/x_window.c
@@ -95,7 +95,7 @@ void x_window_free_gc(GschemToplevel *w_current)
  *  \par Function Description
  *
  */
-void x_window_create_drawing(GtkWidget *drawbox, GschemToplevel *w_current)
+void x_window_create_drawing(GtkWidget *scrolled, GschemToplevel *w_current)
 {
   /* drawing next */
   w_current->drawing_area = GTK_WIDGET (gschem_page_view_new ());
@@ -106,16 +106,15 @@ void x_window_create_drawing(GtkWidget *drawbox, GschemToplevel *w_current)
    * 1.3333333 is the desired aspect ratio!
    */
 
-  gtk_drawing_area_size (GTK_DRAWING_AREA (w_current->drawing_area),
-                         w_current->win_width,
-                         w_current->win_height);
+  gtk_widget_set_size_request (w_current->drawing_area,
+                               w_current->win_width,
+                               w_current->win_height);
 
-  gtk_box_pack_start (GTK_BOX (drawbox), w_current->drawing_area,
-                      TRUE, TRUE, 0);
+  gtk_container_add(GTK_CONTAINER(scrolled), w_current->drawing_area);
+
   GTK_WIDGET_SET_FLAGS (w_current->drawing_area, GTK_CAN_FOCUS );
   gtk_widget_grab_focus (w_current->drawing_area);
   gtk_widget_show (w_current->drawing_area);
-
 }
 
 /*! \brief Set up callbacks for window events that affect drawing.
@@ -231,10 +230,10 @@ void x_window_create_main(GschemToplevel *w_current)
 {
   TOPLEVEL *toplevel = gschem_toplevel_get_toplevel (w_current);
 
+  GtkPolicyType policy;
   GtkWidget *label=NULL;
   GtkWidget *main_box=NULL;
   GtkWidget *menubar=NULL;
-  GtkWidget *drawbox=NULL;
   GtkWidget *bottom_box=NULL;
   GtkWidget *toolbar=NULL;
   GtkWidget *handlebox=NULL;
@@ -395,48 +394,46 @@ void x_window_create_main(GschemToplevel *w_current)
   /*  Try to create popup menu (appears in right mouse button  */
   w_current->popup_menu = (GtkWidget *) get_main_popup(w_current);
 
-  drawbox = gtk_hbox_new(FALSE, 0);
-  gtk_container_border_width(GTK_CONTAINER(drawbox), 0);
-  gtk_container_add(GTK_CONTAINER(main_box), drawbox);
 
-  x_window_create_drawing(drawbox, w_current);
+  /* Setup a GtkScrolledWindow for the drawig area */
+  w_current->h_adjustment = GTK_ADJUSTMENT (gtk_adjustment_new (0.0,
+                                                                0.0,
+                                                                toplevel->init_right,
+                                                                100.0,
+                                                                100.0,
+                                                                10.0));
+
+  w_current->v_adjustment = GTK_ADJUSTMENT (gtk_adjustment_new (toplevel->init_bottom,
+                                                                0.0,
+                                                                toplevel->init_bottom,
+                                                                100.0,
+                                                                100.0,
+                                                                10.0));
+
+  w_current->scrolled = gtk_scrolled_window_new (w_current->h_adjustment,
+                                                 w_current->v_adjustment);
+  gtk_container_add(GTK_CONTAINER(main_box), w_current->scrolled);
+  x_window_create_drawing(w_current->scrolled, w_current);
   x_window_setup_draw_events(w_current);
 
-  if (w_current->scrollbars_flag == TRUE) {
-    /* setup scroll bars */
-    w_current->v_adjustment = GTK_ADJUSTMENT (
-      gtk_adjustment_new (toplevel->init_bottom, 0.0, toplevel->init_bottom,
-                          100.0, 100.0, 10.0));
+  policy = (w_current->scrollbars_flag) ? GTK_POLICY_ALWAYS : GTK_POLICY_NEVER;
+  gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (w_current->scrolled), policy, policy);
 
-    w_current->v_scrollbar = gtk_vscrollbar_new (w_current->v_adjustment);
+  w_current->h_scrollbar = gtk_scrolled_window_get_hscrollbar (GTK_SCROLLED_WINDOW (w_current->scrolled));
+  gtk_range_set_update_policy (GTK_RANGE (w_current->h_scrollbar), GTK_UPDATE_CONTINUOUS);
 
-    gtk_range_set_update_policy (GTK_RANGE (w_current->v_scrollbar),
-                                 GTK_UPDATE_CONTINUOUS);
+  w_current->v_scrollbar = gtk_scrolled_window_get_vscrollbar (GTK_SCROLLED_WINDOW (w_current->scrolled));
+  gtk_range_set_update_policy (GTK_RANGE (w_current->v_scrollbar), GTK_UPDATE_CONTINUOUS);
 
-    gtk_box_pack_start (GTK_BOX (drawbox), w_current->v_scrollbar,
-                        FALSE, FALSE, 0);
+  g_signal_connect (w_current->h_adjustment,
+                    "value_changed",
+                    G_CALLBACK (x_event_hschanged),
+                    w_current);
 
-    g_signal_connect (w_current->v_adjustment,
-                      "value_changed",
-                      G_CALLBACK (x_event_vschanged),
-                      w_current);
-
-    w_current->h_adjustment = GTK_ADJUSTMENT (
-      gtk_adjustment_new (0.0, 0.0, toplevel->init_right, 100.0, 100.0, 10.0));
-
-    w_current->h_scrollbar = gtk_hscrollbar_new (w_current->h_adjustment);
-
-    gtk_range_set_update_policy (GTK_RANGE (w_current->h_scrollbar),
-                                 GTK_UPDATE_CONTINUOUS);
-
-    gtk_box_pack_start (GTK_BOX (main_box), w_current->h_scrollbar,
-                        FALSE, FALSE, 0);
-
-    g_signal_connect (w_current->h_adjustment,
-                      "value_changed",
-                      G_CALLBACK (x_event_hschanged),
-                      w_current);
-  }
+  g_signal_connect (w_current->v_adjustment,
+                    "value_changed",
+                    G_CALLBACK (x_event_vschanged),
+                    w_current);
 
   /* macro box */
   w_current->macro_entry = gtk_entry_new();

gschem/include/gschem_page_view.h
@@ -41,17 +41,34 @@ struct _GschemPageView
 {
   GtkWindow parent;
 
+  GtkAdjustment *hadjustment;
+  GtkAdjustment *vadjustment;
+
   PAGE *page;
 };
+
+
+
+GtkAdjustment*
+gschem_page_view_get_hadjustment (GschemPageView *view);
 
 PAGE*
 gschem_page_view_get_page (GschemPageView *view);
 
 GType
-gschem_page_view_get_type();
+gschem_page_view_get_type ();
+
+GtkAdjustment*
+gschem_page_view_get_vadjustment (GschemPageView *view);
 
 GschemPageView*
 gschem_page_view_new ();
 
 void
+gschem_page_view_set_hadjustment (GschemPageView *view, GtkAdjustment *hadjustment);
+
+void
 gschem_page_view_set_page (GschemPageView *view, PAGE *page);
+
+void
+gschem_page_view_set_vadjustment (GschemPageView *view, GtkAdjustment *vadjustment);

gschem/include/gschem_toplevel.h
@@ -35,6 +35,7 @@ struct st_gschem_toplevel {
   GtkWidget *main_window;
 
   GtkWidget *drawing_area;
+  GtkWidget *scrolled;
   GtkWidget *menubar;
   GtkWidget *popup_menu;
 

gschem/src/gschem_page_view.c
@@ -36,12 +36,14 @@
 #include "gschem.h"
 #include <gdk/gdkkeysyms.h>
 
-
+#include "gtk/gtkmarshal.h"
 
 enum
 {
   PROP_0,
-  PROP_PAGE
+  PROP_HADJUSTMENT,
+  PROP_PAGE,
+  PROP_VADJUSTMENT
 };
 
 
@@ -58,6 +60,52 @@ gschem_page_view_init (GschemPageView *view);
 static void
 set_property (GObject *object, guint param_id, const GValue *value, GParamSpec *pspec);
 
+static void
+set_scroll_adjustments (GschemPageView *view, GtkAdjustment *hadjustment, GtkAdjustment *vadjustment);
+
+
+
+/*
+ *  In later versions of GTK+, the GtkScrolledWindow uses an interface, instead
+ *  of signals, to set the scrollbar adjustments. When Gschem uses on of these
+ *  more recent version of GTK+, this function will no longer be needed.
+ */
+static void
+cclosure_marshal_VOID__OBJECT_OBJECT (GClosure     *closure,
+                                      GValue       *return_value G_GNUC_UNUSED,
+                                      guint         n_param_values,
+                                      const GValue *param_values,
+                                      gpointer      invocation_hint G_GNUC_UNUSED,
+                                      gpointer      marshal_data)
+{
+  typedef void (*GMarshalFunc_VOID__OBJECT_OBJECT) (gpointer     data1,
+                                                    gpointer     arg_1,
+                                                    gpointer     arg_2,
+                                                    gpointer     data2);
+
+  register GMarshalFunc_VOID__OBJECT_OBJECT callback;
+  register GCClosure *cc = (GCClosure*) closure;
+  register gpointer data1, data2;
+
+  g_return_if_fail (n_param_values == 3);
+
+  if (G_CCLOSURE_SWAP_DATA (closure)) {
+    data1 = closure->data;
+    data2 = g_value_peek_pointer (param_values + 0);
+  }
+  else {
+    data1 = g_value_peek_pointer (param_values + 0);
+    data2 = closure->data;
+  }
+
+  callback = (GMarshalFunc_VOID__OBJECT_OBJECT) (marshal_data ? marshal_data : cc->callback);
+
+  callback (data1,
+            g_value_get_object (param_values + 1),
+            g_value_get_object (param_values + 2),
+            data2);
+}
+
 
 
 /*! \brief Get a property
@@ -73,8 +121,16 @@ get_property (GObject *object, guint param_id, GValue *value, GParamSpec *pspec)
   GschemPageView *view = GSCHEM_PAGE_VIEW (object);
 
   switch (param_id) {
+    case PROP_HADJUSTMENT:
+      g_value_set_object (value, gschem_page_view_get_hadjustment (view));
+      break;
+
     case PROP_PAGE:
       g_value_set_pointer (value, gschem_page_view_get_page (view));
+      break;
+
+    case PROP_VADJUSTMENT:
+      g_value_set_object (value, gschem_page_view_get_vadjustment (view));
       break;
 
     default:
@@ -95,11 +151,55 @@ gschem_page_view_class_init (GschemPageViewClass *klasse)
   G_OBJECT_CLASS (klasse)->set_property = set_property;
 
   g_object_class_install_property (G_OBJECT_CLASS (klasse),
+                                   PROP_HADJUSTMENT,
+                                   g_param_spec_object ("hadjustment",
+                                                        "Horizontal adjustment",
+                                                        "Horizontal adjustment",
+                                                        GTK_TYPE_ADJUSTMENT,
+                                                        G_PARAM_READWRITE | G_PARAM_CONSTRUCT));
+
+  g_object_class_install_property (G_OBJECT_CLASS (klasse),
                                    PROP_PAGE,
                                    g_param_spec_pointer ("page",
                                                          "Page",
                                                          "Page",
                                                          G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
+
+  g_object_class_install_property (G_OBJECT_CLASS (klasse),
+                                   PROP_VADJUSTMENT,
+                                   g_param_spec_object ("vadjustment",
+                                                        "Vertical adjustment",
+                                                        "Vertical adjustment",
+                                                        GTK_TYPE_ADJUSTMENT,
+                                                        G_PARAM_READWRITE | G_PARAM_CONSTRUCT));
+
+  GTK_WIDGET_CLASS (klasse)->set_scroll_adjustments_signal = g_signal_new (
+    "set-scroll-adjustments",
+    G_OBJECT_CLASS_TYPE (klasse),
+    G_SIGNAL_RUN_LAST | G_SIGNAL_ACTION,
+    0,
+    NULL,
+    NULL,
+    cclosure_marshal_VOID__OBJECT_OBJECT,
+    G_TYPE_NONE,
+    2,
+    GTK_TYPE_ADJUSTMENT,
+    GTK_TYPE_ADJUSTMENT);
+}
+
+
+
+/*! \brief Get the horizontal adjustment for this view
+ *
+ *  \param [in] view The view
+ *  \return The horizontal adjustment for this view
+ */
+GtkAdjustment*
+gschem_page_view_get_hadjustment (GschemPageView *view)
+{
+  g_return_val_if_fail (view != NULL, NULL);
+
+  return view->hadjustment;
 }
 
 
@@ -147,6 +247,21 @@ gschem_page_view_get_type ()
 
 
 
+/*! \brief Get the vertical adjustment for this view
+ *
+ *  \param [in] view The view
+ *  \return The vertical adjustment for this view
+ */
+GtkAdjustment*
+gschem_page_view_get_vadjustment (GschemPageView *view)
+{
+  g_return_val_if_fail (view != NULL, NULL);
+
+  return view->vadjustment;
+}
+
+
+
 /*! \brief Initialize GschemPageView instance
  *
  *  \param [in,out] selection
@@ -157,6 +272,11 @@ gschem_page_view_init (GschemPageView *view)
   g_return_if_fail (view != NULL);
 
   view->page = NULL;
+
+  g_signal_connect (view,
+                    "set-scroll-adjustments",
+                    G_CALLBACK (set_scroll_adjustments),
+                    NULL);
 }
 
 
@@ -169,6 +289,31 @@ GschemPageView*
 gschem_page_view_new ()
 {
   return GSCHEM_PAGE_VIEW (g_object_new (GSCHEM_TYPE_PAGE_VIEW, NULL));
+}
+
+
+
+/*! \brief Set horizontal adjustment for this view
+ *
+ *  \param [in,out] view The view
+ *  \param [in]     hadjustment The horizontal adjustment
+ */
+void
+gschem_page_view_set_hadjustment (GschemPageView *view, GtkAdjustment *hadjustment)
+{
+  g_return_if_fail (view != NULL);
+
+  if (view->hadjustment != NULL) {
+    g_object_unref (view->hadjustment);
+  }
+
+  view->hadjustment = hadjustment;
+
+  if (view->hadjustment != NULL) {
+    g_object_ref (view->hadjustment);
+  }
+
+  g_object_notify (G_OBJECT (view), "hadjustment");
 }
 
 
@@ -209,11 +354,58 @@ set_property (GObject *object, guint param_id, const GValue *value, GParamSpec *
   GschemPageView *view = GSCHEM_PAGE_VIEW (object);
 
   switch (param_id) {
+    case PROP_HADJUSTMENT:
+      gschem_page_view_set_hadjustment (view, g_value_get_object (value));
+      break;
+
     case PROP_PAGE:
       gschem_page_view_set_page (view, g_value_get_pointer (value));
+      break;
+
+    case PROP_VADJUSTMENT:
+      gschem_page_view_set_vadjustment (view, g_value_get_object (value));
       break;
 
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, param_id, pspec);
   }
+}
+
+
+
+/*! \brief Set vertical adjustment for this view
+ *
+ *  \param [in,out] view The view
+ *  \param [in]     vadjustment The vertical adjustment
+ */
+void
+gschem_page_view_set_vadjustment (GschemPageView *view, GtkAdjustment *vadjustment)
+{
+  g_return_if_fail (view != NULL);
+
+  if (view->vadjustment != NULL) {
+    g_object_unref (view->vadjustment);
+  }
+
+  view->vadjustment = vadjustment;
+
+  if (view->vadjustment != NULL) {
+    g_object_ref (view->vadjustment);
+  }
+
+  g_object_notify (G_OBJECT (view), "vadjustment");
+}
+
+
+
+/*! \brief Signal handler for setting the scroll adjustments
+ *
+ *  Sent from the GtkScrolledWindow to set the adjustments for the
+ *  corresponding scroll bars.
+ */
+static void
+set_scroll_adjustments (GschemPageView *view, GtkAdjustment *hadjustment, GtkAdjustment *vadjustment)
+{
+  gschem_page_view_set_hadjustment (view, hadjustment);
+  gschem_page_view_set_vadjustment (view, vadjustment);
 }

gschem/src/gschem_toplevel.c
@@ -149,6 +149,7 @@ GschemToplevel *gschem_toplevel_new ()
   /* main window widgets */
   /* ------------------- */
   w_current->main_window  = NULL;
+  w_current->scrolled     = NULL;
   w_current->drawing_area = NULL;
   w_current->menubar      = NULL;
   w_current->popup_menu   = NULL;

commit b33561b36ea9e33217526d0141ed670e72617042

Fix zooming (broken in commit fb958cc4f6aec2b2a3b13a72f78d3dff2ae6ada9)
Attach our x_event_scroll() handler to the drawing area, not the main
window, so we receive the event before the GtkScrolledWindow container.

Futhermore, x_event_scroll() now returns TRUE, which stops further
processing of the event after our handler. This avoids the built-in
GtkScrolledWindow signal handler also trying to act on the events.

gschem/src/x_event.c
@@ -1377,7 +1377,8 @@ gint x_event_scroll (GtkWidget *widget, GdkEventScroll *event,
     o_undo_savestate(w_current, UNDO_VIEWPORT_ONLY);
   }
 
-  return 0;
+  /* Stop further processing of this signal */
+  return TRUE;
 }
 
 

gschem/src/x_window.c
@@ -141,10 +141,10 @@ void x_window_setup_draw_events(GschemToplevel *w_current)
     { "configure_event",      G_CALLBACK(x_event_configure)       },
     { "key_press_event",      G_CALLBACK(x_event_key)             },
     { "key_release_event",    G_CALLBACK(x_event_key)             },
+    { "scroll_event",         G_CALLBACK(x_event_scroll)          },
     { NULL,                   NULL                                } };
   struct event_reg_t main_window_events[] = {
     { "enter_notify_event",   G_CALLBACK(x_event_enter)           },
-    { "scroll_event",         G_CALLBACK(x_event_scroll)          },
     { NULL,                   NULL                                } };
   struct event_reg_t *tmp;
 