lepton-eda/lepton-eda

commit 6c371aab323c23acab89802a620832d45f9ec532

gschem: Add accessors to GschemPageGeometry

gschem/src/o_basic.c
@@ -64,18 +64,8 @@ void o_redraw_rects (GschemToplevel *w_current,
   g_return_if_fail (page != NULL);
   g_return_if_fail (geometry != NULL);
 
-  /* We need to transform the cairo context to world coordinates while
-   * we're drawing using the renderer. */
-  cairo_matrix_init (&render_mtx,
-                     (double) geometry->to_screen_x_constant,
-                     0,
-                     0,
-                     - (double) geometry->to_screen_y_constant,
-                     (- (double) geometry->world_left * geometry->to_screen_x_constant),
-                     ((double) geometry->to_screen_y_constant * geometry->world_top + geometry->screen_height));
-
   cairo_save (w_current->cr);
-  cairo_set_matrix (w_current->cr, &render_mtx);
+  cairo_set_matrix (w_current->cr, gschem_page_geometry_get_world_to_screen_matrix (geometry));
 
   for (i = 0; i < n_rectangles; i++) {
     x_repaint_background_region (w_current, rectangles[i].x, rectangles[i].y,

gschem/include/gschem_page_geometry.h
@@ -32,6 +32,11 @@ struct _GschemPageGeometry
   int screen_width;
   int screen_height;
 
+  int viewport_left;
+  int viewport_top;
+  int viewport_right;
+  int viewport_bottom;
+
   int world_left;
   int world_top;
   int world_right;
@@ -42,6 +47,10 @@ struct _GschemPageGeometry
 
   double to_world_x_constant;
   double to_world_y_constant;
+
+  gboolean world_to_screen_calculated;
+
+  cairo_matrix_t world_to_screen_matrix;
 };
 
 
@@ -52,8 +61,41 @@ gschem_page_geometry_copy (GschemPageGeometry *geometry);
 void
 gschem_page_geometry_free (GschemPageGeometry *geometry);
 
+int
+gschem_page_geometry_get_screen_height (GschemPageGeometry *geometry);
+
+int
+gschem_page_geometry_get_screen_width (GschemPageGeometry *geometry);
+
 GType
 gschem_page_geometry_get_type ();
+
+int
+gschem_page_geometry_get_viewport_bottom (GschemPageGeometry *geometry);
+
+int
+gschem_page_geometry_get_viewport_left (GschemPageGeometry *geometry);
+
+int
+gschem_page_geometry_get_viewport_right (GschemPageGeometry *geometry);
+
+int
+gschem_page_geometry_get_viewport_top (GschemPageGeometry *geometry);
+
+int
+gschem_page_geometry_get_world_bottom (GschemPageGeometry *geometry);
+
+int
+gschem_page_geometry_get_world_left (GschemPageGeometry *geometry);
+
+int
+gschem_page_geometry_get_world_right (GschemPageGeometry *geometry);
+
+cairo_matrix_t*
+gschem_page_geometry_get_world_to_screen_matrix (GschemPageGeometry *geometry);
+
+int
+gschem_page_geometry_get_world_top (GschemPageGeometry *geometry);
 
 int
 gschem_page_geometry_mil_x (GschemPageGeometry *geometry, int value);
@@ -64,6 +106,10 @@ gschem_page_geometry_mil_y (GschemPageGeometry *geometry, int value);
 GschemPageGeometry*
 gschem_page_geometry_new_with_values (int screen_width,
                                       int screen_height,
+                                      int viewport_left,
+                                      int viewport_top,
+                                      int viewport_right,
+                                      int viewport_bottom,
                                       int world_left,
                                       int world_top,
                                       int world_right,
@@ -79,7 +125,19 @@ void
 gschem_page_geometry_set_values (GschemPageGeometry *geometry,
                                  int screen_width,
                                  int screen_height,
-                                 int world_left,
-                                 int world_top,
-                                 int world_right,
-                                 int world_bottom);
+                                 int viewport_left,
+                                 int viewport_top,
+                                 int viewport_right,
+                                 int viewport_bottom);
+
+void
+gschem_page_geometry_set_viewport_bottom (GschemPageGeometry *geometry, int viewport_bottom);
+
+void
+gschem_page_geometry_set_viewport_left (GschemPageGeometry *geometry, int viewport_left);
+
+void
+gschem_page_geometry_set_viewport_right (GschemPageGeometry *geometry, int viewport_right);
+
+void
+gschem_page_geometry_set_viewport_top (GschemPageGeometry *geometry, int viewport_top);

gschem/src/gschem_page_geometry.c
@@ -38,6 +38,12 @@
 #include "gschem.h"
 
 
+
+static void
+update_constants (GschemPageGeometry *geometry);
+
+
+
 /*! \brief Copy a page geometry
  *
  *  \param [in] geometry The page geometry to copy
@@ -63,6 +69,40 @@ gschem_page_geometry_free (GschemPageGeometry *geometry)
 
 
 
+/*! \brief Get the screen height in pixels.
+ *
+ *  \param [in] geometry The GschemPageGeometry
+ *  \return The screen height in pixels.
+ */
+int
+gschem_page_geometry_get_screen_height (GschemPageGeometry *geometry)
+{
+  g_return_val_if_fail (geometry != NULL, 0);
+
+  return geometry->screen_height;
+}
+
+
+
+/*! \brief Get the screen width in pixels.
+ *
+ *  \param [in] geometry The GschemPageGeometry
+ *  \return The screen width in pixels.
+ */
+int
+gschem_page_geometry_get_screen_width (GschemPageGeometry *geometry)
+{
+  g_return_val_if_fail (geometry != NULL, 0);
+
+  return geometry->screen_width;
+}
+
+
+
+/*! \brief Get/register the GschemPageGeometry type.
+ *
+ *  \return The GschemPageGeometry type.
+ */
 GType
 gschem_page_geometry_get_type ()
 {
@@ -75,6 +115,153 @@ gschem_page_geometry_get_type ()
   }
 
   return type;
+}
+
+
+
+/*! \brief Get the bottom edge of the viewport in world coordinates.
+ *
+ *  \param [in] geometry The GschemPageGeometry
+ *  \return The bottom edge of the viewport in world coordinates.
+ */
+int
+gschem_page_geometry_get_viewport_bottom (GschemPageGeometry *geometry)
+{
+  g_return_val_if_fail (geometry != NULL, 0);
+
+  return geometry->viewport_bottom;
+}
+
+
+
+/*! \brief Get the left edge of the viewport in world coordinates.
+ *
+ *  \param [in] geometry The GschemPageGeometry
+ *  \return The left edge of the viewport in world coordinates.
+ */
+int
+gschem_page_geometry_get_viewport_left (GschemPageGeometry *geometry)
+{
+  g_return_val_if_fail (geometry != NULL, 0);
+
+  return geometry->viewport_left;
+}
+
+
+
+/*! \brief Get the right edge of the viewport in world coordinates.
+ *
+ *  \param [in] geometry The GschemPageGeometry
+ *  \return The right edge of the viewport in world coordinates.
+ */
+int
+gschem_page_geometry_get_viewport_right (GschemPageGeometry *geometry)
+{
+  g_return_val_if_fail (geometry != NULL, 0);
+
+  return geometry->viewport_right;
+}
+
+
+
+/*! \brief Get the top edge of the viewport in world coordinates.
+ *
+ *  \param [in] geometry The GschemPageGeometry
+ *  \return The top edge of the viewport in world coordinates.
+ */
+int
+gschem_page_geometry_get_viewport_top (GschemPageGeometry *geometry)
+{
+  g_return_val_if_fail (geometry != NULL, 0);
+
+  return geometry->viewport_top;
+}
+
+
+
+/*! \brief Get the top edge of the world in world coordinates.
+ *
+ *  \param [in] geometry The GschemPageGeometry
+ *  \return The top edge of the world in world coordinates.
+ */
+int
+gschem_page_geometry_get_world_bottom (GschemPageGeometry *geometry)
+{
+  g_return_val_if_fail (geometry != NULL, 0);
+
+  return geometry->world_top;
+}
+
+
+
+/*! \brief Get the top edge of the world in world coordinates.
+ *
+ *  \param [in] geometry The GschemPageGeometry
+ *  \return The top edge of the world in world coordinates.
+ */
+int
+gschem_page_geometry_get_world_left (GschemPageGeometry *geometry)
+{
+  g_return_val_if_fail (geometry != NULL, 0);
+
+  return geometry->world_top;
+}
+
+
+
+/*! \brief Get the top edge of the world in world coordinates.
+ *
+ *  \param [in] geometry The GschemPageGeometry
+ *  \return The top edge of the world in world coordinates.
+ */
+int
+gschem_page_geometry_get_world_right (GschemPageGeometry *geometry)
+{
+  g_return_val_if_fail (geometry != NULL, 0);
+
+  return geometry->world_top;
+}
+
+
+
+/*! \brief Get the world to screen transformation matrix.
+ *
+ *  \param [in] geometry The GschemPageGeometry
+ *  \return The world to screen transformation matrix.
+ */
+cairo_matrix_t*
+gschem_page_geometry_get_world_to_screen_matrix (GschemPageGeometry *geometry)
+{
+  g_return_val_if_fail (geometry != NULL, NULL);
+
+  if (!geometry->world_to_screen_calculated) {
+    cairo_matrix_init (&(geometry->world_to_screen_matrix),
+                       (double) geometry->to_screen_x_constant,
+                       0,
+                       0,
+                       - (double) geometry->to_screen_y_constant,
+                       (- (double) geometry->viewport_left * geometry->to_screen_x_constant),
+                       ((double) geometry->to_screen_y_constant * geometry->viewport_top + geometry->screen_height));
+
+    geometry->world_to_screen_calculated = TRUE;
+  }
+
+  return &(geometry->world_to_screen_matrix);
+}
+
+
+
+/*! \brief Get the top edge of the world in world coordinates.
+ *
+ *  \param [in] geometry The GschemPageGeometry
+ *  \return The top edge of the world in world coordinates.
+ */
+int
+gschem_page_geometry_get_world_top (GschemPageGeometry *geometry)
+{
+  g_return_val_if_fail (geometry != NULL, 0);
+
+  return geometry->world_top;
 }
 
 
@@ -97,7 +284,7 @@ gschem_page_geometry_mil_x (GschemPageGeometry *geometry, int value)
   g_return_val_if_fail (geometry != NULL, 0);
 
   fval = value;
-  i = fval * geometry->to_world_x_constant + geometry->world_left;
+  i = fval * geometry->to_world_x_constant + geometry->viewport_left;
 
 #ifdef HAS_RINT
   j = rint(i);
@@ -126,7 +313,7 @@ gschem_page_geometry_mil_y(GschemPageGeometry *geometry, int value)
   g_return_val_if_fail (geometry != NULL, 0);
 
   fval = geometry->screen_height - value;
-  i = fval * geometry->to_world_y_constant + geometry->world_top;
+  i = fval * geometry->to_world_y_constant + geometry->viewport_top;
 
 #ifdef HAS_RINT
   j = rint(i);
@@ -147,6 +334,10 @@ gschem_page_geometry_mil_y(GschemPageGeometry *geometry, int value)
 GschemPageGeometry*
 gschem_page_geometry_new_with_values (int screen_width,
                                       int screen_height,
+                                      int viewport_left,
+                                      int viewport_top,
+                                      int viewport_right,
+                                      int viewport_bottom,
                                       int world_left,
                                       int world_top,
                                       int world_right,
@@ -157,10 +348,15 @@ gschem_page_geometry_new_with_values (int screen_width,
   gschem_page_geometry_set_values (geometry,
                                    screen_width,
                                    screen_height,
-                                   world_left,
-                                   world_top,
-                                   world_right,
-                                   world_bottom);
+                                   viewport_left,
+                                   viewport_top,
+                                   viewport_right,
+                                   viewport_bottom);
+
+  geometry->world_left   = world_left;
+  geometry->world_top    = world_top;
+  geometry->world_right  = world_right;
+  geometry->world_bottom = world_bottom;
 
   return geometry;
 }
@@ -181,7 +377,7 @@ gschem_page_geometry_pix_x (GschemPageGeometry *geometry, int value)
 
   g_return_val_if_fail (geometry != NULL, 0);
 
-  i = geometry->to_screen_x_constant * (double)(value - geometry->world_left);
+  i = geometry->to_screen_x_constant * (double)(value - geometry->viewport_left);
 
 #ifdef HAS_RINT
   j = rint(i);
@@ -217,7 +413,7 @@ gschem_page_geometry_pix_y (GschemPageGeometry *geometry, int value)
 
   g_return_val_if_fail (geometry != NULL, 0);
 
-  i = geometry->screen_height - (geometry->to_screen_y_constant * (double)(value - geometry->world_top));
+  i = geometry->screen_height - (geometry->to_screen_y_constant * (double)(value - geometry->viewport_top));
 
 #ifdef HAS_RINT
   j = rint(i);
@@ -239,6 +435,42 @@ gschem_page_geometry_pix_y (GschemPageGeometry *geometry, int value)
 
 
 
+/*! \brief Set the screen height in pixels
+ *
+ *  \param [in,out] geometry The GschemPageGeometry
+ *  \param [in] screen_height The screen height in pixels.
+ */
+void
+gschem_page_geometry_set_screen_height (GschemPageGeometry *geometry, int screen_height)
+{
+  g_return_if_fail (geometry != NULL);
+
+  geometry->screen_height = screen_height;
+
+  update_constants (geometry);
+  geometry->world_to_screen_calculated = FALSE;
+}
+
+
+
+/*! \brief Set the screen width in pixels
+ *
+ *  \param [in,out] geometry The GschemPageGeometry
+ *  \param [in] screen_width The screen width in pixels.
+ */
+void
+gschem_page_geometry_set_screen_width (GschemPageGeometry *geometry, int screen_width)
+{
+  g_return_if_fail (geometry != NULL);
+
+  geometry->screen_width = screen_width;
+
+  update_constants (geometry);
+  geometry->world_to_screen_calculated = FALSE;
+}
+
+
+
 /*! \brief Get page geometry for this view
  *
  *  \param [in] view The view
@@ -248,36 +480,121 @@ void
 gschem_page_geometry_set_values (GschemPageGeometry *geometry,
                                  int screen_width,
                                  int screen_height,
-                                 int world_left,
-                                 int world_top,
-                                 int world_right,
-                                 int world_bottom)
+                                 int viewport_left,
+                                 int viewport_top,
+                                 int viewport_right,
+                                 int viewport_bottom)
 {
   g_return_if_fail (geometry != NULL);
   g_return_if_fail (screen_width > 0);
   g_return_if_fail (screen_height > 0);
-  g_return_if_fail (world_left != world_right);
-  g_return_if_fail (world_top != world_bottom);
+  g_return_if_fail (viewport_left != viewport_right);
+  g_return_if_fail (viewport_top != viewport_bottom);
 
   geometry->screen_width  = screen_width;
   geometry->screen_height = screen_height;
 
-  geometry->world_left   = MIN (world_left, world_right);
-  geometry->world_top    = MIN (world_top, world_bottom);
-  geometry->world_right  = MAX (world_left, world_right);
-  geometry->world_bottom = MAX (world_top, world_bottom);
+  geometry->viewport_left   = MIN (viewport_left, viewport_right);
+  geometry->viewport_top    = MIN (viewport_top, viewport_bottom);
+  geometry->viewport_right  = MAX (viewport_left, viewport_right);
+  geometry->viewport_bottom = MAX (viewport_top, viewport_bottom);
 
+  update_constants (geometry);
+  geometry->world_to_screen_calculated = FALSE;
+}
+
+
+
+/*! \brief Set the bottom edge of the viewport in world coordinates
+ *
+ *  \param [in,out] geometry The GschemPageGeometry
+ *  \param [in] viewport_bottom The bottom edge of the viewport in world coordinates.
+ */
+void
+gschem_page_geometry_set_viewport_bottom (GschemPageGeometry *geometry, int viewport_bottom)
+{
+  g_return_if_fail (geometry != NULL);
+
+  geometry->viewport_bottom = viewport_bottom;
+
+  update_constants (geometry);
+  geometry->world_to_screen_calculated = FALSE;
+}
+
+
+
+/*! \brief Set the left edge of the viewport in world coordinates
+ *
+ *  \param [in,out] geometry The GschemPageGeometry
+ *  \param [in] viewport_left The left edge of the viewport in world coordinates.
+ */
+void
+gschem_page_geometry_set_viewport_left (GschemPageGeometry *geometry, int viewport_left)
+{
+  g_return_if_fail (geometry != NULL);
+
+  geometry->viewport_left = viewport_left;
+
+  update_constants (geometry);
+  geometry->world_to_screen_calculated = FALSE;
+}
+
+
+
+/*! \brief Set the right edge of the viewport in world coordinates
+ *
+ *  \param [in,out] geometry The GschemPageGeometry
+ *  \param [in] viewport_right The right edge of the viewport in world coordinates.
+ */
+void
+gschem_page_geometry_set_viewport_right (GschemPageGeometry *geometry, int viewport_right)
+{
+  g_return_if_fail (geometry != NULL);
+
+  geometry->viewport_right = viewport_right;
+
+  update_constants (geometry);
+  geometry->world_to_screen_calculated = FALSE;
+}
+
+
+
+/*! \brief Set the top edge of the viewport in world coordinates
+ *
+ *  \param [in,out] geometry The GschemPageGeometry
+ *  \param [in] viewport_top The top edge of the viewport in world coordinates.
+ */
+void
+gschem_page_geometry_set_viewport_top (GschemPageGeometry *geometry, int viewport_top)
+{
+  g_return_if_fail (geometry != NULL);
+
+  geometry->viewport_top = viewport_top;
+
+  update_constants (geometry);
+  geometry->world_to_screen_calculated = FALSE;
+}
+
+
+
+/*! \brief Update the constants (coefficients) for calculations
+ *
+ *  \param [in,out] geometry The GschemPageGeometry
+ */
+static void
+update_constants (GschemPageGeometry *geometry)
+{
   /* now do the constant setups */
 
   /* pix_x */
-  geometry->to_screen_x_constant = (double)geometry->screen_width / (double)(geometry->world_right - geometry->world_left);
+  geometry->to_screen_x_constant = (double)geometry->screen_width / (double)(geometry->viewport_right - geometry->viewport_left);
 
   /* pix_y */
-  geometry->to_screen_y_constant = (double)geometry->screen_height / (double)(geometry->world_bottom - geometry->world_top);
+  geometry->to_screen_y_constant = (double)geometry->screen_height / (double)(geometry->viewport_bottom - geometry->viewport_top);
 
   /* mil_x */
-  geometry->to_world_x_constant = (double)(geometry->world_right - geometry->world_left) / (double)geometry->screen_width;
+  geometry->to_world_x_constant = (double)(geometry->viewport_right - geometry->viewport_left) / (double)geometry->screen_width;
 
   /* mil_y */
-  geometry->to_world_y_constant = (double)(geometry->world_bottom - geometry->world_top) / (double)geometry->screen_height;
+  geometry->to_world_y_constant = (double)(geometry->viewport_bottom - geometry->viewport_top) / (double)geometry->screen_height;
 }

gschem/src/gschem_page_view.c
@@ -362,7 +362,11 @@ gschem_page_view_get_page_geometry (GschemPageView *view)
                                                      page->left,
                                                      page->top,
                                                      page->right,
-                                                     page->bottom);
+                                                     page->bottom,
+                                                     view->toplevel->init_left,
+                                                     view->toplevel->init_top,
+                                                     view->toplevel->init_right,
+                                                     view->toplevel->init_bottom);
 
     g_hash_table_insert (view->geometry_table, page, geometry);
     s_page_weak_ref (page, (NotifyFunction) page_deleted, view);

gschem/src/x_image.c
@@ -668,7 +668,11 @@ GdkPixbuf *x_image_get_pixbuf (GschemToplevel *w_current)
                                                    toplevel.page_current->left,
                                                    toplevel.page_current->top,
                                                    toplevel.page_current->right,
-                                                   toplevel.page_current->bottom);
+                                                   toplevel.page_current->bottom,
+                                                   toplevel.init_left,
+                                                   toplevel.init_top,
+                                                   toplevel.init_right,
+                                                   toplevel.init_bottom);
 
   o_redraw_rects (&new_w_current, toplevel.page_current, geometry, &rect, 1);
 

commit 272be87fa27e1b9f306f615022a28cb987cd5fc7

gschem: Fix bug with drawing rubber during place/move/net/bus
During place, move, net, and bus operations, the rubber would not
be drawn.

Bug introduced in 6c371aab323c23acab89802a620832d45f9ec532

gschem/src/o_basic.c
@@ -56,7 +56,6 @@ void o_redraw_rects (GschemToplevel *w_current,
   int render_flags;
   GArray *render_color_map = NULL;
   GArray *render_outline_color_map = NULL;
-  cairo_matrix_t render_mtx;
 
   g_return_if_fail (w_current != NULL);
   g_return_if_fail (toplevel != NULL);
@@ -183,9 +182,8 @@ void o_redraw_rects (GschemToplevel *w_current,
       case MOVE:
       case ENDMOVE:
         if (w_current->last_drawb_mode != -1) {
-          /* FIXME shouldn't need to save/restore matrix/colormap here */
+          /* FIXME shouldn't need to save/restore colormap here */
           cairo_save (w_current->cr);
-          cairo_set_matrix (w_current->cr, &render_mtx);
           eda_renderer_set_color_map (renderer, render_outline_color_map);
 
           o_move_draw_rubber (w_current, renderer);
@@ -201,9 +199,8 @@ void o_redraw_rects (GschemToplevel *w_current,
       case ENDTEXT:
       case ENDPASTE:
         if (w_current->rubber_visible) {
-          /* FIXME shouldn't need to save/restore matrix/colormap here */
+          /* FIXME shouldn't need to save/restore colormap here */
           cairo_save (w_current->cr);
-          cairo_set_matrix (w_current->cr, &render_mtx);
           eda_renderer_set_color_map (renderer, render_outline_color_map);
 
           o_place_draw_rubber (w_current, renderer);
@@ -217,9 +214,8 @@ void o_redraw_rects (GschemToplevel *w_current,
       case DRAWNET:
       case NETCONT:
         if (w_current->rubber_visible) {
-          /* FIXME shouldn't need to save/restore matrix/colormap here */
+          /* FIXME shouldn't need to save/restore colormap here */
           cairo_save (w_current->cr);
-          cairo_set_matrix (w_current->cr, &render_mtx);
           eda_renderer_set_color_map (renderer, render_outline_color_map);
 
           o_net_draw_rubber (w_current, renderer);
@@ -233,9 +229,8 @@ void o_redraw_rects (GschemToplevel *w_current,
       case DRAWBUS:
       case BUSCONT:
         if (w_current->rubber_visible) {
-          /* FIXME shouldn't need to save/restore matrix/colormap here */
+          /* FIXME shouldn't need to save/restore colormap here */
           cairo_save (w_current->cr);
-          cairo_set_matrix (w_current->cr, &render_mtx);
           eda_renderer_set_color_map (renderer, render_outline_color_map);
 
           o_bus_draw_rubber(w_current, renderer);