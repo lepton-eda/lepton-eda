c123c76289d33f77885af345285a7140cb2c0d8d
Vladimir Zhbanov <vzhbanov@gmail.com>
2014-03-17 20:50:51 +0400

    gnetlist: simplified the cascade backend source code

--- gnetlist/scheme/gnet-cascade.scm
+++ gnetlist/scheme/gnet-cascade.scm
@@ -21,26 +21,26 @@
 
 ;; Locate and print out the global defaults if the element exists
 (define cascade:write-defaults-top
-  (lambda (port pkgs)
+  (lambda (pkgs)
     (if (not (null? pkgs))
         (let ( (pkg (car pkgs)) )
           (if (string=? (get-device pkg) "cascade-defaults-top")
               (begin
-                (display "# Initial global defaults\n" port)
-                (display "defaults " port)
+                (display "# Initial global defaults\n")
+                (display "defaults ")
                 (map (lambda (attrib)
                        (let ((val (gnetlist:get-package-attribute pkg attrib)))
                          (if (not (string=? val "unknown"))
-                             (display (string-append attrib "=" val " ") port)
+                             (display (string-append attrib "=" val " "))
                              )
                          )
                        )
                      (list "rin" "RIN" "rout" "ROUT" "rho" "RHO")
                      )
-                (newline port)
-                (newline port)
+                (newline)
+                (newline)
                 )
-              (cascade:write-defaults-top port (cdr pkgs))
+              (cascade:write-defaults-top (cdr pkgs))
               )
           )
         )
@@ -50,7 +50,7 @@
 ;; Locate and print out the "source" line and return the refdes of
 ;; the first element in the cascade
 (define cascade:write-source
-  (lambda (port pkgs)
+  (lambda (pkgs)
     (if (not (null? pkgs))
         (let ( (package (car pkgs) )
                (sourcenet #f)
@@ -58,24 +58,24 @@
           (if (string=? (get-device package) "cascade-source")
               (begin
                 (set! sourcenet (gnetlist:get-nets package "1"))
-                (display "source " port)
+                (display "source ")
                 (map (lambda (attrib)
                        (let ((val (gnetlist:get-package-attribute package attrib)))
                          (if (not (string=? val "unknown"))
-                             (display (string-append attrib "=" val " ") port)
+                             (display (string-append attrib "=" val " "))
                              )
                          )
                        )
                      (list "c" "C" "cn0" "CN0" "cn" "CN" "bw" "BW")
                      )
-                (newline port)
+                (newline)
 
                 (if (string=? (caadr sourcenet) package)
                     (caaddr sourcenet)
                     (caadr sourcenet)
                     )
                 )
-              (cascade:write-source port (cdr pkgs) )
+              (cascade:write-source (cdr pkgs) )
               )
           )
         ;; the list of packages is now empty
@@ -87,7 +87,7 @@
 ;; recursively follow the cascade and print out each element as its
 ;; found
 (define cascade:follow-cascade
-  (lambda (port pkg)
+  (lambda (pkg)
     (if (not (null? pkg))
         (begin
           (let ( (outnet (gnetlist:get-nets pkg "2"))
@@ -98,8 +98,8 @@
             ;; instead of the reference designator because thats
             ;; a keyword for cascade.
             (if (string=? (get-device pkg) "cascade-defaults")
-                (display "defaults " port)
-                (display (string-append pkg " ") port)
+                (display "defaults ")
+                (display (string-append pkg " "))
                 )
 
             ;; spit out all the relevant attributes for element or
@@ -107,7 +107,7 @@
             (map (lambda (attrib)
                    (let ((val (gnetlist:get-package-attribute pkg attrib)))
                      (if (not (string=? val "unknown"))
-                         (display (string-append attrib "=" val " ") port)
+                         (display (string-append attrib "=" val " "))
                          )
                      )
                    )
@@ -115,15 +115,15 @@
                        "IIP3" "r" "R" "rin" "RIN" "rout" "ROUT"
                        "rho" "RHO")
                  )
-            (newline port)
+            (newline)
 
             ;;(display "cascade:follow-cascade  -- outnet = ")
             ;;(display outnet)
             ;;(newline)
             (if (>= (length outnet) 3)
                 (if (string=? (caadr outnet) pkg)
-                    (cascade:follow-cascade port(caaddr outnet))
-                    (cascade:follow-cascade port (caadr outnet))
+                    (cascade:follow-cascade(caaddr outnet))
+                    (cascade:follow-cascade (caadr outnet))
                     )
                 )
             )
@@ -142,37 +142,36 @@
 
      (message (string-append "Writing to output file \"" output-filename
                              "\"... ") )
-      (let ((port (gnetlist:output-port output-filename))
-            (first_block #f)
-            )
+     (set-current-output-port (gnetlist:output-port output-filename))
+
+      (let ((first_block #f))
 
         ;; write the header
-        (display "# Cascade (http://rfcascade.sourceforge.net)\n"
-                 port)
-        (display "# Created with gEDA/gnetlist\n\n" port)
+        (display "# Cascade (http://rfcascade.sourceforge.net)\n")
+        (display "# Created with gEDA/gnetlist\n\n")
 
         ;; Write out an initial "defaults" line if it exists
-        (cascade:write-defaults-top port packages)
+        (cascade:write-defaults-top packages)
 
         ;; Write out the "source" line and keep track of what its
         ;; connected to.  If we couldn't find the source, then
         ;; exit out.
-        (display "# Source definition\n" port)
-        (set! first_block (cascade:write-source port packages))
+        (display "# Source definition\n")
+        (set! first_block (cascade:write-source packages))
         (if (null? first_block)
             (error "You must include a source element in your schematic!")
             )
 
         ;; write the components
-        (display "\n# Cascaded system\n" port)
-        (cascade:follow-cascade port first_block)
+        (display "\n# Cascaded system\n")
+        (cascade:follow-cascade first_block)
 
         ;; write the footer
-        (newline port)
-        (display "# End of netlist created by gEDA/gnetlist\n\n" port)
+        (newline)
+        (display "# End of netlist created by gEDA/gnetlist\n\n")
 
         ;; close netlist
-        (close-output-port port)
+    (close-output-port (current-output-port))
 
         )
 
