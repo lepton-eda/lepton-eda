e04a516b7f91a972c0addc93efeeb18035d2b0de
Vladimir Zhbanov <vzhbanov@gmail.com>
2014-03-17 20:41:43 +0400

    gnetlist: simplified source code of the PCB backend

--- gnetlist/scheme/gnet-PCB.scm
+++ gnetlist/scheme/gnet-PCB.scm
@@ -38,16 +38,16 @@
 ;      "\n"))
 
 
-(define (PCB:write-net netnames port)
+(define (PCB:write-net netnames)
   (if (not (null? netnames))
       (let ((netname (car netnames)))
-        (display netname port)
-        (display "\t" port)
-        (display (gnetlist:wrap (PCB:display-connections (gnetlist:get-all-connections netname)) 200 " \\") port)
-        (PCB:write-net (cdr netnames) port))))
+	(display netname)
+	(display "\t")
+	(display (gnetlist:wrap (PCB:display-connections (gnetlist:get-all-connections netname)) 200 " \\"))
+	(PCB:write-net (cdr netnames)))))
 
 
 (define (PCB output-filename)
-  (let ((port (gnetlist:output-port output-filename)))
-    (PCB:write-net (gnetlist:get-all-unique-nets "dummy") port)
-    (close-output-port port)))
+  (set-current-output-port (gnetlist:output-port output-filename))
+  (PCB:write-net (gnetlist:get-all-unique-nets "dummy"))
+  (close-output-port (current-output-port)))
