c02cbd9208150a4ec68625e2e548e9c34ff71d84
Vladimir Zhbanov <vzhbanov@gmail.com>
2014-03-19 14:08:24 +0400

    gnetlist: simplified the ewnet backend source code

--- gnetlist/scheme/gnet-ewnet.scm
+++ gnetlist/scheme/gnet-ewnet.scm
@@ -80,48 +80,46 @@
 
 ;; write out the pins for a particular component
 (define ewnet:component_pins
-  (lambda (port package pins)
+  (lambda (package pins)
     (if (and (not (null? package)) (not (null? pins)))
         (begin
           (let (
                 (pin (car pins)))
 
             ;; pin number
-            (display "\t\t(pin \"" port)
+            (display "\t\t(pin \"")
             (display
-             (gnetlist:get-attribute-by-pinnumber package pin "pinnumber")
-             port)
-            (display "\"\n" port)
+             (gnetlist:get-attribute-by-pinnumber package pin "pinnumber"))
+            (display "\"\n")
 
             ;; net
-            (display "\t\t\t(net \"" port)
+            (display "\t\t\t(net \"")
             (display
-             (gnetlist:alias-net (car (gnetlist:get-nets package pin)))
-             port)
-            (display "\")\n" port)
+             (gnetlist:alias-net (car (gnetlist:get-nets package pin))))
+            (display "\")\n")
 
             ;; pin type.  I have seen "PWR", "GND", "IN", "OUT", "BIDIR"
-            (display "\t\t\t(pintype \"" port)
+            (display "\t\t\t(pintype \"")
             ;; FIXME -- need to translate between geda and the allowed
             ;; ewnet types here
-            (display "BIDIR" port)
-            (display "\")\n" port)
+            (display "BIDIR")
+            (display "\")\n")
 
-            (display "\t\t\t(gategroup \"\")\n" port)
-            (display "\t\t\t(pingroup \"\")\n" port)
+            (display "\t\t\t(gategroup \"\")\n")
+            (display "\t\t\t(pingroup \"\")\n")
 
             ;; label (pin name)
-            (display "\t\t\t(label \"" port)
-            (display pin port)
-            (display "\")\n" port)
+            (display "\t\t\t(label \"")
+            (display pin)
+            (display "\")\n")
 
             ;; gate
-            (display "\t\t\t(gate \"\")\n" port)
+            (display "\t\t\t(gate \"\")\n")
 
 
-            (display "\t\t)\n" port)
+            (display "\t\t)\n")
             )
-          (ewnet:component_pins port package (cdr pins))
+          (ewnet:component_pins package (cdr pins))
           )
         )
     )
@@ -130,7 +128,7 @@
 
 ;; write out the components
 (define ewnet:components
-   (lambda (port packages)
+   (lambda (packages)
       (if (not (null? packages))
          (begin
             (let (
@@ -144,23 +142,22 @@
                   (package (car packages)))
 
               ;; start the instance
-              (display "\t(instance \"" port)
+              (display "\t(instance \"")
 
               ;; write the footprint
               (display (gnetlist:get-package-attribute package
-                                                       "footprint")
-                       port)
-              (display "\" \"" port)
+                                                       "footprint"))
+              (display "\" \"")
 
               ;; write the reference designator
-              (display (gnetlist:alias-refdes package) port)
-              (display "\"\n" port)
+              (display (gnetlist:alias-refdes package))
+              (display "\"\n")
 
 
               ;; device
-              (display "\t\t(device \"" port)
-              (display device port)
-              (display "\")\n" port)
+              (display "\t\t(device \"")
+              (display device)
+              (display "\")\n")
 
               ;; If there is a "value" attribute, output that.
               ;; Otherwise output the "device" attribute (the symbol name).
@@ -169,27 +166,27 @@
                   )
 
               ;; value
-              (display "\t\t(value \"" port)
-              (display value port)
-              (display "\")\n" port)
+              (display "\t\t(value \"")
+              (display value)
+              (display "\")\n")
 
-              (display "\t\t(gateswap \"0\")\n" port)
-              (display "\t\t(pinswap \"0\")\n" port)
-              (display "\t\t(component_space \"0.00000000e+000\")\n" port)
-              (display "\t\t(component_group \"\")\n" port)
-              (display "\t\t(settings_locked \"0\")\n" port)
-              (display "\t\t(comp_variants \"Default1;\")\n" port)
-              (display "\t\t(comp_variant_independent \"0\")\n" port)
+              (display "\t\t(gateswap \"0\")\n")
+              (display "\t\t(pinswap \"0\")\n")
+              (display "\t\t(component_space \"0.00000000e+000\")\n")
+              (display "\t\t(component_group \"\")\n")
+              (display "\t\t(settings_locked \"0\")\n")
+              (display "\t\t(comp_variants \"Default1;\")\n")
+              (display "\t\t(comp_variant_independent \"0\")\n")
 
               ;; write the pins
-              (ewnet:component_pins port package
+              (ewnet:component_pins package
                                          (gnetlist:get-pins package))
 
               ;; close the part
-              (display "\t)\n" port)
+              (display "\t)\n")
 
               )
-            (ewnet:components port (cdr packages) )
+            (ewnet:components (cdr packages) )
             )
          )
       )
@@ -197,31 +194,31 @@
 
 ;; write out the nets
 (define ewnet:write-net
-   (lambda (port netnames)
+   (lambda (netnames)
       (if (not (null? netnames))
          (let (
                (netname (car netnames))
                (alias (gnetlist:alias-net (car netnames)))
                )
-           (display "\t( net \"" port)
-           (display alias port)
-           (display "\"\n" port)
+           (display "\t( net \"")
+           (display alias)
+           (display "\"\n")
 
-           (display "\t\t(trackwidth \"-1.00000000e+000\")\n" port)
-           (display "\t\t(trackwidth_max \"-1.00000000e+000\")\n" port)
-           (display "\t\t(trackwidth_min \"-1.00000000e+000\")\n" port)
-           (display "\t\t(tracklength_max \"-1.00000000e+000\")\n" port)
-           (display "\t\t(tracklength_min \"-1.00000000e+000\")\n" port)
-           (display "\t\t(clearance_to_trace \"-1.00000000e+000\")\n" port)
-           (display "\t\t(clearance_to_pad \"-1.00000000e+000\")\n" port)
-           (display "\t\t(clearance_to_via \"-1.00000000e+000\")\n" port)
-           (display "\t\t(clearance_to_copper \"-1.00000000e+000\")\n" port)
-           (display "\t\t(routing_layer \"\")\n" port)
-           (display "\t\t(settings_locked \"0\")\n" port)
-           (display "\t\t(net_group \"\")\n" port)
-           (display "\t)\n" port)
+           (display "\t\t(trackwidth \"-1.00000000e+000\")\n")
+           (display "\t\t(trackwidth_max \"-1.00000000e+000\")\n")
+           (display "\t\t(trackwidth_min \"-1.00000000e+000\")\n")
+           (display "\t\t(tracklength_max \"-1.00000000e+000\")\n")
+           (display "\t\t(tracklength_min \"-1.00000000e+000\")\n")
+           (display "\t\t(clearance_to_trace \"-1.00000000e+000\")\n")
+           (display "\t\t(clearance_to_pad \"-1.00000000e+000\")\n")
+           (display "\t\t(clearance_to_via \"-1.00000000e+000\")\n")
+           (display "\t\t(clearance_to_copper \"-1.00000000e+000\")\n")
+           (display "\t\t(routing_layer \"\")\n")
+           (display "\t\t(settings_locked \"0\")\n")
+           (display "\t\t(net_group \"\")\n")
+           (display "\t)\n")
 
-           (ewnet:write-net port (cdr netnames))
+           (ewnet:write-net (cdr netnames))
            )
          )
       )
@@ -229,18 +226,18 @@
 
 ;; write out the header
 (define ewnet:write-header
-  (lambda (port)
+  (lambda ()
 
-     (display "(ToolInfo\n" port)
-     (display "\t(netlist \"ULTIboard\" 7 0 0)\n" port)
-     (display "\t(tool \"Multisim\" 7 0 0)\n" port)
-     (display "\t(timestamp \"15:19:8\" \"9-6-2006\")\n" port)
-     (display "\t(version 3 0 0)\n" port)
-     (display "\t(gateswap 2)\n" port)
-     (display "\t(pinswap 1)\n" port)
-     (display "\t(crossprobe {D6EE9C01-C93E-4246-9BC1-214A35C4954C})\n" port)
-     (display "\t(units Mil)\n" port)
-     (display ")\n" port)
+     (display "(ToolInfo\n")
+     (display "\t(netlist \"ULTIboard\" 7 0 0)\n")
+     (display "\t(tool \"Multisim\" 7 0 0)\n")
+     (display "\t(timestamp \"15:19:8\" \"9-6-2006\")\n")
+     (display "\t(version 3 0 0)\n")
+     (display "\t(gateswap 2)\n")
+     (display "\t(pinswap 1)\n")
+     (display "\t(crossprobe {D6EE9C01-C93E-4246-9BC1-214A35C4954C})\n")
+     (display "\t(units Mil)\n")
+     (display ")\n")
      )
 )
 
@@ -249,16 +246,16 @@
 ;; FIXME -- can this just be left out?  If not, how shall we define
 ;; stackup in a way where gnetlist can find it?
 (define ewnet:layers
-  (lambda (port)
+  (lambda ()
 
-    (display "\t(layer \"Copper Bottom\"\n" port)
-    (display "\t\t(routable \"1\")\n" port)
-    (display "\t\t(type \"Signal\")\n" port)
-    (display "\t)\n" port)
-    (display "\t(layer \"Copper Top\"\n" port)
-    (display "\t\t(routable \"1\")\n" port)
-    (display "\t\t(type \"Signal\")\n" port)
-    (display "\t)\n" port)
+    (display "\t(layer \"Copper Bottom\"\n")
+    (display "\t\t(routable \"1\")\n")
+    (display "\t\t(type \"Signal\")\n")
+    (display "\t)\n")
+    (display "\t(layer \"Copper Top\"\n")
+    (display "\t\t(routable \"1\")\n")
+    (display "\t\t(type \"Signal\")\n")
+    (display "\t)\n")
     )
 )
 
@@ -274,9 +271,8 @@
     (message "windows based layout tools\n")
     (message "---------------------------------\n\n")
 
-    (let ((port (gnetlist:output-port output-filename))
-          (all-nets (gnetlist:get-all-unique-nets "dummy"))
-          )
+    (set-current-output-port (gnetlist:output-port output-filename))
+    (let ((all-nets (gnetlist:get-all-unique-nets "dummy")))
 
       ;; initialize the net-name aliasing
       (gnetlist:build-net-aliases ewnet:map-net-names all-unique-nets)
@@ -285,25 +281,25 @@
       (gnetlist:build-refdes-aliases ewnet:map-refdes packages)
 
       ;; write the header
-      (ewnet:write-header port)
+      (ewnet:write-header)
 
       ;; write the nets
-      (display "(nets\n" port)
-      (ewnet:write-net port all-nets)
-      (display ")\n" port)
+      (display "(nets\n")
+      (ewnet:write-net all-nets)
+      (display ")\n")
 
       ;; write the components
-      (display "(components\n" port)
-      (ewnet:components port packages)
-      (display ")\n" port)
+      (display "(components\n")
+      (ewnet:components packages)
+      (display ")\n")
 
       ;; write the board layers
-      (display "(layers\n" port)
-      (ewnet:layers port)
-      (display ")\n" port)
+      (display "(layers\n")
+      (ewnet:layers)
+      (display ")\n")
 
       ;; close netlist
-      (close-output-port port)
       )
+    (close-output-port (current-output-port))
     )
   )
