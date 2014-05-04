43c428fbca41ec2b0f17afeff0157cf274cd1b32
Roland Lutz <rlutz@hedmen.org>
2014-05-04 13:08:28 +0200

    extension: Put documentation placeholder file under the GPL

--- src/python/storage.py
+++ src/python/storage.py
@@ -1,11 +1,18 @@
 # Copyright (C) 2013, 2014 Roland Lutz
 #
-# Permission is granted to copy, distribute and/or modify this document
-# under the terms of the GNU Free Documentation License, Version 1.2 or
-# any later version published by the Free Software Foundation; with no
-# Invariant Sections, with no Front-Cover Texts, and with no Back-Cover
-# Texts.  A copy of the license is included in the ``GNU Free
-# Documentation License'' file as part of this distribution.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 ## \file storage.py
 #  \brief Placeholder file for xorn.storage documentation.
