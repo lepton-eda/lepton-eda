5cf9e2c96b923c4f1e29089c502a7de552b302a3
Riccardo Lucchese <riccardo.lucchese@gmail.com>
2014-09-18 13:06:20 +0200

    Remove deprecated use of GTK_WIDGET_VISIBLE()
    Verbatim from Gtk+ 2 Reference Manual: "GTK_WIDGET_VISIBLE has been
    deprecated since version 2.20 and should not be used in newly-written
    code. Use gtk_widget_get_visible() instead."

--- gattrib/src/gtksheet_2_2.c
+++ gattrib/src/gtksheet_2_2.c
@@ -1502,7 +1502,7 @@ gtk_sheet_hide_column_titles(GtkSheet *sheet)
  if(gtk_widget_get_realized(GTK_WIDGET(sheet))){
   if(sheet->column_title_window) 
     gdk_window_hide(sheet->column_title_window);
-  if(GTK_WIDGET_VISIBLE(sheet->button)) 
+  if(gtk_widget_get_visible(sheet->button)) 
     gtk_widget_hide(sheet->button);
 
   for(col = MIN_VISIBLE_COLUMN(sheet); col <= MAX_VISIBLE_COLUMN(sheet); col++){
@@ -1534,7 +1534,7 @@ gtk_sheet_hide_row_titles(GtkSheet *sheet)
  if(gtk_widget_get_realized(GTK_WIDGET(sheet))){
   if(sheet->row_title_window) 
     gdk_window_hide(sheet->row_title_window);
-  if(GTK_WIDGET_VISIBLE(sheet->button)) 
+  if(gtk_widget_get_visible(sheet->button)) 
     gtk_widget_hide(sheet->button);
   for(row = MIN_VISIBLE_ROW(sheet); row <= MAX_VISIBLE_ROW(sheet); row++){
     GtkSheetChild *child;
@@ -2848,14 +2848,14 @@ gtk_sheet_map (GtkWidget * widget)
    	          gtk_widget_map (sheet->sheet_entry);
       }
 
-      if (GTK_WIDGET_VISIBLE (sheet->button) &&
+      if (gtk_widget_get_visible(sheet->button) &&
 	  !gtk_widget_get_mapped(sheet->button)){
                   gtk_widget_show(sheet->button);
 	          gtk_widget_map (sheet->button);
       }
 
       if(GTK_BIN(sheet->button)->child)
-        if (GTK_WIDGET_VISIBLE (GTK_BIN(sheet->button)->child) &&
+        if (gtk_widget_get_visible(GTK_BIN(sheet->button)->child) &&
   	   !gtk_widget_get_mapped(GTK_BIN(sheet->button)->child))
   	          gtk_widget_map (GTK_BIN(sheet->button)->child);
 
@@ -2870,7 +2870,7 @@ gtk_sheet_map (GtkWidget * widget)
         child = children->data;
         children = g_list_next(children);
 
-        if (GTK_WIDGET_VISIBLE (child->widget) &&
+        if (gtk_widget_get_visible(child->widget) &&
     	    !gtk_widget_get_mapped(child->widget)){
 	  gtk_widget_map (child->widget);
           gtk_sheet_position_child(sheet, child);
@@ -2915,7 +2915,7 @@ gtk_sheet_unmap (GtkWidget * widget)
           child = children->data;
           children = g_list_next(children);
 
-          if (GTK_WIDGET_VISIBLE (child->widget) &&
+          if (gtk_widget_get_visible(child->widget) &&
 	      gtk_widget_get_mapped(child->widget))
                 {
   	             gtk_widget_unmap (child->widget);
@@ -3882,7 +3882,7 @@ gtk_sheet_entry_changed(GtkWidget *widget, gpointer data)
 
  sheet=GTK_SHEET(data);
 
- if(!GTK_WIDGET_VISIBLE(widget)) return;
+ if(!gtk_widget_get_visible(widget)) return;
  if(sheet->state != GTK_STATE_NORMAL) return;
 
  row=sheet->active_cell.row;
@@ -8031,7 +8031,7 @@ gtk_sheet_put(GtkSheet *sheet, GtkWidget *child, gint x, gint y)
 
   gtk_widget_size_request(child, &child_requisition);
 
-  if (GTK_WIDGET_VISIBLE(GTK_WIDGET(sheet)))
+  if (gtk_widget_get_visible(GTK_WIDGET(sheet)))
     {
        if(gtk_widget_get_realized(GTK_WIDGET(sheet)) && 
           (!gtk_widget_get_realized(child) || GTK_WIDGET_NO_WINDOW(child)))
@@ -8129,7 +8129,7 @@ gtk_sheet_attach        (GtkSheet *sheet,
   child->x = area.x + child->xpadding;
   child->y = area.y + child->ypadding;
 
-  if (GTK_WIDGET_VISIBLE(GTK_WIDGET(sheet)))
+  if (gtk_widget_get_visible(GTK_WIDGET(sheet)))
     {
        if(gtk_widget_get_realized(GTK_WIDGET(sheet)) &&
           (!gtk_widget_get_realized(widget) || GTK_WIDGET_NO_WINDOW(widget)))
@@ -8206,7 +8206,7 @@ gtk_sheet_button_attach		(GtkSheet *sheet,
              sheet->row[row].height = button_requisition.height; 
   }
 
-  if (GTK_WIDGET_VISIBLE(GTK_WIDGET(sheet)))
+  if (gtk_widget_get_visible(GTK_WIDGET(sheet)))
     {
        if(gtk_widget_get_realized(GTK_WIDGET(sheet)) && 
           (!gtk_widget_get_realized(widget) || GTK_WIDGET_NO_WINDOW(widget)))
