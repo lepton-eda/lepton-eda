c7d5297cc69c88c7fa84cf2083d571ac930aab68
Riccardo Lucchese <riccardo.lucchese@gmail.com>
2014-10-14 22:17:35 +0200

    Remove deprecated use of gtk_object_ref()
    Verbatim from Gtk+ 2 Reference Manual: "gtk_object_ref is deprecated
    and should not be used in newly-written code. Use g_object_ref()
    instead."
    
    Reviewed-by: Vladimir Zhbanov <vzhbanov@gmail.com>

--- gattrib/src/gtksheet_2_2.c
+++ gattrib/src/gtksheet_2_2.c
@@ -2388,7 +2388,7 @@ gtk_sheet_set_vadjustment (GtkSheet      *sheet,
 
   if (sheet->vadjustment)
     {
-      gtk_object_ref (GTK_OBJECT (sheet->vadjustment));
+      g_object_ref (sheet->vadjustment);
       gtk_object_sink (GTK_OBJECT (sheet->vadjustment));
 
       gtk_signal_connect (GTK_OBJECT (sheet->vadjustment), "changed",
@@ -2434,7 +2434,7 @@ gtk_sheet_set_hadjustment (GtkSheet      *sheet,
 
   if (sheet->hadjustment)
     {
-      gtk_object_ref (GTK_OBJECT (sheet->hadjustment));
+      g_object_ref (sheet->hadjustment);
       gtk_object_sink (GTK_OBJECT (sheet->hadjustment));
 
       gtk_signal_connect (GTK_OBJECT (sheet->hadjustment), "changed",
