lepton-eda/lepton-eda

commit 80ae51ce61b198a8a4bc4879ad76669b7a465c1b

gschem: Replace STARTSELECT with the combination of SELECT & inside_action.

gschem/src/x_event.c
@@ -132,13 +132,11 @@ x_event_button_pressed(GschemPageView *page_view, GdkEventButton *event, GschemT
   w_y = snap_grid (w_current, unsnapped_wy);
 
   if (event->type == GDK_2BUTTON_PRESS &&
-      (w_current->event_state == STARTSELECT ||
-       w_current->event_state == SELECT)) {
+      w_current->event_state == SELECT) {
     /* Don't re-select an object (lp-912978) */
     /* o_find_object(w_current, w_x, w_y, TRUE); */
     if (o_select_selected (w_current)) {
        o_edit(w_current, geda_list_get_glist( page->selection_list ));
-       i_set_state(w_current, SELECT);
        scm_dynwind_end ();
        return(0);
     }
@@ -180,13 +178,12 @@ x_event_button_pressed(GschemPageView *page_view, GdkEventButton *event, GschemT
         case (PICTUREMODE): o_picture_start(w_current, w_x, w_y); break;
         case (PINMODE)    : o_pin_start (w_current, w_x, w_y); break;
         case (ZOOMBOX)    : a_zoom_box_start(w_current, unsnapped_wx, unsnapped_wy); break;
+        case(SELECT)      : o_select_start(w_current, w_x, w_y); break;
         default: break;
       }
     }
 
     switch(w_current->event_state) {
-      case(SELECT):       o_select_start(w_current, w_x, w_y); break;
-
       case(STARTCOPY):
         if (o_select_selected(w_current)) {
           o_copy_start(w_current, w_x, w_y);
@@ -392,9 +389,6 @@ x_event_button_released (GschemPageView *page_view, GdkEventButton *event, Gsche
   if (event->button == 1) {
 
     switch(w_current->event_state) {
-      case(SELECT):
-        /* do nothing */
-        break;
       case(MOVE):
         i_set_state (w_current, ENDMOVE);
         break;
@@ -428,14 +422,13 @@ x_event_button_released (GschemPageView *page_view, GdkEventButton *event, Gsche
         i_set_state(w_current, ENDMCOPY);
         o_undo_savestate_old(w_current, UNDO_ALL);
         break;
-
-      case(STARTSELECT): o_select_end(w_current, unsnapped_wx, unsnapped_wy); break;
     }
     if (w_current->inside_action) {
       switch(w_current->event_state) {
         case(GRIPS)      : o_grips_end(w_current); break;
         case(PATHMODE)   : o_path_end (w_current, w_x, w_y); break;
         case(SBOX)       : o_select_box_end(w_current, unsnapped_wx, unsnapped_wy); break;
+        case(SELECT)     : o_select_end(w_current, unsnapped_wx, unsnapped_wy); break;
         case(ZOOMBOX)    : a_zoom_box_end(w_current, unsnapped_wx, unsnapped_wy); break;
         default: break;
       }
@@ -588,6 +581,7 @@ x_event_motion (GschemPageView *page_view, GdkEventMotion *event, GschemToplevel
       case(GRIPS)      :   o_grips_motion(w_current, w_x, w_y); break;
       case(SBOX)       :   o_select_box_motion (w_current, unsnapped_wx, unsnapped_wy); break;
       case(ZOOMBOX)    :   a_zoom_box_motion (w_current, unsnapped_wx, unsnapped_wy); break;
+      case(SELECT)     :   o_select_motion (w_current, w_x, w_y); break;
       default: break;
     }
   } else {
@@ -599,11 +593,6 @@ x_event_motion (GschemPageView *page_view, GdkEventMotion *event, GschemToplevel
 
   switch(w_current->event_state) {
 
-    case(SELECT):
-    /* do nothing */
-    break;
-
-    case(STARTSELECT): o_select_motion (w_current, unsnapped_wx, unsnapped_wy); break;
     case(ENDMOVE):
     case(MOVE):
     if (w_current->inside_action)

gschem/include/x_states.h
@@ -41,7 +41,7 @@ enum x_states {
 //STARTDRAWNET, /* 13 */
   DRAWCOMP,     /* 14 */
   SBOX,         /* 15 */
-  STARTSELECT,  /* 16 */
+//STARTSELECT,  /* 16 */
   STARTCOPY,    /* 17 */
   STARTMOVE,    /* 18 */
   ENDCOPY,      /* 19 */

gschem/src/i_basic.c
@@ -64,7 +64,6 @@ static const char *i_status_string(GschemToplevel *w_current)
   switch ( w_current->event_state ) {
     case NONE:
       return "";
-    case STARTSELECT:
     case SELECT:
     case SBOX:
       return _("Select Mode");
@@ -290,7 +289,6 @@ void i_update_toolbar(GschemToplevel *w_current)
   switch(w_current->event_state) {
     case(NONE):
     case(SELECT):
-    case(STARTSELECT):
       gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(
 				   w_current->toolbar_select), TRUE);
       break;

gschem/src/o_basic.c
@@ -379,7 +379,6 @@ int o_redraw_cleanstates(GschemToplevel *w_current)
     case(STARTMCOPY):
     case(STARTMOVE):
     case(STARTPASTE):
-    case(STARTSELECT):
       return FALSE;
   }
 

gschem/src/o_select.c
@@ -41,10 +41,11 @@
 /*! \brief Start the process of selection
  *  \par Function Description
  *  Chooses the way of how to start the selection process. If no
- *  grip was found at the given coordinates the function toggles
- *  the current state into the STARTSELECT mode in order to define
- *  what to do farther. Otherwise, it switches on the GRIPS mode
- *  for working with the grip found.
+ *  grip was found at the given coordinates the function sets
+ *  \a w_current->inside_action in order to force other functions
+ *  (\a o_select_motion() or \a o_select_end()) to decide that.
+ *  Otherwise, it switches on the GRIPS mode for working with the
+ *  grip found.
  *
  *  The function is intended to be called by pressing the left
  *  mouse button.
@@ -60,7 +61,7 @@ void o_select_start (GschemToplevel *w_current, int wx, int wy)
 
   if (w_current->event_state != GRIPS) {
     /* now go into normal SELECT */
-    i_set_state (w_current, STARTSELECT);
+    w_current->inside_action = 1;
     w_current->first_wx = w_current->second_wx = wx;
     w_current->first_wy = w_current->second_wy = wy;
   }
@@ -82,6 +83,8 @@ void o_select_start (GschemToplevel *w_current, int wx, int wy)
  */
 void o_select_end (GschemToplevel *w_current, int wx, int wy)
 {
+  g_assert (w_current->inside_action != 0);
+
   /* look for objects to select */
   o_find_object(w_current, wx, wy, TRUE);
   w_current->inside_action = 0;
@@ -106,6 +109,8 @@ void o_select_end (GschemToplevel *w_current, int wx, int wy)
  */
 void o_select_motion (GschemToplevel *w_current, int wx, int wy)
 {
+  g_assert (w_current->inside_action != 0);
+
   /* Check if a mod key is pressed or there is no selected object
    * under the cursor */
   if (w_current->SHIFTKEY || w_current->CONTROLKEY

commit c4ea28f6ccb237c84c1cebe855ed986ccefd1503

gschem: Prevent moving of attributes after edit
This patch fixes the regression introduced in commit 80ae51c.
Edited attribute sticks to the mouse after the edit if its editing
is started with double-click. The reason of such behaviour is
emitting of the left button press event, which sets the
w_current->inside_action flag, before the double-click event. The
modal edit attribute dialog intercepts the release button event so
inside_action is still active after closing it. Further, a large
offset of the mouse pointer invokes changing of the current mode
to MOVEMODE or (in some cases) to SBOX. Manual resetting of the
inside_action flag prevents this behaviour.

Reported-by: Kai-Martin Knaak <knaak@iqo.uni-hannover.de>

gschem/src/x_event.c
@@ -135,6 +135,14 @@ x_event_button_pressed(GschemPageView *page_view, GdkEventButton *event, GschemT
       w_current->event_state == SELECT) {
     /* Don't re-select an object (lp-912978) */
     /* o_find_object(w_current, w_x, w_y, TRUE); */
+
+    /* GDK_BUTTON_EVENT is emitted before GDK_2BUTTON_EVENT, which
+     * leads to setting of the inside_action flag.  If o_edit()
+     * brings up a modal window (e.g., the edit attribute dialog),
+     * it intercepts the release button event and thus doesn't
+     * allow resetting of the inside_action flag so we do it
+     * manually here before processing the double-click event. */
+    i_action_stop (w_current);
     o_edit(w_current, geda_list_get_glist( page->selection_list ));
     scm_dynwind_end ();
     return(0);