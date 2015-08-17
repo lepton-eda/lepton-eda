fb9ab7d47f4b95398f3f3824c64a53ed7193b827
Edward Hennessy <ehennes@sbcglobal.net>
2015-08-16 22:00:47 -0700

    gschem: don't SIGABRT when zooming on invisible text

--- gschem/src/gschem_page_view.c
+++ gschem/src/gschem_page_view.c
@@ -1310,7 +1310,7 @@ gschem_page_view_zoom_extents (GschemPageView *view, const GList *objects)
 void
 gschem_page_view_zoom_text (GschemPageView *view, OBJECT *object, GschemToplevel *w_current)
 {
-  int text_screen_height;
+  int success;
   int x[2];
   int y[2];
 
@@ -1319,24 +1319,29 @@ gschem_page_view_zoom_text (GschemPageView *view, OBJECT *object, GschemToplevel
   g_return_if_fail (object->page->toplevel != NULL);
   g_return_if_fail (object->text != NULL);
 
-  a_zoom (w_current, view, ZOOM_FULL, DONTCARE);
-
-  g_assert (world_get_single_object_bounds (object->page->toplevel,
+  success = world_get_single_object_bounds (object->page->toplevel,
                                             object,
                                             &x[0],
                                             &y[0],
                                             &x[1],
-                                            &y[1]));
+                                            &y[1]);
 
-  text_screen_height = gschem_page_view_SCREENabs (view, y[1] - y[0]);
+  if (success) {
+    int text_screen_height;
 
-  /* this code will zoom/pan till the text screen height is about */
-  /* 50 pixels high, perhaps a future enhancement will be to make */
-  /* this number configurable */
-  while (text_screen_height < 50) {
-    a_zoom (w_current, view, ZOOM_IN, DONTCARE);
+    a_zoom (w_current, view, ZOOM_FULL, DONTCARE);
+
     text_screen_height = gschem_page_view_SCREENabs (view, y[1] - y[0]);
+
+    /* this code will zoom/pan till the text screen height is about */
+    /* 50 pixels high, perhaps a future enhancement will be to make */
+    /* this number configurable */
+    while (text_screen_height < 50) {
+      a_zoom (w_current, view, ZOOM_IN, DONTCARE);
+      text_screen_height = gschem_page_view_SCREENabs (view, y[1] - y[0]);
+    }
+
+    gschem_page_view_pan (view, object->text->x, object->text->y);
   }
 
-  gschem_page_view_pan (view, object->text->x, object->text->y);
 }
