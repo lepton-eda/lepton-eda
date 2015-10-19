7e2c9c0b39b26a0b391fc4c171a71845d20be81e
Vladimir Zhbanov <vzhbanov@gmail.com>
2015-10-19 10:59:46 +0300

    symcheck: Rewrite check_symbol_text() in Scheme.
    As a side effect, an incorrect warning message about a trailing
    backslash has been fixed :-)

--- symcheck/scheme/symbol/check/text.scm
+++ symcheck/scheme/symbol/check/text.scm
@@ -1,10 +1,14 @@
 (define-module (symbol check text)
+  ;; See a note about UTF below.
+  ;; #:use-module (rnrs bytevectors)
+  ;; #:use-module (rnrs io ports)
   #:use-module (geda object)
   #:use-module (geda attrib)
   #:use-module (symbol gettext)
   #:use-module (symbol blame)
 
-  #:export (check-text-visibility))
+  #:export (check-text-visibility
+            check-text-string))
 
 (define (check-text-visibility object)
   "Checks if text OBJECT being non-attribute has inappropriate
@@ -17,3 +21,69 @@ visibility mode."
                      (format #f
                              (_ "Found a simple text object with only SHOW_NAME or SHOW_VALUE set [~A]\n")
                              (text-string object)))))
+
+
+(define (check-text-string-errors text)
+  (define (add-error-if condition type ls)
+    (if condition `(,type . ,ls) ls))
+
+  (with-input-from-string text
+    (lambda ()
+      (let read-next ((escape #f)
+                      (overbar #f)
+                      (ls '()))
+        (let ((c (read-char)))
+          (if (eof-object? c)
+              (reverse (add-error-if escape 'trailing-backslash
+                                     (add-error-if overbar 'unbalanced-overbar ls)))
+
+            (case c
+              ((#\\) (read-next (not escape) overbar ls))
+              ((#\_) (if escape
+                       (read-next (not escape) (not overbar) ls)
+                       (read-next escape overbar ls)))
+              (else  (if escape
+                       (read-next (not escape) overbar `(unbalanced-escape . ,ls))
+                       (read-next escape overbar ls))))))))))
+
+
+(define (check-text-string object)
+  "Checks string of text OBJECT."
+  (and (text? object)
+       (let ((s (text-string object)))
+         (for-each
+          (lambda (error-type)
+            (blame-object object
+                          'warning
+                          (case error-type
+                            ((trailing-backslash)
+                             (format #f (_ "Found text with a trailing '\\': consider to escape it with '\\\\' [~A]\n") s))
+                            ((unbalanced-overbar)
+                             (format #f (_ "Found text with unbalanced overbar markers '\\_' in it' [~A]\n") s))
+                            ((unbalanced-escape)
+                             (format #f (_ "Found text with a '\\' in it: consider to escape it with '\\\\' [~A]\n") s)))))
+          (check-text-string-errors s)))))
+
+;;; Incorrect UTF-8 encoded strings are blocked in
+;;; o_read_buffer(), so there is no need to check them here (at
+;;; least yet).
+#|
+
+(define-syntax decoding-error?
+  (syntax-rules ()
+    ((_ exp)
+     (catch 'decoding-error
+       (lambda ()
+         ;; Debug output
+         ;; (pk 'exp exp)
+         #f)
+            (lambda (key subr message errno p) (not (= 0 errno)))))))
+
+(define (valid-utf8-string? str)
+  (not (decoding-error?
+         (utf8->string
+           (with-input-from-string str
+             (lambda () (get-bytevector-all
+               (current-input-port))))))))
+
+|#
