lepton-eda/lepton-eda

commit ee29aa57a0b348fb7f1a42d7664abfeb2fe4a2df

gnetlist: Make traverse() available in Scheme.

gnetlist/src/gnetlist.c
@@ -142,6 +142,7 @@ void main_prog(void *closure, int argc, char *argv[])
     gchar *filename;
 
     TOPLEVEL *pr_current;
+    SCM traverse;
 
     /* set default output filename */
     output_filename = g_strdup("output.net");
@@ -181,6 +182,9 @@ void main_prog(void *closure, int argc, char *argv[])
 
     /* register guile (scheme) functions */
     g_register_funcs();
+    s_init_traverse ();
+    traverse = scm_c_public_lookup ("gnetlist traverse", "traverse");
+
 
     scm_dynwind_begin (0);
     pr_current = s_toplevel_new ();
@@ -301,7 +305,7 @@ void main_prog(void *closure, int argc, char *argv[])
       scm_eval (post_backend_list, scm_current_module ());
     }
 
-    s_traverse (pr_current);
+    (scm_call_0 (scm_variable_ref (traverse)));
 
     /* Change back to the directory where we started AGAIN.  This is done */
     /* because the s_traverse functions can change the Current Working Directory. */

gnetlist/scheme/Makefile.am
@@ -24,7 +24,8 @@ DIST_SCM = gnet-PCB.scm gnet-allegro.scm gnet-bom.scm gnet-ewnet.scm \
 	   gnet-mathematica.scm gnet-liquidpcb.scm \
 	   gnet-makedepend.scm \
 	   gnetlist/backend-getopt.scm gnetlist/partlist.scm \
-	   gnetlist/partlist/common.scm gnetlist/attrib/compare.scm
+	   gnetlist/partlist/common.scm gnetlist/attrib/compare.scm \
+	   gnetlist/traverse.scm
 
 TESTS = unit-tests/test-gnetlist-partlist.scm \
 	unit-tests/test-gnetlist-attrib.scm

gnetlist/src/Makefile.am
@@ -2,6 +2,9 @@
 
 bin_PROGRAMS = gnetlist
 
+BUILT_SOURCES = \
+	s_traverse.x
+
 # don't forget all *.h files */
 gnetlist_SOURCES = \
 	g_netlist.c \
@@ -31,7 +34,15 @@ gnetlist_LDADD = $(top_builddir)/libgeda/src/libgeda.la
 localedir = @datadir@/locale
 DEFS = -DLOCALEDIR=\"$(localedir)\" @DEFS@
 
+# This is used to generate boilerplate for defining Scheme functions
+# in C.
+SUFFIXES = .x
+snarf_cpp_opts = $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(AM_CPPFLAGS) \
+	$(gnetlist_CPPFLAGS) $(AM_CFLAGS) $(gnetlist_CFLAGS)
+.c.x:
+	CPP="$(CPP)" $(GUILE_SNARF) -o $@ $< $(snarf_cpp_opts)
+
 MOSTLYCLEANFILES = *.log *.ps core FILE *~
-CLEANFILES = *.log *.ps core FILE *~
+CLEANFILES = *.log *.ps core FILE *~ $(BUILT_SOURCES)
 DISTCLEANFILES = *.log core FILE *~
 MAINTAINERCLEANFILES = *.log *.ps core FILE *~ Makefile.in

gnetlist/include/prototype.h
@@ -91,10 +91,10 @@ void s_rename_all_lowlevel(NETLIST *netlist_head, char *src, char *dest);
 void s_rename_all (NETLIST *netlist_head);
 SCM g_get_renamed_nets(SCM scm_level);
 /* s_traverse.c */
-void s_traverse (TOPLEVEL *pr_current);
 void s_traverse_sheet(TOPLEVEL *pr_current, const GList *obj_list, char *hierarchy_tag);
 CPINLIST *s_traverse_component(TOPLEVEL *pr_current, OBJECT *component, char *hierarchy_tag);
 NET *s_traverse_net (NET *nets, int starting, OBJECT *object, char *hierarchy_tag, int type);
+void s_init_traverse (void);
 /* vams_misc.c */
 SCM vams_get_attribs_list(OBJECT *object);
 SCM vams_get_package_attributes(SCM scm_uref);

gnetlist/scheme/gnetlist/traverse.scm
@@ -1,0 +1,7 @@
+(define-module (gnetlist traverse)
+
+  ; Import C procedures and variables
+  #:use-module (gnetlist core traverse)
+  #:export (traverse))
+
+(define traverse %traverse)

gnetlist/src/.gitignore
@@ -5,4 +5,5 @@ Makefile.in
 gnetlist
 gnetlist.exe
 *.o
+*.x
 *~

gnetlist/src/s_traverse.c
@@ -27,6 +27,7 @@
 #include <math.h>
 
 #include <libgeda/libgeda.h>
+#include <libgeda/libgedaguile.h>
 
 #include "../include/globals.h"
 #include "../include/prototype.h"
@@ -112,13 +113,16 @@ s_traverse_init (void)
                                     g_direct_equal);
 }
 
-void
-s_traverse (TOPLEVEL *pr_current)
+SCM_DEFINE (traverse, "%traverse", 0, 0, 0,
+            (), "Traverse hierarchy.")
 {
+  TOPLEVEL *pr_current;
   GList *iter;
   PAGE *p_current;
 
   s_traverse_init ();
+
+  pr_current = edascm_c_current_toplevel ();
 
   for ( iter = geda_list_get_glist( pr_current->pages );
         iter != NULL;
@@ -144,6 +148,8 @@ s_traverse (TOPLEVEL *pr_current)
     printf("\nInternal netlist representation:\n\n");
     s_netlist_print(netlist_head);
   }
+
+  return SCM_BOOL_T;
 }
 
 
@@ -439,4 +445,26 @@ s_traverse_net (NET *nets, int starting, OBJECT *object, char *hierarchy_tag, in
   }
 
   return (nets);
+}
+
+
+static void
+init_module_gnetlist_core_traverse ()
+{
+  /* Register the functions */
+  #include "s_traverse.x"
+
+  /* Register the functions and add them to the module's public
+   * definitions. */
+  scm_c_export (s_traverse,
+                NULL);
+}
+
+void
+s_init_traverse ()
+{
+  /* Define the (gsymcheck core check) module */
+  scm_c_define_module ("gnetlist core traverse",
+                       init_module_gnetlist_core_traverse,
+                       NULL);
 }

commit 0b8b1de5b0702359d4c11f41ef6654b244b6837c

gnetlist: Fix `make distcheck'
Broken in ee29aa57a0b348fb7f1a42d7664abfeb2fe4a2df.

gnetlist/src/gnetlist.c
@@ -183,8 +183,6 @@ void main_prog(void *closure, int argc, char *argv[])
     /* register guile (scheme) functions */
     g_register_funcs();
     s_init_traverse ();
-    traverse = scm_c_public_lookup ("gnetlist traverse", "traverse");
-
 
     scm_dynwind_begin (0);
     pr_current = s_toplevel_new ();
@@ -313,6 +311,8 @@ void main_prog(void *closure, int argc, char *argv[])
       /* Evaluate second set of Scheme expressions. */
       scm_eval (post_backend_list, scm_current_module ());
     }
+
+    traverse = scm_c_public_lookup ("gnetlist traverse", "traverse");
 
     (scm_call_0 (scm_variable_ref (traverse)));
 

gnetlist/tests/common/inputs/gafrc
@@ -3,6 +3,7 @@
 (scheme-directory "${SCMDIR}")
 (scheme-directory "${GEDASCMDIR}")
 (scheme-directory "${GEDABUILTSCMDIR}")
+(scheme-directory "${GNETLISTSCMDIR}")
 
 ;; We want to use the symbols that haven't been installed yet
 (map (lambda (x) (component-library (string-join (list "${SYMDIR}/" x) "")))

gnetlist/tests/common/run_backend_tests.sh
@@ -95,12 +95,14 @@ GEDADATARC="${builddir}/../../lib"
 SCMDIR="${builddir}/../../scheme"
 GEDASCMDIR="${srcdir}/../../../libgeda/scheme"
 GEDABUILTSCMDIR="${builddir}/../../../libgeda/scheme"
+GNETLISTSCMDIR="${srcdir}/../../../gnetlist/scheme"
 SYMDIR="${srcdir}/../../../symbols"
 export GEDADATA
 export GEDADATARC
 export SCMDIR
 export GEDASCMDIR
 export GEDABUILTSCMDIR
+export GNETLISTSCMDIR
 export SYMDIR
 
 rundir=${here}/run
@@ -147,6 +149,7 @@ GEDADATARC: ${GEDATADARC}
 SCMDIR:     ${SCMDIR}
 GEDASCMDIR: ${GEDASCMDIR}
 GEDABUILTSCMDIR: ${GEDABUILTSCMDIR}
+GNETLISTSCMDIR: ${GNETLISTSCMDIR}
 SYMDIR:     ${SYMDIR}
 all_tests:
 

gnetlist/tests/drc2/Makefile.am
@@ -67,6 +67,7 @@ tests:
 	    SYMDIR=${top_srcdir}/symbols \
 	    $(GNETLIST) -L$(top_srcdir)/libgeda/scheme \
 	                -L$(top_builddir)/libgeda/scheme \
+	                -L$(top_srcdir)/gnetlist/scheme \
 	                -g drc2 \
 	                -o $(BUILDDIR)/new_$$file_basename.drc2 \
 	                $$file ); \

gnetlist/tests/hierarchy/Makefile.am
@@ -23,6 +23,7 @@ tests:
 	    $(GNETLIST) \
 	      -L $(top_srcdir)/libgeda/scheme \
 	      -L $(top_builddir)/libgeda/scheme \
+	      -L $(top_srcdir)/gnetlist/scheme \
 	      -o $(BUILDDIR)/new_hierarchy.geda \
 	      -g geda $(SRCDIR)/top.sch )
 	diff $(SRCDIR)/hierarchy.geda $(BUILDDIR)/new_hierarchy.geda;

gnetlist/tests/hierarchy2/Makefile.am
@@ -20,6 +20,7 @@ tests:
 	  $(GNETLIST) \
 	     -L $(top_srcdir)/libgeda/scheme \
 	     -L $(top_builddir)/libgeda/scheme \
+	     -L $(top_srcdir)/gnetlist/scheme \
 	     -o $(BUILDDIR)/new_hierarchy2.geda -g geda \
 	     $(SRCDIR)/top.sch )
 	diff $(SRCDIR)/hierarchy2.geda $(BUILDDIR)/new_hierarchy2.geda;

gnetlist/tests/runtest.sh
@@ -16,6 +16,7 @@ SYMDIR=$SRCDIR/../../symbols \
 GEDADATARC=$BUILDDIR/../lib \
 ../src/gnetlist -L ${SRCDIR}/../../libgeda/scheme \
   -L ${BUILDDIR}/../../libgeda/scheme \
+  -L ${SRCDIR}/../../gnetlist/scheme \
   -o ${BUILDDIR}/new_${schbasename}.$BACKEND -g $BACKEND $INPUT
 status=$?
 