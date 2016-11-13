a526964610ce6dca85e8ccffe72e4fd496580972
Vladimir Zhbanov <vzhbanov@gmail.com>
2016-11-13 17:35:00 +0300

    osmond: Sort part output by package name.

--- gnetlist/scheme/gnet-osmond.scm
+++ gnetlist/scheme/gnet-osmond.scm
@@ -19,9 +19,11 @@
 
 ; Export a design to Osmond PCB
 
+(use-modules (gnetlist attrib compare))
+
 (define (osmond output-filename)
         (set-current-output-port (gnetlist:output-port output-filename))
-        (for-each osmond:part packages)
+        (for-each osmond:part (sort packages refdes<?))
         (for-each osmond:signal all-unique-nets))
 
 
