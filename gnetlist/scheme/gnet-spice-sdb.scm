227bcac9d7b85e55185dbda05c3887b450f84776
Vladimir Zhbanov <vzhbanov@gmail.com>
2016-11-14 13:31:51 +0300

    spice-sdb: Sort output netlist by package names.
    Of course, sorted mode output has not been affected by this change :-)

--- gnetlist/scheme/gnet-spice-sdb.scm
+++ gnetlist/scheme/gnet-spice-sdb.scm
@@ -951,7 +951,8 @@ the name is changed to canonical."
   (with-output-to-port
       (gnetlist:output-port output-filename)
     (lambda ()
-      (let ((subckt? (spice-sdb:get-schematic-type packages)))
+      (let* ((packages (sort packages refdes<?))
+             (subckt? (spice-sdb:get-schematic-type packages)))
 
         (if subckt?
             ;; now write out .SUBCKT header and .SUBCKT line
