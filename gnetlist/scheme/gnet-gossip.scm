78d5b9e6e591c401034f48e57aebca4bf0f902c1
Vladimir Zhbanov <vzhbanov@gmail.com>
2016-11-17 16:53:40 +0300

    gossip: Simplified all procedures.
    - Use format() instead of display().
    - Move procedure arguments into define lines.

--- gnetlist/scheme/gnet-gossip.scm
+++ gnetlist/scheme/gnet-gossip.scm
@@ -24,111 +24,81 @@
 ;;  For more info see http://gossip.sourceforge.net
 ;;
 
-(define gossip:write-top-header
-   (lambda ()
-      (display ";; Gossip Netlist Created by gNetlist")
-      (newline)
-      (newline)
-      (display ";; Created By Matt Ettus <matt@ettus.com>")
-      (newline)
-      (display ";; Libraries:")
-      (newline)
-      (newline)))
+(define (gossip:write-top-header)
+  (display ";; Gossip Netlist Created by gNetlist
 
-(define gossip:get-libraries
-  (lambda (components done)
-    (if (not (null? components))
+;; Created By Matt Ettus <matt@ettus.com>
+;; Libraries:
+
+"))
+
+(define (gossip:get-libraries components done)
+  (if (not (null? components))
       (let ((lib (gnetlist:get-package-attribute (car components) "library")))
         (if (string=? "unknown" lib)
-          (begin
-            (message "Component ")
-            (message (car components))
-            (message " does not have a library attribute\n")))
+            (message (format #f "Component ~A does not have a library attribute\n"
+                             (car components))))
         (if (contains? done lib)
-          (gossip:get-libraries (cdr components) done)
-          (begin
-            (display "(use-library ")
-            (display lib)
-            (display " *)")
-            (newline)
-            (gossip:get-libraries (cdr components) (cons lib done))))))))
-
-(define gossip:list-pins
-   (lambda (allnets uref pin)
-      (let ((pinname (gnetlist:get-attribute-by-pinnumber uref (number->string pin) "label")))
-         (if (string=? "unknown" pinname)
-            (display ")\n")
+            (gossip:get-libraries (cdr components) done)
             (begin
-               (display " :")
-               (display pinname)
-               (write-char #\space)
-               (display (gossip:find-net uref pin allnets))
-               (gossip:list-pins allnets uref (+ 1 pin)))))))
+              (format #t "(use-library ~A *)\n" lib)
+              (gossip:get-libraries (cdr components) (cons lib done)))))))
 
-;(define gossip:reverse-netlist
-;   (lambda (allnets)
-;      (if (null? allnets)
-;         '()
-;         (let ((connections (gnetlist:get-all-connections (car allnets))))
-;            (cons (gossip:connectionlist connections)
-;                  (gossip:reverse-netlist (cdr allnets))))))
+(define (gossip:list-pins allnets uref pin)
+  (let ((pinname (gnetlist:get-attribute-by-pinnumber uref (number->string pin) "label")))
+    (if (string=? "unknown" pinname)
+        (display ")\n")
+        (begin
+          (display " :")
+          (display pinname)
+          (write-char #\space)
+          (display (gossip:find-net uref pin allnets))
+          (gossip:list-pins allnets uref (+ 1 pin))))))
 
-(define gossip:find-net
-   (lambda (uref pin allnets)
-      (cond
-         ((null? allnets) "Not Connected" )
-         ((gossip:finder uref pin (gnetlist:get-all-connections (car allnets)))(car allnets))
-         (#t (gossip:find-net uref pin (cdr allnets))))))
+(define (gossip:find-net uref pin allnets)
+  (cond
+   ((null? allnets) "Not Connected" )
+   ((gossip:finder uref pin (gnetlist:get-all-connections (car allnets)))(car allnets))
+   (#t (gossip:find-net uref pin (cdr allnets)))))
 
-(define gossip:finder
-   (lambda (uref pin list)
-      (cond
-         ((null? list)#f)
-         ((and (string=? uref (caar list)) (string=? (number->string pin) (cadar list))) #t)
-         (#t (gossip:finder uref pin (cdr list))))))
+(define (gossip:finder uref pin list)
+  (cond
+   ((null? list)#f)
+   ((and (string=? uref (caar list)) (string=? (number->string pin) (cadar list))) #t)
+   (#t (gossip:finder uref pin (cdr list)))))
 
-(define gossip:display-connections
-   (lambda (nets)
-      (if (not (null? nets))
-         (begin
-            (display (car (car nets)))
-            (write-char #\space)
-            (display (car (cdr (car nets))))
-            (if (not (null? (cdr nets)))
-               (begin
-                  (write-char #\,)
-                  (write-char #\space)))
-               (gossip:display-connections (cdr nets))))))
-
-(define gossip:display-name-nets
-   (lambda (nets)
+(define (gossip:display-connections nets)
+  (if (not (null? nets))
       (begin
-         (gossip:display-connections nets)
-         (write-char #\space)
-         (newline))))
+        (display (car (car nets)))
+        (write-char #\space)
+        (display (car (cdr (car nets))))
+        (if (not (null? (cdr nets)))
+            (begin
+              (write-char #\,)
+              (write-char #\space)))
+        (gossip:display-connections (cdr nets)))))
 
-(define gossip:blocks
-   (lambda (ls allnets)
-      (if (not (null? ls))
-         (let ((package (car ls)))
-            (display "   (")
-            (display package)
-            (gossip:list-pins allnets package 1)
-            (gossip:blocks (cdr ls) allnets)))))
+(define (gossip:display-name-nets nets)
+  (gossip:display-connections nets)
+  (write-char #\space)
+  (newline))
 
-(define gossip:signals
-   (lambda ()
-      (display "(signals ")
-      (display (gnetlist:get-all-unique-nets "dummy"))
-      (display ")\n")))
+(define (gossip:blocks ls allnets)
+  (if (not (null? ls))
+      (let ((package (car ls)))
+        (display "   (")
+        (display package)
+        (gossip:list-pins allnets package 1)
+        (gossip:blocks (cdr ls) allnets))))
 
-(define gossip:write-block-header
-   (lambda ()
-      (let ((blockname (gnetlist:get-toplevel-attribute "blockname")))
-         (display "(define-block (")
-         (display blockname)
-         (display " (")
-         (newline))))
+(define (gossip:signals)
+  (format #t "(signals ~A)\n"
+          (gnetlist:get-all-unique-nets "dummy")))
+
+(define (gossip:write-block-header)
+  (format #t "(define-block (~A (\n"
+          (gnetlist:get-toplevel-attribute "blockname")))
 
 (define (gossip output-filename)
   (with-output-to-port (gnetlist:output-port output-filename)
