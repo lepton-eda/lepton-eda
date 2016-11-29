35779dd0b129c3246a671dee519c10360b772a13
Vladimir Zhbanov <vzhbanov@gmail.com>
2016-11-29 14:27:37 +0300

    futurenet2: Replace gnetlist:get-all-unique-nets() with its Scheme counterpart.

--- gnetlist/scheme/gnet-futurenet2.scm
+++ gnetlist/scheme/gnet-futurenet2.scm
@@ -213,10 +213,10 @@
      (message "---------------------------------\n\n")
 
       (set-current-output-port (gnetlist:output-port output-filename))
-      (let ((all-nets (gnetlist:get-all-unique-nets "dummy")))
+      (let ((all-nets (get-all-unique-nets)))
 
         ;; initialize the net-name aliasing
-        (gnetlist:build-net-aliases futurenet2:map-net-names all-unique-nets)
+        (gnetlist:build-net-aliases futurenet2:map-net-names all-nets)
 
         ;; initialize the refdes aliasing
         (gnetlist:build-refdes-aliases futurenet2:map-refdes packages)
