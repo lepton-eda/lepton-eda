lepton-eda/lepton-eda

commit 0a40fe8389ded59ba021ad4f1ea1555f26174552

gnetlist: Optimised rename tracking
This patch improves rename performance from O(N^2) in the number of
renames to O(1) by using a hashtable to track renames instead of an
alist.

gnetlist/scheme/gnetlist/rename.scm
@@ -1,5 +1,5 @@
 (define-module (gnetlist rename)
-  #:use-module (srfi srfi-26)
+  #:use-module (srfi srfi-69)
   #:use-module (gnetlist core gettext)
   #:use-module (gnetlist verbose)
   #:use-module (gnetlist package)
@@ -9,132 +9,90 @@
             add-rename
             get-rename-list
             reset-rename!
-            rename-all))
+            rename-all
+            get-rename-list))
 
-;;; List of renamings
-(define %rename-list '())
+(define (rename-lowlevel netlist from to)
 
-;;; Setter for the list of renamings
-(define (set-rename-list! ls)
-  (set! %rename-list ls)
-  %rename-list)
+  (define (rename-in-pin pin)
+    (if (string=? from (package-pin-name pin))
+        (set-package-pin-name! pin to)))
 
-;;; Getter for the list of renamings
-;;
-;; Returns the rename alist sorted by rename destination, then by
-;; rename source.
-(define (%get-rename-list)
-  %rename-list)
+  (define (rename-in-package package)
+    (for-each rename-in-pin (package-pins package)))
 
-(define (get-rename-list)
-  (sort (%get-rename-list) rename<?))
+  (for-each rename-in-package netlist)
+  netlist)
 
-(let ((rename-ls '()))
-  (set! set-rename-list!
-        (lambda (ls) (set! rename-ls ls) rename-ls))
-  (set! %get-rename-list
-        (lambda () rename-ls)))
+(define %renames (make-hash-table))
 
-;;; Reset the list of renamings to empty
-(define (reset-rename!) (set-rename-list! '()))
+(define (renamed? from)
+  (hash-table-ref/default %renames from #f))
 
-;;; Getters for rename pairs
-(define source car)
-(define destination cdr)
+(define (set-rename! from to)
+  (hash-table-set! %renames from to))
 
-;;; Comparison function for renames
-(define (rename<? left right)
-  (or
-   (string<? (destination left) (destination right))
-   (and (string=? (destination left) (destination right))
-        (string<? (source left) (source right)))))
+(define-public (reset-rename!)
+  (set! %renames (make-hash-table)))
+
+(define (update-rename from to)
+  (let ((from-rename (renamed? from))
+        (to-rename   (renamed? to)))
+    (cond
+     ;; Forward rename already exists
+     ((and from-rename (equal? to from-rename)) #t)
+     ;; Reverse rename already exists
+     ((and to-rename (equal? from to-rename)) #t)
+     ;; we found a -> b, while adding c -> a.
+     ;; hence we would have c -> a -> b, so add c -> b.
+     ;; avoid renaming if b is same as c!
+     (to-rename (set-rename! from to-rename))
+     ;; we found a -> b, while adding a -> c.
+     ;; hence b <==> c, so add c -> b.
+     ;; avoid renaming if b is same as c!
+     (from-rename (set-rename! to from-rename))
+     ;; No pre-existing renames
+     (else (set-rename! from to)))))
+
+(define-public (add-rename from to)
+  (and from to (update-rename from to)))
 
 ;;; if the src is found, return true
 ;;; if the dest is found, also return true, but warn user
 ;;; If quiet flag is true than don't print anything
-(define (search-rename src dest quiet)
-  (let loop ((ls (%get-rename-list)))
-    (and (not (null? ls))
-         (let* ((rename (car ls))
-                (current-source (source rename)))
-           (or (string=? src current-source)
-               (and (string=? dest current-source)
-                    (when (not quiet)
-                      (format (current-error-port)
-                              (_ "WARNING: Trying to rename something twice:
+(define (search-rename from to quiet)
+  ;; FIXME[2017-02-20] This warning message is very unhelpful
+  (define (warn)
+    (when (not quiet)
+          (format (current-error-port)
+                  (_ "WARNING: Trying to rename something twice:
 \t~A and ~A
 are both a src and dest name
 This warning is okay if you have multiple levels of hierarchy!
 ")
-                              dest
-                              current-source))
-                    #t)
-               (loop (cdr ls)))))))
+                  dest
+                  dest)))
 
-(define (update-rename-list rename-list dest src)
-  (define (get-new-rename src dest rename)
-    (or (and (string=? dest (source rename))
-             (not (string=? src (destination rename)))
-             ;; we found a -> b, while adding c -> a.
-             ;; hence we would have c -> a -> b, so add c -> b.
-             ;; avoid renaming if b is same as c!
-             (cons src (destination rename)))
-        (and (string=? src (source rename))
-             (not (string=? dest (destination rename)))
-             ;; we found a -> b, while adding a -> c.
-             ;; hence b <==> c, so add c -> b.
-             ;; avoid renaming if b is same as c!
-             (cons dest (destination rename)))))
+  (cond
+   ((renamed? from) #t)
+   ((renamed? to) (begin (warn) #t))
+   (else #f)))
 
-  (let loop ((ls rename-list)
-             (new-ls '()))
-    (if (null? ls)
-        (append new-ls rename-list)
-        (loop (cdr ls)
-              (let ((rename (get-new-rename src dest (car ls))))
-                (if rename
-                    (cons rename new-ls)
-                    new-ls))))))
-
-(define (add-rename src dest)
-  (and src
-       dest
-       (set-rename-list!
-        (if (search-rename src dest #f)
-            (update-rename-list (%get-rename-list) src dest)
-            `((,src . ,dest) . ,(%get-rename-list))))))
-
-
-(define (rename-lowlevel netlist rename)
-  (define (rename-it net-name rename)
-    (if (and net-name
-             (string=? net-name (source rename)))
-        (destination rename)
-        net-name))
-
-  (define (rename-in-pin pin rename)
-    (set-package-pin-name! pin (rename-it (package-pin-name pin) rename)))
-
-  (define (rename-in-pin-list pins rename)
-    (for-each (cut rename-in-pin <> rename) pins))
-
-  (define (rename-in-package package rename)
-    (rename-in-pin-list (package-pins package) rename))
-
-  (for-each (cut rename-in-package <> rename) netlist)
+(define-public (rename-all netlist)
+  (verbose-print "- Renaming nets:\n")
+  (hash-table-walk %renames
+    (lambda (from to)
+      (verbose-print "R")
+      (rename-lowlevel netlist from to)))
+  (verbose-done)
   netlist)
 
-
-(define (rename-all netlist)
-  (verbose-print "- Renaming nets:\n")
-  (let loop ((rename-list (%get-rename-list))
-             (netlist netlist))
-    (if (null? rename-list)
-        (begin
-          (verbose-done)
-          netlist)
-        (begin
-          (verbose-print "R")
-          (loop (cdr rename-list)
-                (rename-lowlevel netlist
-                                 (car rename-list)))))))
+;; Return the alist of renames.  Sort it so that it's in a canonical
+;; order no matter what the internal implementation of the hash table
+;; is.
+(define-public (get-rename-list)
+  (sort! (hash-table->alist %renames)
+         (lambda (left right)
+           (or (string<? (cdr left) (cdr right))
+               (and (string= (cdr left) (cdr right))
+                    (string<? (car left) (car right)))))))

commit 6e44d4d95ffb0cf2a528db5895e11c4055ef162e

gnetlist: Fix variable names in rename.scm.
Fix regression introduced in commit 0a40fe8. The names there were
a rudiment of previous code.

gnetlist/scheme/gnetlist/rename.scm
@@ -70,8 +70,8 @@
 are both a src and dest name
 This warning is okay if you have multiple levels of hierarchy!
 ")
-                  dest
-                  dest)))
+                  to
+                  to)))
 
   (cond
    ((renamed? from) #t)