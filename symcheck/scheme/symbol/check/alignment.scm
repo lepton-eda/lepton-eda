fb63a0e6691bb00b796b8a43cebd97eca90477b7
Vladimir Zhbanov <vzhbanov@gmail.com>
2017-04-28 14:29:54 +0300

    symcheck: Improve messages and add docstring in check-pin-alignment().

--- symcheck/scheme/symbol/check/alignment.scm
+++ symcheck/scheme/symbol/check/alignment.scm
@@ -8,6 +8,8 @@
 (define %grid-step 100)
 
 (define (check-pin-alignment pin)
+  "Checks if both PIN ends of given object are aligned by grid.
+Returns PIN."
   (define x car)
   (define y cdr)
   (define (on-grid? num)
@@ -17,19 +19,25 @@
     (and (on-grid? (x coord))
          (on-grid? (y coord))))
 
-  (define (check-on-grid pin msg-type coord)
+  (define (check-on-grid pin msg-type msg coord)
     ;; Consider line-start to be the first pin point
     (let ((num (if (eq? msg-type 'error) 1 2)))
       (or (is-on-grid? coord)
           (blame-object pin
                         msg-type
                         (format #f
-                                (_ "Found offgrid pin at location (x~A=~A,y~A=~A)\n")
+                                msg
                                 num
                                 (x coord)
                                 num
                                 (y coord))))))
 
   ;; line-start is the connectible point (whichend)
-  (check-on-grid pin 'error (line-start pin))
-  (check-on-grid pin 'warning (line-end pin)))
+  (check-on-grid pin
+                 'error
+                 "Connectible end of pin is off grid (x~A=~A,y~A=~A)"
+                 (line-start pin))
+  (check-on-grid pin
+                 'warning
+                 "Non-connectible end of pin is off grid (x~A=~A,y~A=~A)"
+                 (line-end pin)))
