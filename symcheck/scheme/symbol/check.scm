79808ea98f0b96ebea308bb2da02e67bb2b2370a
Vladimir Zhbanov <vzhbanov@gmail.com>
2017-05-16 16:48:37 +0300

    symcheck: Factor out only symcheck related code into a module (symcheck check).

--- symcheck/scheme/symbol/check.scm
+++ symcheck/scheme/symbol/check.scm
@@ -1,41 +1,17 @@
 (define-module (symbol check)
-  #:use-module ((ice-9 rdelim)
-                #:select (read-string)
-                #:prefix rdelim:)
   #:use-module (ice-9 receive)
   #:use-module (srfi srfi-1)
-  #:use-module (srfi srfi-26)
-  #:use-module (symbol gettext)
-  #:use-module (symcheck option)
-  #:use-module (symbol blame)
   #:use-module (geda page)
-  #:use-module (geda log)
-  #:use-module (geda object)
-  #:use-module (geda attrib)
-  #:use-module (geda repl)
+  #:use-module (symbol blame)
   #:use-module (symbol check attrib)
-  #:use-module (symbol check log)
   #:use-module (symbol check pin)
   #:use-module (symbol check pin-attrib)
   #:use-module (symbol check primitive)
   #:use-module (symbol check slot)
-  #:use-module (ice-9 regex)
 
-  #:export (check-all-symbols))
+  #:export (check-symbol))
 
-(define (usage)
-  (format #t
-          (_ "Usage: ~A [OPTIONS] FILENAME ...
-  -h, --help        Print usage
-  -q, --quiet       Quiet mode
-  -v, --verbose     Verbose mode (cumulative: errors, warnings, info)
-                    Use this to get the actual symbol error messages
-FILENAME ... are the symbols to check.
-")
-          (car (program-arguments)))
-  (primitive-exit 0))
-
-(define-public (check-symbol page)
+(define (check-symbol page)
   (let ((objects (page-contents page)))
 
     ;; First reset all blaming info collected previously.
@@ -74,40 +50,3 @@ FILENAME ... are the symbols to check.
 
               `(lepton-symbol (@ ,@symbol-attribs (slotting-info ,@slotting-info))
                               ,@pins))))))))
-
-;;; Reads file NAME and outputs a page named NAME
-(define (file->page name)
-  (with-input-from-file name
-    (lambda ()
-      (unless (symcheck-option-ref 'quiet)
-        (log! 'message (_ "Loading schematic ~S") name))
-      (string->page name (rdelim:read-string)))))
-
-
-(define (check-all-symbols)
-  (define (report-symbol-statistics page)
-    (unless (symcheck-option-ref 'quiet)
-      (check-log! 'message (_ "Checking: ~A\n") (page-filename page)))
-    (check-symbol page)
-    (blame-statistics `(,page . ,(page-contents page))))
-
-  ;; Symcheck logs to stdout by default.
-  (set-check-log-destination! 'stdout)
-
-  (let ((files (symcheck-option-ref '()))
-        (help (symcheck-option-ref 'help))
-        (interactive (symcheck-option-ref 'interactive)))
-    (if help
-        (usage)
-        (if (null? files)
-            (error (format #f
-                           (_ "No schematic files specified for processing.
-Run `~A --help' for more information.
-")
-                           (car (program-arguments))))
-            (let ((pages (map file->page files)))
-              (if interactive
-                  (lepton-repl)
-
-                  ;; now report the info/warnings/errors to the user
-                  (primitive-exit (apply + (map report-symbol-statistics pages)))))))))
