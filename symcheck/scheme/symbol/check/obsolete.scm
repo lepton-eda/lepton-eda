9a3ba87431f040be43a3165214e1d18dd5ae11f8
Vladimir Zhbanov <vzhbanov@gmail.com>
2017-05-22 09:59:20 +0300

    symcheck: Fix a module cross-dependency.
    This eliminates some wargings about possibly not defined variables.

--- symcheck/scheme/symbol/check/obsolete.scm
+++ symcheck/scheme/symbol/check/obsolete.scm
@@ -21,8 +21,6 @@
   #:use-module (geda attrib)
   #:use-module (symbol gettext)
   #:use-module (symbol blame)
-  #:use-module (symbol check attrib)
-  #:use-module (symbol check pin-attrib)
 
   #:export (check-obsolete-pin-attrib
             check-obsolete-floating-attrib))
@@ -35,27 +33,18 @@
 (define regex-old-pin (make-regexp "^pin[0-9]+$"))
 (define regex-old-slot (make-regexp "^slot[0-9]+$"))
 
-;;; Blames OBJECT if it is an obsolete attribute. The attribute
-;;; type is tested using PREDICATE. ERROR-MSG is used to blame it
-;;; if its name matches REGEX. Returns OBJECT.
-(define (check-obsolete-attrib object predicate regex error-msg)
-  (when (predicate object)
-    (let ((name (attrib-name object))
-          (value (attrib-value object)))
-      (when (regexp-exec regex name)
-        (blame-error object error-msg name value))))
-  object)
-
 (define (check-obsolete-pin-attrib object)
-  "Checks if OBJECT is an obsolete pin attribute."
-  (check-obsolete-attrib object
-                         pin-attrib?
-                         regex-old-pin
-                         "Obsolete pin#=# attribute: ~A=~A"))
+  "Checks if OBJECT is an obsolete pin attribute. Returns OBJECT."
+  (let ((name (attrib-name object))
+        (value (attrib-value object)))
+    (when (regexp-exec regex-old-pin name)
+      (blame-error object "Obsolete pin#=# attribute: ~A=~A" name value))
+    object))
 
 (define (check-obsolete-floating-attrib object)
-  "Checks if OBJECT is an obsolete floating attribute."
-  (check-obsolete-attrib object
-                         floating-attrib?
-                         regex-old-slot
-                         "Obsolete slot#=# attribute: ~A=~A"))
+  "Checks if OBJECT is an obsolete floating attribute. Returns OBJECT."
+  (let ((name (attrib-name object))
+        (value (attrib-value object)))
+    (when (regexp-exec regex-old-slot name)
+      (blame-error object "Obsolete slot#=# attribute: ~A=~A" name value))
+    object))
