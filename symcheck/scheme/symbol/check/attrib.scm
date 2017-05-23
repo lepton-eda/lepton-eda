08120044fd29f15cb9bf3354ec233ad76d152af2
Vladimir Zhbanov <vzhbanov@gmail.com>
2017-05-23 16:52:31 +0300

    symcheck: Use more concise messages in check-attribute().

--- symcheck/scheme/symbol/check/attrib.scm
+++ symcheck/scheme/symbol/check/attrib.scm
@@ -70,61 +70,44 @@ returns #f."
 (define (check-attribute object)
   "Checks attribute OBJECT."
   (and (attribute? object)
-   (let ((aname  (string->symbol (attrib-name object)))
-         (avalue (attrib-value object)))
+       (let ((aname (string->symbol (attrib-name object)))
+             (s (text-string object)))
 
-     (case aname
-       ((type name)
-        (blame-object object
-                      'error
-                      (format #f
-                              (_ "Found forbidden ~A= attribute: [~A=~A]\n")
-                              aname
-                              aname
-                              avalue)))
-
-       ((uref label email)
-        (blame-object object
-                      'warning
-                      (format #f
-                              (_ "Found obsolete ~A= attribute: [~A=~A]\n")
-                              aname
-                              aname
-                              avalue)))
-
-       ;; Valid pin attributes.
-       ((pinlabel pintype pinseq pinnumber)
-        (if (or (floating-attrib? object)
-                (not (pin? (attrib-attachment object))))
+         (case aname
+           ((type name)
             (blame-object object
                           'error
-                          (format #f
-                                  (_ "Found misplaced pin attribute: [~A=~A]\n")
-                                  aname
-                                  avalue))))
+                          (format #f (_ "Forbidden attribute: ~A") s)))
 
-       ;; Valid attributes.
-       ((device graphical description author
-                comment numslots slotdef footprint
-                documentation refdes slot net
-                value symversion dist-license use-license)
-        ;; Check if they are floating (not attached to anything).
-        (unless (floating-attrib? object)
-          (blame-object object
-                        'error
-                        (format #f
-                                (_ "Found wrongly attached attribute: [~A=~A]\n")
-                                aname
-                                avalue))))
+           ((uref label email)
+            (blame-object object
+                          'warning
+                          (format #f (_ "Obsolete attribute: ~A") s)))
 
-       ;; All other attributes are unknown.
-       (else (blame-object object
-                           'warning
-                           (format #f
-                                   (_ "Found unknown ~A= attribute: [~A=~A]\n")
-                                   aname
-                                   aname
-                                   avalue)))))))
+           ;; Valid pin attributes.
+           ((pinlabel pintype pinseq pinnumber)
+            (if (or (floating-attrib? object)
+                    (not (pin? (attrib-attachment object))))
+                (blame-object object
+                              'error
+                              (format #f (_ "Misplaced pin attribute: ~A") s))))
+
+           ;; Valid attributes.
+           ((device graphical description author
+                    comment numslots slotdef footprint
+                    documentation refdes slot net
+                    value symversion dist-license use-license)
+
+            ;; Check if they are floating (not attached to anything).
+            (unless (floating-attrib? object)
+              (blame-object object
+                            'error
+                            (format #f (_ "Wrongly attached attribute: ~A") s))))
+
+           ;; All other attributes are unknown.
+           (else (blame-object object
+                               'warning
+                               (format #f (_ "Unknown attribute: ~A") s)))))))
 
 (define (check-floating-attrib-duplicates ls)
   "Checks for duplicated attributes in object list LS."
