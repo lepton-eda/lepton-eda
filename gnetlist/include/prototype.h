439e5c1b3a5deb947ccbee72ebed5895eafa3245
Vladimir Zhbanov <vzhbanov@gmail.com>
2017-06-20 20:36:12 +0300

    gnetlist: Finish rewriting `gnetlist' in Scheme and rename it to `lepton-netlist'.

--- gnetlist/include/prototype.h
+++
@@ -1,6 +0,0 @@
-/* gnetlist.c */
-void gnetlist_quit(void);
-void main_prog(void *closure, int argc, char *argv[]);
-int main(int argc, char *argv[]);
-/* parsecmd.c */
-int parse_commandline(int argc, char *argv[]);
