d09fdc9b74420394abebeddc0d04b8b8cd66da50
Vladimir Zhbanov <vzhbanov@gmail.com>
2018-09-09 16:02:46 +0300

    netlist: Update copyright header in the geda backend code.

--- netlist/scheme/gnet-geda.scm
+++ netlist/scheme/gnet-geda.scm
@@ -2,6 +2,7 @@
 ;;; gnetlist - gEDA Netlist
 ;;; Copyright (C) 1998-2010 Ales Hvezda
 ;;; Copyright (C) 1998-2010 gEDA Contributors (see ChangeLog for details)
+;;; Copyright (C) 2018 Lepton EDA Contributors
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
