65e1b72ef85a7ce116075dc5e2fdb49069bbc169
dmn <graahnul.grom@gmail.com>
2019-02-22 21:56:57 +0300

    schematic: Simplify code of hotkeys dialog.

--- schematic/src/gschem_hotkey_dialog.c
+++ schematic/src/gschem_hotkey_dialog.c
@@ -31,10 +31,6 @@
 
 #include "gschem.h"
 
-#define GLADE_HOOKUP_OBJECT(component,widget,name) \
-  g_object_set_data_full (G_OBJECT (component), name, \
-    gtk_widget_ref (widget), (GDestroyNotify) g_object_unref)
-
 
 
 /***************** Start of help/keymapping dialog box **************/
@@ -172,116 +168,118 @@ filter_setup (GtkTreeView* tree, GtkTreeModel* model, GtkEntry* entry)
  */
 void x_dialog_hotkeys (GschemToplevel *w_current)
 {
-  GtkWidget *vbox, *scrolled_win;
-  GtkTreeModel *store;
-  GtkWidget *treeview;
-  GtkCellRenderer *renderer;
-  GtkTreeViewColumn *column;
+  GtkWidget *vbox = NULL;
+  GtkWidget *scrolled_win = NULL;
+  GtkWidget *treeview = NULL;
+  GtkTreeModel *store = NULL;
+  GtkCellRenderer *renderer = NULL;
+  GtkTreeViewColumn *column = NULL;
 
-  if (!w_current->hkwindow) {
-    w_current->hkwindow = gschem_dialog_new_with_buttons(_("Hotkeys"),
-                                                         GTK_WINDOW(w_current->main_window),
-                                                         (GtkDialogFlags) 0, /* not modal */
-                                                         "hotkeys", w_current,
-                                                         GTK_STOCK_CLOSE,
-                                                         GTK_RESPONSE_NONE,
-                                                         NULL);
-
-    gtk_window_set_position (GTK_WINDOW (w_current->hkwindow), GTK_WIN_POS_NONE);
-
-
-    /* Just hide the dialog.
-     * Do not destroy it every time it's closed:
-    */
-    g_signal_connect (G_OBJECT (w_current->hkwindow), "response",
-                      G_CALLBACK (&gtk_widget_hide),
-                      NULL);
-
-    g_signal_connect (G_OBJECT (w_current->hkwindow), "delete-event",
-                      G_CALLBACK (&gtk_widget_hide_on_delete),
-                      NULL);
-
-
-    gtk_dialog_set_default_response(GTK_DIALOG(w_current->hkwindow),
-                                    GTK_RESPONSE_ACCEPT);
-
-    gtk_container_set_border_width (GTK_CONTAINER (w_current->hkwindow),
-                                    DIALOG_BORDER_SPACING);
-    gtk_widget_set_size_request (w_current->hkwindow, 300, 300);
-
-    vbox = GTK_DIALOG(w_current->hkwindow)->vbox;
-    gtk_box_set_spacing(GTK_BOX(vbox), DIALOG_V_SPACING);
-
-
-    /*
-    *  filter text entry, label and tooltip:
-    */
-    GtkWidget* entry = gtk_entry_new();
-    const gchar* txt = _("Start typing action name or keystroke to filter the list.\n"
-                         "Type hotkeys as they are displayed in \"Keystroke(s)\" column.");
-    gtk_widget_set_tooltip_text (entry, txt);
-
-    GtkWidget* label = gtk_label_new_with_mnemonic (_("_Filter:"));
-    gtk_label_set_mnemonic_widget (GTK_LABEL (label), entry);
-
-    GtkWidget* hbox = gtk_hbox_new (FALSE, 0);
-    gtk_box_pack_start (GTK_BOX (hbox), label, FALSE, FALSE, 5);
-    gtk_box_pack_start (GTK_BOX (hbox), entry, TRUE,  TRUE,  5);
-    gtk_box_pack_start (GTK_BOX (vbox), hbox,  FALSE, TRUE,  0);
-
-
-    scrolled_win = gtk_scrolled_window_new (NULL, NULL);
-    gtk_box_pack_start (GTK_BOX (vbox), scrolled_win, TRUE, TRUE, 0);
-    gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_win),
-                                    GTK_POLICY_AUTOMATIC,
-                                    GTK_POLICY_AUTOMATIC);
-
-    /* the model */
-    store = GTK_TREE_MODEL (gschem_hotkey_store_new ());
-
-    /* the tree view */
-    treeview = gtk_tree_view_new_with_model (store);
-    gtk_container_add(GTK_CONTAINER(scrolled_win), treeview);
-
-    /* the columns */
-    /* The first column contains the action's icon (if one was set)
-     * and its label. */
-    renderer = gtk_cell_renderer_pixbuf_new ();
-    column = gtk_tree_view_column_new_with_attributes (_("Action"),
-                                                       renderer,
-                                                       "stock-id",
-                                                       GSCHEM_HOTKEY_STORE_COLUMN_ICON,
-                                                       NULL);
-    /* Fix things up to show stock icons *and* theme icons. */
-    g_signal_connect (renderer, "notify::stock-id",
-                      G_CALLBACK (x_dialog_hotkeys_cell_stock_id_notify),
-                      NULL);
-
-    renderer = gtk_cell_renderer_text_new ();
-    gtk_tree_view_column_pack_start (column, renderer, FALSE);
-    gtk_tree_view_column_set_attributes (column, renderer,
-                                         "text", GSCHEM_HOTKEY_STORE_COLUMN_LABEL,
-                                         NULL);
-
-    /* The second column contains the action's keybinding */
-    gtk_tree_view_append_column (GTK_TREE_VIEW(treeview), column);
-    column = gtk_tree_view_column_new_with_attributes (_("Keystroke(s)"),
-                                                       renderer,
-                                                       "text",
-                                                       GSCHEM_HOTKEY_STORE_COLUMN_KEYS,
-                                                       NULL);
-    gtk_tree_view_append_column (GTK_TREE_VIEW(treeview), column);
-
-    /* setup tree filtering: */
-    filter_setup (GTK_TREE_VIEW (treeview), store, GTK_ENTRY (entry));
-
-    /* show all recursively */
-    gtk_widget_show_all(w_current->hkwindow);
+  if (w_current->hkwindow != NULL)
+  {
+    gtk_window_present (GTK_WINDOW (w_current->hkwindow));
+    return;
   }
 
-  else { /* dialog already created */
-    gtk_window_present(GTK_WINDOW(w_current->hkwindow));
-  }
-}
 
-/***************** End of help/keymapping dialog box ****************/
+  w_current->hkwindow = gschem_dialog_new_with_buttons(
+    _("Hotkeys"),
+    GTK_WINDOW (w_current->main_window),
+    (GtkDialogFlags) 0, /* not modal */
+    "hotkeys", w_current,
+    GTK_STOCK_CLOSE, GTK_RESPONSE_NONE,
+    NULL);
+
+  gtk_window_set_position (GTK_WINDOW (w_current->hkwindow), GTK_WIN_POS_NONE);
+
+
+  /* Just hide the dialog.
+   * Do not destroy it every time it's closed:
+  */
+  g_signal_connect (G_OBJECT (w_current->hkwindow), "response",
+                    G_CALLBACK (&gtk_widget_hide),
+                    NULL);
+
+  g_signal_connect (G_OBJECT (w_current->hkwindow), "delete-event",
+                    G_CALLBACK (&gtk_widget_hide_on_delete),
+                    NULL);
+
+
+  gtk_dialog_set_default_response(GTK_DIALOG(w_current->hkwindow),
+                                  GTK_RESPONSE_ACCEPT);
+
+  gtk_container_set_border_width (GTK_CONTAINER (w_current->hkwindow),
+                                  DIALOG_BORDER_SPACING);
+  gtk_widget_set_size_request (w_current->hkwindow, 300, 300);
+
+  vbox = GTK_DIALOG(w_current->hkwindow)->vbox;
+  gtk_box_set_spacing(GTK_BOX(vbox), DIALOG_V_SPACING);
+
+
+  /*
+  *  filter text entry, label and tooltip:
+  */
+  GtkWidget* entry = gtk_entry_new();
+  const gchar* txt = _("Start typing action name or keystroke to filter the list.\n"
+                       "Type hotkeys as they are displayed in \"Keystroke(s)\" column.");
+  gtk_widget_set_tooltip_text (entry, txt);
+
+  GtkWidget* label = gtk_label_new_with_mnemonic (_("_Filter:"));
+  gtk_label_set_mnemonic_widget (GTK_LABEL (label), entry);
+
+  GtkWidget* hbox = gtk_hbox_new (FALSE, 0);
+  gtk_box_pack_start (GTK_BOX (hbox), label, FALSE, FALSE, 5);
+  gtk_box_pack_start (GTK_BOX (hbox), entry, TRUE,  TRUE,  5);
+  gtk_box_pack_start (GTK_BOX (vbox), hbox,  FALSE, TRUE,  0);
+
+
+  scrolled_win = gtk_scrolled_window_new (NULL, NULL);
+  gtk_box_pack_start (GTK_BOX (vbox), scrolled_win, TRUE, TRUE, 0);
+  gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_win),
+                                  GTK_POLICY_AUTOMATIC,
+                                  GTK_POLICY_AUTOMATIC);
+
+  /* the model */
+  store = GTK_TREE_MODEL (gschem_hotkey_store_new ());
+
+  /* the tree view */
+  treeview = gtk_tree_view_new_with_model (store);
+  gtk_container_add(GTK_CONTAINER(scrolled_win), treeview);
+
+  /* the columns */
+  /* The first column contains the action's icon (if one was set)
+   * and its label. */
+  renderer = gtk_cell_renderer_pixbuf_new ();
+  column = gtk_tree_view_column_new_with_attributes (_("Action"),
+                                                     renderer,
+                                                     "stock-id",
+                                                     GSCHEM_HOTKEY_STORE_COLUMN_ICON,
+                                                     NULL);
+  /* Fix things up to show stock icons *and* theme icons. */
+  g_signal_connect (renderer, "notify::stock-id",
+                    G_CALLBACK (x_dialog_hotkeys_cell_stock_id_notify),
+                    NULL);
+
+  renderer = gtk_cell_renderer_text_new ();
+  gtk_tree_view_column_pack_start (column, renderer, FALSE);
+  gtk_tree_view_column_set_attributes (column, renderer,
+                                       "text", GSCHEM_HOTKEY_STORE_COLUMN_LABEL,
+                                       NULL);
+
+  /* The second column contains the action's keybinding */
+  gtk_tree_view_append_column (GTK_TREE_VIEW(treeview), column);
+  column = gtk_tree_view_column_new_with_attributes (_("Keystroke(s)"),
+                                                     renderer,
+                                                     "text",
+                                                     GSCHEM_HOTKEY_STORE_COLUMN_KEYS,
+                                                     NULL);
+  gtk_tree_view_append_column (GTK_TREE_VIEW(treeview), column);
+
+  /* setup tree filtering: */
+  filter_setup (GTK_TREE_VIEW (treeview), store, GTK_ENTRY (entry));
+
+  /* show all recursively */
+  gtk_widget_show_all(w_current->hkwindow);
+
+} /* x_dialog_hotkeys() */
+
