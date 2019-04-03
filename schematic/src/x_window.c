157d0664fea44d7d24eb5b8d381e2461859e45dd
dmn <graahnul.grom@gmail.com>
2019-04-03 10:47:37 +0300

    schematic: Log skipped untitled names.
    When constructing file name for new "untitled"
    page, the names of already opened files and
    existing files in current directory are taken
    into account. These names are not reused to
    prevent possible consequent data loss.
    Print such skipped names to the log when making
    new pages.

--- schematic/src/x_window.c
+++ schematic/src/x_window.c
@@ -97,7 +97,7 @@ static int
 untitled_next_index (GschemToplevel* w_current);
 
 static gchar*
-untitled_filename (GschemToplevel* w_current);
+untitled_filename (GschemToplevel* w_current, gboolean log_skipped);
 
 static PAGE*
 x_window_new_page (GschemToplevel* w_current);
@@ -1534,7 +1534,7 @@ x_window_new_page (GschemToplevel* w_current)
   g_return_val_if_fail (toplevel != NULL, NULL);
 
   /* New page file name: */
-  gchar* filename = untitled_filename (w_current);
+  gchar* filename = untitled_filename (w_current, TRUE);
 
   /* Create a new page: */
   PAGE* page = s_page_new (toplevel, filename);
@@ -1643,12 +1643,16 @@ untitled_next_index (GschemToplevel* w_current)
  *
  * Determine "untitled" schematic file name (used for new pages)
  * and build full path from this name and current working directory.
+ * When constructing this name, avoid reusing names of already opened
+ * files and existing files in current directory; if \a log_skipped
+ * is TRUE, report such (avoided) names to the log.
  *
- *  \param w_current The toplevel environment.
- *  \return          Newly-allocated untitled file path.
+ *  \param  w_current   The toplevel environment.
+ *  \param  log_skipped Print skipped file names to the log.
+ *  \return             Newly-allocated untitled file path.
  */
 static gchar*
-untitled_filename (GschemToplevel* w_current)
+untitled_filename (GschemToplevel* w_current, gboolean log_skipped)
 {
   g_return_val_if_fail (w_current != NULL, NULL);
 
@@ -1674,8 +1678,8 @@ untitled_filename (GschemToplevel* w_current)
      * TODO: define _("untitled") string globally somewhere:
     */
     fname = g_strdup_printf ("%s_%d.sch",
-                           name ? name : _("untitled"),
-                           untitled_next_index (w_current));
+                             name ? name : _("untitled"),
+                             untitled_next_index (w_current));
 
     /* Build full path for file name:
     */
@@ -1687,6 +1691,11 @@ untitled_filename (GschemToplevel* w_current)
     if ( s_page_search_by_basename (toplevel, fname) ||
          g_file_test (fpath, G_FILE_TEST_EXISTS) )
     {
+      if (log_skipped)
+      {
+        s_log_message (_("Skipping existing file [%s]"), fname);
+      }
+
       g_free (fname);
       g_free (fpath);
     }
