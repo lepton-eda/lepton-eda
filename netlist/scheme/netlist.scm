113c9b3ba3722dde2daa50211d79dea69cba110b
dmn <graahnul.grom@gmail.com>
2019-06-02 16:39:42 +0300

    netlist: Update --version output.

--- netlist/scheme/netlist.scm
+++ netlist/scheme/netlist.scm
@@ -1,7 +1,7 @@
 ;;; Lepton EDA netlister
 ;;; Copyright (C) 1998-2010 Ales Hvezda
 ;;; Copyright (C) 1998-2017 gEDA Contributors
-;;; Copyright (C) 2017-2018 Lepton EDA Contributors
+;;; Copyright (C) 2017-2019 Lepton EDA Contributors
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
@@ -100,7 +100,7 @@ to current standard output port and exit with exit status 0."
   (define version-msg "Lepton EDA/lepton-netlist ~A~A.~A (git: ~A)\n")
 
   (define copyright-msg (_ "Copyright (C) 1998-2016 gEDA developers
-Copyright (C) 2017-2018 Lepton EDA Contributors
+Copyright (C) 2017-2019 Lepton EDA Contributors
 This is free software, and you are welcome to redistribute it under
 certain conditions. For details, see the file `COPYING', which is
 included in the Lepton EDA distribution.
