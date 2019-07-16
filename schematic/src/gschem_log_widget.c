lepton-eda/lepton-eda

commit ec094906d718944b822c4d63e49d1b53b9e683de

schematic: Fix signal handling in log widget code.
Every instance of GschemLogWidget installs signal
handler for the "changed" signal of GtkTextBuffer.
The text buffer is a member of
GschemLogWidgetClass structure, so it is shared
among all instances of GschemLogWidget.
When the latter is destroyed, the signal handler
should be disconnected.

schematic/src/gschem_log_widget.c
@@ -55,6 +55,9 @@ populate_popup_menu (GtkTextView* txtview,
                      GtkMenu*     menu,
                      gpointer     data);
 
+static void
+finalize (GObject* object);
+
 
 
 /*! \brief create a new status log widget
@@ -154,7 +157,9 @@ static void
 gschem_log_widget_class_init (GschemLogWidgetClass *klass)
 {
   gchar *contents;
-/*   GObjectClass *gobject_class = G_OBJECT_CLASS (klass); */
+  GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+
+  gobject_class->finalize = &finalize;
 
   klass->buffer = create_text_buffer ();
 
@@ -487,3 +492,18 @@ populate_popup_menu (GtkTextView* txtview,
 
 } /* populate_popup_menu() */
 
+
+
+/*! \brief "destructor" function for gobject instance
+ */
+static void
+finalize (GObject* object)
+{
+  GschemLogWidgetClass* cls = GSCHEM_LOG_WIDGET_GET_CLASS (object);
+
+  /* disconnect the "changed" signal handler:
+  */
+  g_signal_handlers_disconnect_by_func (cls->buffer, &changed_cb, object);
+
+  G_OBJECT_CLASS (gschem_log_widget_parent_class)->finalize (object);
+}

commit 3a046220e29c6cbf6247a1d53b4288abed38d33c

schematic: Fix compilation errors triggered when built with g++.
The issue has been introduced in commit ec09490.

schematic/src/gschem_log_widget.c
@@ -503,7 +503,7 @@ finalize (GObject* object)
 
   /* disconnect the "changed" signal handler:
   */
-  g_signal_handlers_disconnect_by_func (cls->buffer, &changed_cb, object);
+  g_signal_handlers_disconnect_by_func (cls->buffer, (gpointer) changed_cb, object);
 
   G_OBJECT_CLASS (gschem_log_widget_parent_class)->finalize (object);
 }