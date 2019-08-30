3ee40e1447e30612d3152b7bebc08bd236a59163
Vladimir Zhbanov <vzhbanov@gmail.com>
2019-08-30 11:31:15 +0300

    liblepton: Add unit tests for the deprecated module (geda page).

--- liblepton/scheme/unit-tests/t0203-page-string-syntax.scm
+++ liblepton/scheme/unit-tests/t0203-page-string-syntax.scm
@@ -1,9 +1,17 @@
 ;; Test Scheme procedures related to converting pages to & from
 ;; strings.
 
-(use-modules (unit-test))
-(use-modules (geda page))
-(use-modules (geda object))
+(use-modules (unit-test)
+             (geda object)
+             ((geda page) #:renamer (symbol-prefix-proc 'geda:))
+             (lepton page))
 
 (begin-test 'string->page
   (assert-thrown 'string-format (string->page "/test/page/A" "__GARBAGE__")))
+
+
+;;; The same tests for the deprecated (geda page) module
+;;; functions.
+
+(begin-test 'geda:string->page
+  (assert-thrown 'string-format (geda:string->page "/test/page/A" "__GARBAGE__")))
