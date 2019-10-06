31d45d2fcb49583a017f6551d390151fd3c8e15b
dmn <graahnul.grom@gmail.com>
2019-10-06 20:37:45 +0300

    examples: Fix gTAG script and config files.
    - fix files list and program name in crdist.sh
    - remove gnetlistrc file with deprecated options,
      add geda.conf with the same settings

--- examples/gTAG/crdist.sh
+++ examples/gTAG/crdist.sh
@@ -4,9 +4,9 @@ DATETAG=`date '+%Y%m%d'`
 DISTFILE=gTAG-sch-${DATETAG}
 
 rm -f gTAG-sch-*.tar.gz gTAG.bom gTAG.pcb
-gnetlist -g bom -o gTAG.bom gTAG.sch
-gnetlist -g PCB -o gTAG-pcb.net gTAG.sch
+lepton-netlist -g bom -o gTAG.bom gTAG.sch
+lepton-netlist -g PCB -o gTAG-pcb.net gTAG.sch
 cd ..
-tar cf gTAG/${DISTFILE}.tar gTAG/*.sym gTAG/*.sch gTAG/*rc gTAG/*.ps gTAG/gTAG.bom gTAG/gTAG-pcb.net  gTAG/README gTAG/COPYING gTAG/ChangeLog gTAG/crdist.sh
+tar cf gTAG/${DISTFILE}.tar gTAG/*.sym gTAG/*.sch gTAG/*rc gTAG/*.ps gTAG/gTAG.bom gTAG/gTAG-pcb.net  gTAG/README gTAG/ChangeLog-1.0 gTAG/crdist.sh
 cd gTAG
 gzip ${DISTFILE}.tar
