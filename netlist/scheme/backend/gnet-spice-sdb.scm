966e61a599a92b64f7eea7b22bd90ddddb912a44
Vladimir Zhbanov <vzhbanov@gmail.com>
2019-10-11 13:08:02 +0300

    netlist: Fix crashes of the spice-sdb backend on IO components.
    The crashes were triggered by wrong variable names in the
    "spice-sdb:get-spice-io-pins" procedure.

--- netlist/scheme/backend/gnet-spice-sdb.scm
+++ netlist/scheme/backend/gnet-spice-sdb.scm
@@ -209,7 +209,7 @@
     (and (string-ci=? (gnetlist:get-package-attribute package "device")
                       "spice-io")
          package))
-  (filter-map spice-io? ls))
+  (filter-map spice-io? package-list))
 
 
 ;;; Given a list of spice-IO packages (refdeses), this function
