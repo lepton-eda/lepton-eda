c6cfe3bbeeddb760613b64c0ada96628ff05c7f9
dmn <graahnul.grom@gmail.com>
2019-10-24 00:57:09 +0300

    examples/gTAG: Amend the crdist.sh script.
    - paths have changed
    - include *.conf files in archive

--- examples/gTAG/crdist.sh
+++ examples/gTAG/crdist.sh
@@ -3,10 +3,10 @@
 DATETAG=`date '+%Y%m%d'`
 DISTFILE=gTAG-sch-${DATETAG}
 
-rm -f gTAG-sch-*.tar.gz gTAG.bom gTAG.pcb
+rm -f gTAG-sch-*.tar.gz
 lepton-netlist -g bom -o gTAG.bom gTAG.sch
 lepton-netlist -g PCB -o gTAG-pcb.net gTAG.sch
 cd ..
-tar cf gTAG/${DISTFILE}.tar gTAG/*.sym gTAG/*.sch gTAG/*rc gTAG/*.ps gTAG/gTAG.bom gTAG/gTAG-pcb.net  gTAG/README gTAG/ChangeLog-1.0 gTAG/crdist.sh
+tar cf gTAG/${DISTFILE}.tar gTAG/sym/*.sym gTAG/*.sch gTAG/*rc gTAG/*conf gTAG/ps/*.ps gTAG/gTAG.bom gTAG/gTAG-pcb.net gTAG/README gTAG/ChangeLog-1.0 gTAG/crdist.sh
 cd gTAG
 gzip ${DISTFILE}.tar
