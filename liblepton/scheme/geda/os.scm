681014a24cb77d528ca2c5232c47f62c499574a2
Vladimir Zhbanov <vzhbanov@gmail.com>
2019-11-18 13:21:04 +0300

    liblepton: Add new Scheme procedure user-cache-dir().
    The procedure returns the directory name where per-user cache data
    should be stored.

--- liblepton/scheme/geda/os.scm
+++ liblepton/scheme/geda/os.scm
@@ -49,6 +49,7 @@
 (define-public sys-config-dirs %sys-config-dirs)
 (define-public user-data-dir %user-data-dir)
 (define-public user-config-dir %user-config-dir)
+(define-public user-cache-dir %user-cache-dir)
 
 (define-public expand-env-variables
   ;; Only compile regular expression once
