03c9d755ed128e02f9b37fc3a07623d210b659cc
Vladimir Zhbanov <vzhbanov@gmail.com>
2019-12-03 16:51:06 +0300

    liblepton: Add more tests for component-library-search().

--- netlist/scheme/unit-tests/test-component-library-search.scm
+++ netlist/scheme/unit-tests/test-component-library-search.scm
@@ -28,11 +28,13 @@
     (mkdir (make-filename *toplevel-dir* "a"))
     (mkdir (make-filename *toplevel-dir* "b"))
     (mkdir (make-filename *toplevel-dir* "b" "b"))
+    (mkdir (make-filename *toplevel-dir* "b" "b" "c"))
     ;; Make symbol files.
     (touch (make-filename *toplevel-dir* "toplevel.sym"))
     (touch (make-filename *toplevel-dir* "a" "a.sym"))
     (touch (make-filename *toplevel-dir* "b" "b.sym"))
-    (touch (make-filename *toplevel-dir* "b" "b" "bb.sym")))
+    (touch (make-filename *toplevel-dir* "b" "b" "bb.sym"))
+    (touch (make-filename *toplevel-dir* "b" "b" "c" "bbc.sym")))
 
   ;; Test body.
   (lambda ()
@@ -43,18 +45,52 @@
     ;; Just check that all symbols but the toplevel one exist in
     ;; the component library created.
     (test-assert
-        (and (absolute-component-name "a.sym")
+        (and (absolute-component-name "toplevel.sym")
+             (absolute-component-name "a.sym")
              (absolute-component-name "b.sym")
-             (absolute-component-name "bb.sym")))
-
-    (test-assert (not (absolute-component-name "toplevel.sym")))
+             (absolute-component-name "bb.sym")
+             (absolute-component-name "bbc.sym")))
 
     ;; The same as above but the directory ends with "/".
     (reset-component-library)
     (component-library-search (string-append *toplevel-dir*
                                              file-name-separator-string))
 
-    (test-assert (not (absolute-component-name "toplevel.sym"))))
+    (test-assert (absolute-component-name "toplevel.sym"))
+
+
+    ;; Test of libraries with same directory basenames.
+    (reset-component-library)
+    (component-library-search (string-append (make-filename *toplevel-dir* "b" "b")
+                                             file-name-separator-string)
+                              "myprefix")
+    (test-assert (absolute-component-name "bbc.sym"))
+
+    ;; Test of toplevel library with symbols without prefix.
+    (reset-component-library)
+
+    (component-library-search *toplevel-dir*)
+
+    (let ((libs (filter
+                 (lambda (lib) (string= (symbol-library-path lib) *toplevel-dir*))
+                 (component-libraries))))
+      (test-equal (map symbol-library-name libs)
+        (list (basename *toplevel-dir*))))
+
+    ;; Test of toplevel library with symbols with prefix.
+    (reset-component-library)
+
+    (component-library-search *toplevel-dir* "xxx")
+
+    (let ((libs (component-libraries))
+          ;; On *nix the following should result in
+          ;; '("xxx" "xxxa" "xxxb" "xxxb/b" "xxxb/b/c")
+          (result (map (lambda (x) (string-append "xxx" x))
+                       (map (lambda (ls) (string-join ls file-name-separator-string))
+                            '(("") ("a") ("b") ("b" "b") ("b" "b" "c"))))))
+      (test-eq (length libs) 5)
+      (test-equal (sort (map symbol-library-name libs) string<)
+        result)))
 
   ;; Get rid of the test directory.
   (lambda ()
