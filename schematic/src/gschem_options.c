a6b6ab034cc97412c0e87d970bf904be1ffc8793
Vladimir Zhbanov <vzhbanov@gmail.com>
2019-12-11 21:24:30 +0300

    schematic: Fix a couple of comments.

--- schematic/src/gschem_options.c
+++ schematic/src/gschem_options.c
@@ -167,10 +167,10 @@ gschem_options_get_magnetic_net_mode (GschemOptions *options)
 
 
 
-/*! \brief Get the magnetic net mode
+/*! \brief Get the rubber-band net mode
  *
  *  \param [in] options These options
- *  \return The magnetic net mode
+ *  \return The rubber-band net mode
  */
 gboolean
 gschem_options_get_net_rubber_band_mode (GschemOptions *options)
