9e20e2e5bad9b9cc704564e41cb2f18b2d0645b1
dmn <graahnul.grom@gmail.com>
2020-01-05 01:05:21 +0300

    schematic: Add bus_ripper_symname toplevel field.
    Add bus_ripper_symname toplevel field (gchar*)
    to the st_gschem_toplevel structure.

--- schematic/include/gschem_toplevel.h
+++ schematic/include/gschem_toplevel.h
@@ -241,6 +241,8 @@ struct st_gschem_toplevel {
   int zoom_gain;          /* Percentage increase in size for a zoom-in operation */
   int scrollpan_steps;    /* Number of scroll pan events required to traverse the viewed area */
 
+  gchar* bus_ripper_symname; /* default bus ripper symbol file name */
+
   gboolean dont_invalidate;
 
   SCM smob;               /* The Scheme representation of this window */
