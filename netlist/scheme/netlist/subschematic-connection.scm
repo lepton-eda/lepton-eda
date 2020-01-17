191ae5a8272ef24329b39f5fff738ccfa0006354
Vladimir Zhbanov <vzhbanov@gmail.com>
2020-01-17 09:19:14 +0300

    netlist: Update copyrights.

--- netlist/scheme/netlist/subschematic-connection.scm
+++ netlist/scheme/netlist/subschematic-connection.scm
@@ -1,5 +1,5 @@
 ;;; Lepton EDA netlister
-;;; Copyright (C) 2019 Lepton EDA Contributors
+;;; Copyright (C) 2019-2020 Lepton EDA Contributors
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
