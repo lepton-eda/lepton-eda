2b13dbaf5777e4b76216de677dd92c82a1a03c81
dmn <graahnul.grom@gmail.com>
2020-01-30 20:43:51 +0300

    schematic: Fix color editor widget resizing.
    Prevent vertical expansion of the colors combo
    box in the color editor widget.

--- schematic/src/font_select_widget.c
+++ schematic/src/font_select_widget.c
@@ -1,6 +1,6 @@
 /* Lepton EDA Schematic Capture
  * Copyright (C) 2018 dmn <graahnul.grom@gmail.com>
- * Copyright (C) 2018-2019 Lepton EDA Contributors
+ * Copyright (C) 2018-2020 Lepton EDA Contributors
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
