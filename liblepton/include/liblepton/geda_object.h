de28c6fb7d0d1722dff6e7570b20c93ebd2f4435
Vladimir Zhbanov <vzhbanov@gmail.com>
2020-02-01 18:09:38 +0300

    rename: the type 'COMPLEX' to 'COMPONENT'.

--- liblepton/include/liblepton/geda_object.h
+++ liblepton/include/liblepton/geda_object.h
@@ -1,7 +1,7 @@
-/* gEDA - GPL Electronic Design Automation
- * libgeda - gEDA's library
+/* Lepton EDA library
  * Copyright (C) 1998-2010 Ales Hvezda
- * Copyright (C) 1998-2010 gEDA Contributors (see ChangeLog for details)
+ * Copyright (C) 1998-2016 gEDA Contributors
+ * Copyright (C) 2017-2020 Lepton EDA Contributors
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -33,7 +33,7 @@ struct st_object
   GedaBounds bounds;
   TOPLEVEL *w_bounds_valid_for;
 
-  COMPLEX *complex;
+  COMPONENT *complex;
   GedaLine *line;
   GedaCircle *circle;
   GedaArc *arc;
