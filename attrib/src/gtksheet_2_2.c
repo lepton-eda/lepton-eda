f8a7294da2de3d924681adb2730d5f2b6d23a13d
Vladimir Zhbanov <vzhbanov@gmail.com>
2020-03-10 16:54:07 +0300

    attrib: Update copyrights.

--- attrib/src/gtksheet_2_2.c
+++ attrib/src/gtksheet_2_2.c
@@ -2,7 +2,7 @@
  * GtkSheet widget for Gtk+.
  * Copyright (C) 1999-2001 Adrian E. Feiguin <adrian@ifir.ifir.edu.ar>
  * Copyright (C) 2004-2015 gEDA Contributors
- * Copyright (C) 2017-2018 Lepton EDA Contributors
+ * Copyright (C) 2017-2020 Lepton EDA Contributors
  *
  * Based on GtkClist widget by Jay Painter, but major changes.
  * Memory allocation routines inspired on SC (Spreadsheet Calculator)
