420969eb94d4f3accf2d5d782fe49565fc0d83b1
Vladimir Zhbanov <vzhbanov@gmail.com>
2020-03-10 20:17:59 +0300

    liblepton: Update copyrights.

--- liblepton/src/edaconfig.c
+++ liblepton/src/edaconfig.c
@@ -1,7 +1,7 @@
 /* Lepton EDA library
  * Copyright (C) 2011-2012 gEDA Contributors
  * Copyright (C) 2016 Peter Brett <peter@peter-b.co.uk>
- * Copyright (C) 2017-2019 Lepton EDA Contributors
+ * Copyright (C) 2017-2020 Lepton EDA Contributors
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
