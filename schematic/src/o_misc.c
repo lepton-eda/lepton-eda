c5be0f3a952caef6a4394b84dacda18bf523a4cb
Vladimir Zhbanov <vzhbanov@gmail.com>
2020-03-14 18:56:35 +0300

    liblepton: Get rid of TOPLEVEL argument in s_page_replace().

--- schematic/src/o_misc.c
+++ schematic/src/o_misc.c
@@ -528,7 +528,7 @@ o_update_component (GschemToplevel *w_current, OBJECT *o_current)
   s_slot_update_object (toplevel, o_new);
 
   /* Replace old OBJECT with new OBJECT */
-  s_page_replace (toplevel, page, o_current, o_new);
+  s_page_replace (page, o_current, o_new);
   s_delete_object (o_current);
 
   /* Select new OBJECT */
