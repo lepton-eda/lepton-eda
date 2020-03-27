6a952b50cdb2379320f8e6b73d309d1ccb3468f5
Vladimir Zhbanov <vzhbanov@gmail.com>
2020-03-27 03:02:14 +0300

    schematic: Simplify procedure getting bounds of text objects.
    The third parameter of the function, 'user_data', where it is
    used, is replaced with EdaRenderer object.  This allows to get rid
    of using TOPLEVEL and GschemToplevel inside the function.

--- schematic/src/x_window.c
+++ schematic/src/x_window.c
@@ -1021,7 +1021,8 @@ GschemToplevel* x_window_new (TOPLEVEL *toplevel)
   gschem_toplevel_get_toplevel (w_current)->load_newer_backup_data = w_current;
 
   o_text_set_rendered_bounds_func (gschem_toplevel_get_toplevel (w_current),
-                                   o_text_get_rendered_bounds, w_current);
+                                   o_text_get_rendered_bounds,
+                                   w_current->renderer);
 
   /* Damage notifications should invalidate the object on screen */
   o_add_change_notify (gschem_toplevel_get_toplevel (w_current),
