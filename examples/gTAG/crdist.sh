14e44885f7ef2069cb1005d86944fa655c6d1de2
Vladimir Zhbanov <vzhbanov@gmail.com>
2020-04-01 21:11:34 +0300

    docs: Move all ChangeLog-1.0 files into one directory and rename them.
    Those files are old enough and the user can find the same
    information using 'git'.  Many tools have changed significantly
    since the logs were generated, and their code is now in other
    places, so the files just cluttered up Makefiles.

--- examples/gTAG/crdist.sh
+++ examples/gTAG/crdist.sh
@@ -7,6 +7,6 @@ rm -f gTAG-sch-*.tar.gz
 lepton-netlist -g bom -o gTAG.bom gTAG.sch
 lepton-netlist -g PCB -o gTAG-pcb.net gTAG.sch
 cd ..
-tar cf gTAG/${DISTFILE}.tar gTAG/sym/*.sym gTAG/*.sch gTAG/*rc gTAG/*conf gTAG/ps/*.ps gTAG/gTAG.bom gTAG/gTAG-pcb.net gTAG/README gTAG/ChangeLog-1.0 gTAG/crdist.sh
+tar cf gTAG/${DISTFILE}.tar gTAG/sym/*.sym gTAG/*.sch gTAG/*rc gTAG/*conf gTAG/ps/*.ps gTAG/gTAG.bom gTAG/gTAG-pcb.net gTAG/README gTAG/crdist.sh
 cd gTAG
 gzip ${DISTFILE}.tar
