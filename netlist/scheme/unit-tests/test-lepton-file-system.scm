d29f4f711d6648a950caafdc1a5e1fe7c96a75ea
Vladimir Zhbanov <vzhbanov@gmail.com>
2020-04-28 19:04:11 +0300

    tests: Use SRFI-64 to test (lepton file-system).

---
+++ netlist/scheme/unit-tests/test-lepton-file-system.scm
@@ -0,0 +1,13 @@
+(use-modules (lepton file-system))
+
+;;; Makes blatant assumptions about the current directory. Oh
+;;; well.
+(test-begin "regular-file?" 1)
+(test-assert (regular-file? "Makefile"))
+(test-assert (not (regular-file? ".")))
+(test-end "regular-file?")
+
+(test-begin "directory?" 1)
+(test-assert (directory? "."))
+(test-assert (not (directory? "Makefile")))
+(test-end "regular-file?")
