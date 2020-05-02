61febea55c90a92bd68828283e363bf610a78956
Vladimir Zhbanov <vzhbanov@gmail.com>
2020-05-02 16:34:01 +0300

    tests: Use SRFI-64 to test functions related to basic page operations.

---
+++ netlist/scheme/unit-tests/test-lepton-page-basic.scm
@@ -0,0 +1,93 @@
+;;; Test Scheme procedures related to pages.
+
+(use-modules (lepton attrib)
+             (lepton object)
+             (lepton page))
+
+(test-begin "page")
+
+(let ((page-a (make-page "/test/page/A"))
+      (page-b (make-page "/test/page/B")))
+  (test-assert (page? page-a))
+
+  (test-equal "/test/page/A" (page-filename page-a))
+  (test-equal (list page-a page-b) (active-pages))
+
+  (test-equal page-a (set-page-filename! page-a "/test/page/C"))
+  (test-equal "/test/page/C" (page-filename page-a))
+
+  (close-page! page-a)
+  (test-equal (list page-b) (active-pages))
+  (close-page! page-b))
+
+(test-end "page")
+
+
+(test-begin "page-append")
+
+(let ((A (make-page "/test/page/C"))
+      (B (make-page "/test/page/D"))
+      (x (make-line '(0 . 0) '(1 . 2)))
+      (y (make-line '(0 . 1) '(2 . 2))))
+
+  (dynamic-wind                   ; Make sure pages are cleaned up
+    (lambda () #f)
+    (lambda ()
+      (test-equal '() (page-contents A))
+
+      (test-equal A (page-append! A x))
+      (test-equal (list x) (page-contents A))
+
+      (test-equal A (page-append! A x y))
+      (test-equal (list x y) (page-contents A))
+
+      (test-assert-thrown 'object-state
+                          (page-append! B x))
+
+      (test-assert-thrown 'object-state
+                          (let* ((C (make-component "test component" '(1 . 2) 0 #t #f))
+                                 (z (make-line '(1 . 0) '(2 . 2))))
+                            (component-append! C z)
+                            (page-append! A z))))
+
+    (lambda ()
+      (close-page! A)
+      (close-page! B))))
+
+(test-end "page-append")
+
+
+(test-begin "page-remove")
+
+(let ((A (make-page "/test/page/E"))
+      (B (make-page "/test/page/F"))
+      (C (make-component "test component" '(1 . 2) 0 #t #f))
+      (x (make-line '(0 . 0) '(2 . 0)))
+      (y (make-line '(0 . 0) '(0 . 2)))
+      (z (make-line '(1 . 0) '(2 . 2))))
+
+  (dynamic-wind                   ; Make sure pages are cleaned up
+    (lambda () #f)
+    (lambda ()
+      (page-append! A x)
+      (test-equal A (page-remove! A x))
+      (test-equal '() (page-contents A))
+      (test-equal A (page-remove! A x))
+      (test-equal B (page-remove! B x))
+
+      (page-append! A x y)
+      (test-equal A (page-remove! A x))
+      (test-equal (list y) (page-contents A))
+
+      (test-assert-thrown 'object-state
+                          (page-remove! B y))
+
+      (component-append! C z)
+      (test-assert-thrown 'object-state
+                          (page-remove! A z)))
+
+    (lambda ()
+      (close-page! A)
+      (close-page! B))))
+
+(test-end "page-remove")
