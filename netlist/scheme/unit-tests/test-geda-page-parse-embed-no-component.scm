156c4403651826f90ebaf3b8c1e2cc21508a3a86
Vladimir Zhbanov <vzhbanov@gmail.com>
2020-05-02 19:19:02 +0300

    tests: Use SRFI-64 to test functions related to parsing of wrong embedded object.

---
+++ netlist/scheme/unit-tests/test-geda-page-parse-embed-no-component.scm
@@ -0,0 +1,18 @@
+;;; Test that loading a schematic file returns an error when an
+;;; embedded component data section is found without a
+;;; corresponding object.
+
+(use-modules ((geda page) #:renamer (symbol-prefix-proc 'geda:)))
+
+(define test-page
+"v 20111231 2
+[
+L 0 0 1000 0 3 0 0 0 -1 -1
+]
+")
+
+(test-begin "geda:parse-embedded-section-without-component")
+
+(test-assert-thrown 'string-format (geda:string->page "test/page/A" test-page))
+
+(test-end "geda:parse-embedded-section-without-component")
