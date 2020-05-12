7a6519b6ddd65eaeeeb35fbe3c7c5185dfd11af7
Vladimir Zhbanov <vzhbanov@gmail.com>
2020-05-12 22:32:38 +0300

    Update copyrights in modified files.

--- liblepton/src/scheme_version.c
+++ liblepton/src/scheme_version.c
@@ -1,5 +1,5 @@
 /* Lepton EDA library - Scheme API
- * Copyright (C) 2017-2019 Lepton Developers
+ * Copyright (C) 2017-2020 Lepton Developers
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
