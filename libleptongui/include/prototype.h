9812fdfd0aebe79e65b2f4e47d87648906901f71
Vladimir Zhbanov <vzhbanov@gmail.com>
2020-06-24 21:01:07 +0300

    schematic: Get rid of parsecmd.c.

--- libleptongui/include/prototype.h
+++ libleptongui/include/prototype.h
@@ -522,8 +522,6 @@ char *o_undo_find_prev_filename(UNDO *start);
 GList *o_undo_find_prev_object_head(UNDO *start);
 void o_undo_callback(GschemToplevel *w_current, PAGE *page, int type);
 void o_undo_cleanup(void);
-/* parsecmd.c */
-int parse_commandline(int argc, char *argv[]);
 /* s_stretch.c */
 GList *s_stretch_add(GList *list, OBJECT *object, int whichone);
 GList *s_stretch_remove(GList *list, OBJECT *object);
