004b55118aded9e5449722e789e81eeb4d13d270
Vladimir Zhbanov <vzhbanov@gmail.com>
2020-08-24 22:57:31 +0300

    gui: Get rid of gtk_dialog_set_has_separator().
    It has been deprecated since Gtk+ 2.22.

--- libleptongui/src/gschem_dialog.c
+++ libleptongui/src/gschem_dialog.c
@@ -426,9 +426,6 @@ static GtkWidget* gschem_dialog_new_empty (const gchar     *title,
   if (flags & GTK_DIALOG_DESTROY_WITH_PARENT)
     gtk_window_set_destroy_with_parent (GTK_WINDOW (dialog), TRUE);
 
-  if (flags & GTK_DIALOG_NO_SEPARATOR)
-    gtk_dialog_set_has_separator (GTK_DIALOG (dialog), FALSE);
-
   return GTK_WIDGET (dialog);
 }
 
