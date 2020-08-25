f5a22171cb2692ceedd7da58a5355c1b954aa0d8
Vladimir Zhbanov <vzhbanov@gmail.com>
2020-08-25 21:02:28 +0300

    gui: Use getter for GtkWidget's 'window' field.

--- libleptongui/src/gschem_accel_label.c
+++ libleptongui/src/gschem_accel_label.c
@@ -254,7 +254,7 @@ gschem_accel_label_expose_event (GtkWidget      *widget,
           y += get_first_baseline (label_layout) - get_first_baseline (accel_layout);
 
           gtk_paint_layout (widget->style,
-                            widget->window,
+                            gtk_widget_get_window (widget),
                             gtk_widget_get_state (widget),
                             FALSE,
                             &event->area,
