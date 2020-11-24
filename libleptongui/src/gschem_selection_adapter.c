aebb9feef995ddbca2219b16fe0a02dadb7c1c5e
dmn <graahnul.grom@gmail.com>
2020-11-24 12:44:08 +0300

    gui: Allow changing of bus colors in GUI.
    Allow users to change a color of bus objects
    via the standard color selection combo box of the
    "Object Properties" dialog in lepton-schematic GUI.

--- libleptongui/src/gschem_selection_adapter.c
+++ libleptongui/src/gschem_selection_adapter.c
@@ -653,7 +653,8 @@ gschem_selection_adapter_get_object_color (GschemSelectionAdapter *adapter)
         (object->type == OBJ_LINE)   ||
         (object->type == OBJ_PATH)   ||
         (object->type == OBJ_TEXT)   ||
-        (object->type == OBJ_NET))) {
+        (object->type == OBJ_NET)    ||
+        (object->type == OBJ_BUS))) {
       color = geda_object_get_color (object);
       break;
     }
