ef56fead321942b68199f527a30ed97384f92a8c
Vladimir Zhbanov <vzhbanov@gmail.com>
2021-01-29 19:34:59 +0300

    its[sch2pcb]: Add a test of pcb creation.
    In this test only m4 elements are used.

---
+++ tools/sch2pcb/tests/one.cmd
@@ -0,0 +1,36 @@
+# Pin name action command file
+
+# Start of element CONN201
+ChangePinName(CONN201, 1, 1)
+ChangePinName(CONN201, 2, 2)
+
+# Start of element Q201
+ChangePinName(Q201, 1, 1)
+ChangePinName(Q201, 2, 2)
+ChangePinName(Q201, 3, 3)
+
+# Start of element R101
+ChangePinName(R101, 1, 1)
+ChangePinName(R101, 2, 2)
+
+# Start of element R102
+ChangePinName(R102, 1, 1)
+ChangePinName(R102, 2, 2)
+
+# Start of element R103
+ChangePinName(R103, 1, 1)
+ChangePinName(R103, 2, 2)
+
+# Start of element R201
+ChangePinName(R201, 1, 1)
+ChangePinName(R201, 2, 2)
+
+# Start of element U101
+ChangePinName(U101, 1, OUT)
+ChangePinName(U101, 2, IN-)
+ChangePinName(U101, 3, IN+)
+ChangePinName(U101, 4, V-)
+ChangePinName(U101, 5, IN+)
+ChangePinName(U101, 6, IN-)
+ChangePinName(U101, 7, OUT)
+ChangePinName(U101, 8, V+)
