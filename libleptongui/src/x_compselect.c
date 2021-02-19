lepton-eda/lepton-eda

commit e542a0a50360f88dbf6d0f234653b894727e6c94

gui: Get rid of unused variable of deprecated GtkOptionMenu type.

libleptongui/src/x_compselect.c
@@ -683,12 +683,10 @@ compselect_callback_filter_button_clicked (GtkButton *button,
  *  that the requested behavior for the next adding of a component has
  *  been changed.
  *
- *  \param [in] optionmenu The behavior option menu.
  *  \param [in] user_data  The component selection dialog.
  */
 static void
-compselect_callback_behavior_changed (GtkOptionMenu *optionmenu,
-                                      gpointer user_data)
+compselect_callback_behavior_changed (gpointer user_data)
 {
   Compselect *compselect = (Compselect*)user_data;
 

commit 0ce7f1f33df16ef324993bb2db46e9a4316958b7

gui: Fix behaviour of the lower combobox in the Component select dialog.
I broke it myself in the commit e542a0a before 1.9.12.  While
getting rid of a wrong and deprecated type was OK, I shouldn't
have changed the number of callback arguments as the signal
"changed" requires exactly two.

libleptongui/src/x_compselect.c
@@ -683,10 +683,12 @@ compselect_callback_filter_button_clicked (GtkButton *button,
  *  that the requested behavior for the next adding of a component has
  *  been changed.
  *
+ *  \param [in] optionmenu The behavior option menu.
  *  \param [in] user_data  The component selection dialog.
  */
 static void
-compselect_callback_behavior_changed (gpointer user_data)
+compselect_callback_behavior_changed (GtkComboBox *widget,
+                                      gpointer user_data)
 {
   Compselect *compselect = (Compselect*)user_data;
 