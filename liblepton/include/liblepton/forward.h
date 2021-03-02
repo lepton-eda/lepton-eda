f3ede851803633ca4af16f0024c10a09c76c033a
Vladimir Zhbanov <vzhbanov@gmail.com>
2021-03-02 07:25:57 +0300

    lib: Actually get rid of the type TOPLEVEL.

--- liblepton/include/liblepton/forward.h
+++ liblepton/include/liblepton/forward.h
@@ -25,7 +25,6 @@ typedef struct st_object LeptonObject;
 typedef struct st_page LeptonPage;
 
 typedef struct st_toplevel LeptonToplevel;
-typedef struct st_toplevel TOPLEVEL;
 
 typedef struct st_undo LeptonUndo;
 typedef struct st_undo UNDO;
