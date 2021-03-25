5c27df58a8e5a1b6eff9185e0ab465898f0a11c9
Vladimir Zhbanov <vzhbanov@gmail.com>
2021-03-26 02:48:37 +0300

    lepton: Add more tests for wrong arguments in set-arc!().

--- liblepton/scheme/unit-tests/lepton-object-arc.scm
+++ liblepton/scheme/unit-tests/lepton-object-arc.scm
@@ -52,6 +52,24 @@
 (test-assert-thrown 'wrong-type-arg (arc-sweep-angle 'a))
 (test-assert-thrown 'wrong-type-arg (arc-end-angle 'a))
 
+(let ((a (make-arc '(1 . 2) 3 45 90 3)))
+  ;; Wrong object.
+  (test-assert-thrown 'wrong-type-arg (set-arc! 'a '(1 . 2) 3 45 90 3))
+  ;; Wrong coord.
+  (test-assert-thrown 'wrong-type-arg (set-arc! a 'c 3 45 90 3))
+  ;; Wrong x.
+  (test-assert-thrown 'wrong-type-arg (set-arc! a '(x . 2) 3 45 90 3))
+  ;; Wrong y.
+  (test-assert-thrown 'wrong-type-arg (set-arc! a '(1 . y) 3 45 90 3))
+  ;; Wrong radius.
+  (test-assert-thrown 'wrong-type-arg (set-arc! a '(1 . 2) 'r 45 90 3))
+  ;; Wrong start angle.
+  (test-assert-thrown 'wrong-type-arg (set-arc! a '(1 . 2) 3 'start 90 3))
+  ;; Wrong sweep angle.
+  (test-assert-thrown 'wrong-type-arg (set-arc! a '(1 . 2) 3 45 'sweep 3))
+  ;; Wrong color.
+  (test-assert-thrown 'wrong-type-arg (set-arc! a '(1 . 2) 3 45 90 'color)))
+
 (test-end "arc-wrong-argument")
 
 
