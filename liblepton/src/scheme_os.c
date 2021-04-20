b81514f9d2d4e690cb09cb996ee986cbbc0ae7f8
Vladimir Zhbanov <vzhbanov@gmail.com>
2021-04-20 17:05:38 +0300

    lepton: Rewrite platform() using Scheme FFI.
    A new module, (lepton os platform), has been added.  It contains
    data obtained from m4 platform related files.

--- liblepton/src/scheme_os.c
+++ liblepton/src/scheme_os.c
@@ -28,49 +28,6 @@
 #include <liblepton_priv.h>
 #include <libleptonguile_priv.h>
 
-SCM_SYMBOL (carbon_sym , "carbon");
-SCM_SYMBOL (cygwin_sym , "cygwin");
-SCM_SYMBOL (linux_sym , "linux");
-SCM_SYMBOL (win32_sym , "win32");
-SCM_SYMBOL (win32_native_sym , "win32-native");
-
-/*! \brief Get host operating system information.
- * \par Function Description
- * Returns a list of symbols describing the operating system. The
- * symbols may include:
- *
- * - win32 -- Windows
- * - win32-native -- Windows, not via Cygwin
- * - cygwin -- Cygwin
- * - carbon -- Mac OS X Carbon
- * - linux -- Linux
- *
- * \return a list of symbols.
- */
-SCM_DEFINE (platform, "%platform", 0, 0, 0, (),
-            "Return a list of symbols describing the host platform.")
-{
-  SCM result = SCM_EOL;
-
-# if defined (OS_CARBON)
-  result = scm_cons (carbon_sym, result);
-# endif
-# if defined (OS_CYGWIN)
-  result = scm_cons (cygwin_sym, result);
-# endif
-# if defined (OS_LINUX)
-  result = scm_cons (linux_sym, result);
-# endif
-# if defined (OS_WIN32)
-  result = scm_cons (win32_sym, result);
-# endif
-# if defined (OS_WIN32_NATIVE)
-  result = scm_cons (win32_native_sym, result);
-# endif
-
-  return result;
-}
-
 /*!
  * \brief Create the (lepton core os) Scheme module.
  * \par Function Description
@@ -84,8 +41,7 @@ init_module_lepton_core_os (void *unused)
   #include "scheme_os.x"
 
   /* Add them to the module's public definitions. */
-  scm_c_export (s_platform,
-                NULL);
+  scm_c_export (NULL);
 }
 
 /*!
