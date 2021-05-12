8391e2f5fcb9b1a66402392bd10b06759cdb04b2
Vladimir Zhbanov <vzhbanov@gmail.com>
2021-05-12 21:26:24 +0300

    tests: Add a set of automatic tests for linear objects.
    The tests are mostly the same as for box objects.  The following
    functions are tested here:
    
    - line-end()
    - line-info()
    - line-start()
    - make-bus()
    - make-bus-pin()
    - make-line()
    - make-net()
    - make-net-pin()
    - set-line!()

--- liblepton/scheme/unit-tests/lepton-object-line.scm
+++ liblepton/scheme/unit-tests/lepton-object-line.scm
@@ -1,6 +1,7 @@
 ;;; Test Scheme procedures related to line objects.
 
-(use-modules (lepton object))
+(use-modules (srfi srfi-26)
+             (lepton object))
 
 
 (test-begin "lines" 13)
@@ -33,6 +34,187 @@
   (test-equal 21 (list-ref (line-info a) 2)))
 
 (test-end "lines")
+
+(define object-func-list
+  `((,(make-bus '(1 . 2) '(3 . 4)) . ,make-bus)
+    (,(make-bus-pin '(1 . 2) '(3 . 4)) . ,make-bus-pin)
+    (,(make-net '(1 . 2) '(3 . 4)) . ,make-net)
+    (,(make-net-pin '(1 . 2) '(3 . 4)) . ,make-net-pin)
+    (,(make-line '(1 . 2) '(3 . 4) 3) . ,make-line)))
+
+(define func-list
+  (map cdr object-func-list))
+
+(test-begin "line-wrong-argument")
+
+(test-assert-thrown 'wrong-type-arg (line-info 'l))
+(test-assert-thrown 'wrong-type-arg (line-start 'l))
+(test-assert-thrown 'wrong-type-arg (line-end 'l))
+
+(for-each
+ (lambda (x)
+   (let ((object (car x))
+         (func (cdr x)))
+     ;; Wrong object.
+     (test-assert-thrown 'wrong-type-arg (set-line! 'l '(1 . 2) '(3 . 4) 3))
+     ;; Wrong first coord.
+     (test-assert-thrown 'wrong-type-arg (func 'c '(3 . 4) 3))
+     (test-assert-thrown 'wrong-type-arg (set-line! object 'c '(3 . 4) 3))
+     ;; Wrong first x.
+     (test-assert-thrown 'wrong-type-arg (func '(x . 2) '(3 . 4) 3))
+     (test-assert-thrown 'wrong-type-arg (set-line! object '(x . 2) '(3 . 4) 3))
+     ;; Wrong first y.
+     (test-assert-thrown 'wrong-type-arg (func '(1 . y) '(3 . 4) 3))
+     (test-assert-thrown 'wrong-type-arg (set-line! object '(1 . y) '(3 . 4) 3))
+     ;; Wrong second coord.
+     (test-assert-thrown 'wrong-type-arg (func '(1 . 2) 'c 3))
+     (test-assert-thrown 'wrong-type-arg (set-line! object '(1 . 2) 'c 3))
+     ;; Wrong second x.
+     (test-assert-thrown 'wrong-type-arg (func '(1 . 2) '(x . 4) 3))
+     (test-assert-thrown 'wrong-type-arg (set-line! object '(1 . 2) '(x . 4) 3))
+     ;; Wrong second y.
+     (test-assert-thrown 'wrong-type-arg (func '(1 . 2) '(3 . y) 3))
+     (test-assert-thrown 'wrong-type-arg (set-line! object '(1 . 2) '(3 . y) 3))
+     ;; Wrong color.
+     (test-assert-thrown 'wrong-type-arg (func '(1 . 2) '(3 . 4) 'color))
+     (test-assert-thrown 'wrong-type-arg (set-line! object '(1 . 2) '(3 . 4) 'color))))
+ object-func-list)
+
+(test-end "line-wrong-argument")
+
+
+;;; Common functions for transformations.
+
+;;; Line info without unrelated colors.
+(define (stripped-info line)
+  (define (strip-color info)
+    (reverse (cdr (reverse info))))
+  (strip-color (line-info line)))
+
+
+(test-begin "line-translation")
+
+(for-each
+ (lambda (func)
+   (let ((make-object (cut func '(100 . 100) '(300 . 400))))
+     (test-equal (stripped-info (car (translate-objects! '(500 . 500) (make-object))))
+       '((600 . 600) (800 . 900)))
+     (test-equal (stripped-info (car (translate-objects! '(-500 . 500) (make-object))))
+       '((-400 . 600) (-200 . 900)))
+     (test-equal (stripped-info (car (translate-objects! '(500 . -500) (make-object))))
+       '((600 . -400) (800 . -100)))
+     (test-equal (stripped-info (car (translate-objects! '(-500 . -500) (make-object))))
+       '((-400 . -400) (-200 . -100)))))
+ func-list)
+
+(test-end "line-translation")
+
+
+(test-begin "line-mirror")
+
+(for-each
+ (lambda (func)
+   (let ((make-object (cut func '(100 . 100) '(300 . 400))))
+     (test-equal (stripped-info (car (mirror-objects! 0 (make-object))))
+       '((-100 . 100) (-300 . 400)))
+     (test-equal (stripped-info (car (mirror-objects! 500 (make-object))))
+       '((900 . 100) (700 . 400)))
+     (test-equal (stripped-info (car (mirror-objects! -500 (make-object))))
+       '((-1100 . 100) (-1300 . 400)))
+     ;; Double mirror around the same point returns almost initial
+     ;; result (coords returned for another pair of corners).
+     (test-equal (stripped-info
+                  (car (mirror-objects! 500
+                                        (car (mirror-objects! 500 (make-object))))))
+       '((100 . 100) (300 . 400)))))
+ func-list)
+
+(test-end "line-mirror")
+
+
+(test-begin "line-rotation")
+
+(define degree-ls
+  '(-900 -360 -270 -180 -90 0 90 180 270 360 900))
+
+(for-each
+ (lambda (func)
+   (let ((make-object (cut func '(100 . 100) '(300 . 400))))
+
+     (define (rotate-at+500+500 angle)
+       (stripped-info (car (rotate-objects! '(500 . 500) angle (make-object)))))
+     (define (rotate-at-500+500 angle)
+       (stripped-info (car (rotate-objects! '(-500 . 500) angle (make-object)))))
+     (define (rotate-at+500-500 angle)
+       (stripped-info (car (rotate-objects! '(500 . -500) angle (make-object)))))
+     (define (rotate-at-500-500 angle)
+       (stripped-info (car (rotate-objects! '(-500 . -500) angle (make-object)))))
+
+     ;; The output format is
+     ;; '((center-x . center-y) radius start-angle sweep-angle)
+     ;; Radius and sweep angle should never change.
+     (test-equal (map rotate-at+500+500 degree-ls)
+       '(((900 . 900) (700 . 600))
+         ((100 . 100) (300 . 400))
+         ((900 . 100) (600 . 300))
+         ((900 . 900) (700 . 600))
+         ((100 . 900) (400 . 700))
+         ((100 . 100) (300 . 400))
+         ((900 . 100) (600 . 300))
+         ((900 . 900) (700 . 600))
+         ((100 . 900) (400 . 700))
+         ((100 . 100) (300 . 400))
+         ((900 . 900) (700 . 600))))
+
+     (test-equal (map rotate-at-500+500 degree-ls)
+       '(((-1100 . 900) (-1300 . 600))
+         ((100 . 100) (300 . 400))
+         ((-100 . 1100) (-400 . 1300))
+         ((-1100 . 900) (-1300 . 600))
+         ((-900 . -100) (-600 . -300))
+         ((100 . 100) (300 . 400))
+         ((-100 . 1100) (-400 . 1300))
+         ((-1100 . 900) (-1300 . 600))
+         ((-900 . -100) (-600 . -300))
+         ((100 . 100) (300 . 400))
+         ((-1100 . 900) (-1300 . 600))))
+
+     (test-equal (map rotate-at+500-500 degree-ls)
+       '(((900 . -1100) (700 . -1400))
+         ((100 . 100) (300 . 400))
+         ((-100 . -900) (-400 . -700))
+         ((900 . -1100) (700 . -1400))
+         ((1100 . -100) (1400 . -300))
+         ((100 . 100) (300 . 400))
+         ((-100 . -900) (-400 . -700))
+         ((900 . -1100) (700 . -1400))
+         ((1100 . -100) (1400 . -300))
+         ((100 . 100) (300 . 400))
+         ((900 . -1100) (700 . -1400))))
+
+     (test-equal (map rotate-at-500-500 degree-ls)
+       '(((-1100 . -1100) (-1300 . -1400))
+         ((100 . 100) (300 . 400))
+         ((-1100 . 100) (-1400 . 300))
+         ((-1100 . -1100) (-1300 . -1400))
+         ((100 . -1100) (400 . -1300))
+         ((100 . 100) (300 . 400))
+         ((-1100 . 100) (-1400 . 300))
+         ((-1100 . -1100) (-1300 . -1400))
+         ((100 . -1100) (400 . -1300))
+         ((100 . 100) (300 . 400))
+         ((-1100 . -1100) (-1300 . -1400))))
+
+     ;; Invalid rotation angles, not multiple of 90 degree.
+     (test-assert-thrown 'misc-error (rotate-at+500+500 100))
+     (test-assert-thrown 'misc-error (rotate-at+500+500 -100))
+     (test-assert-thrown 'misc-error (rotate-at+500+500 3000))
+     (test-assert-thrown 'misc-error (rotate-at+500+500 -3000))
+     ;; End of let.
+     ))
+ func-list)
+
+(test-end "line-rotation")
 
 
 (test-begin "nets" 13)
