e0f7fea7063dac4747f858dc2d016890be9d7925
Vladimir Zhbanov <vzhbanov@gmail.com>
2021-05-13 17:55:13 +0300

    tests: Add tests for circle transformations and wrong function args.

--- liblepton/scheme/unit-tests/lepton-object-circle.scm
+++ liblepton/scheme/unit-tests/lepton-object-circle.scm
@@ -33,3 +33,151 @@
   (test-equal 21 (list-ref (circle-info a) 2)))
 
 (test-end "circles")
+
+
+(test-begin "circle-wrong-argument")
+
+(test-assert-thrown 'wrong-type-arg (circle-info 'c))
+(test-assert-thrown 'wrong-type-arg (circle-center 'c))
+(test-assert-thrown 'wrong-type-arg (circle-radius 'c))
+
+(let ((c (make-circle '(1 . 2) 3 3)))
+  ;; Wrong object.
+  (test-assert-thrown 'wrong-type-arg (set-circle! 'c '(1 . 2) 3 3))
+  ;; Wrong coord.
+  (test-assert-thrown 'wrong-type-arg (make-circle 'c 3 3))
+  (test-assert-thrown 'wrong-type-arg (set-circle! c 'c 3 3))
+  ;; Wrong x.
+  (test-assert-thrown 'wrong-type-arg (make-circle '(x . 2) 3 3))
+  (test-assert-thrown 'wrong-type-arg (set-circle! c '(x . 2) 3 3))
+  ;; Wrong y.
+  (test-assert-thrown 'wrong-type-arg (make-circle '(1 . y) 3 3))
+  (test-assert-thrown 'wrong-type-arg (set-circle! c '(1 . y) 3 3))
+  ;; Wrong radius.
+  (test-assert-thrown 'wrong-type-arg (make-circle '(1 . 2) 'r 3))
+  (test-assert-thrown 'wrong-type-arg (set-circle! c '(1 . 2) 'r 3))
+  ;; Wrong color.
+  (test-assert-thrown 'wrong-type-arg (make-circle '(1 . 2) 3 'color))
+  (test-assert-thrown 'wrong-type-arg (set-circle! c '(1 . 2) 3 'color)))
+
+(test-end "circle-wrong-argument")
+
+
+;;; Common functions for transformations.
+;;; Make the same circle every time.
+(define (new-circle)
+  (make-circle '(100 . 100) 50))
+;;; Arc info without unrelated colors.
+(define (stripped-info circle)
+  (define (strip-color info)
+    (reverse (cdr (reverse info))))
+  (strip-color (circle-info circle)))
+
+
+(test-begin "circle-translation")
+
+(test-equal (stripped-info (car (translate-objects! '(500 . 500) (new-circle))))
+  '((600 . 600) 50))
+(test-equal (stripped-info (car (translate-objects! '(-500 . 500) (new-circle))))
+  '((-400 . 600) 50))
+(test-equal (stripped-info (car (translate-objects! '(500 . -500) (new-circle))))
+  '((600 . -400) 50))
+(test-equal (stripped-info (car (translate-objects! '(-500 . -500) (new-circle))))
+  '((-400 . -400) 50))
+
+(test-end "circle-translation")
+
+
+(test-begin "circle-mirror")
+
+(test-equal (stripped-info (car (mirror-objects! 0 (new-circle))))
+  '((-100 . 100) 50))
+(test-equal (stripped-info (car (mirror-objects! 500 (new-circle))))
+  '((900 . 100) 50))
+(test-equal (stripped-info (car (mirror-objects! -500 (new-circle))))
+  '((-1100 . 100) 50))
+;;; Double mirror around the same point returns initial result.
+(test-equal (stripped-info
+             (car (mirror-objects! 500
+                                   (car (mirror-objects! 500 (new-circle))))))
+  '((100 . 100) 50))
+
+(test-end "circle-mirror")
+
+
+(test-begin "circle-rotation")
+
+(define degree-ls
+  '(-900 -360 -270 -180 -90 0 90 180 270 360 900))
+
+(define (rotate-at+500+500 angle)
+  (stripped-info (car (rotate-objects! '(500 . 500) angle (new-circle)))))
+(define (rotate-at-500+500 angle)
+  (stripped-info (car (rotate-objects! '(-500 . 500) angle (new-circle)))))
+(define (rotate-at+500-500 angle)
+  (stripped-info (car (rotate-objects! '(500 . -500) angle (new-circle)))))
+(define (rotate-at-500-500 angle)
+  (stripped-info (car (rotate-objects! '(-500 . -500) angle (new-circle)))))
+
+;;; The output format is
+;;; '((center-x . center-y) radius start-angle sweep-angle)
+;;; Radius and sweep angle should never change.
+(test-equal (map rotate-at+500+500 degree-ls)
+  '(((900 . 900) 50)
+    ((100 . 100) 50)
+    ((900 . 100) 50)
+    ((900 . 900) 50)
+    ((100 . 900) 50)
+    ((100 . 100) 50)
+    ((900 . 100) 50)
+    ((900 . 900) 50)
+    ((100 . 900) 50)
+    ((100 . 100) 50)
+    ((900 . 900) 50)))
+
+(test-equal (map rotate-at-500+500 degree-ls)
+  '(((-1100 . 900) 50)
+    ((100 . 100) 50)
+    ((-100 . 1100) 50)
+    ((-1100 . 900) 50)
+    ((-900 . -100) 50)
+    ((100 . 100) 50)
+    ((-100 . 1100) 50)
+    ((-1100 . 900) 50)
+    ((-900 . -100) 50)
+    ((100 . 100) 50)
+    ((-1100 . 900) 50)))
+
+(test-equal (map rotate-at+500-500 degree-ls)
+  '(((900 . -1100) 50)
+    ((100 . 100) 50)
+    ((-100 . -900) 50)
+    ((900 . -1100) 50)
+    ((1100 . -100) 50)
+    ((100 . 100) 50)
+    ((-100 . -900) 50)
+    ((900 . -1100) 50)
+    ((1100 . -100) 50)
+    ((100 . 100) 50)
+    ((900 . -1100) 50)))
+
+(test-equal (map rotate-at-500-500 degree-ls)
+  '(((-1100 . -1100) 50)
+    ((100 . 100) 50)
+    ((-1100 . 100) 50)
+    ((-1100 . -1100) 50)
+    ((100 . -1100) 50)
+    ((100 . 100) 50)
+    ((-1100 . 100) 50)
+    ((-1100 . -1100) 50)
+    ((100 . -1100) 50)
+    ((100 . 100) 50)
+    ((-1100 . -1100) 50)))
+
+;;; Invalid rotation angles, not multiple of 90 degree.
+(test-assert-thrown 'misc-error (rotate-at+500+500 100))
+(test-assert-thrown 'misc-error (rotate-at+500+500 -100))
+(test-assert-thrown 'misc-error (rotate-at+500+500 3000))
+(test-assert-thrown 'misc-error (rotate-at+500+500 -3000))
+
+(test-end "circle-rotation")
