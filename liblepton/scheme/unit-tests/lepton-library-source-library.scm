6a1bbafe5842946cf68bbed6420015110141e6d0
Vladimir Zhbanov <vzhbanov@gmail.com>
2021-07-06 04:26:04 +0300

    uts: Guard test code using dynamic-wind().

--- liblepton/scheme/unit-tests/lepton-library-source-library.scm
+++ liblepton/scheme/unit-tests/lepton-library-source-library.scm
@@ -8,10 +8,16 @@
 
 
 (test-begin "source-library-contents")
-(mkdir *testdir*)
-(set-source-library-contents! %default-source-library (list *testdir*))
-(test-equal (source-library-contents %default-source-library) (list *testdir*))
-(set-source-library-contents! %default-source-library '())
-(test-equal (source-library-contents %default-source-library) '())
-(rmdir *testdir*)
+
+(dynamic-wind
+  (lambda ()
+    (mkdir *testdir*))
+  (lambda ()
+    (set-source-library-contents! %default-source-library (list *testdir*))
+    (test-equal (source-library-contents %default-source-library) (list *testdir*))
+    (set-source-library-contents! %default-source-library '())
+    (test-eq (source-library-contents %default-source-library) '()))
+  (lambda ()
+    (rmdir *testdir*)))
+
 (test-end "source-library-contents")
