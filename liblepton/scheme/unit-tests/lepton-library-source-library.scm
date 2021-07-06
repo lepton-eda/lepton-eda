26e5f62faae76e4dd87ea0f2b40b67c027c5c8b5
Vladimir Zhbanov <vzhbanov@gmail.com>
2021-07-06 04:25:07 +0300

    uts: Get rid of unused commented out code.

--- liblepton/scheme/unit-tests/lepton-library-source-library.scm
+++ liblepton/scheme/unit-tests/lepton-library-source-library.scm
@@ -3,16 +3,13 @@
 (use-modules (lepton library))
 
 (define *testdir*      (string-append (getcwd)   file-name-separator-string "t0500-tmp"))
-;; (define *testcir*      (string-append *testdir*  file-name-separator-string "file.cir"))
 
 
 (test-begin "source-library-contents")
 (mkdir *testdir*)
-;; (with-output-to-file *testcir* newline)
 (set-source-library-contents! %default-source-library (list *testdir*))
 (test-equal (source-library-contents %default-source-library) (list *testdir*))
 (set-source-library-contents! %default-source-library '())
 (test-equal (source-library-contents %default-source-library) '())
-;; (delete-file *testcir*)
 (rmdir *testdir*)
 (test-end "source-library-contents")
