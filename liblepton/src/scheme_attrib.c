f9441c2fd87df87dbec6c3624a6489a9da2ae3f1
Vladimir Zhbanov <vzhbanov@gmail.com>
2021-08-26 09:03:48 +0300

    lib: Utilize object parent getter in %attach-attrib!().

--- liblepton/src/scheme_attrib.c
+++ liblepton/src/scheme_attrib.c
@@ -69,10 +69,11 @@ SCM_DEFINE (attach_attrib_x, "%attach-attrib!", 2, 0, 0,
   /* Check that attachment doesn't already exist */
   if (attrib->attached_to == obj) return obj_s;
 
+  LeptonObject *parent = lepton_object_get_parent (obj);
   /* Check that both are in the same page and/or component object */
-  if ((obj->parent != attrib->parent)
+  if ((parent != lepton_object_get_parent (attrib))
       || (lepton_object_get_page (obj) != lepton_object_get_page (attrib))
-      || ((obj->parent == NULL) && (lepton_object_get_page (obj) == NULL))) {
+      || ((parent == NULL) && (lepton_object_get_page (obj) == NULL))) {
     scm_error (edascm_object_state_sym, s_attach_attrib_x,
                _("Objects ~A and ~A are not part of the same page and/or component object"),
                scm_list_2 (obj_s, attrib_s), SCM_EOL);
