1e60ef55becb97ec81e3fdbd710604c413b3355c
Vladimir Zhbanov <vzhbanov@gmail.com>
2021-09-06 21:02:09 +0300

    lib: Utilize getters for component angle in %component-info().

--- liblepton/src/scheme_component.c
+++ liblepton/src/scheme_component.c
@@ -174,7 +174,7 @@ SCM_DEFINE (component_info, "%component-info", 1, 0, 0,
   return scm_list_n (scm_from_utf8_string (lepton_component_object_get_basename (obj)),
                      scm_from_int (lepton_component_object_get_x (obj)),
                      scm_from_int (lepton_component_object_get_y (obj)),
-                     scm_from_int (obj->component->angle),
+                     scm_from_int (lepton_component_object_get_angle (obj)),
                      obj->component->mirror ? SCM_BOOL_T : SCM_BOOL_F,
                      obj->selectable ? SCM_BOOL_F : SCM_BOOL_T,
                      SCM_UNDEFINED);
