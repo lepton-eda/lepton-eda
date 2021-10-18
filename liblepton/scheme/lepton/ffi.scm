ee05703b2b1867e8ff16630b0881d49ea689e647
Vladimir Zhbanov <vzhbanov@gmail.com>
2021-10-18 14:16:40 +0300

    lib: Rewrite set-config-parent!() using Scheme FFI.

--- liblepton/scheme/lepton/ffi.scm
+++ liblepton/scheme/lepton/ffi.scm
@@ -79,6 +79,7 @@
             eda_config_get_cache_context
             eda_config_get_filename
             eda_config_get_parent
+            eda_config_set_parent
             eda_config_is_changed
             eda_config_is_loaded
             eda_config_save
@@ -450,6 +451,7 @@
 (define-lff eda_config_get_filename '* '(*))
 (define-lff eda_config_is_changed int '(*))
 (define-lff eda_config_get_parent '* '(*))
+(define-lff eda_config_set_parent void '(* *))
 (define-lff eda_config_is_loaded int '(*))
 (define-lff eda_config_save int '(* *))
 (define-lff eda_config_remove_group int '(* * *))
