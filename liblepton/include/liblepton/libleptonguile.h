8c29dfa4debd0670b2938ec1d91b4e84cf44a09d
Vladimir Zhbanov <vzhbanov@gmail.com>
2021-10-26 12:14:35 +0300

    lib: Get rid of edascmhookproxy.c and edascmvaluetypes.c.

--- liblepton/include/liblepton/libleptonguile.h
+++ liblepton/include/liblepton/libleptonguile.h
@@ -17,9 +17,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-#include <liblepton/edascmvaluetypes.h>
-#include <liblepton/edascmhookproxy.h>
-
 G_BEGIN_DECLS
 
 /*!
