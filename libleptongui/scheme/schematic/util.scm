77ef2d6c8d9319dc675cda22ef91f1e6a4bd2e1b
Vladimir Zhbanov <vzhbanov@gmail.com>
2022-01-27 12:48:09 +0300

    gui: Rewrite show-uri() in Scheme.

--- libleptongui/scheme/schematic/util.scm
+++ libleptongui/scheme/schematic/util.scm
@@ -1,7 +1,7 @@
 ;; Lepton EDA Schematic Capture
 ;; Scheme API
 ;; Copyright (C) 2011 Peter Brett <peter@peter-b.co.uk>
-;; Copyright (C) 2017-2020 Lepton EDA Contributors
+;; Copyright (C) 2017-2022 Lepton EDA Contributors
 ;;
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -18,14 +18,48 @@
 ;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 ;;
 (define-module (schematic util)
+  #:use-module (rnrs bytevectors)
+  #:use-module (system foreign)
 
-  ; Import C procedures
-  #:use-module (schematic core util)
+  #:use-module (lepton ffi)
+  #:use-module (lepton gerror)
+
+  #:use-module (schematic core gettext)
+  #:use-module (schematic ffi)
+  #:use-module (schematic window)
 
   #:export (show-file
             show-uri))
 
-(define show-uri %show-uri)
+(define (show-uri uri)
+  "Shows URI in the associated default application.  Raises an
+error on failure."
+  (define unknown-error (G_ "Unknown error"))
+
+  (define (get-gerror-msg *error)
+    (let ((*err (dereference-pointer *error)))
+      (if (null-pointer? *err)
+          unknown-error
+          (gerror-message *err))))
+
+  (define (process-gerror *error)
+    (if (null-pointer? *error)
+        unknown-error
+        (let ((message (get-gerror-msg *error)))
+          (g_clear_error *error)
+          message)))
+
+  (check-string uri 1)
+
+  (let ((*error (bytevector->pointer (make-bytevector (sizeof '*) 0))))
+    (unless (true? (x_show_uri (current-window)
+                               (string->pointer uri)
+                               *error))
+
+      (error (G_ "Could not launch URI ~S: ~A")
+             uri
+             (process-gerror *error)))))
+
 
 (define (show-file filename)
   (show-uri (string-append "file://" filename)))
