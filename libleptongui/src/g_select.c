2248fe8009a26b5d8abd66af9964db177cd526a4
Vladimir Zhbanov <vzhbanov@gmail.com>
2022-02-05 12:47:22 +0300

    gui: Rewrite select-object!() using Scheme FFI.

--- libleptongui/src/g_select.c
+++ libleptongui/src/g_select.c
@@ -22,45 +22,6 @@
 
 SCM_SYMBOL (object_state_sym, "object-state");
 
-/*! \brief Select an object.
- * \par Function Description
- * Add \a obj_s to its associated page's selection.  If \a obj_s is
- * not included directly in a page (i.e. inclusion in a component is
- * not permitted), throws a Scheme error.  If \a obj_s is already
- * selected, does nothing.
- *
- * \note Scheme API: Implements the %select-object! procedure in the
- * (schematic core selection) module.
- *
- * \param obj_s #LeptonObject smob for object to be selected.
- * \return obj_s.
- */
-SCM_DEFINE (select_object_x, "%select-object!", 1, 0, 0,
-            (SCM obj_s), "Select an object.")
-{
-  /* Ensure that the argument is an object smob */
-  SCM_ASSERT (edascm_is_object (obj_s), obj_s,
-              SCM_ARG1, s_select_object_x);
-
-  LeptonObject *obj = edascm_to_object (obj_s);
-  LeptonPage *page = lepton_object_get_page (obj);
-  if ((page == NULL) ||
-      (lepton_object_get_parent (obj) != NULL))
-  {
-    scm_error (object_state_sym,
-               s_select_object_x,
-               _("Object ~A is not directly included in a page."),
-               scm_list_1 (obj_s), SCM_EOL);
-  }
-
-  if (!lepton_object_get_selected (obj))
-  {
-    o_selection_add (lepton_page_get_selection_list (page), obj);
-  }
-
-  return obj_s;
-}
-
 /*! \brief Deselect an object.
  * \par Function Description
  * Remove \a obj_s from its associated page's selection.  If \a obj_s
@@ -112,7 +73,7 @@ init_module_schematic_core_select (void *unused)
   #include "g_select.x"
 
   /* Add them to the module's public definitions. */
-  scm_c_export (s_select_object_x, s_deselect_object_x,
+  scm_c_export (s_deselect_object_x,
                 NULL);
 }
 
