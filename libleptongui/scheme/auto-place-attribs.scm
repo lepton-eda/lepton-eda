0d89d11ce7aa53e5ddf955fb806237bce4851832
Vladimir Zhbanov <vzhbanov@gmail.com>
2022-02-24 15:25:50 +0300

    schematic: Import missing modules in auto-place-attribs.scm.

--- libleptongui/scheme/auto-place-attribs.scm
+++ libleptongui/scheme/auto-place-attribs.scm
@@ -18,6 +18,8 @@
 ;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 (use-modules (ice-9 regex)
+             (geda deprecated)
+             (gschem deprecated)
              (lepton object))
 
 ;; --------------------------------------------------------------------------
