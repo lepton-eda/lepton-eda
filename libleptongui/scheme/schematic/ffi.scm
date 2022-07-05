4f03c55fbfdb3ad711734e69076c4d5c0b41dc1c
Vladimir Zhbanov <vzhbanov@gmail.com>
2022-07-05 04:48:57 +0300

    gui: Get rid of i_callback_add_net().

--- libleptongui/scheme/schematic/ffi.scm
+++ libleptongui/scheme/schematic/ffi.scm
@@ -33,7 +33,6 @@
 
             i_callback_add_attribute
             i_callback_add_bus
-            i_callback_add_net
             i_callback_attributes_show_both
             i_callback_attributes_show_name
             i_callback_attributes_show_value
@@ -107,6 +106,9 @@
             o_update_component
 
             o_move_start
+
+            o_net_reset
+            o_net_start
 
             picture_selection_dialog
 
@@ -507,7 +509,6 @@
 ;;; i_callbacks.c
 (define-lff i_callback_add_attribute void '(* *))
 (define-lff i_callback_add_bus void '(* *))
-(define-lff i_callback_add_net void '(* *))
 (define-lff i_callback_attributes_show_both void '(* *))
 (define-lff i_callback_attributes_show_name void '(* *))
 (define-lff i_callback_attributes_show_value void '(* *))
@@ -581,6 +582,10 @@
 
 ;;; o_move.c
 (define-lff o_move_start void (list '* int int))
+
+;;; o_net.c
+(define-lff o_net_reset void '(*))
+(define-lff o_net_start void (list '* int int))
 
 ;;; o_picture.c
 (define-lff picture_selection_dialog void '(*))
