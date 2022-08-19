06f698f9fe1245115b2f610d4ea45218f9503e05
Vladimir Zhbanov <vzhbanov@gmail.com>
2022-08-19 05:30:32 +0300

    gui: Utilize accessors for 'rubber_visible' in o_bus_motion()

--- libleptongui/src/o_bus.c
+++ libleptongui/src/o_bus.c
@@ -128,7 +128,7 @@ void o_bus_motion (GschemToplevel *w_current, int w_x, int w_y)
 
   g_assert (schematic_window_get_inside_action (w_current) != 0);
 
-  if (w_current->rubber_visible)
+  if (schematic_window_get_rubber_visible (w_current))
     o_bus_invalidate_rubber (w_current);
 
   w_current->second_wx = w_x;
@@ -147,7 +147,7 @@ void o_bus_motion (GschemToplevel *w_current, int w_x, int w_y)
   }
 
   o_bus_invalidate_rubber (w_current);
-  w_current->rubber_visible = 1;
+  schematic_window_set_rubber_visible (w_current, 1);
 }
 
 /*! \todo Finish function documentation!!!
