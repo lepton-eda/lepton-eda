ffbf5671bb41eea3d766dbab72e9e49656d69e55
Vladimir Zhbanov <vzhbanov@gmail.com>
2022-08-19 03:46:41 +0300

    gui: Utilize setter for 'ALTKEY' in x_event_scroll()

--- libleptongui/src/x_event.c
+++ libleptongui/src/x_event.c
@@ -392,7 +392,7 @@ gint x_event_scroll (GtkWidget *widget, GdkEventScroll *event,
   /* update the state of the modifiers */
   schematic_window_set_shift_key_pressed (w_current, (event->state & GDK_SHIFT_MASK) ? 1 : 0);
   schematic_window_set_control_key_pressed (w_current, (event->state & GDK_CONTROL_MASK) ? 1 : 0);
-  w_current->ALTKEY     = (event->state & GDK_MOD1_MASK) ? 1 : 0;
+  schematic_window_set_alt_key_pressed (w_current, (event->state & GDK_MOD1_MASK) ? 1 : 0);
 
   if (w_current->scroll_wheel == SCROLL_WHEEL_CLASSIC) {
     /* Classic gschem behaviour */
