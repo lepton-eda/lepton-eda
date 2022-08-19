674768b32a33cb0b55455b0c22eeac17cc7f7e20
Vladimir Zhbanov <vzhbanov@gmail.com>
2022-08-19 03:18:53 +0300

    gui: Utilize setter for 'CONTROLKEY' in x_event_button_pressed().

--- libleptongui/src/x_event.c
+++ libleptongui/src/x_event.c
@@ -192,7 +192,7 @@ x_event_button_pressed (GschemPageView *page_view,
   }
 
   schematic_window_set_shift_key_pressed (w_current, (event->state & GDK_SHIFT_MASK) ? 1 : 0);
-  w_current->CONTROLKEY = (event->state & GDK_CONTROL_MASK) ? 1 : 0;
+  schematic_window_set_control_key_pressed (w_current, (event->state & GDK_CONTROL_MASK) ? 1 : 0);
   w_current->ALTKEY     = (event->state & GDK_MOD1_MASK) ? 1 : 0;
 
   /* Huge switch statement to evaluate state transitions. Jump to
