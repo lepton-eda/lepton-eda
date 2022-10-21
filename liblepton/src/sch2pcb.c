5f94085264468665a8233165ba1e6b75c879ce8a
Vladimir Zhbanov <vzhbanov@gmail.com>
2022-10-21 14:28:17 +0300

    sch2pcb: Utilize getter for PcbElement's 'tail' field in sch2pcb_prune_elements().

--- liblepton/src/sch2pcb.c
+++ liblepton/src/sch2pcb.c
@@ -1106,7 +1106,7 @@ sch2pcb_prune_elements (gchar *pcb_file,
                       "Element%c%s \"%s\" \"%s\" \"%s\" %s %s%s\n");
       fprintf (f_out, fmt,
                el->res_char, pcb_element_get_flags (el), pcb_element_get_description (el), pcb_element_get_refdes (el),
-               pcb_element_get_changed_value (el_exists), el->x, el->y, el->tail);
+               pcb_element_get_changed_value (el_exists), el->x, el->y, pcb_element_get_tail (el));
       if (sch2pcb_get_verbose_mode () != 0)
         printf ("%s: changed element %s value: %s -> %s\n",
                 pcb_element_get_refdes (el),
