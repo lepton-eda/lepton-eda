ee02c8093a6a1fadd38e74ac5c71b24091e12678
Vladimir Zhbanov <vzhbanov@gmail.com>
2022-10-21 14:33:06 +0300

    sch2pcb: Utilize getter for PcbElement's 'x' field in pcb_element_free().

--- liblepton/src/sch2pcb.c
+++ liblepton/src/sch2pcb.c
@@ -493,7 +493,7 @@ pcb_element_free (PcbElement * el)
   g_free (pcb_element_get_changed_value (el));
   g_free (pcb_element_get_refdes (el));
   g_free (pcb_element_get_value (el));
-  g_free (el->x);
+  g_free (pcb_element_get_x (el));
   g_free (el->y);
   g_free (pcb_element_get_tail (el));
   g_free (el->pkg_name_fix);
