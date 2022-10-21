003fee6176d24109538d291d659f418f2ba5d2bd
Vladimir Zhbanov <vzhbanov@gmail.com>
2022-10-21 14:12:31 +0300

    sch2pcb: Utilize getter for PcbElement's 'flags' field in sch2pcb_prune_elements().

--- liblepton/src/sch2pcb.c
+++ liblepton/src/sch2pcb.c
@@ -1105,7 +1105,7 @@ sch2pcb_prune_elements (gchar *pcb_file,
                       "Element%c\"%s\" \"%s\" \"%s\" \"%s\" %s %s%s\n" :
                       "Element%c%s \"%s\" \"%s\" \"%s\" %s %s%s\n");
       fprintf (f_out, fmt,
-               el->res_char, el->flags, pcb_element_get_description (el), pcb_element_get_refdes (el),
+               el->res_char, pcb_element_get_flags (el), pcb_element_get_description (el), pcb_element_get_refdes (el),
                pcb_element_get_changed_value (el_exists), el->x, el->y, el->tail);
       if (sch2pcb_get_verbose_mode () != 0)
         printf ("%s: changed element %s value: %s -> %s\n",
