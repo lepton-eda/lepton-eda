dfb745682a1f28a44c65ff93f38db26e4aad7d4c
Vladimir Zhbanov <vzhbanov@gmail.com>
2022-10-21 14:39:54 +0300

    sch2pcb: Utilize getter for PcbElement's 'y' field in sch2pcb_update_element_descriptions().

--- liblepton/src/sch2pcb.c
+++ liblepton/src/sch2pcb.c
@@ -1073,7 +1073,7 @@ sch2pcb_update_element_descriptions (gchar *pcb_file,
                pcb_element_get_refdes (el),
                pcb_element_get_value (el),
                pcb_element_get_x (el),
-               el->y,
+               pcb_element_get_y (el),
                pcb_element_get_tail (el));
       printf ("%s: updating element Description: %s -> %s\n",
               pcb_element_get_refdes (el),
