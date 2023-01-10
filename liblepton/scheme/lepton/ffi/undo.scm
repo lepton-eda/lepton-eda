fef04e7fc107288fa1391296badcb62e4abefe6c
Vladimir Zhbanov <vzhbanov@gmail.com>
2023-01-10 13:18:04 +0300

    lepton: Add module (lepton ffi undo).
    The module provides FFI for undo code in liblepton.

---
+++ liblepton/scheme/lepton/ffi/undo.scm
@@ -0,0 +1,33 @@
+;;; Lepton EDA library - Scheme API
+;;; Copyright (C) 2023 Lepton EDA Contributors
+;;;
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+;;; This module provides FFI for Lepton undo system.
+
+(define-module (lepton ffi undo)
+  #:use-module (lepton ffi lib)
+  #:use-module (lepton ffi lff)
+
+  #:export (lepton_undo_get_next
+            lepton_undo_get_prev))
+
+;;; Simplify definition of functions by omitting the library
+;;; argument.
+(define-syntax-rule (define-lff arg ...)
+  (define-lff-lib arg ... liblepton))
+
+(define-lff lepton_undo_get_next '* '(*))
+(define-lff lepton_undo_get_prev '* '(*))
