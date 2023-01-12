26af5cdcf96e8f67dab1d2c8002a1e373137fcf3
Vladimir Zhbanov <vzhbanov@gmail.com>
2023-01-12 12:50:57 +0300

    gui: Utilize getter for LeptonUndo's field 'page_control' in o_undo_callback().

--- libleptongui/src/o_undo.c
+++ libleptongui/src/o_undo.c
@@ -503,7 +503,7 @@ o_undo_callback (GschemToplevel *w_current,
                                                NULL));
   }
 
-  lepton_page_set_page_control (page, undo_to_do->page_control);
+  lepton_page_set_page_control (page, lepton_undo_get_page_control (undo_to_do));
   lepton_page_set_up (page, undo_to_do->up);
   gschem_toplevel_page_content_changed (w_current, page);
 
