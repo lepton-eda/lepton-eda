7620c096c8217b9e42b710cb07e1130e0c9cdbc5
Vladimir Zhbanov <vzhbanov@gmail.com>
2023-02-23 11:31:05 +0300

    schematic: Add procedure set-action-mode!().
    The procedure is a Scheme twin of i_set_state().

--- libleptongui/scheme/schematic/action-mode.scm
+++ libleptongui/scheme/schematic/action-mode.scm
@@ -1,6 +1,6 @@
 ;;; Lepton EDA Schematic Capture
 ;;; Scheme API
-;;; Copyright (C) 2022 Lepton EDA Contributors
+;;; Copyright (C) 2022-2023 Lepton EDA Contributors
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
@@ -21,9 +21,11 @@
   #:use-module (system foreign)
 
   #:use-module (schematic ffi)
+  #:use-module (schematic window foreign)
 
   #:export (action-mode->symbol
-            symbol->action-mode))
+            symbol->action-mode
+            set-action-mode!))
 
 (define (action-mode->symbol mode)
   "Returns a Scheme symbol corresponding to integer MODE value."
@@ -32,3 +34,12 @@
 (define (symbol->action-mode sym)
   "Returns integer action mode value corresponding to symbol SYM."
   (schematic_action_mode_from_string (string->pointer (symbol->string sym))))
+
+
+(define* (set-action-mode! #:window (window #f) mode)
+  "Set action mode to MODE.  If WINDOW is specified, the mode is
+set in it, otherwise it is set in the currently active window as
+returned by the function current-window()."
+  (define *window (check-window (or window (current-window)) 1))
+
+  (i_set_state *window (symbol->action-mode mode)))
