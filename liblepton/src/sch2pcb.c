0ad55d0ac7385097a0221ec5dbcd504ca8a27280
Vladimir Zhbanov <vzhbanov@gmail.com>
2023-04-26 18:54:21 +0300

    sch2pcb: Use auxiliary functions to open/close files.

--- liblepton/src/sch2pcb.c
+++ liblepton/src/sch2pcb.c
@@ -1039,10 +1039,13 @@ sch2pcb_update_element_descriptions (gchar *pcb_file,
   PcbElement *el, *el_exists;
   gchar *fmt, *s, buf[1024];
 
-  if ((f_in = fopen (pcb_file, "r")) == NULL)
+  if ((f_in = sch2pcb_open_file_to_read (pcb_file)) == NULL)
+  {
     return;
-  if ((f_out = fopen (tmp, "wb")) == NULL) {
-    fclose (f_in);
+  }
+  if ((f_out = sch2pcb_open_file_to_write (tmp)) == NULL)
+  {
+    sch2pcb_close_file (f_in);
     return;
   }
   while ((fgets (buf, sizeof (buf), f_in)) != NULL) {
@@ -1071,8 +1074,8 @@ sch2pcb_update_element_descriptions (gchar *pcb_file,
       fputs (buf, f_out);
     pcb_element_free (el);
   }
-  fclose (f_in);
-  fclose (f_out);
+  sch2pcb_close_file (f_in);
+  sch2pcb_close_file (f_out);
 
   if (!sch2pcb_get_bak_done ())
   {
