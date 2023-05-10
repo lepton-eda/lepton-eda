93f4f95f2c8aab958d9ed2cc5b8eba4cd262848d
dmn <graahnul.grom@gmail.com>
2023-05-10 18:45:26 +0300

    gui: Add new module (schematic sellock).

---
+++ libleptongui/scheme/schematic/sellock.scm
@@ -0,0 +1,18 @@
+;; Lepton EDA
+;; ( schematic sellock ) module: select locked components.
+;; Copyright (C) 2020-2023 dmn <graahnul.grom@ya.ru>
+;; License: GPLv2+. See the COPYING file.
+;;
+
+( define-module ( schematic sellock )
+  #:use-module ( lepton log )
+
+  #:export ( select-locked )
+)
+
+
+
+( define ( select-locked )
+    ( log! 'message ".. select-locked()" )
+)
+
