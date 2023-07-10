76f3436a6e94d887eb797004ca70a3f456c7ebe4
Vladimir Zhbanov <vzhbanov@gmail.com>
2023-07-10 10:10:19 +0300

    export: Avoid test failures when user's gafrc contains component library commands.

--- tools/cli/scheme/lepton-export.scm
+++ tools/cli/scheme/lepton-export.scm
@@ -1,7 +1,7 @@
 ;;; Lepton EDA command-line utility
 ;;; Copyright (C) 2012 Peter Brett <peter@peter-b.co.uk>
 ;;; Copyright (C) 2014-2016 gEDA Contributors
-;;; Copyright (C) 2017-2022 Lepton EDA Contributors
+;;; Copyright (C) 2017-2023 Lepton EDA Contributors
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
@@ -21,6 +21,7 @@
              (system foreign)
              (lepton ffi)
              (lepton gettext)
+             (lepton library)
              (lepton page)
              (lepton rc)
              (lepton srfi-37)
