1c5568d3c308b9fea34bd5e3a797f4c7093862ed
Vladimir Zhbanov <vzhbanov@gmail.com>
2023-07-25 10:17:31 +0300

    uts[sch2pcb]: Add tests for looking up for a footprint in the current directory

--- lepton-eda/scheme/unit-tests/sch2pcb-lookup.scm
+++ lepton-eda/scheme/unit-tests/sch2pcb-lookup.scm
@@ -1,6 +1,9 @@
 (use-modules (system foreign)
              (sch2pcb lookup))
 
+(define (touch file)
+  (with-output-to-file file
+    (lambda () (display ""))))
 
 (test-begin "lookup-in-missing-directory")
 
@@ -12,5 +15,24 @@
   ;; Currently, the function returns %null-pointer if the
   ;; directory to search in is missing.
   (test-assert (null-pointer? *element)))
+
+(test-end)
+
+
+(test-begin "lookup-footprint")
+
+(touch "x.fp")
+
+(let ((footprint-x (lookup-footprint "." "x"))
+      (footprint-x.fp (lookup-footprint "." "x.fp")))
+  (test-assert (not (null-pointer? footprint-x)))
+  (test-assert (not (null-pointer? footprint-x.fp)))
+  (test-equal (pointer->string footprint-x) "./x.fp")
+  (test-equal (pointer->string footprint-x.fp) "./x.fp"))
+
+(delete-file "x.fp")
+
+(test-assert (null-pointer? (lookup-footprint "." "x")))
+(test-assert (null-pointer? (lookup-footprint "." "x.fp")))
 
 (test-end)
