ae327ad05b36441efdeb9045a7cac4f5a73118dc
Vladimir Zhbanov <vzhbanov@gmail.com>
2023-08-11 12:54:38 +0300

    sch2pcb: Make pcb_element_get_hi_res_format() available in Scheme.

--- liblepton/scheme/lepton/ffi/sch2pcb.scm
+++ liblepton/scheme/lepton/ffi/sch2pcb.scm
@@ -29,6 +29,7 @@
             pcb_element_set_changed_value
             pcb_element_get_description
             pcb_element_get_flags
+            pcb_element_get_hi_res_format
             pcb_element_set_hi_res_format
             pcb_element_get_omit_PKG
             pcb_element_get_pkg_name_fix
@@ -73,6 +74,7 @@
 (define-lff pcb_element_set_changed_value void '(* *))
 (define-lff pcb_element_get_description '* '(*))
 (define-lff pcb_element_get_flags '* '(*))
+(define-lff pcb_element_get_hi_res_format int '(*))
 (define-lff pcb_element_set_hi_res_format void (list '* int))
 (define-lff pcb_element_get_omit_PKG int '(*))
 (define-lff pcb_element_get_pkg_name_fix '* '(*))
