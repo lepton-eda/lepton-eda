8ebfe90d0491fb41bf11d6e96d208f8eb6796ef4
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-03-05 20:04:45 +0300

    lib: Fix parameter name in a doxygen comment

--- liblepton/src/bounds.c
+++ liblepton/src/bounds.c
@@ -1,7 +1,7 @@
 /* Lepton EDA library
  * Copyright (C) 1998-2010 Ales Hvezda
  * Copyright (C) 1998-2015 gEDA Contributors
- * Copyright (C) 2017-2021 Lepton EDA Contributors
+ * Copyright (C) 2017-2024 Lepton EDA Contributors
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -39,7 +39,7 @@ lepton_bounds_empty (const LeptonBounds *bounds)
 /*! \brief Check if the bounds are equal
  *
  *  \param [in] a The first bound operand
- *  \param [in] a The second bound operand
+ *  \param [in] b The second bound operand
  *  \return TRUE of the two bounds are equal
  */
 gboolean
