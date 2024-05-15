ed7f73942330a0d88d20a629f03cec20e8b583cc
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-05-15 12:58:18 +0300

    uts[sch2pcb]: Add first tests for insert-file-element()
    One of the tests is skipped yet since the foreign C functions do
    not work properly with Scheme ports and seem to output their
    errors directly to stderr.  It will be fixed after rewriting them
    in Scheme.

---
+++ lepton-eda/scheme/unit-tests/sch2pcb-insert.scm
@@ -0,0 +1,50 @@
+;;; Test Scheme procedures related to pcb elements.
+
+(use-modules (system foreign)
+             (lepton ffi sch2pcb)
+             (sch2pcb element)
+             (sch2pcb insert))
+
+(define *testdir*
+  (string-append (getcwd)
+                 file-name-separator-string
+                 "sch2pcb-insert-element"))
+
+;;; Setup/teardown of directories / files needed by tests.
+(define (config-test-setup)
+  (mkdir *testdir*))
+
+(define (config-test-teardown)
+  (system* "rm" "-rf" *testdir*))
+
+
+(test-begin "insert-file-element-missing-file")
+(test-group-with-cleanup "insert-file-element-missing-file-grp"
+  (config-test-setup)
+  ;; Test an exit status and side effects of the function when
+  ;; called with some non-existing element file.
+  (let* ((*output-file
+          (sch2pcb_open_file_to_write
+           (string->pointer
+            (string-append *testdir*
+                           file-name-separator-string
+                           "output.pcb"))))
+         (element-filename "some-non-existing-element.fp")
+         (*element (pkg-line->element "PKG_DIP14(DIP14,U100,unknown)"))
+         (<stderr>
+          (with-error-to-string
+            (lambda ()
+              (let ((<result>
+                     (insert-file-element *output-file element-filename *element)))
+                (test-assert (not <result>)))))))
+    ;; Skip the next tests.  Currently, the C code deals with error
+    ;; output.  It seems, it outputs errors directly to stderr file
+    ;; using its file description info, so I cannot catch it.
+    (test-skip 1)
+    (test-assert (string-contains <stderr>
+                                  (string-append "insert_element() can't open "
+                                                 element-filename)))
+    (sch2pcb_close_file *output-file))
+  ;; Clean up.
+  (config-test-teardown))
+(test-end)
