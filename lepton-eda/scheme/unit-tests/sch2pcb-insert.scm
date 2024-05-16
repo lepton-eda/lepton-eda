f083b2b54bdce035ebd0333b7d43f1ead0468b0d
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-05-16 16:12:39 +0300

    uts[sch2pcb]: Add a test for an element file really being a PCB file

--- lepton-eda/scheme/unit-tests/sch2pcb-insert.scm
+++ lepton-eda/scheme/unit-tests/sch2pcb-insert.scm
@@ -63,3 +63,45 @@
   ;; Clean up.
   (config-test-teardown))
 (test-end)
+
+
+(test-begin "insert-file-element-really-pcb-file")
+(test-group-with-cleanup "insert-file-element-really-pcb-file-grp"
+  (config-test-setup)
+  ;; Test an exit status and side effects of the function when
+  ;; called with a file element that is really a PCB file.
+  (let* ((*output-file
+          (sch2pcb_open_file_to_write
+           (string->pointer (string-append *testdir*
+                                           file-name-separator-string
+                                           "output.pcb"))))
+         (element-filename (string-join (list *testdir* "file.pcb")
+                                        file-name-separator-string
+                                        'infix))
+         (element-file-contents
+          "  #some comment
+
+          \t   PCB"))
+    ;; Create the element file.
+    (with-output-to-file element-filename
+      (lambda () (display element-file-contents)))
+
+    (let* ((*element (pkg-line->element "PKG_DIP14(DIP14,U100,unknown)"))
+           (<stderr>
+            (with-error-to-string
+              (lambda ()
+                (let ((<result>
+                       (insert-file-element *output-file element-filename *element)))
+                  (test-assert (not <result>)))))))
+      ;; Skip the test until the C functions producing this text
+      ;; will be replaced with Scheme ones.  The main issue with
+      ;; foreign C functions is that they do not use Scheme ports
+      ;; and output errors directly to stderr.
+      (test-skip 1)
+      (test-assert (string-contains <stderr>
+                                    (format #f "Warning: ~A appears to be a PCB layout file. Skipping.\n"
+                                            element-filename))))
+    (sch2pcb_close_file *output-file))
+  ;; Clean up.
+  (config-test-teardown))
+(test-end)
