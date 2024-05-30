ae61357a60bf4b1a11a0055a4b05335ce1bfd90d
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-05-30 16:16:43 +0300

    schematic: Add a new module, (schematic canvas)
    Currently, the module exports an only function,
    invalidate-canvas(), that redraws the window canvas after changing
    its page contents.

---
+++ libleptongui/scheme/schematic/canvas.scm
@@ -0,0 +1,30 @@
+;;; Lepton EDA library - Scheme API
+;;; Copyright (C) 2024 Lepton EDA Contributors
+;;;
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+
+(define-module (schematic canvas)
+  #:use-module (schematic ffi)
+  #:use-module (schematic canvas foreign)
+
+  #:export (invalidate-canvas))
+
+
+(define (invalidate-canvas canvas)
+  "Schedule redraw for the entire window CANVAS."
+  (define *canvas (check-canvas canvas 1))
+
+  (gschem_page_view_invalidate_all *canvas))
