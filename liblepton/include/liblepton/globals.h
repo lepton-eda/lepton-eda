96f7d13ee7196d3bdc3f0315652b340370f8d377
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-07-24 21:25:34 +0300

    lib: Replace 'libgeda' with 'liblepton' in a comment

--- liblepton/include/liblepton/globals.h
+++ liblepton/include/liblepton/globals.h
@@ -1,7 +1,7 @@
 /* Lepton EDA library
  * Copyright (C) 1998-2010 Ales Hvezda
  * Copyright (C) 1998-2015 gEDA Contributors
- * Copyright (C) 2017-2020 Lepton EDA Contributors
+ * Copyright (C) 2017-2024 Lepton EDA Contributors
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@
  */
 
 /*! \file globals.h
- *  \brief global libgeda variables
+ *  \brief global liblepton variables
  */
 
 #ifndef _GLOBALS_H_INCL
