32a411e325c2d0dba5a4e9533b76b59aeb6d5625
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-08-02 09:46:31 +0300

    lib: Update copyrights in defines_priv.h

--- liblepton/include/defines_priv.h
+++ liblepton/include/defines_priv.h
@@ -1,5 +1,6 @@
 /* Lepton EDA library
  * Copyright (C) 2012 gEDA Contributors
+ * Copyright (C) 2017-2024 Lepton EDA Contributors
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
