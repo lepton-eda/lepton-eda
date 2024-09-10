58d5cbab96586393bb7c8b62a5c9bc7186878143
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-09-10 09:27:34 +0300

    schematic: Add Scheme mate for i_callback_cancel()
    A new function, callback-cancel(), has been added.  To make it
    work with all current modules without circular dependences, it is
    exported in a new separate module, (schematic callback cancel).

---
+++ lepton-eda/scheme/schematic/callback/cancel.scm
@@ -0,0 +1,29 @@
+;;; Lepton EDA Schematic Capture
+;;; Scheme API
+;;; Copyright (C) 2024-2025 Lepton EDA Contributors
+;;;
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+
+(define-module (schematic callback cancel)
+  #:use-module (system foreign)
+
+  #:use-module (schematic ffi)
+
+  #:export (callback-cancel))
+
+
+(define (callback-cancel *window)
+  (i_callback_cancel %null-pointer *window))
