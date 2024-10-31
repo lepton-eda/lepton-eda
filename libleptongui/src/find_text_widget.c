83f924872303a422a3d1a653a4470572d4b31651
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-10-31 15:46:33 +0300

    gui: Get rid of an unnecessary check in find_text_dialog()

--- libleptongui/src/find_text_widget.c
+++ libleptongui/src/find_text_widget.c
@@ -197,7 +197,6 @@ find_text_dialog (SchematicWindow *w_current)
   LeptonObject *object;
 
   g_return_if_fail (w_current != NULL);
-  g_return_if_fail (w_current->toplevel != NULL);
 
   object = o_select_return_first_object(w_current);
 
