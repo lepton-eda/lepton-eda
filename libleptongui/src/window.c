1b4dc869f54eb521ef570a9fc4dd017cc7cc9743
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-11-02 14:33:17 +0300

    gui: Initialize the field 'hide_text_widget' in schematic_window_new()

--- libleptongui/src/window.c
+++ libleptongui/src/window.c
@@ -164,6 +164,7 @@ SchematicWindow *schematic_window_new ()
   schematic_window_set_menubar (w_current, NULL);
   schematic_window_set_popup_menu (w_current, NULL);
   schematic_window_set_find_text_widget (w_current, NULL);
+  w_current->hide_text_widget = NULL;
   w_current->macro_widget  = NULL;
   w_current->bottom_widget = NULL;
   w_current->translate_widget = NULL;
