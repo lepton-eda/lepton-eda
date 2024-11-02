2557a44654fc63497df3cac372d0f5aa91109b50
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-11-02 12:17:32 +0300

    schematic: Make SchematicWindow's accessors for 'find_text_widget' available in Scheme

--- lepton-eda/scheme/schematic/ffi.scm
+++ lepton-eda/scheme/schematic/ffi.scm
@@ -441,6 +441,8 @@
             schematic_window_set_second_wy
             schematic_window_get_find_text_state_widget
             schematic_window_set_find_text_state_widget
+            schematic_window_get_find_text_widget
+            schematic_window_set_find_text_widget
             schematic_window_set_font_select_widget
             schematic_window_get_gdk_display
             schematic_window_get_keyboardpan_gain
@@ -819,6 +821,8 @@
 (define-lff schematic_window_set_second_wy void (list '* int))
 (define-lff schematic_window_get_find_text_state_widget '* '(*))
 (define-lff schematic_window_set_find_text_state_widget void '(* *))
+(define-lff schematic_window_get_find_text_widget '* '(*))
+(define-lff schematic_window_set_find_text_widget void '(* *))
 (define-lff schematic_window_set_font_select_widget void '(* *))
 (define-lff schematic_window_get_gdk_display '* '(*))
 (define-lff schematic_window_get_keyboardpan_gain int '(*))
