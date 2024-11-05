4224c3a1467fb9a6e3678e9bb326ea6ce7e29047
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-11-05 08:50:26 +0300

    gui: Initialize SchematicWindow's field 'show_text_widget' in schematic_window_new()

--- libleptongui/src/window.c
+++ libleptongui/src/window.c
@@ -165,6 +165,7 @@ SchematicWindow *schematic_window_new ()
   schematic_window_set_popup_menu (w_current, NULL);
   schematic_window_set_find_text_widget (w_current, NULL);
   schematic_window_set_hide_text_widget (w_current, NULL);
+  w_current->show_text_widget  = NULL;
   w_current->macro_widget  = NULL;
   w_current->bottom_widget = NULL;
   w_current->translate_widget = NULL;
