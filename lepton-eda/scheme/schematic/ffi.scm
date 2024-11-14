83acf12ac17e78dd8c00535ad7532f383e7a449b
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-11-14 11:02:51 +0300

    gui: Make schematic_window_get_drawing_area() available in Scheme

--- lepton-eda/scheme/schematic/ffi.scm
+++ lepton-eda/scheme/schematic/ffi.scm
@@ -443,6 +443,7 @@
             schematic_window_set_color_edit_widget
             schematic_window_get_draw_grips
             schematic_window_set_draw_grips
+            schematic_window_get_drawing_area
             schematic_window_get_enforce_hierarchy
             schematic_window_get_file_preview
             schematic_window_set_file_preview
@@ -824,6 +825,7 @@
 (define-lff schematic_window_set_color_edit_widget void '(* *))
 (define-lff schematic_window_get_draw_grips int '(*))
 (define-lff schematic_window_set_draw_grips void (list '* int))
+(define-lff schematic_window_get_drawing_area '* '(*))
 (define-lff schematic_window_get_enforce_hierarchy int '(*))
 (define-lff schematic_window_get_file_preview int '(*))
 (define-lff schematic_window_set_file_preview void (list int '*))
