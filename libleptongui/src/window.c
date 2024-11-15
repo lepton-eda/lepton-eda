05c0d0e22df501be0b58e78ecbec7955f116bb4d
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-11-15 09:17:28 +0300

    gui: Utilize setter for 'translate_widget' in schematic_window_new()

--- libleptongui/src/window.c
+++ libleptongui/src/window.c
@@ -168,7 +168,7 @@ SchematicWindow *schematic_window_new ()
   schematic_window_set_show_text_widget (w_current, NULL);
   schematic_window_set_macro_widget (w_current, NULL);
   w_current->bottom_widget = NULL;
-  w_current->translate_widget = NULL;
+  schematic_window_set_translate_widget (w_current, NULL);
 
   /* tabbed GUI: notebook: */
   w_current->xtabs_nbook = NULL;
