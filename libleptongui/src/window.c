e390ed7e9cd400ba277c6b742c15e46649d4c8d9
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-11-15 12:01:22 +0300

    gui: Utilize setter for 'font_select_dialog' in schematic_window_new()

--- libleptongui/src/window.c
+++ libleptongui/src/window.c
@@ -206,7 +206,7 @@ SchematicWindow *schematic_window_new ()
   schematic_window_set_log_widget_dialog (w_current, NULL);
   schematic_window_set_find_text_state_dialog (w_current, NULL);
   schematic_window_set_color_edit_dialog (w_current, NULL);
-  w_current->font_select_dialog       = NULL;
+  schematic_window_set_font_select_dialog (w_current, NULL);
   w_current->page_select_dialog       = NULL;
 
 
