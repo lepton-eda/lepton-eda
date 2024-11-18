48736de8605f17f4beb4cf9e423f603344d1c3b7
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-11-18 12:37:49 +0300

    gui: Utilize getter for 'main_window' in x_clipboard_get()

--- libleptongui/src/x_clipboard.c
+++ libleptongui/src/x_clipboard.c
@@ -239,8 +239,10 @@ x_clipboard_get (SchematicWindow *w_current)
                                (gchar *) buf, -1, "Clipboard", &err);
 
   if (err) {
+    GtkWidget *main_window =
+      schematic_window_get_main_window (w_current);
     GtkWidget * dialog = gtk_message_dialog_new_with_markup
-      (GTK_WINDOW (w_current->main_window),
+      (GTK_WINDOW (main_window),
        GTK_DIALOG_DESTROY_WITH_PARENT,
        GTK_MESSAGE_ERROR,
        GTK_BUTTONS_OK,
