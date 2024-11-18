fc8a64a1e32a94c48ba2544cc2f07c8c97f24672
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-11-18 15:42:32 +0300

    gui: Utilize getter for 'drawing_area' in x_window_show_text()

--- libleptongui/src/x_window.c
+++ libleptongui/src/x_window.c
@@ -249,7 +249,9 @@ x_window_show_text (GtkWidget *widget,
                                schematic_show_hide_text_widget_get_text_string (SCHEMATIC_SHOW_HIDE_TEXT_WIDGET (widget)));
   }
 
-  gtk_widget_grab_focus (w_current->drawing_area);
+  GtkWidget *drawing_area =
+    schematic_window_get_drawing_area (w_current);
+  gtk_widget_grab_focus (drawing_area);
   gtk_widget_hide (GTK_WIDGET (widget));
 }
 
