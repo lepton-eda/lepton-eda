a245ea348b9e5945c9598705dd9e3222db04336d
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-11-22 13:20:18 +0300

    gui: Utilize getter for 'color_edit_widget' in x_widgets_show_color_edit()

--- libleptongui/src/x_widgets.c
+++ libleptongui/src/x_widgets.c
@@ -251,8 +251,11 @@ void x_widgets_show_color_edit (SchematicWindow* w_current)
 {
   g_return_if_fail (w_current != NULL);
 
+  GtkWidget *color_edit_widget =
+    schematic_window_get_color_edit_widget (w_current);
+
   x_widgets_show_in_dialog (w_current,
-                            GTK_WIDGET (w_current->color_edit_widget),
+                            GTK_WIDGET (color_edit_widget),
                             &w_current->color_edit_dialog,
                             _("Color Scheme Editor"),
                             "colored");
