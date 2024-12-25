be03db5082ec6f98e239ede1fc9f213a9edd7e0a
Vladimir Zhbanov <vzhbanov@gmail.com>
2024-12-25 15:22:23 +0300

    schematic: Catch the "delete-event" signal to avoid destroying the Multiattrib dialog
    This commit fixes the bug introduced in the previous one.  When
    the user hits the "Close" button in the Multiattrib dialog, the
    dialog gets hidden and is not destroyed.  OTOH, if the user hits
    Escape or closes the window using a WM shortkey or its delete
    button, the "delete-event" signal leads to destroying the parent
    GtkDialog structure, which leads to crashes if the user tries to
    open the dialog window once again.  The issue has been fixed by
    catching the signal in a new callback and stopping propagating it
    to avoid processing it in the lower level code of the GtkDialog
    parent of the widget.

--- libleptongui/scheme/schematic/dialog/multiattrib.scm
+++ libleptongui/scheme/schematic/dialog/multiattrib.scm
@@ -19,6 +19,8 @@
 (define-module (schematic dialog multiattrib)
   #:use-module (system foreign)
 
+  #:use-module (lepton ffi boolean)
+
   #:use-module (schematic ffi gtk)
   #:use-module (schematic ffi)
   #:use-module (schematic gtk helper)
@@ -38,6 +40,21 @@
   (procedure->pointer void response-callback (list '* int '*)))
 
 
+(define (delete-event-callback *widget *event *window)
+  (if (null-pointer? *window)
+      (error "NULL window.")
+      (gtk_widget_hide *widget))
+  ;; Stop further propagation of the "delete-event" signal for the
+  ;; widget.  Otherwise the widget will be destroyed in the code
+  ;; of the parent GtkDialog structure.
+  TRUE)
+
+;;; The signal is emitted when you click the close button on the
+;;; widget dialog window or hit Escape.
+(define *delete-event-callback
+  (procedure->pointer int delete-event-callback '(* * *)))
+
+
 (define (multiattrib-dialog window)
   "Open multiple attribute editor dialog to edit selected objects in
 WINDOW."
@@ -52,6 +69,11 @@ WINDOW."
         (schematic_window_set_multiattrib_widget *window *new-widget)
         (gtk_window_set_transient_for (gtk_widget_get_gtk_window *new-widget)
                                       (gtk_widget_get_gtk_window *main-window))
+
+        (schematic_signal_connect *new-widget
+                                  (string->pointer "delete-event")
+                                  *delete-event-callback
+                                  *window)
 
         (schematic_signal_connect *new-widget
                                   (string->pointer "response")
