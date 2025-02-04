72eb0f6fc858321395b0e0b079caa3a27e5d7e1e
Vladimir Zhbanov <vzhbanov@gmail.com>
2025-02-04 10:35:09 +0300

    lib: Add descriptions for the 'st_picture' struct fields

--- liblepton/include/liblepton/picture.h
+++ liblepton/include/liblepton/picture.h
@@ -1,7 +1,7 @@
 /* Lepton EDA library
  * Copyright (C) 1998-2010 Ales Hvezda
  * Copyright (C) 1998-2016 gEDA Contributors
- * Copyright (C) 2021 Lepton EDA Contributors
+ * Copyright (C) 2021-2025 Lepton EDA Contributors
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,18 +26,27 @@ typedef struct st_picture LeptonPicture;
 
 struct st_picture
 {
+  /* Picture pixbuf. */
   GdkPixbuf *pixbuf;
+  /* A pointer to a buffer containing Base64 encoded raw image
+     data, or NULL. */
   gchar *file_content;
+  /* The length of the above raw image data buffer. */
   gsize file_length;
 
+  /* Width/height ratio. */
   double ratio;
+  /* File name backing the picture, or NULL. */
   char *filename;
+  /* Rotation angle. It must be orthogonal. */
   int angle;
+  /* Whether the image is mirrored. */
   gboolean mirrored;
+  /* Whether the image is embedded. */
   gboolean embedded;
 
-  /* upper is considered the origin */
-  int upper_x, upper_y; /* world */
+  /* World coordinates. Upper is considered the origin */
+  int upper_x, upper_y;
   int lower_x, lower_y;
 };
 
