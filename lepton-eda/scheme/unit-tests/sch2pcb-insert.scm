95ff83d1ae5c3dbc43b04b9fef06434a7061049d
Vladimir Zhbanov <vzhbanov@gmail.com>
2025-09-08 17:08:33 +0300

    uts[sch2pcb]: Add a test for inserting a non-readable file element

--- lepton-eda/scheme/unit-tests/sch2pcb-insert.scm
+++ lepton-eda/scheme/unit-tests/sch2pcb-insert.scm
@@ -144,3 +144,49 @@
   ;; Clean up.
   (config-test-teardown))
 (test-end)
+
+
+(test-begin "insert-file-element-non-readable")
+(test-group-with-cleanup "insert-file-element-non-readable-grp"
+  (config-test-setup)
+  ;; Test an exit status and side effects of the function when
+  ;; called with a file element that is really a PCB file.
+  (let* ((*output-file
+          (sch2pcb_open_file_to_write
+           (string->pointer (string-append *testdir*
+                                           file-name-separator-string
+                                           "output.pcb"))))
+         (element-filename (string-join (list *testdir* "file.fp")
+                                        file-name-separator-string
+                                        'infix))
+         (element-file-contents
+          " # comment
+Element(0x00 \"DIP8 package\" \"\" \"DIP8\" 220 100 3 100 0x00)
+(
+        Pin(50 50 60 28 \"1\" 0x101)
+        Mark(50 50)
+)
+"))
+    ;; Create the element.
+    (with-output-to-file element-filename
+      (lambda () (display "")))
+    ;; Make the element file non-readable.
+    (chmod element-filename #o000)
+
+    (let* ((*element (pkg-line->element "PKG_DIP14(DIP14,U100,unknown)"))
+           (<stderr>
+            (with-error-to-string
+              (lambda ()
+                (let ((<result>
+                       (insert-file-element *output-file element-filename *element)))
+                  (test-assert (not <result>)))))))
+      ;; Skip the test until Scheme code will check whether the
+      ;; path is a readable file.
+      (test-skip 1)
+      (test-assert (string-contains <stderr>
+                                    (format #f "ERROR: ~A is not readable. Skipping.\n"
+                                            element-filename))))
+    (sch2pcb_close_file *output-file))
+  ;; Clean up.
+  (config-test-teardown))
+(test-end)
