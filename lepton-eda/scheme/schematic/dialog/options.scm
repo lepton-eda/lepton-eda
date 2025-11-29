bcdd2fd734df0c844f9741f9ea92a713d56f385a
Vladimir Zhbanov <vzhbanov@gmail.com>
2025-11-29 20:53:09 +0300

    schematic: Factor out a new module, (schematic dialog options)

---
+++ lepton-eda/scheme/schematic/dialog/options.scm
@@ -0,0 +1,53 @@
+;;; Lepton EDA Schematic Capture
+;;; Scheme API
+;;; Copyright (C) 2025-2026 Lepton EDA Contributors
+;;;
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+
+(define-module (schematic dialog options)
+  #:use-module (system foreign)
+
+  #:use-module (lepton ffi boolean)
+  #:use-module (lepton gettext)
+
+  #:use-module (schematic ffi gtk)
+  #:use-module (schematic ffi)
+
+  #:export (options-dialog))
+
+
+(define (options-dialog *window)
+  "Create and/or show the Options dialog in *WINDOW."
+  (when (null-pointer? *window)
+    (error "NULL window."))
+
+  (let ((*options-widget (schematic_window_get_options_widget *window)))
+    (if (true? (x_widgets_use_docks))
+        (let ((*right-notebook (schematic_window_get_right_notebook *window)))
+          (x_widgets_show_in_dock *right-notebook *options-widget))
+
+        (let ((*dialog (schematic_window_get_options_widget_dialog *window)))
+
+          (if (not (null-pointer? *dialog))
+              (gtk_window_present *dialog)
+
+              (let ((*new-dialog
+                     (x_widgets_dialog_new *window
+                                           *options-widget
+                                           (string->pointer (G_ "Options"))
+                                           (string->pointer "options"))))
+                (schematic_window_set_options_widget_dialog *window
+                                                            *new-dialog)))))))
