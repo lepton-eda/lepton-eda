02df76af15a8b0ee73ff945ce995489b98e0021e
Vladimir Zhbanov <vzhbanov@gmail.com>
2025-11-29 14:33:25 +0300

    schematic: Add a new module, (schematic dialog edit-text)

---
+++ lepton-eda/scheme/schematic/dialog/edit-text.scm
@@ -0,0 +1,29 @@
+;;; Lepton EDA Schematic Capture
+;;; Scheme API
+;;; Copyright (C) 2025-2026 Lepton EDA Contributors
+;;;
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+
+(define-module (schematic dialog edit-text)
+  #:use-module (schematic ffi)
+
+  #:export (text-edit-dialog))
+
+
+(define (text-edit-dialog *window)
+  "Run the Edit text dialog in *WINDOW."
+
+  (text_edit_dialog *window))
