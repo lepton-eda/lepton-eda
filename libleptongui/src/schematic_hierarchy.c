86ba359872b3160b547eb51299c4e495df03ad8b
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-01-24 09:57:59 +0300

    gui: Make explicit the second part of a condition

--- libleptongui/src/schematic_hierarchy.c
+++ libleptongui/src/schematic_hierarchy.c
@@ -262,45 +262,48 @@ s_hierarchy_traversepages (SchematicWindow *w_current,
   {
     return pages;  /* drop the page subtree */
   }
-  pages = g_list_append (pages, p_current);
+  else
+  {
+    pages = g_list_append (pages, p_current);
 
-  /* walk throught the page objects and search for underlaying schematics */
-  for (iter = lepton_page_objects (p_current);
-       iter != NULL ;
-       iter = g_list_next (iter)) {
-    o_current = (LeptonObject *)iter->data;
+    /* walk throught the page objects and search for underlaying schematics */
+    for (iter = lepton_page_objects (p_current);
+         iter != NULL ;
+         iter = g_list_next (iter)) {
+      o_current = (LeptonObject *)iter->data;
 
-    /* only complex things like symbols can contain attributes */
-    if (!lepton_object_is_component (o_current)) continue;
+      /* only complex things like symbols can contain attributes */
+      if (!lepton_object_is_component (o_current)) continue;
 
-    filename =
-      lepton_attrib_search_attached_attribs_by_name (o_current, "source", 0);
-
-    /* if above is NULL, then look inside symbol */
-    if (filename == NULL) {
       filename =
-        lepton_attrib_search_inherited_attribs_by_name (o_current, "source", 0);
+        lepton_attrib_search_attached_attribs_by_name (o_current, "source", 0);
+
+      /* if above is NULL, then look inside symbol */
+      if (filename == NULL) {
+        filename =
+          lepton_attrib_search_inherited_attribs_by_name (o_current, "source", 0);
+      }
+
+      if (filename == NULL) continue;
+
+      /* we got a schematic source attribute
+         lets load the page and dive into it */
+      GError *err = NULL;
+      child_page =
+        s_hierarchy_down_schematic_single (w_current, filename, p_current, 0, &err);
+      if (child_page != NULL) {
+        /* call the recursive function */
+        s_hierarchy_traversepages (w_current, child_page, pages);
+      } else {
+        g_message (_("Failed to descend hierarchy into '%1$s': %2$s"),
+                   filename, err->message);
+        g_error_free (err);
+      }
+
+      g_free (filename);
+      filename = NULL;
     }
 
-    if (filename == NULL) continue;
-
-    /* we got a schematic source attribute
-       lets load the page and dive into it */
-    GError *err = NULL;
-    child_page =
-      s_hierarchy_down_schematic_single (w_current, filename, p_current, 0, &err);
-    if (child_page != NULL) {
-      /* call the recursive function */
-      s_hierarchy_traversepages (w_current, child_page, pages);
-    } else {
-      g_message (_("Failed to descend hierarchy into '%1$s': %2$s"),
-                 filename, err->message);
-      g_error_free (err);
-    }
-
-    g_free (filename);
-    filename = NULL;
+    return pages;
   }
-
-  return pages;
 }
