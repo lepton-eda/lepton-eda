8cfdc35704b0b0a8ef768984810ec75b592c4942
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-02-04 14:22:49 +0300

    gui: Get rid of an unused function argument

--- libleptongui/include/prototype.h
+++ libleptongui/include/prototype.h
@@ -1114,8 +1114,7 @@ GtkWidget*
 schematic_window_create_work_box ();
 
 void
-schematic_window_create_menubar (SchematicWindow *w_current,
-                                 GtkWidget *main_box,
+schematic_window_create_menubar (GtkWidget *main_box,
                                  GtkWidget *menubar,
                                  int add_handlebox);
 void
