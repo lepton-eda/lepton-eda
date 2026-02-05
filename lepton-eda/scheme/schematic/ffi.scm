7496aae51462a89e55f6554be1d47c964dc57545
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-02-05 08:29:00 +0300

    gui: Assign the Translate widget with a window in Scheme

--- lepton-eda/scheme/schematic/ffi.scm
+++ lepton-eda/scheme/schematic/ffi.scm
@@ -502,6 +502,7 @@
             schematic_window_set_show_text_widget
             schematic_window_get_third_button
             schematic_window_get_third_button_cancel
+            schematic_window_set_translate_widget
             schematic_window_get_undo_panzoom
             schematic_window_get_keyaccel_string
             schematic_window_set_keyaccel_string
@@ -920,6 +921,7 @@
 (define-lff schematic_window_set_show_text_widget void '(* *))
 (define-lff schematic_window_get_third_button int '(*))
 (define-lff schematic_window_get_third_button_cancel int '(*))
+(define-lff schematic_window_set_translate_widget void '(* *))
 (define-lff schematic_window_get_undo_panzoom int '(*))
 (define-lff schematic_window_get_keyaccel_string '* '(*))
 (define-lff schematic_window_set_keyaccel_string void '(* *))
