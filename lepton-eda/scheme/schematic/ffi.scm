a43e36dd70b0b90cb8dade40a49e74644108708e
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-02-09 08:12:02 +0300

    gui: Check page existence in Scheme

--- lepton-eda/scheme/schematic/ffi.scm
+++ lepton-eda/scheme/schematic/ffi.scm
@@ -985,7 +985,7 @@
 
 ;;; x_window.c
 (define-lff open_page_error_dialog void '(* * *))
-(define-lff untitled_filename '* '(* * * * *))
+(define-lff untitled_filename '* (list '* '* '* '* '* int))
 (define-lff untitled_next_index int '(*))
 (define-lff recent_manager_add void '(* *))
 (define-lff x_window_save_page int '(* * *))
