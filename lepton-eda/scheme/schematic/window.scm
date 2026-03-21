lepton-eda/lepton-eda

commit be31d89d9d3e56a5f0ea6ff66142ff82396aac2e

gui: Rewrite x_tabs_tl_page_find() in Scheme

lepton-eda/scheme/schematic/window.scm
@@ -1081,8 +1081,18 @@ the new or found page."
   "Sets page of the current tab in *WINDOW to *PAGE.  If there's a
 tab that contains *PAGE, it will be activated, otherwise a new tab
 for *PAGE page will be created and set active."
+  ;; Find a page in the list of loaded pages.
   (define (find-page *window *page)
-    (x_tabs_tl_page_find *window *page))
+    (define *toplevel (schematic_window_get_toplevel *window))
+
+    (let loop ((*page-ls (glist->list (lepton_list_get_glist
+                                       (lepton_toplevel_get_pages *toplevel))
+                                      pointer->page)))
+      (if (null? *page-ls)
+          FALSE
+          (if (eq? (pointer->page *page) (car *page-ls))
+              TRUE
+              (loop (cdr *page-ls))))))
 
   (when (null-pointer? *window)
     (error "NULL window pointer in set-tab-page!()"))

lepton-eda/scheme/schematic/ffi.scm
@@ -352,7 +352,6 @@
             x_tabs_info_find_by_wtab
             x_tabs_info_rm
             x_tabs_nbook_create
-            x_tabs_tl_page_find
             schematic_window_set_current_canvas
             schematic_tab_info_get_page
             schematic_tab_info_set_page
@@ -1031,7 +1030,6 @@
 (define-lff x_tabs_info_rm void '(* *))
 (define-lff x_tabs_nbook_create '* '(* *))
 (define-lff schematic_tabs_add_canvas void '(* *))
-(define-lff x_tabs_tl_page_find int '(* *))
 (define-lff schematic_window_set_current_canvas void '(* *))
 (define-lff schematic_tab_info_get_page '* '(*))
 (define-lff schematic_tab_info_set_page void '(* *))

libleptongui/include/prototype.h
@@ -1267,9 +1267,6 @@ x_tabs_nbook_create (SchematicWindow* w_current,
 void
 schematic_tabs_add_canvas (SchematicCanvas *pview,
                            GtkWidget *wtab);
-gboolean
-x_tabs_tl_page_find (SchematicWindow* w_current,
-                     LeptonPage* page);
 void
 x_tabs_hdr_set (GtkNotebook* nbook,
                 TabInfo* nfo);

libleptongui/src/x_tabs.c
@@ -359,41 +359,6 @@ x_tabs_info_find_by_wtab (GList* nfos, GtkWidget* wtab)
 
 
 
-
-/* --------------------------------------------------------
- *
- * implementation: SchematicWindow accessors:
- *
- */
-
-
-/*! \brief Find a \a page in the list of loaded pages.
- *
- *  \return TRUE if found.
- *
- */
-gboolean
-x_tabs_tl_page_find (SchematicWindow* w_current,
-                     LeptonPage* page)
-{
-  LeptonToplevel *toplevel =
-    schematic_window_get_toplevel (w_current);
-  GList* ptr = lepton_list_get_glist (lepton_toplevel_get_pages (toplevel));
-
-  for ( ; ptr != NULL; ptr = g_list_next (ptr) )
-  {
-    LeptonPage* pg = (LeptonPage*) ptr->data;
-    if (pg == page)
-      return TRUE;
-  }
-
-  return FALSE;
-
-} /* x_tabs_tl_page_find() */
-
-
-
-
 /* --------------------------------------------------------
  *
  * implementation: notebook:

commit bd8ab41108a1bf74ebb32dfc82acc496f2c7d326

schematic: Fix program crashes on starting Page Manager
The regression has been introduced in commit be31d89.

NULL page argument is no longer valid in the function
set-tab-page!() while it is still valid
in *window-set-current-page() which calls it, as some C functions
return NULL to indicate that the current page has not be changed
to another.  In such cases, the callbacks calling the latter
function crashed due to Scheme errors in the former.  The bug has
been fixed by stopping processing NULL pages when changing the
current page.

lepton-eda/scheme/schematic/window.scm
@@ -1496,9 +1496,13 @@ for *PAGE page will be created and set active."
 
 (define (*window-set-current-page! *window *page)
   "Sets current page of *WINDOW to *PAGE."
-  (if (true? (x_tabs_enabled))
-      (set-tab-page! *window *page)
-      (set-window-current-page! *window *page)))
+  ;; NULL is a valid value for *page.  Some lower level functions
+  ;; return NULL to indicate that the current page should not be
+  ;; changed to another.
+  (unless (null-pointer? *page)
+    (if (true? (x_tabs_enabled))
+        (set-tab-page! *window *page)
+        (set-window-current-page! *window *page))))
 
 
 ;;; Return the subpages of PAGE in *WINDOW.  If any subpages are