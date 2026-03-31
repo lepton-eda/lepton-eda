13afc471e9c08dedcd169e58ee4b330bb0e50432
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-03-31 03:38:41 +0300

    schematic: Skip zoom factor calculation when the same zoom is requested

--- lepton-eda/scheme/schematic/zoom.scm
+++ lepton-eda/scheme/schematic/zoom.scm
@@ -56,79 +56,80 @@ is not #f, zooming with panning is enabled."
     (when (null-pointer? *viewport)
       (error "NULL viewport"))
 
-    ;; NB: zoom-gain is a percentage increase.
-    (let ((relative-zoom-factor
-           (case direction
-            ((zoom-in) (/ (+ 100.0 zoom-gain) 100.0))
-            ((zoom-out) (/ 100.0 (+ 100.0 zoom-gain)))
-            ;; Indicate the zoom full with a negative zoomfactor.
-            ((zoom-full) -1)
-            ;; Don't zoom.
-            ((zoom-same) 1)
-            (else -1)))
-          (start-x-bv (make-bytevector (sizeof int) 0))
-          (start-y-bv (make-bytevector (sizeof int) 0))
-          (warp-cursor (schematic_window_get_warp_cursor *window)))
+    ;; Skip calculations when no zoom change is requested.
+    (unless (eq? direction 'zoom-same)
 
-      (unless (and zoom-with-pan?
-                   position
-                   (false? (x_event_get_pointer_position
-                            *window
-                            FALSE
-                            (bytevector->pointer start-x-bv)
-                            (bytevector->pointer start-y-bv))))
-        (let* ((start-x (bytevector-sint-ref start-x-bv
-                                             0
-                                             (native-endianness)
-                                             (sizeof int)))
-               (start-y (bytevector-sint-ref start-y-bv
-                                             0
-                                             (native-endianness)
-                                             (sizeof int)))
-               (viewport-left (schematic_viewport_get_left *viewport))
-               (viewport-right (schematic_viewport_get_right *viewport))
-               (viewport-top (schematic_viewport_get_top *viewport))
-               (viewport-bottom (schematic_viewport_get_bottom *viewport))
-               (viewport-center-x (/ (+ viewport-left viewport-right) 2))
-               (viewport-center-y (/ (+ viewport-top viewport-bottom) 2))
-               (new-pan-center-x
-                (+ (/ (- viewport-center-x start-x) relative-zoom-factor)
-                   start-x))
-               (new-pan-center-y
-                (+ (/ (- viewport-center-y start-y) relative-zoom-factor)
-                   start-y))
-               (warp-cursor? (true? warp-cursor))
-               ;; Depending on the configuration settings, the new
-               ;; viewport center is either the current mouse
-               ;; position if the cursor should be warped, the
-               ;; current center, or a new virtual center.
-               (pan-center (if (and zoom-with-pan? position)
-                               (if warp-cursor?
-                                   (cons start-x start-y)
-                                   (cons new-pan-center-x new-pan-center-y))
-                               (cons viewport-center-x viewport-center-y))))
-          ;; Calculate new viewport and draw it.
-          (schematic_canvas_pan_general
-           *canvas
-           (inexact->exact (round (car pan-center)))
-           (inexact->exact (round (cdr pan-center)))
-           relative-zoom-factor)
+      ;; NB: zoom-gain is a percentage increase.
+      (let ((relative-zoom-factor
+             (case direction
+               ((zoom-in) (/ (+ 100.0 zoom-gain) 100.0))
+               ((zoom-out) (/ 100.0 (+ 100.0 zoom-gain)))
+               ;; Indicate the zoom full with a negative zoomfactor.
+               ((zoom-full) -1)
+               (else -1)))
+            (start-x-bv (make-bytevector (sizeof int) 0))
+            (start-y-bv (make-bytevector (sizeof int) 0))
+            (warp-cursor (schematic_window_get_warp_cursor *window)))
 
-          ;; Before warping the cursor, filter out any consecutive
-          ;; scroll events from the event queue.  If the program
-          ;; receives more than one scroll event before it can process
-          ;; the first one, then the globals mouse_x and mouse_y won't
-          ;; contain the proper mouse position, because the handler
-          ;; for the mouse moved event needs to run first to set these
-          ;; values.
-          (filter-out-scroll-events)
+        (unless (and zoom-with-pan?
+                     position
+                     (false? (x_event_get_pointer_position
+                              *window
+                              FALSE
+                              (bytevector->pointer start-x-bv)
+                              (bytevector->pointer start-y-bv))))
+          (let* ((start-x (bytevector-sint-ref start-x-bv
+                                               0
+                                               (native-endianness)
+                                               (sizeof int)))
+                 (start-y (bytevector-sint-ref start-y-bv
+                                               0
+                                               (native-endianness)
+                                               (sizeof int)))
+                 (viewport-left (schematic_viewport_get_left *viewport))
+                 (viewport-right (schematic_viewport_get_right *viewport))
+                 (viewport-top (schematic_viewport_get_top *viewport))
+                 (viewport-bottom (schematic_viewport_get_bottom *viewport))
+                 (viewport-center-x (/ (+ viewport-left viewport-right) 2))
+                 (viewport-center-y (/ (+ viewport-top viewport-bottom) 2))
+                 (new-pan-center-x
+                  (+ (/ (- viewport-center-x start-x) relative-zoom-factor)
+                     start-x))
+                 (new-pan-center-y
+                  (+ (/ (- viewport-center-y start-y) relative-zoom-factor)
+                     start-y))
+                 (warp-cursor? (true? warp-cursor))
+                 ;; Depending on the configuration settings, the new
+                 ;; viewport center is either the current mouse
+                 ;; position if the cursor should be warped, the
+                 ;; current center, or a new virtual center.
+                 (pan-center (if (and zoom-with-pan? position)
+                                 (if warp-cursor?
+                                     (cons start-x start-y)
+                                     (cons new-pan-center-x new-pan-center-y))
+                                 (cons viewport-center-x viewport-center-y))))
+            ;; Calculate new viewport and draw it.
+            (schematic_canvas_pan_general
+             *canvas
+             (inexact->exact (round (car pan-center)))
+             (inexact->exact (round (cdr pan-center)))
+             relative-zoom-factor)
 
-          ;; Warp the cursor to the right position.
-          (when warp-cursor?
-            (let ((x (schematic_viewport_pix_x
-                      *viewport
-                      (inexact->exact (round (car pan-center)))))
-                  (y (schematic_viewport_pix_y
-                      *viewport
-                      (inexact->exact (round (cdr pan-center))))))
-              (x_basic_warp_cursor *canvas x y))))))))
+            ;; Before warping the cursor, filter out any consecutive
+            ;; scroll events from the event queue.  If the program
+            ;; receives more than one scroll event before it can process
+            ;; the first one, then the globals mouse_x and mouse_y won't
+            ;; contain the proper mouse position, because the handler
+            ;; for the mouse moved event needs to run first to set these
+            ;; values.
+            (filter-out-scroll-events)
+
+            ;; Warp the cursor to the right position.
+            (when warp-cursor?
+              (let ((x (schematic_viewport_pix_x
+                        *viewport
+                        (inexact->exact (round (car pan-center)))))
+                    (y (schematic_viewport_pix_y
+                        *viewport
+                        (inexact->exact (round (cdr pan-center))))))
+                (x_basic_warp_cursor *canvas x y)))))))))
