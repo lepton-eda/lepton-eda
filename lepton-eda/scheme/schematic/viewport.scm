8ac0e8e89433a5b44738950cc8ef43360540ec44
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-04-03 21:25:06 +0300

    schematic: Add a new module, (schematic viewport)
    The module exports 4 new functions returning the coords of a
    <viewport> object:
    - viewport-left()
    - viewport-right()
    - viewport-top()
    - viewport-bottom()

---
+++ lepton-eda/scheme/schematic/viewport.scm
@@ -0,0 +1,50 @@
+;;; Lepton EDA Schematic Capture
+;;; Scheme API
+;;; Copyright (C) 2026 Lepton EDA Contributors
+;;;
+;;; This program is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+
+(define-module (schematic viewport)
+  #:use-module (schematic ffi)
+  #:use-module (schematic viewport foreign)
+
+  #:export (viewport-left
+            viewport-right
+            viewport-top
+            viewport-bottom))
+
+(define (viewport-left viewport)
+  "Returns the minimum left world X coordinate of VIEWPORT."
+  (define *viewport (check-viewport viewport 1))
+  (schematic_viewport_get_left *viewport))
+
+
+(define (viewport-right viewport)
+  "Returns the maximum right world X coordinate of VIEWPORT."
+  (define *viewport (check-viewport viewport 1))
+  (schematic_viewport_get_right *viewport))
+
+
+(define (viewport-bottom viewport)
+  "Returns the minimum bottom world Y coordinate of VIEWPORT."
+  (define *viewport (check-viewport viewport 1))
+  (schematic_viewport_get_bottom *viewport))
+
+
+(define (viewport-top viewport)
+  "Returns the maximum top world Y coordinate of VIEWPORT."
+  (define *viewport (check-viewport viewport 1))
+  (schematic_viewport_get_top *viewport))
