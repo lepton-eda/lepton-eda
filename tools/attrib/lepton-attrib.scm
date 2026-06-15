d0ccaf9195126725526c7aea271fa47d94b54353
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-06-15 20:28:20 +0300

    attrib: Make name-in-list?() return Scheme boolean

--- tools/attrib/lepton-attrib.scm
+++ tools/attrib/lepton-attrib.scm
@@ -336,26 +336,25 @@ failure."
 ;;; Detects "name" in STRING_LIST.
 ;;;
 ;;; This function is passed a STRING_LIST of name=value pairs
-;;; *NAME-VALUE-LIST, and *NAME.  Returns 1 (TRUE) if the name is
-;;; in the STRING_LIST, otherwise it returns 0 (FALSE).
+;;; *NAME-VALUE-LIST, and *NAME.  Returns #t if the name is in the
+;;; STRING_LIST, otherwise it returns #f.
 (define (name-in-list? *name-value-list *name)
   (let loop ((*item *name-value-list))
-    (if (null-pointer? *item)
-        FALSE
-        (let ((*name-value (attrib_string_list_get_data *item)))
-          (if (null-pointer? *name-value)
-              (loop (attrib_string_list_get_next *item))
-              (let ((*found-name (u_basic_breakup_string *name-value
-                                                         (char->integer #\=)
-                                                         0)))
-                (if (string= (pointer->string *found-name)
-                             (pointer->string *name))
-                    (begin
-                      (g_free *found-name)
-                      TRUE)
-                    (begin
-                      (g_free *found-name)
-                      (loop (attrib_string_list_get_next *item))))))))))
+    (and (not (null-pointer? *item))
+         (let ((*name-value (attrib_string_list_get_data *item)))
+           (if (null-pointer? *name-value)
+               (loop (attrib_string_list_get_next *item))
+               (let ((*found-name (u_basic_breakup_string *name-value
+                                                          (char->integer #\=)
+                                                          0)))
+                 (if (string= (pointer->string *found-name)
+                              (pointer->string *name))
+                     (begin
+                       (g_free *found-name)
+                       #t)
+                     (begin
+                       (g_free *found-name)
+                       (loop (attrib_string_list_get_next *item))))))))))
 
 
 ;;; Updates *OBJECT component attributes in *TOPLEVEL using the
@@ -424,9 +423,9 @@ failure."
          (when (and (not (string= old-attrib-name "refdes"))
                     (not (string= old-attrib-name "net"))
                     (not (string= old-attrib-name "slot"))
-                    (false? (name-in-list?
-                             *new-component-attrib-pair-list
-                             *old-attrib-name)))
+                    (not (name-in-list?
+                          *new-component-attrib-pair-list
+                          *old-attrib-name)))
            (s_string_list_add_item *complete-component-attrib-list
                                    *count
                                    *old-name-value-pair))
