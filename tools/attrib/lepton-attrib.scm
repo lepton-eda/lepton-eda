e1dd1c0a06d93e2f939dd79d217cdd46b1ec4458
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-08-27 13:33:42 +0300

    attrib: Factor out a function for running a Delete attrib dialog

--- tools/attrib/lepton-attrib.scm
+++ tools/attrib/lepton-attrib.scm
@@ -1400,6 +1400,25 @@ failure."
         (s_sheet_data_set_changed *sheet-data TRUE))))
 
 
+(define (delete-attrib-dialog)
+  (define *dialog
+    (gtk_message_dialog_new
+     %null-pointer
+     GTK_DIALOG_MODAL
+     (symbol->gtk-message-type 'question)
+     (symbol->gtk-buttons-type 'yes-no)
+     (string->pointer
+      (G_ "Are you sure you want to delete this attribute?"))))
+
+  (gtk_window_set_title *dialog
+                        (string->pointer (G_ "Delete attribute")))
+  (gtk_dialog_set_default_response *dialog GTK_RESPONSE_NO)
+
+  (let ((response (gtk_dialog_run *dialog)))
+    (gtk_widget_destroy *dialog)
+    (= response GTK_RESPONSE_YES)))
+
+
 ;;; Delete an attribute column.
 (define (delete-attrib-column)
   (define *sheet-data (attrib_get_sheet_data))
@@ -1441,28 +1460,10 @@ failure."
                   (= mincol -1)
                   (= maxcol -1))
         ;; Create the dialog.
-        (let ((*dialog
-               (gtk_message_dialog_new
-                %null-pointer
-                GTK_DIALOG_MODAL
-                (symbol->gtk-message-type 'question)
-                (symbol->gtk-buttons-type 'yes-no)
-                (string->pointer
-                 (G_ "Are you sure you want to delete this attribute?")))))
-
-          (gtk_window_set_title *dialog
-                                (string->pointer (G_ "Delete attribute")))
-          (gtk_dialog_set_default_response *dialog GTK_RESPONSE_NO)
-
-          (let ((response (gtk_dialog_run *dialog)))
-            (cond
-             ((= response GTK_RESPONSE_YES)
-              ;; Actually delete the attrib column.  This function
-              ;; figures out which column to delete.
-              (delete-attrib-column))
-             (else #f)))
-
-          (gtk_widget_destroy *dialog))))))
+        (when (delete-attrib-dialog)
+          ;; Actually delete the attrib column.  This function
+          ;; figures out which column to delete.
+          (delete-attrib-column))))))
 
 
 (define (callback-edit-delete-attrib *action *parameter *data)
