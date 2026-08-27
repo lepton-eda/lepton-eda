8edbdc95f7e3aaaea42c465c748c7561f4fa7edd
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-08-27 10:01:36 +0300

    attrib: Return a list of files instead of a GSlist pointer in file-chooser-dialog()

--- tools/attrib/lepton-attrib.scm
+++ tools/attrib/lepton-attrib.scm
@@ -2825,7 +2825,7 @@ Please check your design.")))
              (gtk_file_chooser_get_filenames *dialog)
              %null-pointer)))
     (gtk_widget_destroy *dialog)
-    *filenames))
+    (gslist->list *filenames pointer->string 'free)))
 
 
 ;;; Init logging.
@@ -2862,9 +2862,7 @@ Please check your design.")))
           (let ((files (if (null? readable-files)
                            ;; No files specified on the command
                            ;; line, pop up the File open dialog.
-                           (gslist->list
-                            (file-chooser-dialog %null-pointer)
-                            pointer->string 'free)
+                           (file-chooser-dialog %null-pointer)
                            readable-files)))
             (if (null? files)
                 (exit 0)
