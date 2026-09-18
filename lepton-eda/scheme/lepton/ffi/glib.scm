43497c60707be9ef80b7fbddb3e4c2db48d83ce7
Vladimir Zhbanov <vzhbanov@gmail.com>
2026-09-18 22:43:04 +0300

    lepton: Fix another bug in gslist->list()
    Same named variables were used for the function GSList argument
    and for the corresponding internal loop variable.  Fortunately,
    the function could not crash so far since when g_slist_free_full()
    was called, its argument always was NULL.  However, it didn't free
    the argument list, which has been fixed by renaming the loop
    variable.

--- lepton-eda/scheme/lepton/ffi/glib.scm
+++ lepton-eda/scheme/lepton/ffi/glib.scm
@@ -115,14 +115,16 @@
 function CONVERT-FUNC to transform foreign pointers to Scheme
 objects.  If the optional argument FREE? is #t, GSLS is freed
 after conversion with g_slist_free_full()."
-  (let loop ((gsls gsls)
+  (define g_free (dynamic-func "g_free" libglib))
+
+  (let loop ((*gsls gsls)
              (ls '()))
-    (if (null-pointer? gsls)
-        (let ((g_free (dynamic-func "g_free" libglib)))
+    (if (null-pointer? *gsls)
+        (begin
           (when free? (g_slist_free_full gsls g_free))
           (reverse ls))
-        (loop (gslist-next gsls)
-              (cons (convert-func (gslist-data gsls)) ls)))))
+        (loop (gslist-next *gsls)
+              (cons (convert-func (gslist-data *gsls)) ls)))))
 
 
 ;;; GList: doubly-linked list.
